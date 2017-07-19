f8c6d9ac901fd1993d3780cf9661a370dadb1fdd
Shahin Shayandeh <shahins@microsoft.com>
2017-07-19 12:55:43 -0700

    Add dynamic context and bot.builder project

---
+++ library/Microsoft.Bot.Builder/DynamicContext.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace Microsoft.Bot.Builder
+{
+    public class DynamicContext : DynamicObject
+    {
+        private Dictionary<string, object> dynamicProperties { get; set; } = new Dictionary<string, object>();
+        private string[] _objectProperties = null;
+
+        protected string[] objectProperties
+        {
+            get
+            {
+                if (_objectProperties == null)
+                {
+                    lock (dynamicProperties)
+                    {
+                        if (_objectProperties == null)
+                            _objectProperties = this.GetType().GetTypeInfo().DeclaredProperties
+                                   .Where(pi => pi.Name != "Item")
+                                   .Select(pi => pi.Name)
+                                   .ToArray();
+                    }
+                }
+                return _objectProperties;
+            }
+        }
+
+        public override System.Collections.Generic.IEnumerable<string> GetDynamicMemberNames()
+        {
+            foreach (var p in this.objectProperties)
+                yield return p;
+            foreach (var key in this.dynamicProperties.Keys)
+                yield return key;
+            yield break;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            return TryGetMember(binder.Name, out result);
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            return TrySetMember(binder.Name, value);
+        }
+
+        public dynamic this[string name]
+        {
+            get
+            {
+                object value;
+                this.TryGetMember(name, out value);
+                return value;
+            }
+            set { this.dynamicProperties[name] = value; }
+        }
+
+        private bool TryGetMember(string name, out object result)
+        {
+            if (this.dynamicProperties.ContainsKey(name))
+            {
+                result = this.dynamicProperties[name];
+                return true;
+            }
+            else if (this.objectProperties.Contains(name))
+            {
+                var prop = this.GetType().GetTypeInfo().GetDeclaredProperty(name);
+                result = prop.GetValue(this);
+                return true;
+            }
+
+            result = null;
+            return false;
+        }
+
+        private bool TrySetMember(string name, object value)
+        {
+            var prop = this.GetType().GetTypeInfo().GetDeclaredProperty(name);
+            if (prop != null)
+            {
+                prop.SetValue(this, value);
+                return true;
+            }
+            dynamicProperties[name] = value;
+            return true;
+        }
+    }
+}
---
+++ test/Microsoft.Bot.Builder.Tests/DynamicContextTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Tests
+{
+    [TestClass]
+    public class DynamicContextTests
+    {
+        [TestMethod]
+        public void DynamicContext_DynamicProperties()
+        {
+            var testValue = "testValue";
+            var context = new DynamicContext();
+            context["test"] = testValue;
+            Assert.AreEqual(context["test"], "testValue");
+        }
+
+        class Context : DynamicContext
+        {
+            public string Name { get; set; }
+        }
+
+        class TestBinder : GetMemberBinder
+        {
+            public TestBinder(string name, bool ignoreCase) : base(name, ignoreCase)
+            {
+            }
+
+            public override DynamicMetaObject FallbackGetMember(DynamicMetaObject target, DynamicMetaObject errorSuggestion)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        [TestMethod]
+        public void DynamicContext_AccessDeclaredProperties()
+        {
+            var testValue = "testValue";
+            dynamic context = new Context();
+            context.Name = testValue;
+            context.Test = testValue;
+
+            Assert.AreEqual(context.Name, context.Test);
+
+            object value;
+            Assert.IsTrue((context as Context).TryGetMember(new TestBinder("Name", false), out value));
+            Assert.AreEqual(testValue, value as string);
+        }
+
+
+    }
+}
