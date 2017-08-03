1a8b2e2d4e557e5c244f0644fdcc28f47c17f419
Shahin Shayandeh <shahins@microsoft.com>
2017-08-03 14:11:46 -0700

    Add ActivityResolver necessary for IConnectorClient

---
+++ library/Microsoft.Bot.Builder/ActivityResolver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Bot.Connector;
+
+namespace Microsoft.Bot.Builder
+{
+    public class ActivityResolver
+    {
+        private IActivity activity;
+
+        public void Register(IActivity activity)
+        {
+            SetField.NotNull(out this.activity, nameof(activity), activity);
+        }
+
+        public IActivity Resolve()
+        {
+            return this.activity;
+        }
+    }
+}
