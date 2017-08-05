0fa26272ee8abe6734d4e440a1dc587377944d73
Tom Laird-McConnell <tomlm@microsoft.com>
2017-08-05 08:37:42 -0700

    merge

---
+++ library/Microsoft.Bot.Builder/IStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder
+{
+    /// <summary>
+    /// collection of named StoreItem objects
+    /// </summary>
+    public class StoreItems : Dictionary<string, StoreItems>
+    {
+
+    }
+
+    interface IStorage
+    {
+        /// <summary>
+        /// Read StoreItems from storage
+        /// </summary>
+        /// <param name="keys">keys of the storeItems to read</param>
+        /// <returns>StoreItem dictionary</returns>
+        Task<StoreItems> Read(string[] keys);
+
+        /// <summary>
+        /// Write StoreItems to storage
+        /// </summary>
+        /// <param name="changes"></param>
+        Task Write(StoreItems changes);
+
+        /// <summary>
+        /// Delete StoreItems from storage
+        /// </summary>
+        /// <param name="keys">keys of the storeItems to delete</param>
+        Task Delete(string[] keys);
+    }
+}
