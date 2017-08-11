fe3dab6e1a6298c5a87fecd426601e8e4fb7b5d3
Shahin Shayandeh <shahins@microsoft.com>
2017-08-11 15:36:49 -0700

    Add TestConnector and Echo middleware test

---
+++ test/Microsoft.Bot.Builder.Tests/MiddlewareTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Tests
+{
+    [TestClass]
+    public class MiddlewareTest
+    {
+        [TestMethod]
+        public async Task EchoMiddleware_Should_Echo()
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IMiddleware, EchoMiddleWare>();
+            serviceCollection.UseBotServices().UseTestConnector();
+
+            var testRunner = new TestRunner(serviceCollection);
+            await testRunner.Test("test", async (IList<IActivity> responses) =>
+            {
+                Assert.AreEqual(1, responses.Count);
+                Assert.AreEqual("test", (responses.First() as IMessageActivity).Text);
+            });
+        }
+
+        [TestMethod]
+        public async Task Multi_EchoMiddleware_Should_Echo_Multiple()
+        {
+            int count = 3;
+            var serviceCollection = new ServiceCollection();
+            for (int i = 0; i < count; i++)
+            {
+                serviceCollection.AddSingleton<IMiddleware>(new EchoMiddleWare(i == count -1));
+            }
+            serviceCollection.UseBotServices().UseTestConnector();
+
+            var testRunner = new TestRunner(serviceCollection);
+            await testRunner.Test("test", async (IList<IActivity> responses) =>
+            {
+                Assert.AreEqual(count, responses.Count);
+                Assert.AreEqual(count, responses.Cast<IMessageActivity>().Where(r => r.Text == "test").Count());
+            });
+        }
+    }
+
+    public class EchoMiddleWare : IPostToBot
+    {
+        private readonly bool handled; 
+
+        public EchoMiddleWare(bool handled = true)
+        {
+            this.handled = handled;
+        }
+
+        public Task<bool> ReceiveActivity(BotContext context, CancellationToken token)
+        {
+            var response = (context.Request as Activity).CreateReply();
+            response.Text = (context.Request as Activity).Text;
+            context.Responses.Add(response);
+            return Task.FromResult(handled);
+        }
+    }
+}
---
+++ test/Microsoft.Bot.Builder.Tests/TestConnector.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Bot.Builder.Tests
+{
+    public delegate Task TestValidator(IList<IActivity> activities);
+    public class TestConnector : Connector
+    {
+        public TestConnector(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+        
+        public override async Task Post(IList<IActivity> activities, CancellationToken token)
+        {
+            var validator = this.serviceProvider.GetRequiredService<TestValidator>();
+            await validator(activities);
+        }
+    }
+
+    public static partial class TestConnectorExtensions
+    {
+        public static IServiceCollection UseTestConnector(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<TestConnector>();
+            serviceCollection.AddScoped<IConnector>(t => t.GetRequiredService<TestConnector>());
+            return serviceCollection;
+        }
+    }
+    
+    public class TestRunner
+    {
+        private readonly IServiceCollection serviceCollection;
+
+        public TestRunner(IServiceCollection serviceCollection)
+        {
+            SetField.NotNull(out this.serviceCollection, nameof(serviceCollection), serviceCollection);
+        }
+
+        public static class ChannelID
+        {
+            public const string User = "testUser";
+            public const string Bot = "testBot";
+        }
+
+        public static Activity MakeTestMessage()
+        {
+            return new Activity()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Type = ActivityTypes.Message,
+                From = new ChannelAccount { Id = ChannelID.User },
+                Conversation = new ConversationAccount { Id = Guid.NewGuid().ToString() },
+                Recipient = new ChannelAccount { Id = ChannelID.Bot },
+                ServiceUrl = "InvalidServiceUrl",
+                ChannelId = "Test",
+                Attachments = Array.Empty<Attachment>(),
+                Entities = Array.Empty<Entity>(),
+            };
+        }
+        
+        public async Task<TestRunner> Test(string testMessage, TestValidator validator, CancellationToken token = default(CancellationToken))
+        {
+            serviceCollection.AddSingleton<TestValidator>(validator);
+
+            var provider = serviceCollection.BuildServiceProvider();
+            using (var scope = provider.CreateScope())
+            {
+                var connector = scope.ServiceProvider.GetRequiredService<TestConnector>();
+                var message = MakeTestMessage();
+                message.Text = testMessage;
+                await connector.Receive(message, token);
+            }
+            return this; 
+        }
+    }
+}
