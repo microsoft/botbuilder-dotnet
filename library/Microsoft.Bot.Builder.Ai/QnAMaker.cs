0a171d30c347ff87694716a405f46692e18b1b86
Tom Laird-McConnell <tomlm@microsoft.com>
2017-12-04 21:24:03 -0800

    add qnamaker

---
+++ library/Microsoft.Bot.Builder.Ai/QnAMaker.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.Bot.Connector;
+
+namespace Microsoft.Bot.Builder.Ai
+{
+
+    public class QueryResult
+    {
+        [JsonProperty("questions")]
+        public string[] Questions { get; set; }
+
+        [JsonProperty("answer")]
+        public string Answer { get; set; }
+
+        [JsonProperty("score")]
+        public float Score { get; set; }
+    }
+
+    public class QueryResults
+    {
+        [JsonProperty("answers")]
+        public QueryResult[] Answers { get; set; }
+    }
+
+
+    public class QnAMakerOptions
+    {
+        public string SubscriptionKey { get; set; }
+        public string KnowledgeBaseId { get; set; }
+        public float ScoreThreshold { get; set; }
+        public int Top { get; set; }
+    }
+
+    public class QnAMaker : IReceiveActivity
+    {
+        public const string qnaMakerServiceEndpoint = "https://westus.api.cognitive.microsoft.com/qnamaker/v2.0/knowledgebases/";
+        private string answerUrl;                      
+        private QnAMakerOptions options;
+
+        public QnAMaker(QnAMakerOptions options = null)
+        {
+            this.answerUrl = $"{qnaMakerServiceEndpoint}{options.KnowledgeBaseId}/generateanswer";
+            if (options.ScoreThreshold == 0)
+                options.ScoreThreshold = 0.3F;
+            if (options.Top == 0)
+                options.Top = 1;
+            this.options = options;
+        }
+
+        public async Task<QueryResult[]> GetAnswers(string question)
+        {
+            var request = new HttpRequestMessage(HttpMethod.Post, this.answerUrl);
+
+            string json = JsonConvert.SerializeObject(new
+            {
+                question = question,
+                top = this.options.Top
+            }, Formatting.None);
+
+            using (HttpClient http = new HttpClient())
+            {
+                var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                content.Headers.Add("Ocp-Apim-Subscription-Key", this.options.SubscriptionKey);
+                var response = await http.PostAsync(this.answerUrl, content).ConfigureAwait(false);
+                if (response.IsSuccessStatusCode)
+                {
+                    json = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    var results = JsonConvert.DeserializeObject<QueryResults>(json);
+                    foreach(var answer in results.Answers)
+                        answer.Score = answer.Score / 100;
+                    return results.Answers.Where(answer => answer.Score > this.options.ScoreThreshold).ToArray();
+                }
+            }
+            return null;
+        }
+
+        public async Task<ReceiveResponse> ReceiveActivity(BotContext context)
+        {
+            if (context.Request.Type == ActivityTypes.Message)
+            {
+                var results = await this.GetAnswers(context.Request.Text.Trim()).ConfigureAwait(false);
+                if (results.Any())
+                {
+                    context.Reply(results.First().Answer);
+                    return new ReceiveResponse(true);
+                }
+            }
+            return new ReceiveResponse(false);
+        }
+    }
+}
---
+++ tests/Microsoft.Bot.Builder.Ai.Tests/QnAMakerTests.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Builder.Storage;
+using Microsoft.Bot.Builder.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Ai.Tests
+{
+    [TestClass]
+    public class QnaMakerTests
+    {
+        public string knowlegeBaseId = TestUtilities.GetKey("QNAKNOWLEDGEBASEID");
+        public string subscriptionKey = TestUtilities.GetKey("QNASUBSCRIPTIONKEY");
+
+        [TestMethod]
+        [TestCategory("AI")]
+        [TestCategory("QnAMaker")]
+        public async Task QnaMaker_ReturnsAnswer()
+        {
+            var qna = new QnAMaker(new QnAMakerOptions()
+            {
+                KnowledgeBaseId = knowlegeBaseId,
+                SubscriptionKey = subscriptionKey,
+                Top = 1
+            });
+
+            var results = await qna.GetAnswers("how do I clean the stove?");
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Length, 1, "should get one result");
+            Assert.IsTrue(results[0].Answer.StartsWith("BaseCamp: You can use a damp rag to clean around the Power Pack"));
+        }
+
+        [TestMethod]
+        [TestCategory("AI")]
+        [TestCategory("QnAMaker")]
+        public async Task QnaMaker_TestThreshold()
+        {
+
+            var qna = new QnAMaker(new QnAMakerOptions()
+            {
+                KnowledgeBaseId = knowlegeBaseId,
+                SubscriptionKey = subscriptionKey,
+                Top = 1,
+                ScoreThreshold = 0.99F
+            });
+
+            var results = await qna.GetAnswers("how do I clean the stove?");
+            Assert.IsNotNull(results);
+            Assert.AreEqual(results.Length, 0, "should get zero result because threshold");
+        }
+
+        [TestMethod]
+        [TestCategory("AI")]
+        [TestCategory("QnAMaker")]
+        public async Task QnaMaker_TestMiddleware()
+        {
+            TestAdapter adapter = new TestAdapter();
+            Bot bot = new Bot(adapter)
+                .OnReceive(async (context) =>
+                {
+                    if (context.Request.Text == "foo")
+                    {
+                        context.Reply(context.Request.Text);
+                        return new ReceiveResponse(true);
+                    }
+                    return new ReceiveResponse(false);
+                })
+                .Use(new QnAMaker(new QnAMakerOptions()
+                {
+                    KnowledgeBaseId = knowlegeBaseId,
+                    SubscriptionKey = subscriptionKey,
+                    Top = 1
+                }));
+
+            await adapter
+                .Send("foo")
+                    .AssertReply("foo", "passthrough")
+                .Send("how do I clean the stove?")
+                    .AssertReply("BaseCamp: You can use a damp rag to clean around the Power Pack. Do not attempt to detach it from the stove body. As with any electronic device, never pour water on it directly. CampStove 2 &amp; CookStove: Power module: Remove the plastic power module from the fuel chamber and wipe it down with a damp cloth with soap and water. DO NOT submerge the power module in water or get it excessively wet. Fuel chamber: Wipe out with a nylon brush as needed. The pot stand at the top of the fuel chamber can be wiped off with a damp cloth and dried well. The fuel chamber can also be washed in a dishwasher. Dry very thoroughly.")
+                .StartTest();
+        }
+
+    }
+}
