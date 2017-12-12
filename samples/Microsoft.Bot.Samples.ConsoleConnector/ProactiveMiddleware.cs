7f5e1582dc5a8fee4df50f796e8e010ef232bc25
Chris Mullins <cleemullins@users.noreply.github.com>
2017-12-12 13:28:55 -0800

    Added CR feedback from Tom. Cleaner Sample now.

--- samples/Microsoft.Bot.Samples.ConsoleConnector/ProactiveMiddleware.cs
+++ samples/Microsoft.Bot.Samples.ConsoleConnector/ProactiveMiddleware.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.Bot.Builder;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using Microsoft.Bot.Connector;
 using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Samples
 {
-    public class ProactiveMiddleware : IReceiveActivity
+    public class ProactiveMiddleware : IReceiveActivity, IContextCreated
     {
+        private Microsoft.Bot.Builder.Bot _bot;
+        private ConversationReference _conversationReference;
+
+        public Task ContextCreated(BotContext context)
+        {
+            // Keep the Bot around, so it can be used later 
+            // for sending messages to the user
+            _bot = context.Bot;
+            return Task.CompletedTask;
+        }
+
         public async Task<ReceiveResponse> ReceiveActivity(BotContext context)
         {
             if (context.IfIntent("delayIntent"))
@@ -16,21 +25,27 @@ namespace Microsoft.Bot.Samples
 
                 context.Reply($"Scheduling callback in {delay} milliseconds.");
 
+                // used for rehydrating the conversation in the proactive message path
+                _conversationReference = context.ConversationReference;
+
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                Task.Run(async () =>
-                {
-                    await Task.Delay(delay).ConfigureAwait(false);
-                    await context.Bot.CreateContext(context.ConversationReference, async (context2) =>
-                    {
-                        context2.Reply($"--> Delayed {delay} milliseconds. <--");
-                    }).ConfigureAwait(false);
-                });
+                Task.Delay(delay)
+                 .ContinueWith((t) => SendOutOfBandMessage($"--> Delayed {delay} milliseconds. <--"))
+                 .ConfigureAwait(false);
 #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
 
                 return new ReceiveResponse(true);
             }
 
             return new ReceiveResponse(false);
+        }
+
+        private async Task SendOutOfBandMessage(string message)
+        {
+            // recreate context from conversationReference
+            await _bot
+                .CreateContext(_conversationReference, async (context) => context.Reply(message))
+                .ConfigureAwait(false);
         }
     }
 }
