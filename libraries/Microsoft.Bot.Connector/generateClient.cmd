5fb6560bbdf9494cf4139921c6d4b4c7a25dfb3a
pablo costantini <pablo.costantini@southworks.com>
2018-01-23 15:45:16 -0300

    Generate Client with AutoRest config in README.md

--- libraries/Microsoft.Bot.Connector/generateClient.cmd
+++ libraries/Microsoft.Bot.Connector/generateClient.cmd
@@ -1,6 +1,7 @@
 call npm install replace@0.3.0
 
-call autorest --input-file=.\Swagger\ConnectorAPI.json --csharp --namespace:Microsoft.Bot.Connector --output-folder=ConnectorAPI --add-credentials --override-client-name=ConnectorClient --use-datetimeoffset
+REM call autorest --input-file=.\Swagger\ConnectorAPI.json --csharp --namespace:Microsoft.Bot.Connector --output-folder=ConnectorAPI --add-credentials --override-client-name=ConnectorClient --use-datetimeoffset
+call autorest .\Swagger\README.md --csharp
 
 cd ConnectorAPI
 call ..\node_modules\.bin\replace "Microsoft.Bot.Connector.Models" "Microsoft.Bot.Connector" . -r --include="*.cs"
