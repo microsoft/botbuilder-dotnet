bdb90facc00f92d52624250664a9bcbf657c8256
pablo costantini <pablo.costantini@southworks.com>
2018-01-25 13:31:25 -0300

    Validate Emulator tokens

--- libraries/Microsoft.Bot.Connector/Authentication/JwtTokenValidation.cs
+++ libraries/Microsoft.Bot.Connector/Authentication/JwtTokenValidation.cs
@@ -22,7 +22,15 @@ namespace Microsoft.Bot.Connector
             var identity = await tokenExtractor.GetIdentityAsync(authHeader);
             if (identity == null || !identity.IsAuthenticated)
             {
-                return false;
+                // No identity? If we're allowed to, fall back to MSA
+                // This code path is used by the emulator
+                tokenExtractor = new JwtTokenExtractor(ToBotFromEmulatorTokenValidationParameters, JwtConfig.ToBotFromEmulatorOpenIdMetadataUrl, JwtConfig.ToBotFromChannelAllowedSigningAlgorithms, null);
+                identity = await tokenExtractor.GetIdentityAsync(authHeader);
+
+                if (identity == null || !identity.IsAuthenticated)
+                {
+                    return false;
+                }
             }
 
             // Validate audience
@@ -33,8 +41,9 @@ namespace Microsoft.Bot.Connector
             }
 
             // Validate serviceUrl
+            // Emulator token won't have this claim, so bypass if not present
             var serviceUrlClaim = identity.Claims.FirstOrDefault(claim => claim.Type == "serviceurl")?.Value;
-            if(string.IsNullOrWhiteSpace(serviceUrlClaim) || !string.Equals(serviceUrlClaim, serviceUrl))
+            if (!string.IsNullOrWhiteSpace(serviceUrlClaim) && !string.Equals(serviceUrlClaim, serviceUrl))
             {
                 return false;
             }
@@ -50,6 +59,26 @@ namespace Microsoft.Bot.Connector
             {
                 ValidateIssuer = true,
                 ValidIssuers = new[] { "https://api.botframework.com" },
+                // Audience validation takes place in JwtTokenExtractor
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.FromMinutes(5),
+                RequireSignedTokens = true
+            };
+
+        /// <summary>
+        /// TO BOT FROM EMULATOR: Token validation parameters when connecting to a channel
+        /// </summary>
+        public static readonly TokenValidationParameters ToBotFromEmulatorTokenValidationParameters =
+            new TokenValidationParameters()
+            {
+                ValidateIssuer = true,
+                ValidIssuers = new[] {
+                    "https://sts.windows.net/d6d49420-f39b-4df7-a1dc-d59a935871db/",                    // Auth v3.1, 1.0 token
+                    "https://login.microsoftonline.com/d6d49420-f39b-4df7-a1dc-d59a935871db/v2.0",      // Auth v3.1, 2.0 token
+                    "https://sts.windows.net/f8cdef31-a31e-4b4a-93e4-5f571e91255a/",                    // Auth v3.2, 1.0 token
+                    "https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a/v2.0"       // Auth v3.2, 2.0 token
+                },
                 // Audience validation takes place in JwtTokenExtractor
                 ValidateAudience = false,
                 ValidateLifetime = true,
