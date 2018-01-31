89cbeed5c616acc2e157b26f6595f102ddb798f9
Amit Stein <amit.stein@southworks.com>
2018-01-31 09:56:06 -0300

    Added BotConnector and BotConnecor.AspNetCore projects to Bot.Builder solution. Also, changed projects references to BotConnector using project reference instead of nuget.

---
+++ libraries/Microsoft.Bot.Connector.AspNetCore/TrustServiceUrlAttribute.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Mvc.Filters;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Connector
+{
+    public class TrustServiceUrlAttribute : ActionFilterAttribute
+    {
+        private readonly ICredentialProvider Options;
+
+        public TrustServiceUrlAttribute(ICredentialProvider options)
+        {
+            this.Options = options;
+        }
+
+        public async override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!await Options.IsAuthenticationDisabledAsync())
+            {
+                var activities = GetActivities(context);
+
+                foreach (var activity in activities)
+                {
+                    MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
+                }
+            }
+            await next();
+        }
+
+        public static IList<Activity> GetActivities(ActionExecutingContext actionContext)
+        {
+            var activties = actionContext.ActionArguments.Select(t => t.Value).OfType<Activity>().ToList();
+            if (activties.Any())
+            {
+                return activties;
+            }
+            else
+            {
+                var objects =
+                    actionContext.ActionArguments.Where(t => t.Value is JObject || t.Value is JArray)
+                        .Select(t => t.Value).ToArray();
+                if (objects.Any())
+                {
+                    activties = new List<Activity>();
+                    foreach (var obj in objects)
+                    {
+                        activties.AddRange((obj is JObject) ? new Activity[] { ((JObject)obj).ToObject<Activity>() } : ((JArray)obj).ToObject<Activity[]>());
+                    }
+                }
+            }
+            return activties;
+        }
+    }
+}
