92e22103d611df1a0cf4710fa870e8c8b8a10ad5
John Taylor <johtaylo@microsoft.com>
2018-02-13 23:40:47 -0800

    added example of hidding bot framework init

---
+++ samples/Microsoft.Bot.Samples.Simplified.Asp/Controllers/MessagesController.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Bot.Samples.Simplified.Asp.Controllers
+{
+    [Route("api/[controller]")]
+    public class MessagesController : BotController
+    {
+        public MessagesController(IConfiguration configuration)
+            : base(configuration)
+        {
+        }
+
+        protected override Task<List<IActivity>> Receive(IMessageActivity activity)
+        {
+            return Task.FromResult(new List<IActivity> { new Activity { Text = $"echo: {activity.Text}" } });
+        }
+    }
+}
