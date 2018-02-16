8dd9e3bd73d85b2d64089544a778f5a2dd608517
Brandon Hurlburt <hurlburb@microsoft.com>
2018-02-16 11:21:38 -0800

    v3 bot template with will-be-working startup & controller for echobot in new architecture

---
+++ samples/Microsoft.Bot.Samples.EchoBot-AspNet461/App_Start/WebApiConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Storage;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Web.Http;
+
+namespace Microsoft.Bot.Samples.EchoBot_AspNet461
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Json settings
+            config.Formatters.JsonFormatter.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings()
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                Formatting = Newtonsoft.Json.Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+            };
+
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            config.Services.Add(typeof(Builder.Bot), new Builder.Bot(new BotFrameworkAdapter(null, null))
+                .Use(new BotStateManager(new MemoryStorage())));
+        }
+    }
+}
