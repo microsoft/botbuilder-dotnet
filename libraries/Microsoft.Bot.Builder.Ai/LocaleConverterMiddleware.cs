61c62eda52d3b5ec28abd5135363a1a5b4cc0502
Mostafa Elaraby (IBS) <a-moelar@microsoft.com>
2018-02-21 12:51:38 +0200

    Added Locale Converter Middleware to support conversion between different locales

---
+++ libraries/Microsoft.Bot.Builder.Ai/LocaleConverterMiddleware.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Bot.Builder.Middleware;
+using Microsoft.Bot.Schema;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Ai
+{
+    /// <summary>
+    /// Middleware to convert messages between different locales specified
+    /// </summary>
+    public class LocaleConverterMiddleware : IReceiveActivity
+    {
+        private LocaleConverter localeConverter;
+        private string fromLocale;
+        private string toLocale;
+
+        public LocaleConverterMiddleware(string fromLocale, string toLocale)
+        {
+            localeConverter = new LocaleConverter();
+            this.fromLocale = fromLocale;
+            this.toLocale = toLocale;
+        }
+
+        public async Task ReceiveActivity(IBotContext context, MiddlewareSet.NextDelegate next)
+        {
+            IMessageActivity message = context.Request.AsMessageActivity();
+            if (message != null)
+            {
+                if (!String.IsNullOrWhiteSpace(message.Text))
+                {
+                    await ConvertLocaleMessageAsync(message);
+                }
+            }
+            await next().ConfigureAwait(false);
+        }
+
+
+
+        private async Task ConvertLocaleMessageAsync(IMessageActivity message)
+        {
+            if (localeConverter.IsLocaleAvailable(fromLocale) && localeConverter.IsLocaleAvailable(toLocale) && fromLocale != toLocale)
+            {
+                message.Text = await localeConverter.Convert(message.Text, fromLocale, toLocale);
+            }
+        }
+    }
+}
