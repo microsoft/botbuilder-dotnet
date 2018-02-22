454ea4454cfb1b1f9119de2c19e53184c42b03ad
Chris Mullins <cleemullins@users.noreply.github.com>
2018-02-22 15:43:04 -0800

    Stubs for prompt and prompt test project

---
+++ tests/Microsoft.Bot.Builder.Prompts.Tests/TextPrompTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Prompts.Tests
+{
+    [TestClass]
+    [TestCategory("Prompts")]
+    public class TextPrompTests
+    {
+        [TestMethod]
+        public async Task FirstTest()
+        {
+            TextPrompt tp = new TextPrompt();
+            Assert.IsNotNull(tp);
+        }
+    }
+}
