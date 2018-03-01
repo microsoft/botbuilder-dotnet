6dbbbbfd9373cbcf7506b2c115193b4315092da4
Emad Elwany <emadelw@microsoft.com>
2018-03-01 14:39:54 -0800

    Move header to the right location

--- libraries/Microsoft.Bot.Builder.LUIS/LuisRecognizerMiddleware.cs
+++ libraries/Microsoft.Bot.Builder.LUIS/LuisRecognizerMiddleware.cs
@@ -1,11 +1,11 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Middleware;
 using Microsoft.Bot.Schema;
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 using Microsoft.Cognitive.LUIS;
 
 namespace Microsoft.Bot.Builder.LUIS
