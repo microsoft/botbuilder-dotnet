9adb10dd98241fc1f07cd5389a0d91f062003ee2
Emad Elwany <emadelw@microsoft.com>
2018-03-01 14:32:11 -0800

    Scope LUIS Recognizer to Message Activities

--- libraries/Microsoft.Bot.Builder.LUIS/LuisRecognizerMiddleware.cs
+++ libraries/Microsoft.Bot.Builder.LUIS/LuisRecognizerMiddleware.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Middleware;
+using Microsoft.Bot.Schema;
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
@@ -31,10 +32,12 @@ namespace Microsoft.Bot.Builder.LUIS
         {
             BotAssert.ContextNotNull(context);
 
-            var utterance = context.Request.AsMessageActivity()?.Text;
-            var result = await _luisRecognizer.Recognize(utterance, CancellationToken.None).ConfigureAwait(false);
-
-            context.Set(LuisRecognizerResultKey, result);
+            if (context.Request.Type == ActivityTypes.Message)
+            {
+                var utterance = context.Request.AsMessageActivity()?.Text;
+                var result = await _luisRecognizer.Recognize(utterance, CancellationToken.None).ConfigureAwait(false);
+                context.Set(LuisRecognizerResultKey, result);
+            }
             await next().ConfigureAwait(false);
         }
     }
