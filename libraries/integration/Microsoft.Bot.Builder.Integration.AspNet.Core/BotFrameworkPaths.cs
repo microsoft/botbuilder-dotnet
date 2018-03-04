87ceda1c5caef9355812816aed1d1ce04694b27b
Drew Marsh <drmarsh@microsoft.com>
2018-03-04 09:57:49 -0800

    Initial support for config of proactive handler in ASP.NET Core

---
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/BotFrameworkPaths.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Http;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.Core
+{
+    public class BotFrameworkPaths
+    {
+        public BotFrameworkPaths()
+        {
+            this.BasePath = "/api";
+            this.ActivitiesPath = "/messages";
+            this.ProactivePath = "/messages/proactive";
+        }
+
+        public PathString BasePath { get; set; }
+        public PathString ActivitiesPath { get; set; }
+        public PathString ProactivePath { get; set; }
+    }
+}
