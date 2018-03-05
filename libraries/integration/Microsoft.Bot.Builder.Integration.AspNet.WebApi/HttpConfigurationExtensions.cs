a72a4f4f339a24ae35ea755d6b2560e735630e45
Chris Mullins <cleemullins@users.noreply.github.com>
2018-03-05 15:23:00 -0800

    Fixed WebAPI Ssamples

--- libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.WebApi/HttpConfigurationExtensions.cs
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.WebApi/HttpConfigurationExtensions.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Bot.Builder.Integration.AspNet.WebApi
                     botActivitiesRouteUrl += "/";
                 }
 
-                botActivitiesRouteUrl += "activities";
+                botActivitiesRouteUrl += "messages";
 
                 httpConfiguration.Routes.MapHttpRoute(
                         "BotFrameworkV4 Activities Controller",
