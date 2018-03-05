a14c209c5a888ec930b0f47a09b05c7681509aee
Mostafa Elaraby (IBS) <a-moelar@microsoft.com>
2018-03-05 17:54:17 +0200

    Updated LocaleConverterMiddleware to take LocaleConverter interface for DI

---
+++ libraries/Microsoft.Bot.Builder.Ai/ILocaleConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Ai
+{
+    public interface ILocaleConverter
+    {
+        bool IsLocaleAvailable(string locale);
+        Task<string> Convert(string message, string fromLocale, string toLocale);
+        string[] GetAvailableLocales();
+    }
+}
