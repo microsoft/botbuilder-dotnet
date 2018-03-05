36d76dc962251da60576e696a2b1cd287102bd26
Mostafa Elaraby (IBS) <a-moelar@microsoft.com>
2018-03-05 17:19:10 +0200

    Added Support to switch   user locales within the app using an implemented logic by the developper (by inheriting StartupTranslationAndLocale) in the LocaleConverterMiddleware

--- libraries/Microsoft.Bot.Builder.Ai/LocaleConverterMiddleware.cs
+++ libraries/Microsoft.Bot.Builder.Ai/LocaleConverterMiddleware.cs
@@ -10,15 +10,17 @@ namespace Microsoft.Bot.Builder.Ai
     /// </summary>
     public class LocaleConverterMiddleware : IReceiveActivity
     {
-        private LocaleConverter localeConverter;
-        private string fromLocale;
+        private LocaleConverter localeConverter; 
         private string toLocale;
+        private readonly Func<IBotContext, string> _getUserLocale;
+        private readonly Func<IBotContext, Task<bool>> _setUserLocale;
 
-        public LocaleConverterMiddleware(string fromLocale, string toLocale)
+        public LocaleConverterMiddleware(Func<IBotContext, string> getUserLocale, Func<IBotContext, Task<bool>> setUserLocale, string toLocale)
         {
-            localeConverter = new LocaleConverter();
-            this.fromLocale = fromLocale;
+            localeConverter = new LocaleConverter(); 
             this.toLocale = toLocale;
+            _getUserLocale = getUserLocale;
+            _setUserLocale = setUserLocale;
         }
 
         public async Task ReceiveActivity(IBotContext context, MiddlewareSet.NextDelegate next)
@@ -28,8 +30,14 @@ namespace Microsoft.Bot.Builder.Ai
             {
                 if (!String.IsNullOrWhiteSpace(message.Text))
                 {
+                    string fromLocale = _getUserLocale(context);
                     ((BotContext)context)["LocaleConversionOriginalMessage"] = message.Text;
-                    await ConvertLocaleMessageAsync(message);
+                    await ConvertLocaleMessageAsync(message, fromLocale);
+                    var localeWasChanged = await _setUserLocale(context);
+                    if (!localeWasChanged)
+                    {   // if what the user said wasn't a directive to change the locale (or that directive failed), continue the pipeline
+                        await next();
+                    }
                 }
             }
             await next().ConfigureAwait(false);
@@ -41,8 +49,9 @@ namespace Microsoft.Bot.Builder.Ai
             return message;
         }
 
-        private async Task ConvertLocaleMessageAsync(IMessageActivity message)
+        private async Task ConvertLocaleMessageAsync(IMessageActivity message,string fromLocale)
         {
+            
             if (localeConverter.IsLocaleAvailable(fromLocale) && localeConverter.IsLocaleAvailable(toLocale) && fromLocale != toLocale)
             {
                 message.Text = await localeConverter.Convert(message.Text, fromLocale, toLocale);
