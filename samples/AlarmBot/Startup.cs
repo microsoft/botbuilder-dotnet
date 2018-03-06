b802843ef11f64b27b45cce8877935c71d469c25
DESKTOP-K8O6TUG\gary <gary@garypretty.co.uk>
2018-03-06 15:51:53 -0800

    Adding CatchExceptionMiddleware to show very basic exception handling

--- samples/AlarmBot/Startup.cs
+++ samples/AlarmBot/Startup.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using AlarmBot.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +13,8 @@ using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Core.Middleware;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 
 namespace AlarmBot
@@ -41,6 +44,7 @@ namespace AlarmBot
 
                 var middleware = options.Middleware;
 
+                middleware.Add(new CatchExceptionMiddleware(HandleException));
                 middleware.Add(new UserState<UserData>(new MemoryStorage()));
                 middleware.Add(new ConversationState<ConversationData>(new MemoryStorage()));
                 middleware.Add(new RegExpRecognizerMiddleware()
@@ -66,6 +70,14 @@ namespace AlarmBot
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseBotFramework();
+        }
+
+        // This handler is used by the CatchExceptionMiddleware to allow you to inform the user
+        // that something went wrong.
+        private Task HandleException(IBotContext context, string phase, Exception exception)
+        {
+            context.Reply("Sorry, but something went wrong.");
+            return Task.CompletedTask;
         }
     }
 }
---
+++ tests/Microsoft.Bot.Builder.Tests/CatchExceptionMiddlewareTests.cs
@@ -0,0 +1,47 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Builder.Core.Middleware;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Tests
+{
+    [TestClass]
+    public class CatchExceptionMiddlewareTests
+    {
+        [TestMethod]
+        [TestCategory("Middleware")]
+        public async Task CatchException_TestMiddleware()
+        {
+            TestAdapter adapter = new TestAdapter()
+                .Use(new CatchExceptionMiddleware((context, phase, exception) =>
+                    {
+                        context.Reply("Sorry, something went wrong");
+                        return Task.CompletedTask;
+                    }));
+
+
+            await new TestFlow(adapter, (context) =>
+            {
+                if (context.Request.AsMessageActivity().Text == "foo")
+                {
+                    context.Reply(context.Request.AsMessageActivity().Text);
+                }
+                if (context.Request.AsMessageActivity().Text == "error")
+                {
+                    throw new Exception();
+                }
+                return Task.CompletedTask;
+            })
+                .Send("foo")
+                    .AssertReply("foo", "passthrough")
+                .Send("error")
+                    .AssertReply("Sorry, something went wrong")
+                .StartTest();
+        }
+
+    }
+}
