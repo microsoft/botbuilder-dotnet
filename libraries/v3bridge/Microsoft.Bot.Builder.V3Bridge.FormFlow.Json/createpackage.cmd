d454563b5d7c375b59349d376688a557e594e44b
Tom Laird-McConnell <tomlm@microsoft.com>
2018-03-12 15:17:26 -0700

    fix nuget package generation

--- libraries/v3bridge/Microsoft.Bot.Builder.V3Bridge.FormFlow.Json/createpackage.cmd
+++ libraries/v3bridge/Microsoft.Bot.Builder.V3Bridge.FormFlow.Json/createpackage.cmd
@@ -1,14 +1,14 @@
 @echo off
-echo *** Building Microsoft.Bot.Builder.FormFlow.Json
+echo *** Building Microsoft.Bot.Builder.V3Bridge.FormFlow.Json
 setlocal
 setlocal enabledelayedexpansion
 setlocal enableextensions
 set errorlevel=0
 mkdir ..\nuget
-erase /s ..\nuget\Microsoft.Bot.Builder.FormFlow.Json*nupkg
-msbuild /property:Configuration=release Microsoft.Bot.Builder.FormFlow.Json.csproj
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.FormFlow.Json.dll).FileVersionInfo.FileVersion"') do set version=%%v
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set builder=%%v
-..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.FormFlow.Json.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
-echo *** Finished building Microsoft.Bot.Builder.FormFlow.Json
+erase /s ..\nuget\Microsoft.Bot.Builder.V3Bridge.FormFlow.Json*nupkg
+msbuild /property:Configuration=release Microsoft.Bot.Builder.V3Bridge.FormFlow.Json.csproj
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.V3Bridge.FormFlow.Json.dll).FileVersionInfo.FileVersion"') do set version=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.V3Bridge.dll).FileVersionInfo.FileVersion"') do set builder=%%v
+..\..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.V3Bridge.FormFlow.Json.nuspec -symbols -properties version=%version%;builder=%builder% -OutputDirectory ..\nuget
+echo *** Finished building Microsoft.Bot.Builder.V3Bridge.FormFlow.Json
 
--- libraries/v3bridge/Microsoft.Bot.Builder.V3Bridge/createpackage.cmd
+++ libraries/v3bridge/Microsoft.Bot.Builder.V3Bridge/createpackage.cmd
@@ -1,17 +1,14 @@
 @echo off
-echo *** Building Microsoft.Bot.Builder
+echo *** Building Microsoft.Bot.Builder.V3Bridge
 setlocal
 setlocal enabledelayedexpansion
 setlocal enableextensions
 set errorlevel=0
 mkdir ..\nuget
-erase /s ..\nuget\Microsoft.Bot.Connector*.nupkg
-erase /s ..\nuget\Microsoft.Bot.Builder*.nupkg
-msbuild /property:Configuration=release /p:DefineConstants="NET45" ..\Microsoft.Bot.Connector.NetFramework\Microsoft.Bot.Connector.csproj 
-msbuild /property:Configuration=release ..\Microsoft.Bot.Builder.Autofac\Microsoft.Bot.Builder.Autofac.csproj 
-msbuild /property:Configuration=release Microsoft.Bot.Builder.csproj 
-msbuild /property:Configuration=release ..\..\tools\rview\rview.csproj
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.nuspec -symbols -properties version=%version% -OutputDirectory ..\nuget
-echo *** Finished building Microsoft.Bot.Builder
+erase /s ..\nuget\Microsoft.Bot.Builder.V3Bridge*.nupkg
+msbuild /property:Configuration=release Microsoft.Bot.Builder.V3Bridge.csproj 
+msbuild /property:Configuration=release ..\rview\rview.csproj
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.V3Bridge.dll).FileVersionInfo.FileVersion"') do set version=%%v
+..\..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.V3Bridge.nuspec -symbols -properties version=%version% -OutputDirectory ..\nuget
+echo *** Finished building Microsoft.Bot.Builder.V3Bridge
 
