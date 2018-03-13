7c92e095be49af2791276853257127f273a1a05a
Emad Elwany <emadelw@microsoft.com>
2018-03-13 16:16:19 -0700

    Add AlteredQuery to the RecognizerResult and add a GetTopScoringIntent extension method

--- libraries/Microsoft.Bot.Builder.LUIS/RecognizerResult.cs
+++ libraries/Microsoft.Bot.Builder.LUIS/RecognizerResult.cs
@@ -5,6 +5,7 @@ namespace Microsoft.Bot.Builder.LUIS
     public class RecognizerResult
     {
         public string Text { set; get; }
+        public string AlteredText { set; get; }
         public JObject Intents { get; set; }
         public JObject Entities { get; set; }
     }
