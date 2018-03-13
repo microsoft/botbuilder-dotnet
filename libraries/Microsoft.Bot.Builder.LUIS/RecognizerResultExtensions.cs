f7d9bfe854e268c46afb49cde0717c6b29545ff0
Emad Elwany <emadelw@microsoft.com>
2018-03-13 16:16:19 -0700

    Add AlteredQuery to the RecognizerResult and add a GetTopScoringIntent extension method

---
+++ libraries/Microsoft.Bot.Builder.LUIS/RecognizerResultExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Microsoft.Bot.Builder.LUIS
+{
+    public static class RecognizerResultExtensions
+    {
+        public static Tuple<string, double> GetTopScoringIntent(this RecognizerResult result)
+        {
+            if (result == null)
+                throw new ArgumentNullException(nameof(result));
+
+            if (result.Intents == null)
+                throw new ArgumentNullException(nameof(result.Intents));
+
+            var topIntent = Tuple.Create(string.Empty, 0.0d);
+            foreach(var intent in result.Intents)
+            {
+                var score = (double) intent.Value;
+                if (score > topIntent.Item2)
+                {
+                    topIntent = Tuple.Create(intent.Key, score);
+                }
+            }
+            return topIntent;
+        }
+    }
+}
