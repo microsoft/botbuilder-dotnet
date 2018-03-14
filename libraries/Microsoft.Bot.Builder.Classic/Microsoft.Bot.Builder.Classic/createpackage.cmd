eac9f98bca6de7d18db7d06504c18ebd555b3cbf
Tom Laird-McConnell <tomlm@microsoft.com>
2018-03-13 20:07:58 -0700

    updates

--- libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/createpackage.cmd
+++ libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/createpackage.cmd
@@ -7,6 +7,7 @@ set errorlevel=0
 mkdir ..\nuget
 erase /s ..\nuget\Microsoft.Bot.Builder.Classic*.nupkg
 msbuild /property:Configuration=release Microsoft.Bot.Builder.Classic.csproj 
+msbuild /property:Configuration=release ..\Microsoft.Bot.Builder.Classic.Autofac\Microsoft.Bot.Builder.Classic.Autofac.csproj 
 msbuild /property:Configuration=release ..\rview\rview.csproj
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.Classic.dll).FileVersionInfo.FileVersion"') do set version=%%v
 ..\..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.Classic.nuspec -symbols -properties version=%version% -OutputDirectory ..\nuget
--- libraries/Microsoft.Bot.Builder.Classic/createallpackages.cmd
+++ libraries/Microsoft.Bot.Builder.Classic/createallpackages.cmd
@@ -2,9 +2,3 @@ erase /y nuget\*.*
 pushd Microsoft.Bot.Builder.Classic
 call createpackage.cmd
 popd
-pushd Microsoft.Bot.Builder.Classic.FormFlow.Json
-call createpackage.cmd
-popd
-pushd Microsoft.Bot.Builder.Classic.History
-call createpackage.cmd
-popd
