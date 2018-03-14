6827fa86179b41f127d4695bd2cbc797b542b25e
Tom Laird-McConnell <tomlm@microsoft.com>
2018-03-14 13:58:42 -0700

    merge autofac into classic package

--- libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic.Autofac/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: InternalsVisibleTo("Microsoft.Bot.Builder.Classic.Tests")]
-//[assembly: InternalsVisibleTo("Microsoft.Bot.Sample.Tests")]
-
-//[assembly: AssemblyKeyFileAttribute(@"..\\..\\..\\buildtools\\35MSSharedLib1024.snk")]
-//[assembly: AssemblyDelaySignAttribute(true)]
