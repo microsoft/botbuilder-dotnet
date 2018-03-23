fea1f73c4a3286f3f44b507c4c4a2d5ec9f05891
Tom Laird-McConnell <tomlm@microsoft.com>
2018-03-22 20:21:29 -0700

    change to command line param

--- libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/packit.cmd
+++ libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/packit.cmd
@@ -1,4 +1,4 @@
 copy Microsoft.Bot.Builder.Classic.nuspec temp.nuspec
-..\..\..\nuget\rep -find:$version$ -replace:%version% temp.nuspec
+..\..\..\nuget\rep -find:$version$ -replace:%1 temp.nuspec
 NuGet.exe pack -symbols temp.nuspec
 erase temp.nuspec
