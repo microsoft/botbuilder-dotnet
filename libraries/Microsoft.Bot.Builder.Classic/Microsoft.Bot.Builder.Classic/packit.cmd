113ba91190e62fb540c5b601108a501076822744
Tom Laird-McConnell <tomlm@microsoft.com>
2018-03-22 20:40:32 -0700

    target output directory

--- libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/packit.cmd
+++ libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/packit.cmd
@@ -1,4 +1,4 @@
 copy Microsoft.Bot.Builder.Classic.nuspec temp.nuspec
 ..\..\..\nuget\rep -find:$version$ -replace:%1 temp.nuspec
-NuGet.exe pack -symbols temp.nuspec
+NuGet.exe pack -symbols temp.nuspec -outputdirectory bin\release
 erase temp.nuspec
