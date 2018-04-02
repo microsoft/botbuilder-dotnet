2e22cff33645521ea8ad436188a0c018232763b4
BruceHaley <v-brhale@microsoft.com>
2018-04-02 15:01:10 -0700

    Getting rid of the irrelevant format changes put in by VS.

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This repository contains code for the .NET version of the [Microsoft Bot Builder
 The 4.x version of the SDK is being actively developed and should therefore be used for **EXPERIMENTATION PURPOSES ONLY**.
 Production bots should continue to be developed using the [v3 SDK](https://github.com/Microsoft/BotBuilder/tree/master/CSharp).  In addition to the .NET SDK, Bot Builder supports creating bots in other popular programming languages like [JavaScript](https://github.com/Microsoft/botbuilder-js), [Python](https://github.com/Microsoft/botbuilder-python), and [Java](https://github.com/Microsoft/botbuilder-java).
 
-To get started see the [wiki](https://github.com/Microsoft/botbuilder-dotnet/wiki) for the v4 SDK. 
+To get started see the [wiki](https://github.com/Microsoft/botbuilder-dotnet/wiki) for the v4 SDK.
 
 ## Contributing
 
--- libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/Properties/AssemblyInfo.cs
+++ libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/Properties/AssemblyInfo.cs
@@ -8,26 +8,5 @@ using System.Runtime.InteropServices;
 //[assembly: InternalsVisibleTo("Microsoft.Bot.Sample.Tests")]
 [assembly: NeutralResourcesLanguage("en")]
 
-//#if (!DEBUG)
-//[assembly: AssemblyKeyFile(@"..\\35MSSharedLib1024.snk")]
-//[assembly: AssemblyDelaySign(true)]
-//#endif
-
-[assembly: AssemblyTitle("Microsoft.Bot.Builder.Classic")]
-[assembly: AssemblyDescription("Microsoft.Bot.Builder.Classic")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Microsoft Bot Builder SDK")]
-[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+//[assembly: AssemblyKeyFileAttribute(@"..\\..\\..\\buildtools\\35MSSharedLib1024.snk")]
+//[assembly: AssemblyDelaySignAttribute(true)]
