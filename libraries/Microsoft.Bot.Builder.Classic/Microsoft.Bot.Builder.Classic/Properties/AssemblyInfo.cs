8114562f71e6434f67c4a3e9ccd4cf2ead8b23e9
BruceHaley <v-brhale@microsoft.com>
2018-04-02 13:09:53 -0700

    Version into moved to AssemblyInfo.cs.

--- libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/Properties/AssemblyInfo.cs
+++ libraries/Microsoft.Bot.Builder.Classic/Microsoft.Bot.Builder.Classic/Properties/AssemblyInfo.cs
@@ -12,3 +12,25 @@ using System.Runtime.InteropServices;
 //[assembly: AssemblyKeyFile(@"..\\35MSSharedLib1024.snk")]
 //[assembly: AssemblyDelaySign(true)]
 //#endif
+
+[assembly: AssemblyTitle("MicrosoftBotBuilderClassic")]
+[assembly: AssemblyDescription("Microsoft.Bot.Builder.Classic")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Microsoft Bot Builder SDK")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.*")]
+[assembly: AssemblyFileVersion("4.0.*")]
