c370091301737530afb11f934d28c25d3fa090de
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-04-04 11:28:31 -0700

    Add doc XML in Core Integration library
    - ApplicationBuilderExtensions
    - BotFrameworkOptions

--- libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/BotFrameworkOptions.cs
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/BotFrameworkOptions.cs
@@ -8,17 +8,36 @@ using Microsoft.Rest.TransientFaultHandling;
 
 namespace Microsoft.Bot.Builder.Integration.AspNet.Core
 {
+    /// <summary>
+    /// Contains settings that your ASP.NET application uses to initialize the <see cref="BotAdapter"/>
+    /// that it adds to the HTTP request pipeline.
+    /// </summary>
+    /// <seealso cref="ApplicationBuilderExtensions"/>
     public class BotFrameworkOptions
     {
         private readonly List<IMiddleware> _middleware;
 
+        /// <summary>
+        /// Creates a <see cref="BotFrameworkOptions"/> object.
+        /// </summary>
         public BotFrameworkOptions()
         {
             _middleware = new List<IMiddleware>();
         }
 
+        /// <summary>
+        /// The credential provider with which to initialize the adapter.
+        /// </summary>
         public ICredentialProvider CredentialProvider { get; set; }
+
+        /// <summary>
+        /// The middleware collection with which to initialize the adapter.
+        /// </summary>
         public IList<IMiddleware> Middleware { get => _middleware; }
+
+        /// <summary>
+        /// Indicates whether to enable the proactive messages endpoint for the bot.
+        /// </summary>
         public bool EnableProactiveMessages { get; set; }
 
         /// <summary>
