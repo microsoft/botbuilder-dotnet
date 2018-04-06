90a6bcf8b9983902eb597cffb552ee777d35db88
JuanAr <juan.arguello@southworks.com>
2018-04-06 16:53:05 -0300

    AzureBlobStorage Provider

---
+++ libraries/Microsoft.Bot.Builder.Azure/AzureBlobStorage.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Azure
+{
+    /// <summary>
+    /// Models IStorage around a dictionary 
+    /// </summary>
+    public class AzureBlobStorage : IStorage
+    {
+        private readonly static JsonSerializerSettings serializationSettings = new JsonSerializerSettings()
+        {
+            // we use all so that we get typed roundtrip out of storage, but we don't use validation because we don't know what types are valid
+            TypeNameHandling = TypeNameHandling.All
+        };
+
+        public Lazy<CloudBlobContainer> Container { get; private set; }
+
+        public AzureBlobStorage(string dataConnectionString, string containerName)
+            : this(CloudStorageAccount.Parse(dataConnectionString), containerName)
+        {
+        }
+
+        public AzureBlobStorage(CloudStorageAccount storageAccount, string containerName)
+        {
+            this.Container = new Lazy<CloudBlobContainer>(() =>
+              {
+                  var blobClient = storageAccount.CreateCloudBlobClient();
+                  var container = blobClient.GetContainerReference(containerName);
+                  container.CreateIfNotExistsAsync().Wait();
+                  return container;
+              });
+        }
+
+        protected string GetKey(string key)
+        {
+            return HttpUtility.UrlEncode(key);
+        }
+
+        public async Task Delete(string[] keys)
+        {
+            foreach (var key in keys.Select(k => GetKey(k)))
+            {
+                var blobReference = this.Container.Value.GetBlobReference(key);
+                await blobReference.DeleteAsync(
+                    DeleteSnapshotsOption.None, new AccessCondition { IfMatchETag = "*" }, new BlobRequestOptions(), new OperationContext()).ConfigureAwait(false);
+            }
+        }
+
+        public async Task<StoreItems> Read(string[] keys)
+        {
+            var storeItems = new StoreItems();
+            foreach (string key in keys.Select(k => GetKey(k)))
+            {
+                using (var memoryStream = new MemoryStream())
+                {
+                    var blobReference = this.Container.Value.GetBlobReference(key);
+                    await blobReference.DownloadToStreamAsync(memoryStream).ConfigureAwait(false);
+                    using (var streamReader = new StreamReader(memoryStream, Encoding.UTF8))
+                    {
+                        var json = streamReader.ReadToEnd();
+                        var obj = JsonConvert.DeserializeObject(json, serializationSettings);
+                        IStoreItem storeItem = obj as IStoreItem;
+                        if (storeItem != null)
+                        {
+                            storeItem.eTag = blobReference.Properties.ETag;
+                        }
+
+                        storeItems[key] = storeItem;
+                    }
+                }
+            }
+
+            return storeItems;
+        }
+
+
+        public async Task Write(StoreItems changes)
+        {
+            foreach (var change in changes)
+            {
+                var key = GetKey(change.Key);
+                var newValue = change.Value as IStoreItem;
+                var json = JsonConvert.SerializeObject(newValue, Formatting.None, serializationSettings);
+                var blobReference = this.Container.Value.GetBlockBlobReference(key);
+
+                await blobReference.UploadTextAsync(json, new AccessCondition { IfMatchETag = newValue.eTag ?? "*" }, new BlobRequestOptions(), new OperationContext()).ConfigureAwait(false);
+            }
+        }
+    }
+}
---
+++ tests/Microsoft.Bot.Builder.Azure.Tests/BloblStorageTests.cs
@@ -0,0 +1,126 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Builder.Core.Extensions.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Azure.Tests
+{
+    [TestClass]
+    [TestCategory("Storage")]
+    [TestCategory("Storage - Azure Blob")]
+    public class BlobtorageTests : StorageBaseTests
+    {
+        private IStorage storage;
+
+        public TestContext TestContext { get; set; }
+
+        private static TestContext _testContext;
+
+        private static string emulatorPath = Environment.ExpandEnvironmentVariables(@"%ProgramFiles(x86)%\Microsoft SDKs\Azure\Storage Emulator\azurestorageemulator.exe");
+        private const string noEmulatorMessage = "This test requires Azure Storage Emulator! go to https://go.microsoft.com/fwlink/?LinkId=717179 to download and install.";
+
+        private static Lazy<bool> hasStorageEmulator = new Lazy<bool>(() =>
+        {
+            if (File.Exists(emulatorPath))
+            {
+                Process p = new Process();
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.RedirectStandardOutput = true;
+                p.StartInfo.FileName = emulatorPath;
+                p.StartInfo.Arguments = "status";
+                p.Start();
+                var output = p.StandardOutput.ReadToEnd();
+                p.WaitForExit();
+                return output.Contains("IsRunning: True");
+            }
+            return false;
+        });
+
+        [ClassInitialize]
+        public static void SetupTests(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            if (hasStorageEmulator.Value)
+            {
+                storage = new AzureBlobStorage("UseDevelopmentStorage=true", TestContext.TestName.Replace("_","") + TestContext.GetHashCode().ToString("x"));
+            }
+        }
+
+        [TestCleanup]
+        public async Task BlobStorage_TestCleanUp()
+        {
+            if (storage != null)
+            {
+                AzureBlobStorage store = (AzureBlobStorage)storage;
+                await store.Container.Value.DeleteIfExistsAsync();
+            }
+        }
+
+        public bool CheckStorageEmulator()
+        {
+            if (!hasStorageEmulator.Value)
+                Debug.WriteLine(noEmulatorMessage);
+            if (Debugger.IsAttached)
+                Assert.IsTrue(hasStorageEmulator.Value, noEmulatorMessage);
+            return hasStorageEmulator.Value;
+        }
+
+        // NOTE: THESE TESTS REQUIRE THAT THE AZURE STORAGE EMULATOR IS INSTALLED AND STARTED !!!!!!!!!!!!!!!!!
+        [TestMethod]
+        public async Task BlobStorage_CreateObjectTest()
+        {
+            if (CheckStorageEmulator())
+                await base._createObjectTest(storage);
+        }
+
+        // NOTE: THESE TESTS REQUIRE THAT THE AZURE STORAGE EMULATOR IS INSTALLED AND STARTED !!!!!!!!!!!!!!!!!
+        [TestMethod]
+        public async Task BlobStorage_ReadUnknownTest()
+        {
+            if (CheckStorageEmulator())
+                await base._readUnknownTest(storage);
+        }
+
+        // NOTE: THESE TESTS REQUIRE THAT THE AZURE STORAGE EMULATOR IS INSTALLED AND STARTED !!!!!!!!!!!!!!!!!
+        [TestMethod]
+        public async Task BlobStorage_UpdateObjectTest()
+        {
+            if (CheckStorageEmulator())
+                await base._updateObjectTest(storage);
+        }
+
+        // NOTE: THESE TESTS REQUIRE THAT THE AZURE STORAGE EMULATOR IS INSTALLED AND STARTED !!!!!!!!!!!!!!!!!
+        [TestMethod]
+        public async Task BlobStorage_DeleteObjectTest()
+        {
+            if (CheckStorageEmulator())
+                await base._deleteObjectTest(storage);
+        }
+
+        // NOTE: THESE TESTS REQUIRE THAT THE AZURE STORAGE EMULATOR IS INSTALLED AND STARTED !!!!!!!!!!!!!!!!!
+        [TestMethod]
+        public async Task BlobStorage_HandleCrazyKeys()
+        {
+            if (CheckStorageEmulator())
+                await base._handleCrazyKeys(storage);
+        }
+
+        [TestMethod]
+        public async Task BlobStorage_TypedSerialization()
+        {
+            if (CheckStorageEmulator())
+                await base._typedSerialization(this.storage);
+        }
+    }
+}
