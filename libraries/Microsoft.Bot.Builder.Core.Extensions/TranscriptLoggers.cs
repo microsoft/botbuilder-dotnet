7ff8af645f2750fc5f73a7b446b7c9595e6b3797
Tom Laird-McConnell <tomlm@microsoft.com>
2018-04-12 11:02:38 -0700

    add simple transcript loggers

---
+++ libraries/Microsoft.Bot.Builder.Core.Extensions/TranscriptLoggers.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Core.Extensions
+{
+    /// <summary>
+    /// TraceTranscriptLogger, writes activites to Trace output
+    /// </summary>
+    public class TraceTranscriptLogger : ITranscriptLogger
+    {
+        private static JsonSerializerSettings serializationSettings = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented };
+
+        /// <summary>
+        /// Log an activity to the transcript
+        /// </summary>
+        /// <param name="activity">activity to log</param>
+        /// <returns></returns>
+        public async Task LogActivity(IActivity activity)
+        {
+            System.Diagnostics.Trace.TraceInformation(JsonConvert.SerializeObject(activity, serializationSettings));
+        }
+    }
+
+    /// <summary>
+    /// ConsoleTranscriptLogger , writes activites to Console output
+    /// </summary>
+    public class ConsoleTranscriptLogger : ITranscriptLogger
+    {
+        private static JsonSerializerSettings serializationSettings = new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore, Formatting = Formatting.Indented };
+
+        /// <summary>
+        /// Log an activity to the transcript
+        /// </summary>
+        /// <param name="activity">activity to log</param>
+        /// <returns></returns>
+        public async Task LogActivity(IActivity activity)
+        {
+            Console.WriteLine(JsonConvert.SerializeObject(activity, serializationSettings));
+        }
+    }
+
+}
