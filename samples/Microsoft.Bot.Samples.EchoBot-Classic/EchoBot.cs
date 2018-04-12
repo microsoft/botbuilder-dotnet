ef255facb1a7a1d521a02ebf76d9bd78e356456c
Tom Laird-McConnell <tomlm@microsoft.com>
2018-04-12 01:58:53 -0700

    add sample Classic Dialog (lift and shift) sample

---
+++ samples/Microsoft.Bot.Samples.EchoBot-Classic/EchoBot.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Samples.Echo.Classic;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Builder.Classic.Dialogs;
+
+namespace Microsoft.Bot.Samples.Echo.AspNetWebApi
+{
+    public class EchoBot : IBot
+    {
+        public EchoBot() { }
+
+        public async Task OnTurn(ITurnContext context)
+        {
+            switch (context.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    // Use Classic IDialog 
+                    await Conversation.SendAsync(context, () => new EchoDialog());
+                    break;
+
+                case ActivityTypes.ConversationUpdate:
+                    foreach (var newMember in context.Activity.MembersAdded)
+                    {
+                        if (newMember.Id != context.Activity.Recipient.Id)
+                        {
+                            await context.SendActivity("Hello and welcome to the echo bot.");
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
