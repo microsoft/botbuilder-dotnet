0e4515ed19060317d7f98ec4244a6791c62caabc
Emad Elwany <emadelw@microsoft.com>
2018-04-16 14:06:15 -0700

    camelCase RecognizerResult properties

--- libraries/Microsoft.Bot.Builder.Ai.LUIS/RecognizerResult.cs
+++ libraries/Microsoft.Bot.Builder.Ai.LUIS/RecognizerResult.cs
@@ -1,12 +1,20 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Bot.Builder.Ai.LUIS
 {
     public class RecognizerResult
     {
+        [JsonProperty("text")]
         public string Text { set; get; }
+
+        [JsonProperty("alteredText")]
         public string AlteredText { set; get; }
+
+        [JsonProperty("intents")]
         public JObject Intents { get; set; }
+
+        [JsonProperty("entities")]
         public JObject Entities { get; set; }
     }
 }
