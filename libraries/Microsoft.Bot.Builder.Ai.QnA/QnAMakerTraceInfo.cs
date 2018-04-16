19b4c479ab1035e424ca4b5b5a831148cce8be94
Mike Bennett <mibenne@microsoft.com>
2018-04-16 16:38:48 -0700

    Added QnAMaker traces.

---
+++ libraries/Microsoft.Bot.Builder.Ai.QnA/QnAMakerTraceInfo.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Ai.QnA
+{
+
+    /// <summary>
+    /// This class represents all the trace info that we collect from the LUIS Recognizer Middleware
+    /// </summary>
+    public class QnAMakerTraceInfo
+    {
+        [JsonProperty("queryResults")]
+        public QueryResult[] QueryResults { set; get; }
+
+        [JsonProperty("qnaMakerOptions")]
+        public QnAMakerOptions QnAMakerOptions { set; get; }
+    }
+}
