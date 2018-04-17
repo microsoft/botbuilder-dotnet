cda87ffb4ed686a5884c99dade52bdf3603d55b5
Enzo Cano <enzo.cano@southworks.com>
2018-04-17 17:40:02 -0300

    Implement Dialog.ChoicePrompt

---
+++ libraries/Microsoft.Bot.Builder.Prompts/ChannelUtility.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.Prompts
+{
+    public static class ChannelUtility
+    {
+        private const string facebook = "facebook";
+        private const string skype = "skype";
+        private const string msteams = "msteams";
+        private const string telegram = "telegram";
+        private const string kik = "kik";
+        private const string email = "email";
+        private const string slack = "slack";
+        private const string groupme = "groupme";
+        private const string sms = "sms";
+        private const string emulator = "emulator";
+        private const string directline = "directline";
+        private const string webchat = "webchat";
+        private const string console = "console";
+        private const string cortana = "cortana";
+
+        public static string GetChannelId(ITurnContext context) => context?.Activity?.ChannelId ?? "";
+
+        public static bool SupportsSuggestedActions(string channelId, int buttonCnt = 100)
+        {
+            switch (channelId)
+            {
+                case facebook:
+                case skype:
+                    return (buttonCnt <= 10);
+                case kik:
+                    return (buttonCnt <= 20);
+                case slack:
+                case telegram:
+                case emulator:
+                    return (buttonCnt <= 100);
+                default:
+                    return false;
+            }
+        }
+
+        public static bool SupportsCardActions(string channelId, int buttonCnt = 100)
+        {
+            switch (channelId)
+            {
+                case facebook:
+                case skype:
+                case msteams:
+                    return (buttonCnt <= 3);
+                case slack:
+                case emulator:
+                case directline:
+                case webchat:
+                case cortana:
+                    return (buttonCnt <= 100);
+                default:
+                    return false;
+            }
+        }
+
+        public static bool HasMessageFeed(string channelId)
+        {
+
+            switch (channelId)
+            {
+                case cortana:
+                    return false;
+                default:
+                    return true;
+            }
+        }
+
+        public static int MaxActionTitleLength(string channelId)
+        {
+            return 20;
+        }
+    }
+}
---
+++ libraries/Microsoft.Bot.Builder.Prompts/ChoicePromptEx.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.Bot.Builder.Core.Extensions;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.Prompts
+{
+    public interface IChoiceFactoryOptions
+    {
+        string InlineSeparator { get; }
+        string InlineOr { get; }
+        string InlineOrMore { get; }
+    }
+
+    public class DefaultChoiceFactoryOptions : IChoiceFactoryOptions
+    {
+        public string InlineSeparator => ", ";
+        public string InlineOr => " or ";
+        public string InlineOrMore => ", or ";
+    }
+
+    public static class ChoiceEx
+    {
+        public static IDictionary<IEnumerable<string>, string> ToDictionary(this IEnumerable<Choice> source)
+        {
+            var result = new Dictionary<IEnumerable<string>, string>();
+            foreach (var item in source)
+            {
+                var key = item.Synonyms ?? new string[] { item.Value };
+                result.Add(key, item.Value);
+            }
+
+            return result;
+        }
+
+        public static IList<Choice> ToChoices(this IEnumerable<string> choices)
+        {
+            return choices.Select(choice => new Choice() { Value = choice }).ToList();
+        }
+
+        public static IMessageActivity ChoicesForChannel(this IEnumerable<Choice> choices, ITurnContext context, string text, string speak, IChoiceFactoryOptions options = null, bool includeNumbers = true)
+        {
+            return choices.ChoicesForChannel(ChannelUtility.GetChannelId(context), text, speak, options, includeNumbers);
+        }
+
+        public static IMessageActivity ChoicesForChannel(this IEnumerable<Choice> choices, string channel, string text, string speak, IChoiceFactoryOptions options = null, bool includeNumbers = true)
+        {
+            // Normalize choices
+            var values = choices.Select(choice => $"{(choice.Action?.Title ?? choice.Value)}");
+
+            // Find maximum title length
+            var maxTitleLength = values.Max(value => value.Length);
+
+            // Determine list style
+            var choicesLength = values.Count();
+            var supportsSuggestedActions = ChannelUtility.SupportsSuggestedActions(channel, choicesLength);
+            var supportsCardActions = ChannelUtility.SupportsCardActions(channel, choicesLength);
+            var maxActionTitleLength = ChannelUtility.MaxActionTitleLength(channel);
+            var hasMessageFeed = ChannelUtility.HasMessageFeed(channel);
+            var longTitles = maxTitleLength > maxActionTitleLength;
+
+            if (!longTitles && (supportsSuggestedActions || (!hasMessageFeed && supportsCardActions)))
+            {
+                // We always prefer showing choices using suggested actions. If the titles are too long, however,
+                // we'll have to show them as a text list.
+                return choices.SuggestedAction(text, speak);
+            }
+            else if (!longTitles && choicesLength <= 3)
+            {
+                // If the titles are short and there are 3 or less choices we'll use an inline list.
+                return choices.ChoicesToInline(text, speak, options, includeNumbers);
+            }
+
+            // Show a numbered list.
+            return choices.ChoicesToList(text, speak, includeNumbers);
+        }
+
+        public static IMessageActivity ChoicesToInline(this IEnumerable<Choice> choices, string text, string speak, IChoiceFactoryOptions options = null, bool includeNumbers = true)
+        {
+            var opt = options ?? new DefaultChoiceFactoryOptions();
+            
+            // Format list of choices
+            var values = choices.Select((choice, index) => $"{(includeNumbers ? $"{index + 1}. " : "- ")}{(choice.Action?.Title ?? choice.Value)}");
+            var txt = new StringBuilder(text ?? "").Append(" ");
+            if (values.Count() > 2)
+            {
+                txt = txt.Append(string.Join(opt.InlineSeparator, values.Take(values.Count() - 1)))
+                    .Append(opt.InlineOrMore)
+                    .Append(values.Last());
+            }
+            else
+            {
+                txt = txt.Append(string.Join(opt.InlineOr, values));
+            }
+
+            // Return activity with choices as an inline list.
+            return MessageFactory.Text(txt.ToString(), speak, InputHints.ExpectingInput);
+        }
+
+        public static IMessageActivity ChoicesToList(this IEnumerable<Choice> choices, string text, string speak, bool includeNumbers = true)
+        {
+            // Format list of choices
+            var values = choices.Select((choice, index) => $"{(includeNumbers ? $"{index + 1}. " : "- ")}{(choice.Action?.Title ?? choice.Value)}");
+            var txt = new StringBuilder(text ?? "")
+                .AppendLine()
+                .AppendLine()
+                .Append(string.Join(Environment.NewLine, values))
+                .ToString();
+
+            // Return activity with choices as a numbered list.
+            return MessageFactory.Text(txt, speak, InputHints.ExpectingInput);
+        }
+
+        public static IMessageActivity SuggestedAction(this IEnumerable<Choice> choices, string text, string speak)
+        {
+            // Map choices to actions
+            var actions = choices.Select(choice => {
+                return choice.Action ?? new CardAction()
+                {
+                    Type = ActionTypes.ImBack,
+                    Value = choice.Value,
+                    Title = choice.Value
+                };
+            }).ToList();
+
+            // Return activity with choices as suggested actions
+            return MessageFactory.SuggestedActions(actions, text, speak, InputHints.ExpectingInput);
+        }
+    }
+}
