5ad3fb9e6b294afda20b2542c1c26cbb3db7accc
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-04-19 14:53:04 -0700

    Update docXML for types in the Ai.LUIS namespace

--- libraries/Microsoft.Bot.Builder.Ai.LUIS/RecognizerResultExtensions.cs
+++ libraries/Microsoft.Bot.Builder.Ai.LUIS/RecognizerResultExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace Microsoft.Bot.Builder.Ai.LUIS
 {
+    /// <summary>
+    /// Contains methods for working with intent recognizer results.
+    /// </summary>
     public static class RecognizerResultExtensions
     {
+        /// <summary>
+        /// Gets the top-scoring intent from recognition results.
+        /// </summary>
+        /// <param name="result">The recognition results.</param>
+        /// <returns>A tuple of the key the of the top scoring intent and the score associated with that intent.</returns>
         public static (string key, double score) GetTopScoringIntent(this RecognizerResult result)
         {
             if (result == null)
