ae31887e72002bec9ac04b7a53473b6fa748a82f
Francisco Ponce Gomez <v-frgome@microsoft.com>
2018-04-23 14:50:31 -0700

    Initial commit

---
+++ Generator/BotBuilderVSIX-V4/ProjectTemplates/EchoBot/EchoState.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCore_EchoBot_With_State
+{
+    /// <summary>
+    /// Class for storing conversation state. 
+    /// </summary>
+    public class EchoState
+    {
+        public int TurnCount { get; set; } = 0;
+    }
+}
