aca08807c1272d456e9ac892245b4ff3f09819d8
Enzo Cano <enzo.cano@southworks.com>
2018-04-26 18:45:02 -0300

    Add Luis sample bot

---
+++ samples-final/AspNetCore-LUIS-Bot/States.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+
+namespace AspNetCore_LUIS_Bot
+{ 
+    public class Reminder : WaterfallInstance
+    {
+        public string Title { get; set; }
+        public DateTime? Date { get; set; }
+    }
+
+    public class UserState : Dictionary<string, object>
+    {
+        public UserState()
+        {
+            this[nameof(Reminders)] = new List<Reminder>();
+        }
+
+        public IList<Reminder> Reminders
+        {
+            get { return this[nameof(Reminders)] as IList<Reminder>; }
+            set { this[nameof(Reminders)] = value; }
+        }
+    }
+}
