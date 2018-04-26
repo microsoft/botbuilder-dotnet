45af1b6613bec8509fbea3c9fa576782e3177fa7
JuanAr <juan.arguello@southworks.com>
2018-04-26 13:36:49 -0300

    Cleanup code and use NuGet instead of local libraries

--- samples-final/AspNetCore-Luis-Dispatch-Bot/Program.cs
+++ samples-final/AspNetCore-Luis-Dispatch-Bot/Program.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace Microsoft.Bot.Samples.Ai.Luis.Dispatch
+namespace AspNetCore_Luis_Dispatch_Bot
 {
     public class Program
     {
