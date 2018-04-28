fb73bc0a64d91ce7f998dd0a665fbea5501afdc2
Chris McConnell <chrimc@microsoft.com>
2018-04-28 16:06:57 -0700

    Rename NumberWithUnit to NumberWithUnits. Updated typed test with luisgen bugs fixed.

--- libraries/Microsoft.Bot.Builder.Ai.LUIS/Generator/NumberWithUnits.cs
+++ libraries/Microsoft.Bot.Builder.Ai.LUIS/Generator/NumberWithUnits.cs
@@ -6,12 +6,12 @@ using Newtonsoft.Json;
 namespace Microsoft.Bot.Builder.Ai.LUIS
 {
     /// <summary>
-    /// Strongly typed class for LUIS number and unit entity recognition.
+    /// Strongly typed class for LUIS number and units entity recognition.
     /// </summary>
     /// <remarks>
     /// Specific subtypes of this class are generated to match the builtin age, currency, dimension and temperature entities.
     /// </remarks>
-    public class NumberWithUnit
+    public class NumberWithUnits
     {
         /// <summary>
         /// Recognized number, or null if unit only.
@@ -25,7 +25,7 @@ namespace Microsoft.Bot.Builder.Ai.LUIS
         [JsonProperty("units")]
         public readonly string Units;
 
-        public NumberWithUnit(double? number, string units)
+        public NumberWithUnits(double? number, string units)
         {
             Number = number;
             Units = units;
@@ -35,7 +35,7 @@ namespace Microsoft.Bot.Builder.Ai.LUIS
     /// <summary>
     /// Strongly typed LUIS builtin_age.
     /// </summary>
-    public class Age: NumberWithUnit
+    public class Age: NumberWithUnits
     {
         public Age(double number, string units) : base(number, units) { }
 
@@ -45,7 +45,7 @@ namespace Microsoft.Bot.Builder.Ai.LUIS
     /// <summary>
     /// Strongly typed LUIS builtin_dimension.
     /// </summary>
-    public class Dimension: NumberWithUnit
+    public class Dimension: NumberWithUnits
     {
         public Dimension(double number, string units) : base(number, units) { }
         public override string ToString() => $"Dimension({Number} {Units})";
@@ -54,7 +54,7 @@ namespace Microsoft.Bot.Builder.Ai.LUIS
     /// <summary>
     /// Strongly typed LUIS builtin_money.
     /// </summary>
-    public class Money : NumberWithUnit
+    public class Money : NumberWithUnits
     {
         public Money(double number, string units) : base(number, units) { }
         public override string ToString() => $"Currency({Number} {Units})";
@@ -63,7 +63,7 @@ namespace Microsoft.Bot.Builder.Ai.LUIS
     /// <summary>
     /// Strongly typed LUIS builtin_temperature.
     /// </summary>
-    public class Temperature : NumberWithUnit
+    public class Temperature : NumberWithUnits
     {
         public Temperature(double number, string units) : base(number, units) { }
         public override string ToString() => $"Temperature({Number} {Units})";
