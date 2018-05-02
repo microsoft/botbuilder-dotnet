bbaca329006a0c8e87afd917d769e40c18424078
Enzo Cano <enzo.cano@southworks.com>
2018-05-02 12:27:50 -0300

    Update botbuilder version

--- samples-final/AspNetCore-LUIS-Bot/LuisBot.cs
+++ samples-final/AspNetCore-LUIS-Bot/LuisBot.cs
@@ -5,10 +5,8 @@ using Microsoft.Bot.Builder.Core.Extensions;
 using Microsoft.Bot.Schema;
 using Microsoft.Bot.Builder.Ai.LUIS;
 using Microsoft.Bot.Builder.Dialogs;
-using System;
 using System.Collections.Generic;
 using Prompts = Microsoft.Bot.Builder.Prompts;
-using Newtonsoft.Json.Linq;
 using Microsoft.Recognizers.Text;
 using System.Linq;
 
@@ -23,14 +21,10 @@ namespace AspNetCore_LUIS_Bot
         public LuisBot()
         {
             dialogs = new DialogSet();
-            dialogs.Add("Fallback", new WaterfallStep[] { DefaultDialog });
             dialogs.Add("None", new WaterfallStep[] { DefaultDialog });
-            dialogs.Add("Calendar.Add", new WaterfallStep[] { AskRemainderTitle, AskRemainderTime, AskRemainderConfirmation });
-            dialogs.Add("Calendar.Delete", new WaterfallStep[] { DeleteReminder, ConfirmDelete });
-            dialogs.Add("Calendar.Find", new WaterfallStep[] { ShowReminders, ConfirmShow });
+            dialogs.Add("Calendar_Add", new WaterfallStep[] { AskReminderTitle, SaveReminder });
+            dialogs.Add("Calendar_Find", new WaterfallStep[] { ShowReminders, ConfirmShow });
             dialogs.Add("TitlePrompt", new TextPrompt(TitleValidator));
-            dialogs.Add("DatetimePrompt", new TextPrompt(DatetimeValidator));
-            dialogs.Add("DeletePrompt", new ConfirmPrompt(Culture.English));
             dialogs.Add("ShowReminderPrompt", new ChoicePrompt(Culture.English));
         }
 
@@ -43,164 +37,67 @@ namespace AspNetCore_LUIS_Bot
             }
         }
 
-        private async Task DatetimeValidator(ITurnContext context, Prompts.TextResult result)
+        private Task DefaultDialog(DialogContext dialogContext, object args, SkipStepFunction next)
         {
-            if (!string.IsNullOrWhiteSpace(result.Value) && DateTime.TryParse(result.Value, out DateTime datetime))
+            return dialogContext.Context.SendActivity("Hi! I'm a simple reminder bot. I can help add reminders, delete and show them.");
+        }
+
+        private async Task AskReminderTitle(DialogContext dialogContext, object args, SkipStepFunction next)
+        {
+            var reminder = new Reminder(dialogContext.ActiveDialog.State);
+            dialogContext.ActiveDialog.State = reminder;
+            if (!string.IsNullOrEmpty(reminder.Title))
             {
-                result.Value = datetime.ToString();
+                await dialogContext.Continue();
             }
             else
             {
-                result.Status = Prompts.PromptStatus.NotRecognized;
-                await context.SendActivity("Please enter a valid time in the future like \"tomorrow at 9am\" or say \"cancel\".");
+                await dialogContext.Prompt("TitlePrompt", "What would you like to call your reminder?");
             }
         }
 
-        private Task DefaultDialog(DialogContext dc, object args, SkipStepFunction next)
+        private async Task SaveReminder(DialogContext dialogContext, object args, SkipStepFunction next)
         {
-            return dc.Context.SendActivity("Hi! I'm a simple reminder bot. I can help add reminders, delete and show them.");
+            var reminder = new Reminder(dialogContext.ActiveDialog.State);
+            if (args is Prompts.TextResult textResult)
+            {
+                reminder.Title = textResult.Value;
+            }
+            await dialogContext.Context.SendActivity($"Your reminder named '{reminder.Title}' is set.");
+            var userContext = dialogContext.Context.GetUserState<UserState>();
+            userContext.Reminders.Add(reminder);
+            await dialogContext.End();
         }
 
-        private async Task AskRemainderTitle(DialogContext dc, object args, SkipStepFunction next)
+        private async Task ShowReminders(DialogContext dialogContext, object args, SkipStepFunction next)
         {
-            var initState = dc.Instance.State as WaterfallInstance;
-            var state = new Reminder()
-            {
-                Step = initState.Step
-            };
-            if (args is RecognizerResult luisResult)
-            {
-                var title = GetEntity<string>(luisResult, "Calendar_Subject");
-                if (!string.IsNullOrEmpty(title))
-                {
-                    state.Title = title;
-                }
-
-                var date = GetEntity<DateTime?>(luisResult, "datetimeV2");
-                if (date.HasValue)
-                {
-                    state.Date = date;
-                }
-            }
-            dc.Instance.State = state;
-            if (string.IsNullOrEmpty(state.Title))
-            {
-                await dc.Prompt("TitlePrompt", "What would you like to call your reminder?");
-            }
-            else
-            {
-                await dc.Continue();
-            }
-        }
-
-        private async Task AskRemainderTime(DialogContext dc, object args, SkipStepFunction next)
-        {
-            var state = dc.Instance.State as Reminder;
-            if (string.IsNullOrEmpty(state.Title))
-            {
-                if (args is Prompts.TextResult textResult)
-                {
-                    state.Title = textResult.Value;
-                }
-            }
-            if (!state.Date.HasValue)
-            {
-                await dc.Prompt("DatetimePrompt", $"What time would you like to set the {state.Title} reminder for?");
-            }
-            else
-            {
-                await dc.Continue();
-            }
-        }
-
-        private async Task AskRemainderConfirmation(DialogContext dc, object args, SkipStepFunction next)
-        {
-            var state = dc.Instance.State as Reminder;
-            if (state.Date == null)
-            {
-                if (args is Prompts.TextResult textResult)
-                {
-                    state.Date = DateTime.Parse(textResult.Value);
-                }
-            }
-            await dc.Context.SendActivity($"Your reminder named '{state.Title}' is set for {state.Date}.");
-            var userContext = dc.Context.GetUserState<UserState>();
-            userContext.Reminders.Add(state);
-            dc.Instance.State = state as WaterfallInstance;
-            await dc.End();
-        }
-
-        private async Task DeleteReminder(DialogContext dc, object args, SkipStepFunction next)
-        {
-            var userContext = dc.Context.GetUserState<UserState>();
-            if (userContext.Reminders.Count > 0)
-            {
-                await dc.Prompt("DeletePrompt", "Are you sure you want to delete the reminders?");
-            }
-            else
-            {
-                await dc.Context.SendActivity("No reminders set to delete.");
-                await dc.End();
-            }
-        }
-
-        private async Task ConfirmDelete(DialogContext dc, object args, SkipStepFunction next)
-        {
-            var userContext = dc.Context.GetUserState<UserState>();
-            if (args is Prompts.ConfirmResult confirmResult)
-            {
-                if (confirmResult.Confirmation)
-                {
-                    userContext.Reminders.Clear();
-                    await dc.Context.SendActivity("reminders deleted...");
-                }
-                else
-                {
-                    await dc.Context.SendActivity("ok...");
-                }
-            }
-            await dc.End();
-        }
-
-        private async Task ShowReminders(DialogContext dc, object args, SkipStepFunction next)
-        {
-            var userContext = dc.Context.GetUserState<UserState>();
+            var userContext = dialogContext.Context.GetUserState<UserState>();
             if (userContext.Reminders.Count == 0)
             {
-                await dc.Context.SendActivity("No reminders found.");
-                await dc.End();
+                await dialogContext.Context.SendActivity("No reminders found.");
+                await dialogContext.End();
             }
             else
             {
-                var choices = userContext.Reminders.Select(x => new Prompts.Choices.Choice() { Value = x.Title }).ToList();
-                await dc.Prompt("ShowReminderPrompt", "Select the reminder to show: ", new ChoicePromptOptions() { Choices = choices });
+                var choices = userContext.Reminders.Select(x => new Prompts.Choices.Choice() { Value = x.Title.Length < 15 ? x.Title : x.Title.Substring(0, 15) + "..." }).ToList();
+                await dialogContext.Prompt("ShowReminderPrompt", "Select the reminder to show: ", new ChoicePromptOptions() { Choices = choices });
             }
         }
 
-        private async Task ConfirmShow(DialogContext dc, object args, SkipStepFunction next)
+        private async Task ConfirmShow(DialogContext dialogContext, object args, SkipStepFunction next)
         {
-            var userContext = dc.Context.GetUserState<UserState>();
+            var userContext = dialogContext.Context.GetUserState<UserState>();
             if (args is Prompts.ChoiceResult choice)
             {
-                var reminder = userContext.Reminders.FirstOrDefault(x => x.Title == choice.Value.Value);
-                await dc.Context.SendActivity($"Reminder: {reminder.Title} - ({reminder.Date})");
+                var reminder = userContext.Reminders[choice.Value.Index];
+                await dialogContext.Context.SendActivity($"Reminder: {reminder.Title}");
             }
-            await dc.End();
+            await dialogContext.End();
         }
-
-        private T GetEntity<T>(RecognizerResult luisResult, string entityKey)
-        {
-            var data = luisResult.Entities as IDictionary<string, JToken>;
-            if (data.TryGetValue(entityKey, out JToken value))
-            {
-                return value.First.Value<T>();
-            }
-            return default(T);
-        }
-
+        
         public async Task OnTurn(ITurnContext context)
         {
-            if (context.Activity.Type == ActivityTypes.ConversationUpdate)
+            if (context.Activity.Type == ActivityTypes.ConversationUpdate && context.Activity.MembersAdded.FirstOrDefault()?.Id == context.Activity.Recipient.Id)
             {
                 await context.SendActivity("Hi! I'm a simple reminder bot. I can help add reminders, delete and show them.");
             }
@@ -218,7 +115,7 @@ namespace AspNetCore_LUIS_Bot
                 var utterance = context.Activity.Text.ToLowerInvariant();
                 if (utterance == "cancel")
                 {
-                    if (dialogContext.Instance != null)
+                    if (dialogContext.ActiveDialog != null)
                     {
                         await context.SendActivity("Ok... Cancelled");
                         dialogContext.EndAll();
@@ -237,9 +134,9 @@ namespace AspNetCore_LUIS_Bot
                     {
                         var luisResult = context.Services.Get<RecognizerResult>(LuisRecognizerMiddleware.LuisRecognizerResultKey);
                         var (intent, score) = luisResult.GetTopScoringIntent();
-                        var intentResult = score > LUIS_INTENT_THRESHOLD ? intent : "Fallback";
+                        var intentResult = score > LUIS_INTENT_THRESHOLD ? intent : "None";
 
-                        await dialogContext.Begin(intent, luisResult);
+                        await dialogContext.Begin(intent);
                     }
                 }
             }
--- samples-final/AspNetCore-LUIS-Bot/States.cs
+++ samples-final/AspNetCore-LUIS-Bot/States.cs
@@ -1,26 +1,57 @@
-﻿using Microsoft.Bot.Builder.Dialogs;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace AspNetCore_LUIS_Bot
-{ 
-    public class Reminder : WaterfallInstance
+{
+    public class BaseState : Dictionary<string, object>
     {
-        public string Title { get; set; }
-        public DateTime? Date { get; set; }
+        public BaseState(IDictionary<string, object> source)
+        {
+            if (source != null)
+            {
+                source.ToList().ForEach(x => this.Add(x.Key, x.Value));
+            }
+        }
+
+        protected T GetProperty<T>([CallerMemberName]string propName = null)
+        {
+            if (this.TryGetValue(propName, out object value))
+            {
+                return (T)value;
+            }
+            return default(T);
+        }
+
+        protected void SetProperty(object value, [CallerMemberName]string propName = null)
+        {
+            this[propName] = value;
+        }
     }
 
-    public class UserState : Dictionary<string, object>
+    public class Reminder : BaseState
     {
-        public UserState()
+        public Reminder() : base(null) { }
+
+        public Reminder(IDictionary<string, object> source = null) : base(source) { }
+
+        public string Title
         {
-            this[nameof(Reminders)] = new List<Reminder>();
+            get { return GetProperty<string>(); }
+            set { SetProperty(value); }
         }
+    }
+
+    public class UserState : BaseState
+    {
+        public UserState() : base(null) { }
+
+        public UserState(IDictionary<string, object> source) : base(source) { }
 
         public IList<Reminder> Reminders
         {
-            get { return this[nameof(Reminders)] as IList<Reminder>; }
-            set { this[nameof(Reminders)] = value; }
+            get { return GetProperty<IList<Reminder>>(); }
+            set { SetProperty(value); }
         }
     }
 }
