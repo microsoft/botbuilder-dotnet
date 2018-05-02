3fef55e9e5715f6fd7e560ee53fb91b28e91635d
JuanAr <juan.arguello@southworks.com>
2018-05-02 18:02:37 -0300

    Proactive Message Sample Bot

---
+++ samples-final/AspNetCore-ProactiveMessage-Bot/ProactiveBot.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace AspNetCore_ProactiveMessage_Bot
+{
+    public class ProactiveBot : IBot
+    {
+        /// <summary>
+        /// Every Conversation turn for our EchoBot will call this method. In here
+        /// the bot checks the Activty type to verify it's a message, bumps the 
+        /// turn conversation 'Turn' count, and then echoes the users typing
+        /// back to them. 
+        /// </summary>
+        /// <param name="context">Turn scoped context containing all the data needed
+        /// for processing this conversation turn. </param>        
+        public async Task OnTurn(ITurnContext context)
+        {
+            // This bot is only handling Messages
+            if (context.Activity.Type == ActivityTypes.ConversationUpdate &&
+                context.Activity.MembersAdded[0].Name == "Bot")
+            {
+                await context.SendActivity("Hello and welcome to the proactive message bot.");
+
+                // Extract data from the user's message that the bot will need later to send an ad hoc message to the user. 
+                ProactiveMessage.FromActivity(context.Activity, "Hello again, this is a proactive message!");
+
+
+                // Schedule a new proactive message every 10 seconds
+                timer = new Timer(
+                       o =>
+                       {
+                           if (!string.IsNullOrEmpty(ProactiveMessage.fromId))
+                           {
+                               ProactiveMessage.Resume().Wait();
+                           }
+                       },
+                       new object(),
+                       TimeSpan.FromSeconds(10),
+                       TimeSpan.FromSeconds(10));
+            }
+        }
+
+        private static Timer timer;
+    }
+}
---
+++ samples-final/AspNetCore-ProactiveMessage-Bot/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCore_ProactiveMessage_Bot
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBot<ProactiveBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+           
+                options.EnableProactiveMessages = false;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseBotFramework();
+        }
+    }
+}
