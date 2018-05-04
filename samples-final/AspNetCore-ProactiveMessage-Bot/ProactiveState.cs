a0e6ce8cdfa1c483176f082e05d594a8e07a659c
Enzo Cano <enzo.cano@southworks.com>
2018-05-04 15:31:14 -0300

    Add copyright headers

--- samples-final/AspNetCore-ProactiveMessage-Bot/ProactiveState.cs
+++ samples-final/AspNetCore-ProactiveMessage-Bot/ProactiveState.cs
@@ -1,4 +1,7 @@
-﻿namespace AspNetCore_ProactiveMessage_Bot
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace AspNetCore_ProactiveMessage_Bot
 {
     public class ProactiveState
     {
