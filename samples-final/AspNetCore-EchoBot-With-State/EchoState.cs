a4e5075263ff72ec015e20802cf5dc70d7ae2623
Enzo Cano <enzo.cano@southworks.com>
2018-05-04 14:57:07 -0300

    Add copyright headers

--- samples-final/AspNetCore-EchoBot-With-State/EchoState.cs
+++ samples-final/AspNetCore-EchoBot-With-State/EchoState.cs
@@ -1,4 +1,7 @@
-﻿namespace AspNetCore_EchoBot_With_State
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace AspNetCore_EchoBot_With_State
 {
     /// <summary>
     /// Class for storing conversation state. 
