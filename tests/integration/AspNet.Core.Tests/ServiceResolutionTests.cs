8fd28ed54d01a49a1164871cc0afd302336ea744
Drew Marsh <drmarsh@microsoft.com>
2018-05-08 14:24:15 -0700

    Initial unit tests for ASP.NET WebAPI integration
     * Adds initial set of unit tests for ASP.NET WebAPI integration
    `HttpConfigurationExtensions` class
     * Fixes #562 by defaulting to an empty `SimpleCredentialProvider`
    instance whenever an explicit `ICredentialProvider` is not supplied via
    configuration options (NOTE: this enables the local dev + emulator
    scenario)
     * Add unit test for ASP.NET Core integration that validate #562

---
+++ tests/integration/AspNet.Core.Tests/ServiceResolutionTests.cs
@@ -0,0 +1,83 @@
+﻿using FluentAssertions;
+using Microsoft.Bot.Builder.Adapters;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.Core.Tests
+{
+    public class ServiceResolutionTests
+    {
+        public class ResolveBotFrameworkOptions
+        {
+            [Fact]
+            public void DefaultOptionsShouldResolve()
+            {
+                var serviceCollection = new ServiceCollection()
+                    .AddOptions()
+                    .AddBot<TestBot>();
+
+                var serviceProvider = serviceCollection.BuildServiceProvider();
+
+                var botFrameworkOptions = serviceProvider.GetService<IOptions<BotFrameworkOptions>>();
+
+                botFrameworkOptions.Value.Should().NotBeNull();
+            }
+
+            [Fact]
+            public void DefaultOptionsShouldResolveWithDefaultSimpleCredentialProviderWhenNotExplicitlyConfigured()
+            {
+                var serviceCollection = new ServiceCollection()
+                    .AddOptions()
+                    .AddBot<TestBot>();
+
+                var serviceProvider = serviceCollection.BuildServiceProvider();
+
+                var botFrameworkOptions = serviceProvider.GetService<IOptions<BotFrameworkOptions>>();
+
+                botFrameworkOptions.Value.CredentialProvider.Should().NotBeNull()
+                    .And.BeOfType<SimpleCredentialProvider>();
+            }
+        }
+
+        public class ResolveBotFrameworkAdapter
+        {
+            [Fact]
+            public void BotFrameworkAdapterShouldResolve()
+            {
+                var serviceCollection = new ServiceCollection()
+                    .AddOptions()
+                    .AddBot<TestBot>(options =>
+                    {
+                        options.CredentialProvider = Mock.Of<ICredentialProvider>();
+                    });
+
+                var serviceProvider = serviceCollection.BuildServiceProvider();
+
+                var botFrameworkAdapter = serviceProvider.GetService<BotFrameworkAdapter>();
+
+                botFrameworkAdapter.Should().NotBeNull();
+            }
+        }
+
+        public class ResolveIBot
+        {
+            [Fact]
+            public void IBotShouldResolve()
+            {
+                var serviceCollection = new ServiceCollection()
+                    .AddOptions()
+                    .AddBot<TestBot>();
+
+                var serviceProvider = serviceCollection.BuildServiceProvider();
+
+                var bot = serviceProvider.GetService<IBot>();
+
+                bot.Should().NotBeNull()
+                    .And.BeOfType<TestBot>();
+            }
+        }
+    }
+}
---
+++ tests/integration/AspNet.WebApi.Tests/HttpConfigurationExtensionsTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Web.Http;
+using FluentAssertions;
+using Microsoft.Bot.Builder.Integration.AspNet.WebApi.Handlers;
+using Microsoft.Bot.Connector.Authentication;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.WebApi.Tests
+{
+    public class HttpConfigurationExtensionsTests
+    {
+        public class MapBotFramework
+        {
+            [Fact]
+            public void NullHttpConfigurationThrows()
+            {
+                var httpConfiguration = default(HttpConfiguration);
+
+                var action = new Action(() => httpConfiguration.MapBotFramework());
+
+                action.Should().Throw<ArgumentNullException>().And.ParamName.Should().Be("httpConfiguration");
+            }
+
+            [Fact]
+            public void WithoutConfigurationCallback()
+            {
+                var httpConfiguration = new HttpConfiguration();
+
+                httpConfiguration.MapBotFramework();
+
+                var routes = httpConfiguration.Routes;
+
+                routes.Count.Should().Be(1);
+
+                var botMessageHandlerRoute = routes[BotMessageHandler.RouteName];
+                botMessageHandlerRoute.Handler.Should().BeOfType<BotMessageHandler>();
+            }
+
+            [Fact]
+            public void WithConfigurationCallback()
+            {
+                var httpConfiguration = new HttpConfiguration();
+
+                httpConfiguration.MapBotFramework(config =>
+                {
+                    config.Should().NotBeNull();
+                });
+
+                var routes = httpConfiguration.Routes;
+
+                routes.Count.Should().Be(1);
+
+                var botMessageHandlerRoute = routes[BotMessageHandler.RouteName];
+                botMessageHandlerRoute.Handler.Should().BeOfType<BotMessageHandler>();
+            }
+
+            [Fact]
+            public void EnableProactiveShouldMapMultipleHandlers()
+            {
+                var httpConfiguration = new HttpConfiguration();
+
+                httpConfiguration.MapBotFramework(config =>
+                {
+                    config.EnableProactiveMessages();
+                });
+
+                var routes = httpConfiguration.Routes;
+                routes.Count.Should().Be(2);
+
+                var botMessageHandlerRoute = routes[BotMessageHandler.RouteName];
+                botMessageHandlerRoute.Handler.Should().BeOfType<BotMessageHandler>();
+
+                var botProactiveMessageHandlerRoute = routes[BotProactiveMessageHandler.RouteName];
+                botProactiveMessageHandlerRoute.Handler.Should().BeOfType<BotProactiveMessageHandler>();
+            }
+        }
+    }
+}
