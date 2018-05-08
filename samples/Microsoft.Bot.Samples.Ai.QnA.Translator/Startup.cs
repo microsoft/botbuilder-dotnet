04900c5cdc3e611715e586261841e9b98824ba7d
Mostafa Elaraby (IBS) <a-moelar@microsoft.com>
2018-05-08 16:01:15 +0200

    Bug fixes in Luis translator and QnA translator samples

--- samples/Microsoft.Bot.Samples.Ai.QnA.Translator/Startup.cs
+++ samples/Microsoft.Bot.Samples.Ai.QnA.Translator/Startup.cs
@@ -47,8 +47,8 @@ namespace Microsoft.Bot.Samples.Ai.QnA.Translator
                 Dictionary<string, List<string>> patterns = new Dictionary<string, List<string>>();
                 patterns.Add("fr", new List<string> { "mon nom est (.+)" });//single pattern for fr language
                 middleware.Add(new ConversationState<CurrentUserState>(new MemoryStorage()));
-                middleware.Add(new TranslationMiddleware(new string[] { "en" }, "<your translator key here>", patterns, TranslatorLocaleHelper.GetActiveLanguage, TranslatorLocaleHelper.CheckUserChangedLanguage,true));
-                middleware.Add(new LocaleConverterMiddleware(TranslatorLocaleHelper.GetActiveLocale, TranslatorLocaleHelper.CheckUserChangedLocale, "en-us", LocaleConverter.Converter));
+                middleware.Add(new LocaleConverterMiddleware(TranslatorLocaleHelper.GetActiveLocale, TranslatorLocaleHelper.CheckUserChangedLanguageOrLocale, "en-us", LocaleConverter.Converter));
+                middleware.Add(new TranslationMiddleware(new string[] { "en" }, "<your translator key here>", patterns, TranslatorLocaleHelper.GetActiveLanguage, TranslatorLocaleHelper.CheckUserChangedLanguageOrLocale, true)); 
                 middleware.Add(new QnAMakerMiddleware(qnaEndpoint));
 
             });
