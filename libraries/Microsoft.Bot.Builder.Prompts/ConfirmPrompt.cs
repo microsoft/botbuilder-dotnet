9afb1c344f8f2b45f59f58ac90d058dacac2359b
Adrián Del Rincón López <arincon@encamina.com>
2018-05-08 14:08:12 +0200

    Fix Prompts.Confirm to support culture. Consolidated packages.

--- libraries/Microsoft.Bot.Builder.Prompts/ConfirmPrompt.cs
+++ libraries/Microsoft.Bot.Builder.Prompts/ConfirmPrompt.cs
@@ -1,10 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System; 
+using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Microsoft.Bot.Schema; 
+using Microsoft.Bot.Schema;
+using Microsoft.Recognizers.Text;
+using Microsoft.Recognizers.Text.Choice;
 using static Microsoft.Bot.Builder.Prompts.PromptValidatorEx;
 
 namespace Microsoft.Bot.Builder.Prompts
@@ -35,21 +38,13 @@ namespace Microsoft.Bot.Builder.Prompts
     /// </summary>
     public class ConfirmPrompt : BasePrompt<ConfirmResult>
     {
-        // private IModel _model;
-        private Regex yes = new Regex(@"(\byes\b|\byep\b|\bok\b|\byessir\b|\bconfirm\b|^y$)", RegexOptions.IgnoreCase);
-        private Regex no = new Regex(@"(\bno\b|\bnope\b|\bnosir\b|\bcancel\b|^n$)", RegexOptions.IgnoreCase);
+        private readonly IModel model;
 
         public ConfirmPrompt(string culture, PromptValidator<ConfirmResult> validator = null)
             : base(validator)
         {
-
+            model = new ChoiceRecognizer(culture).GetBooleanModel(culture);
         }
-
-        //protected ConfirmPrompt(IModel model, PromptValidator<ConfirmResult> validator = null)
-        //    : base(validator)
-        //{
-        //    this._model = model;
-        //}
 
         public override async Task<ConfirmResult> Recognize(ITurnContext context)
         {
@@ -60,21 +55,17 @@ namespace Microsoft.Bot.Builder.Prompts
 
             IMessageActivity message = context.Activity.AsMessageActivity();
             var confirmResult = new ConfirmResult();
-            Match yesMatch = yes.Match(message.Text);
-            Match noMatch = no.Match(message.Text);
-            if (yesMatch.Success)
+            var results = model.Parse(message.Text);
+            if (results.Any())
             {
-                confirmResult.Status = PromptStatus.Recognized;
-                confirmResult.Confirmation = true;
-                confirmResult.Text = yesMatch.Value;
-                await Validate(context, confirmResult);
-            }
-            else if (noMatch.Success)
-            {
-                confirmResult.Status = PromptStatus.Recognized;
-                confirmResult.Confirmation = false;
-                confirmResult.Text = noMatch.Value;
-                await Validate(context, confirmResult);
+                var result = results.First();
+                if (bool.TryParse(result.Resolution["value"].ToString(), out bool value))
+                {
+                    confirmResult.Status = PromptStatus.Recognized;
+                    confirmResult.Confirmation = value;
+                    confirmResult.Text = result.Text;
+                    await Validate(context, confirmResult);
+                }
             }
             return confirmResult;
         }
