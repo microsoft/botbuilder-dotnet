1ca2cfcdfd28c4200c6f8947b64082bac3914012
MIDDLEEAST\a-amelse <a-amelse@microsoft.com>
2018-05-17 16:11:43 +0200

    adding TranslatedDocumentObject to hold translation and alignment

---
+++ libraries/Microsoft.Bot.Builder.Ai.Translation/TranslatedDocument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.Ai.Translation
+{
+    public class TranslatedDocument
+    {
+        private string sourceMessage;
+        private string targetMessage;
+        private string alignment;
+
+        public string SourceMessage { get => sourceMessage; set => sourceMessage = value; }
+        public string TargetMessage { get => targetMessage; set => targetMessage = value; }
+        public string Alignment { get => alignment; set => alignment = value; }
+    }
+}
