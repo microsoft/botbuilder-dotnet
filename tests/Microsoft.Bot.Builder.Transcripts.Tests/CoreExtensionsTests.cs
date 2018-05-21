32f9a71e321ca9d8a2a985bfe117f20ec55ffded
JuanAr <juan.arguello@southworks.com>
2018-05-21 15:04:53 -0300

    Add empty class files

---
+++ tests/Microsoft.Bot.Builder.Transcripts.Tests/CoreExtensionsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Transcripts.Tests
+{
+    [TestClass]
+    public class CoreExtensionsTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
---
+++ tests/Microsoft.Bot.Builder.Transcripts.Tests/PromptsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Transcripts.Tests
+{
+    [TestClass]
+    public class PromptsTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
