b25de48dfabbf3a639141a67433dc28354a3336e
JuanAr <juan.arguello@southworks.com>
2018-05-21 15:04:53 -0300

    Add empty class files

---
+++ tests/Microsoft.Bot.Builder.Transcripts.Tests/DialogsTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Transcripts.Tests
+{
+    [TestClass]
+    public class DialogsTests
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
