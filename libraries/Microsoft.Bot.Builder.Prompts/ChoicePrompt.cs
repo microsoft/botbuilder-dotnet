dfc76b45e838a986642135048bbf7a09d5f7e2cb
Adrián Del Rincón López <arincon@encamina.com>
2018-05-23 11:23:24 +0200

    Changes ListStyle

--- libraries/Microsoft.Bot.Builder.Prompts/ChoicePrompt.cs
+++ libraries/Microsoft.Bot.Builder.Prompts/ChoicePrompt.cs
@@ -10,38 +10,6 @@ using static Microsoft.Bot.Builder.Prompts.PromptValidatorEx;
 
 namespace Microsoft.Bot.Builder.Prompts
 {
-    ///<summary>
-    /// Controls the way that choices for a `ChoicePrompt` or yes/no options for a `ConfirmPrompt` are
-    /// presented to a user.
-    ///</summary>
-    public enum ListStyle
-    {
-        ///<summary>
-        /// Don't include any choices for prompt.
-        /// </summary>
-        None,
-
-        ///<summary>
-        /// Automatically select the appropriate style for the current channel.
-        /// </summary>
-        Auto,
-
-        ///<summary>
-        /// Add choices to prompt as an inline list.
-        ///</summary>
-        Inline,
-
-        ///<summary>
-        /// Add choices to prompt as a numbered list.
-        ///</summary>
-        List,
-
-        ///<summary>
-        /// Add choices to prompt as suggested actions.
-        ///</summary>
-        SuggestedAction
-    };
-
     /// <summary>
     /// Represents recognition result for the ChoicePrompt.
     /// </summary>
@@ -59,9 +27,9 @@ namespace Microsoft.Bot.Builder.Prompts
 
     public class ChoicePrompt
     {
-        public ChoicePrompt(string culture, PromptValidator<ChoiceResult> validator = null)
+        public ChoicePrompt(string culture, PromptValidator<ChoiceResult> validator = null, ListStyle listStyle = ListStyle.Auto)
         {
-            Style = ListStyle.Auto;
+            Style = listStyle;
             Validator = validator;
             Culture = culture;
         }
