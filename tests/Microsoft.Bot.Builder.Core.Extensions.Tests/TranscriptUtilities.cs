d2944a19f64ccae4cc1a1b1a3cfd2a7ff47a6993
Enzo Cano <enzo.cano@southworks.com>
2018-05-24 17:25:44 -0300

    Use chatdown to converts chat files

--- tests/Microsoft.Bot.Builder.Core.Extensions.Tests/TranscriptUtilities.cs
+++ tests/Microsoft.Bot.Builder.Core.Extensions.Tests/TranscriptUtilities.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using Microsoft.Bot.Schema;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
@@ -24,17 +25,52 @@ namespace Microsoft.Bot.Builder.Core.Extensions.Tests
         /// <returns>A list of activities to test</returns>
         public static IEnumerable<IActivity> GetFromTestContext(TestContext context)
         {
+            var relativePath = Path.Combine(context.FullyQualifiedTestClassName.Split('.').Last(), $"{context.TestName}.chat");
+            return GetActivities(relativePath);
+        }
+
+        public static IEnumerable<IActivity> GetActivities(string relativePath)
+        {
             var transcriptsRootFolder = TestUtilities.GetKey("TranscriptsRootFolder") ?? @"..\..\..\..\..\transcripts";
-            var directory = Path.Combine(transcriptsRootFolder, context.FullyQualifiedTestClassName.Split('.').Last());
-            var fileName = $"{context.TestName}.transcript";
-            var path = Path.Combine(directory, fileName);
+            var path = Path.Combine(transcriptsRootFolder, relativePath);
             if (!File.Exists(path))
             {
                 Assert.Fail($"Required transcript file '{path}' does not exists in '{transcriptsRootFolder}' folder. Review the 'TranscriptsRootFolder' environment variable value.");
             }
 
-            var content = File.ReadAllText(path);
-            return JsonConvert.DeserializeObject<Activity[]>(content);
+            string content;
+            if (string.Equals(path.Split('.').Last(), "chat", StringComparison.InvariantCultureIgnoreCase))
+            {
+                content = Chatdown(path);
+            }
+            else
+            {
+                content = File.ReadAllText(path);
+            }
+
+            var activities = JsonConvert.DeserializeObject<List<Activity>>(content);
+
+            var lastActivity = activities.Last();
+            if (lastActivity.Text.Last() == '\n')
+            {
+                lastActivity.Text = lastActivity.Text.Remove(lastActivity.Text.Length - 1);
+            }
+
+            return activities.Take(activities.Count - 1).Append(lastActivity);
+        }
+
+        private static string Chatdown(string path)
+        {
+            var file = new FileInfo(path);
+            var chatdown = new System.Diagnostics.Process();
+            chatdown.StartInfo.UseShellExecute = false;
+            chatdown.StartInfo.RedirectStandardOutput = true;
+            chatdown.StartInfo.FileName = Environment.ExpandEnvironmentVariables(@"%PROGRAMFILES%\nodejs\chatdown.cmd");
+            chatdown.StartInfo.Arguments = file.FullName;
+            chatdown.Start();
+            var content = chatdown.StandardOutput.ReadToEnd();
+            chatdown.WaitForExit();
+            return content;
         }
 
         /// <summary>
