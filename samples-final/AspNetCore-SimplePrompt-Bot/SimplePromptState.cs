c54c7450a6f141f20f137fd8228d663a21a67e9d
Enzo Cano <enzo.cano@southworks.com>
2018-06-01 14:54:50 -0300

    Add SimplePrompt-Bot

---
+++ samples-final/AspNetCore-SimplePrompt-Bot/SimplePromptState.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCore_SimplePrompt_Bot
+{
+    public class SimplePromptState
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public bool PromptinName { get; set; }
+        public bool PromptinAge { get; set; }
+    }
+}
