2994941c4d4fa9cae02dac8eb49fa64950d7c998
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-06-01 17:25:41 -0700

    Extend SuggestedActions to support arbitrary enumerations as input.

---
+++ libraries/Microsoft.Bot.Schema/SuggestedActionsEx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Bot.Schema
+{
+    public partial class SuggestedActions
+    {
+        /// <summary>
+        /// Initializes a new instance of the SuggestedActions class.
+        /// </summary>
+        /// <param name="to">Ids of the recipients that the actions should be
+        /// shown to.  These Ids are relative to the channelId and a subset of
+        /// all recipients of the activity</param>
+        /// <param name="actions">Actions that can be shown to the user</param>
+        /// <exception cref="ArgumentNullException"/>
+        public SuggestedActions(IEnumerable<string> to, IEnumerable<CardAction> actions) : this(to.ToArray(), actions.ToArray()) { }
+    }
+}
