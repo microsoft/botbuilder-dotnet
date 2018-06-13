26e3387a549a549817b28201622b36252687036f
pablo costantini <pablo.costantini@southworks.com>
2018-06-13 14:52:37 -0300

    Cleanup

--- tests/Microsoft.Bot.Builder.Transcripts.Tests/CoreExtensionsTests.cs
+++ tests/Microsoft.Bot.Builder.Transcripts.Tests/CoreExtensionsTests.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.Bot.Builder.Adapters;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Adapters;
 using Microsoft.Bot.Builder.Core.Extensions;
 using Microsoft.Bot.Builder.Core.Extensions.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Builder.Transcripts.Tests
 {
