3c9008128705acb2e32a7901173523e36175fe1e
John Taylor <johtaylo@microsoft.com>
2018-06-15 15:13:32 -0700

    consolidate test projects

--- tests/Microsoft.Bot.Builder.Tests/BotAdapterTests.cs
+++ tests/Microsoft.Bot.Builder.Tests/BotAdapterTests.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Schema;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Microsoft.Bot.Builder.Core.Tests
+namespace Microsoft.Bot.Builder.Tests
 {
     [TestClass]
     [TestCategory("BotAdapter")]
