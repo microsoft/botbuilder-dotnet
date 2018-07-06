3f11707f696c09807d9c241c55ab97720ab01291
cleemullins <cleemullins@users.noreply.github.com>
2018-07-05 22:24:11 -0700

    Added initial Slot related classes and Stubs

---
+++ libraries/Microsoft.Bot.Builder/IReadOnlySlot.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder
+{
+    public interface IReadOnlySlot<T>
+    {
+        async Task<T> Get(TurnContext context);
+    }
+}
