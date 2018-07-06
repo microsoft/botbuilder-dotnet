a941c754ae196bb13c5c106b8bb07306c07721c2
Chris Mullins <cleemullins@users.noreply.github.com>
2018-07-06 14:37:36 -0700

    Addition of Frame realated interfaces and classes

--- libraries/Microsoft.Bot.Builder/IReadWriteSlot.cs
+++ libraries/Microsoft.Bot.Builder/IReadWriteSlot.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Builder
 {
-    interface IReadWriteSlot
+    public interface IReadWriteSlot<T> : IReadOnlySlot<T>
     {
+        ISlotDefinition Definition { get; }
+
+        IFrame Frame { get;  }
+
+        IReadOnlySlot<T> AsReadOnly();
+
+        Task DeleteAsync(TurnContext context);
+
+        Task SetAsync(TurnContext context, T value);
     }
 }
--- libraries/Microsoft.Bot.Builder/SlotHistoryValue.cs
+++ libraries/Microsoft.Bot.Builder/SlotHistoryValue.cs
@@ -1,10 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
 
 namespace Microsoft.Bot.Builder
 {
-    class SlotHistoryValue
+    public class SlotHistoryValue<T>
     {
+        public T Value { get; set; }
+
+        public DateTime Timestamp { get; set; }
     }
 }
