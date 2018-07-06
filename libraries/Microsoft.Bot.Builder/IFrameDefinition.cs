0b01dbf5eb0bcf0bb7e13734e5c017259728fa1d
Chris Mullins <cleemullins@users.noreply.github.com>
2018-07-06 14:37:36 -0700

    Addition of Frame realated interfaces and classes

---
+++ libraries/Microsoft.Bot.Builder/IFrameDefinition.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Bot.Builder
+{
+    public interface IFrameDefinition
+    {
+        string FrameScope { get; set; }
+
+        string NameSpace { get; set; }
+
+        List<ISlotDefinition> Slots { get; set; }
+    }
+}
