d46fc07320178aa0e5995f51f2aefc45a9a4c9a2
Dave Taniguchi <daveta@microsoft.com>
2018-07-13 12:27:50 -0700

    Fix botbuilder-dotnet/issues/644 - Choice override on ConfirmPrompt

--- libraries/Microsoft.Bot.Builder.Dialogs/Prompts/ConfirmPrompt.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/Prompts/ConfirmPrompt.cs
@@ -8,19 +8,51 @@ using static Microsoft.Bot.Builder.Dialogs.PromptValidatorEx;
 
 namespace Microsoft.Bot.Builder.Dialogs
 {
+    /// <summary>
+    /// Prompts a user to confirm something with a yes/no response.
+    /// 
+    /// <remarks>By default the prompt will return to the calling dialog a `boolean` representing the users
+    /// selection.
+    /// When used with your bots 'DialogSet' you can simply add a new instance of the prompt as a named
+    /// dialog using <code>DialogSet.Add()</code>. You can then start the prompt from a waterfall step using either
+    /// <code>DialogContext.Begin()</code> or <code>DialogContext.Prompt()</code>. The user will be prompted to answer a
+    /// 'yes/no' or 'true/false' question and the users response will be passed as an argument to the
+    /// callers next waterfall step
+    /// </remarks>
+    /// </summary>
     public class ConfirmPrompt : Prompt<ConfirmResult>
     {
         private ConfirmPromptInternal _prompt;
 
+        /// <summary>
+        /// Creates a new ConfirmPrompt instance
+        /// </summary>
+        /// <param name="culture">Culture to use if <code>DialogContext.Context.Activity.Locale</code> property not specified. Defaults to a value of <code>CultureInfo.CurrentCulture</code>.</param>
+        /// <param name="validator">Validator that will be called each time the user responds to the prompt.  If the validator replies with a message no additional retry prompt will be sent.</param>
         public ConfirmPrompt(string culture, PromptValidator<ConfirmResult> validator = null)
         {
             _prompt = new ConfirmPromptInternal(culture, validator);
         }
 
+        /// <summary>
+        /// The style of the yes/no choices rendered to the user when prompting.
+        /// <seealso cref="Choices.ListStyle"/>
+        /// </summary>
         public ListStyle Style
         {
             get { return _prompt.Style; }
             set { _prompt.Style = value; }
+        }
+
+        /// <summary>
+        /// Additional options passed to the 'ChoiceFactory' and used to tweak the style of choices
+        /// rendered to the user.
+        /// <seealso cref="Choices.ChoiceFactoryOptions"/>
+        /// </summary>
+        public ChoiceFactoryOptions ChoiceOptions
+        {
+            get { return _prompt.ChoiceOptions;  }
+            set { _prompt.ChoiceOptions = value;  }
         }
 
         protected override async Task OnPrompt(DialogContext dc, PromptOptions options, bool isRetry)
