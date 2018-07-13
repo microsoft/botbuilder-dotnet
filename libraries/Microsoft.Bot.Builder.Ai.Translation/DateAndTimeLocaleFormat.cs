d495967e8bf1cdd6d9126b9fe5ca2d402dfd32fe
Ezequiel Jadib <ezequiel.jadib@southworks.com>
2018-07-13 11:06:15 -0300

    Moving class to different files to be compliant with StyleCop analyzers

--- libraries/Microsoft.Bot.Builder.Ai.Translation/DateAndTimeLocaleFormat.cs
+++ libraries/Microsoft.Bot.Builder.Ai.Translation/DateAndTimeLocaleFormat.cs
@@ -1,18 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text.RegularExpressions;
-using Microsoft.Recognizers.Text;
-using Microsoft.Recognizers.Text.DateTime;
 
 namespace Microsoft.Bot.Builder.Ai.Translation
 {
-
     /// <summary>
     /// DateAndTimeLocaleFormat Class used to store date format and time format
     /// for different locales.
