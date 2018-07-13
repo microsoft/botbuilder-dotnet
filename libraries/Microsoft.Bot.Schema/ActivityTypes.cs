10638672316e124c2e337e93112f800018617465
Drew Marsh <drmarsh@microsoft.com>
2018-07-13 09:57:21 -0700

    Fleshing out activity runtime type resolution
     * Docs on ActivityTypes::GetRuntimeType
     * Created missing `DeleteUserDataActivity` and `PingActivity` classes
    for those activity types
     * Move `ActivityTypesEx` out of schema, they really aren't part of the
    scheme, they are part of the additional types the bot builder SDK
    introduces for specific cases

--- libraries/Microsoft.Bot.Schema/ActivityTypes.cs
+++ libraries/Microsoft.Bot.Schema/ActivityTypes.cs
@@ -34,18 +34,34 @@ namespace Microsoft.Bot.Schema
         public const string Suggestion = "suggestion";
         public const string Trace = "trace";
 
+        /// <summary>
+        /// Given an <paramref name="activityType">activity type</paramref>, returns the corresponding <see cref="Type"/>.
+        /// </summary>
+        /// <remarks>
+        /// If the specified value in <paramref name="activityType"/> is an unknown type, the runtime type returned 
+        /// will be <see cref="Activity"/>.
+        /// </remarks>
+        /// <param name="activityType">The activity type.</param>
+        /// <returns>
+        /// The runtime type for the specified <paramref name="activityType">activity type</paramref>. If there is no matching runtime type, then just <see cref="Activity"/> will be returned.</returns>
         public static Type GetRuntimeType(string activityType)
         {
+            /*
+             * TODO: this is extremely brittle and not a great maintenance story. Consider a dynamic approach where
+             * Activity classes are marked with an attribute that indicates their schema type name and then lazily assembly
+             * scan for those on first call to this method and build/cache a dictionary lookup for subsequent calls.
+             */
+
             switch(activityType)
-            {
-                case ActivityTypes.Message:
-                    return typeof(MessageActivity);
-
-                case ActivityTypes.Typing:
-                    return typeof(TypingActivity);
-
+            {                
                 case ActivityTypes.ConversationUpdate:
                     return typeof(ConversationUpdateActivity);
+
+                case ActivityTypes.ContactRelationUpdate:
+                    return typeof(ContactRelationUpdateActivity);
+
+                case ActivityTypes.DeleteUserData:
+                    return typeof(DeleteUserDataActivity);
 
                 case ActivityTypes.EndOfConversation:
                     return typeof(EndOfConversationActivity);
@@ -53,8 +69,36 @@ namespace Microsoft.Bot.Schema
                 case ActivityTypes.Event:
                     return typeof(EventActivity);
 
-                // TODO: add rest of types
+                case ActivityTypes.InstallationUpdate:
+                    return typeof(InstallationUpdateActivity);
 
+                case ActivityTypes.Invoke:
+                    return typeof(InvokeActivity);
+
+                case ActivityTypes.Message:
+                    return typeof(MessageActivity);
+
+                case ActivityTypes.MessageDelete:
+                    return typeof(MessageDeleteActivity);
+
+                case ActivityTypes.MessageReaction:
+                    return typeof(MessageReactionActivity);
+
+                case ActivityTypes.MessageUpdate:
+                    return typeof(MessageUpdateActivity);
+
+                case ActivityTypes.Ping:
+                    return typeof(PingActivity);
+
+                case ActivityTypes.Suggestion:
+                    return typeof(SuggestionActivity);
+
+                case ActivityTypes.Trace:
+                    return typeof(TraceActivity);
+
+                case ActivityTypes.Typing:
+                    return typeof(TypingActivity);
+                    
                 default:
                     // TODO: trace a warning that we didn't find a specific type
 
