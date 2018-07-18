e0275821749d467a731f077f55b5a6affff2a5cf
Tom Laird-McConnell <tomlm@microsoft.com>
2018-07-17 21:37:04 -0700

    Dropped IPropertyContainer

--- libraries/Microsoft.Bot.Builder/IPropertyAccessor.cs
+++ libraries/Microsoft.Bot.Builder/IPropertyAccessor.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -6,36 +9,45 @@ using System.Threading.Tasks;
 namespace Microsoft.Bot.Builder
 {
     /// <summary>
-    /// This is metadata about the property including policy info
+    /// This is metadata about the property including policy info.
     /// </summary>
     public interface IPropertyAccessor
     {
+        /// <summary>
+        /// Gets the name of the property.
+        /// </summary>
+        /// <value>
+        /// The name of the property.
+        /// </value>
         string Name { get; }
     }
 
     /// <summary>
-    /// Interface which defines methods for how you can get data from the context
+    /// Interface which defines methods for how you can get data from a property source such as BotState.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
+    /// <typeparam name="T">type of the property.</typeparam>
     public interface IPropertyAccessor<T> : IPropertyAccessor
     {
         /// <summary>
-        /// Get the property from the context
+        /// Get the property value from the source.
         /// </summary>
-        /// <param name="turnContext"></param>
-        /// <returns></returns>
+        /// <param name="turnContext">Turn Context.</param>
+        /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
         Task<T> GetAsync(ITurnContext turnContext);
 
         /// <summary>
-        /// Delete the property
+        /// Delete the property from the source.
         /// </summary>
-        /// <param name="turnContext"></param>
-        /// <returns></returns>
+        /// <param name="turnContext">Turn Context.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task DeleteAsync(ITurnContext turnContext);
 
         /// <summary>
-        /// Set the property on the context
+        /// Set the property value on the source.
         /// </summary>
+        /// <param name="turnContext">Turn Context.</param>
+        /// <param name="value">the value to set</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         Task SetAsync(ITurnContext turnContext, T value);
     }
 }
