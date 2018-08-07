f092c6131ecbad57f93582467be964b797a29d0b
Enzo Cano <enzo.cano@southworks.com>
2018-08-07 12:22:12 -0300

    Change tested texts to const string

--- tests/Microsoft.Bot.Builder.Ai.LUIS.Tests/LuisApplicationTests.cs
+++ tests/Microsoft.Bot.Builder.Ai.LUIS.Tests/LuisApplicationTests.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using Microsoft.Azure.CognitiveServices.Language.LUIS.Runtime;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
 
