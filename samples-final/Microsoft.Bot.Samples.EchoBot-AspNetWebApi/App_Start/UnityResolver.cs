4f17b4cc71a8d951d8b3d41187537168c0990f43
Dave Taniguchi <daveta@microsoft.com>
2018-08-09 17:42:09 -0700

    Add AspNetWebApi Echobot sample

---
+++ samples-final/Microsoft.Bot.Samples.EchoBot-AspNetWebApi/App_Start/UnityResolver.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Dependencies;
+using Unity;
+using Unity.Exceptions;
+
+namespace Microsoft.Bot.Samples.Echo.AspNetWebApi.App_Start
+{
+    public class UnityResolver : IDependencyResolver
+    {
+        private readonly IUnityContainer _container;
+
+        public UnityResolver(IUnityContainer container)
+        {
+            _container = container;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            try
+            {
+                return _container.Resolve(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            try
+            {
+                return _container.ResolveAll(serviceType);
+            }
+            catch (ResolutionFailedException)
+            {
+                return new List<object>();
+            }
+        }
+
+        public IDependencyScope BeginScope()
+        {
+            var child = _container.CreateChildContainer();
+            return new UnityResolver(child);
+        }
+
+        public void Dispose()
+        {
+            _container.Dispose();
+        }
+    }
+}
---
+++ samples-final/Microsoft.Bot.Samples.EchoBot-AspNetWebApi/EchoBot.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Samples.Echo.AspNetWebApi
+{
+    public class EchoBot : IBot
+    {
+        public EchoBot() { }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            switch (turnContext.Activity.Type)
+            {
+                case ActivityTypes.Message:
+                    await turnContext.SendActivityAsync($"You sent '{turnContext.Activity.Text}'");
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    foreach (var newMember in turnContext.Activity.MembersAdded)
+                    {
+                        if (newMember.Id != turnContext.Activity.Recipient.Id)
+                        {
+                            await turnContext.SendActivityAsync("Hello and welcome to the echo bot.");
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
