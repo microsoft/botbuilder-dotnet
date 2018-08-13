038e48e81c7d0d621826bd91e9885630d07d690a
John Taylor <johtaylo@microsoft.com>
2018-08-13 13:20:41 -0700

    a little clean up

--- libraries/Microsoft.Bot.Builder.Dialogs/Prompts/PromptOptions.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/Prompts/PromptOptions.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
 using System.Collections.Generic;
 using Microsoft.Bot.Builder.Dialogs.Choices;
 using Microsoft.Bot.Schema;
---
+++ tests/Microsoft.Bot.Builder.Dialogs.Tests/ComponentDialogTests.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Dialogs.Tests
+{
+    [TestClass]
+    [TestCategory("Prompts")]
+    [TestCategory("ComponentDialog Tests")]
+    public class ComponentDialogTests
+    {
+    }
+}
