dc94c6e1a656d651863735731db2648c71e3b39f
Tom Laird-McConnell <tomlm@microsoft.com>
2018-08-16 20:13:19 -0700

    Switch project to bet dotnet core

--- tests/Microsoft.Bot.Configuration.Tests/Properties/AssemblyInfo.cs
+++ tests/Microsoft.Bot.Configuration.Tests/Properties/AssemblyInfo.cs
@@ -2,18 +2,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("BotConfiguration.Net.Test")]
+[assembly: AssemblyTitle("Microsoft.Bot.Configuration.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("BotConfiguration.Net.Test")]
+[assembly: AssemblyProduct("Microsoft.Bot.Configuration.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
-[assembly: Guid("5ade1aef-47e7-418d-8e37-550f8d54e20a")]
+[assembly: Guid("1b7920e6-5262-4054-b72d-3a8dbba057d2")]
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
