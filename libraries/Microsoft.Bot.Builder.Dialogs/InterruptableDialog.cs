92d9818717f5a96247d838a60f917ed56717f3f2
Lauren Mills <lamil@microsoft.com>
2018-08-27 12:38:56 -0700

    Added interruptable dialog

---
+++ libraries/Microsoft.Bot.Builder.Dialogs/InterruptableDialog.cs
@@ -0,0 +1,31 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Dialogs
+{
+    public abstract class InterruptableDialog : ComponentDialog
+    {
+        public InterruptableDialog(string dialogId)
+            : base(dialogId)
+        {
+        }
+
+        protected override async Task<DialogStatus> OnDialogContinueAsync(DialogContext dc)
+        {
+            var result = await this.OnDialogInterruptionAsync(dc).ConfigureAwait(false);
+
+            if (result == DialogStatus.Interrupted)
+            {
+                // try to reprompt
+                return await dc.RepromptAsync().ConfigureAwait(false);
+            }
+            else if (result == DialogStatus.Waiting)
+            {
+                return result;
+            }
+
+            return await base.OnDialogContinueAsync(dc).ConfigureAwait(false);
+        }
+
+        protected abstract Task<DialogStatus> OnDialogInterruptionAsync(DialogContext dc);
+    }
+}
