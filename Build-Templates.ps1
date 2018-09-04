d94e3826650c6caf8159fe32ede948471a6d47aa
Enzo Cano <enzo.cano@southworks.com>
2018-09-04 11:34:53 -0300

    AutoUpdate VSIX version and Publish to myget

--- Build-Templates.ps1
+++ Build-Templates.ps1
@@ -9,7 +9,7 @@ function Resolve-MsBuild {
 	$msBuild2015 = "${env:ProgramFiles(x86)}\MSBuild\14.0\bin\msbuild.exe"
 
 	if(-not (Test-Path $msBuild2015)) {
-		throw 'Could not find MSBuild 2015 or later.'
+		throw "Could not find MSBuild 2015 or later."
 	}
 
 	Write-Host "Found MSBuild 2015."
@@ -22,7 +22,7 @@ function Resolve-VsixSignTool {
 	$signToolPath = Resolve-Path ".\packages\Microsoft.VSSDK.VsixSignTool*\tools\vssdk\vsixsigntool.exe" -ErrorAction SilentlyContinue
 	
 	if(-not (Test-Path $signToolPath)) {
-		throw 'Could not find VSIX Sign Path.'
+		throw "Could not find VSIX Sign Path."
 	}
 
     Write-Host "Found VSIX Sign Path."
@@ -32,16 +32,23 @@ function Resolve-VsixSignTool {
 
 #Manually generate template from EchoBot-With-State sample
 
-Write-Host 'Generate zip file of template'
+Write-Host "Update VSIX project version"
+[xml]$vsixManifest = Get-Content ".\templates\EchoBot.Template\source.extension.vsixmanifest"
+$vsixManifest.PackageManifest.Metadata.Identity.Version = "1.0.$env:new_version"
+$vsixManifest.Save(".\templates\EchoBot.Template\source.extension.vsixmanifest")
+
+Write-Host "Generate zip file of template"
 Compress-Archive -Force .\templates\EchoBot.Template\ProjectTemplates\EchoBot-With-State\ .\templates\EchoBot.Template\ProjectTemplates\EchoBot-With-State.zip
 
-Write-Host 'Build VSIX project'
+Write-Host "Build VSIX project"
 $msBuild = Resolve-MsBuild
 & $msBuild .\Microsoft.Bot.Builder.Templates.sln /p:Configuration=Release
 
-Write-Host 'Sign VSIX file'
+Write-Host "Sign VSIX file"
 $signTool = Resolve-VsixSignTool
 & $signTool sign /v /f .\templates\EchoBot.Template\certificate.pfx /p $env:templates_sign /fd sha256 .\templates\EchoBot.Template\bin\Release\EchoBot.Template.vsix
 
-Write-Host 'Upload to MyGet'
-Write-Host 'curl -X POST --verbose --data-binary @"path-to-vsix-file.vsix" -H "X-NuGet-ApiKey: api-key-here" https://www.myget.org/F/<feedname>/vsix/upload'
+Write-Host "Upload to MyGet"
+$headers = @{"X-NuGet-ApiKey"=$env:myget_key}  
+$uri = "https://botbuilder.myget.org/F/botframework-emulator/vsix/upload"
+Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -InFile ".\templates\EchoBot.Template\bin\Release\EchoBot.Template.vsix"
