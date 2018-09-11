63689a976094435eac02ea69791092c1ac8a2c75
Tom Laird-McConnell <tomlm@microsoft.com>
2018-09-10 19:45:59 -0700

    Add PrivateConversationState and unit tests

---
+++ libraries/Microsoft.Bot.Builder/PrivateConversationState.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Builder
+{
+    /// <summary>
+    /// Handles persistence of a conversation state object using the conversation ID and From ID as part of the key.
+    /// </summary>
+    public class PrivateConversationState : BotState
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PrivateConversationState"/> class.
+        /// </summary>
+        /// <param name="storage">The storage provider to use.</param>
+        public PrivateConversationState(IStorage storage)
+            : base(storage, nameof(PrivateConversationState))
+        {
+        }
+
+        /// <summary>
+        /// Gets the key to use when reading and writing state to and from storage.
+        /// </summary>
+        /// <param name="turnContext">The context object for this turn.</param>
+        /// <returns>The storage key.</returns>
+        protected override string GetStorageKey(ITurnContext turnContext) => $"conversation/{turnContext.Activity.ChannelId}/{turnContext.Activity.Conversation.Id}/{turnContext.Activity.From.Id}";
+    }
+}
