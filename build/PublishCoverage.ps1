400fc460f96577caf933086597a431b971489c80
Enzo Cano <enzo.cano@southworks.com>
2018-09-18 20:33:42 -0300

    Fix calls to tools

--- build/PublishCoverage.ps1
+++ build/PublishCoverage.ps1
@@ -5,12 +5,12 @@ $coverageUploader = ".\tools\csmacnz.Coveralls.exe"
 
 Write-Host Analyze coverage
 $coverageFiles = Get-ChildItem -Include "*.coverage" -Recurse | Select -Exp FullName
-Write-Host &$coverageAnalyzer analyze /output:coverage.coveragexml $coverageFiles
-&$coverageAnalyzer analyze /output:coverage.coveragexml $coverageFiles
+Write-Host with files: $coverageFiles
+."$coverageAnalyzer" analyze /output:coverage.coveragexml $coverageFiles
 
 Write-Host Upload coverage
-Write-Host &$coverageUploader --dynamiccodecoverage -i "coverage.coveragexml" --repoToken $env:Coveralls_Token --commitId $env:Build_SourceVersion --commitBranch $env:Build_SourceBranchName --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory" -o coverage.json
-&$coverageUploader --dynamiccodecoverage -i "coverage.coveragexml" --repoToken $env:Coveralls_Token --commitId $env:Build_SourceVersion --commitBranch $env:Build_SourceBranchName --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory" -o coverage.json
+Write-Host with args: --repoToken $env:Coveralls_Token --commitId $env:Build_SourceVersion --commitBranch $env:Build_SourceBranchName --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory"
+."$coverageUploader" --dynamiccodecoverage -i "coverage.coveragexml" --repoToken $env:Coveralls_Token --commitId $env:Build_SourceVersion --commitBranch $env:Build_SourceBranchName --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory" -o coverage.json
 
 Write-Host Clean
 Remove-Item coverage.coveragexml -ErrorAction Ignore
