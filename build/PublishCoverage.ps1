864273ba7962bbd88bb0773707dd8ecbc652e27a
Enzo Cano <enzo.cano@southworks.com>
2018-09-18 20:54:45 -0300

    Add Coveralls.Token as a script argument

--- build/PublishCoverage.ps1
+++ build/PublishCoverage.ps1
@@ -1,3 +1,7 @@
+Param(
+    [string]$coverallsToken
+)
+
 Write-Host Install tools
 $coverageAnalyzer = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Team Tools\Dynamic Code Coverage Tools\CodeCoverage.exe"
 dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
@@ -9,8 +13,8 @@ Write-Host with files: $coverageFiles
 ."$coverageAnalyzer" analyze /output:coverage.coveragexml $coverageFiles
 
 Write-Host Upload coverage
-Write-Host with args: --repoToken $env:Coveralls_Token --commitId $env:Build_SourceVersion --commitBranch $env:Build_SourceBranchName --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory"
-."$coverageUploader" --dynamiccodecoverage -i "coverage.coveragexml" --repoToken $env:Coveralls_Token --commitId $env:Build_SourceVersion --commitBranch $env:Build_SourceBranchName --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory" -o coverage.json
+Write-Host with args: --repoToken $coverallsToken --commitId $env:Build_SourceVersion --commitBranch $env:Build_SourceBranch --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory"
+."$coverageUploader" --dynamiccodecoverage -i "coverage.coveragexml" --repoToken $coverallsToken --commitId $env:Build_SourceVersion --commitBranch "$env:Build_SourceBranch" --commitAuthor "$env:Build_RequestedFor" --commitEmail "$env:Build_RequestedForEmail" --commitMessage "$env:Build_SourceVersionMessage" --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory" -o coverage.json
 
 Write-Host Clean
 Remove-Item coverage.coveragexml -ErrorAction Ignore
