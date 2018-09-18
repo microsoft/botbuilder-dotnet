cd2748f38f9c8c1820e14f6c8ea87d2cd98eb754
Tom Laird-McConnell <tomlm@microsoft.com>
2018-09-17 18:45:23 -0700

    make qnamaker ensure that hostname has /qnamaker suffix

---
+++ tests/Microsoft.Bot.Configuration.Tests/ServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Configuration.Tests
+{
+    [TestClass]
+    public class ServiceTests
+    {
+        [TestMethod]
+        public void LuisReturnsCorrectUrl()
+        {
+            var luis = new LuisService() { Region = "westus" };
+            Assert.AreEqual(luis.GetEndpoint(), "https://westus.api.cognitive.microsoft.com");
+        }
+
+
+        [TestMethod]
+        public void QnaMakerSuffixTest()
+        {
+            var qnamaker = new QnAMakerService() { Hostname = "http://foo.azurewebsites.net" };
+            Assert.AreEqual("http://foo.azurewebsites.net/qnamaker", qnamaker.Hostname);
+
+            qnamaker = new QnAMakerService() { Hostname = "http://foo.azurewebsites.net/asdf?x=15" };
+            Assert.AreEqual("http://foo.azurewebsites.net/qnamaker", qnamaker.Hostname);
+        }
+    }
+}
