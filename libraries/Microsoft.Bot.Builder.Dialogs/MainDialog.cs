7ec536e2d6a50207cf41606ca6defd407aa7fd5f
John Taylor <johtaylo@microsoft.com>
2018-09-18 18:15:47 -0700

    port MainDialog DCR from TypeScript

--- libraries/Microsoft.Bot.Builder.Dialogs/MainDialog.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/MainDialog.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Builder.Dialogs
@@ -9,16 +10,20 @@ namespace Microsoft.Bot.Builder.Dialogs
     /// <summary>
     /// MainDialog handles the basic structure of running the root of a dialog stack.
     /// </summary>
-    public class MainDialog : ComponentDialog
+    public abstract class MainDialog : ComponentDialog
     {
         private DialogSet _mainDialogSet;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainDialog"/> class.
+        /// </summary>
+        /// <param name="dialogState">State property used to persist the bots dialog stack.</param>
+        /// <param name="dialogId">(Optional) id to assign to the main dialog on the stack. Defaults to a value of 'main'.</param>
         public MainDialog(IStatePropertyAccessor<DialogState> dialogState, string dialogId = nameof(MainDialog))
             : base(dialogId)
         {
             DialogStateProperty = dialogState ?? throw new ArgumentNullException(nameof(dialogState));
 
-            // create dialog set for the outer dialog
             _mainDialogSet = new DialogSet(DialogStateProperty);
             _mainDialogSet.Add(this);
         }
@@ -34,9 +39,10 @@ namespace Microsoft.Bot.Builder.Dialogs
         /// <summary>
         /// Run the current dialog stack.
         /// </summary>
-        /// <param name="turnContext">The turn context.</param>
+        /// <param name="turnContext">Turn context containing the activity that was received.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
         /// <returns>The result of running the current main dialog.</returns>
-        public async Task<DialogTurnResult> RunAsync(ITurnContext turnContext)
+        public async Task<DialogTurnResult> RunAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
         {
             if (turnContext == null)
             {
@@ -44,18 +50,48 @@ namespace Microsoft.Bot.Builder.Dialogs
             }
 
             // Create a dialog context and try to continue running the current dialog
-            var dialogContext = await _mainDialogSet.CreateContextAsync(turnContext).ConfigureAwait(false);
+            var dialogContext = await _mainDialogSet.CreateContextAsync(turnContext, cancellationToken).ConfigureAwait(false);
 
             // continue any running dialog
-            var result = await dialogContext.ContinueDialogAsync().ConfigureAwait(false);
+            var result = await dialogContext.ContinueDialogAsync(cancellationToken).ConfigureAwait(false);
 
             // if there isn't one running, then start main dialog as the root
             if (result.Status == DialogTurnStatus.Empty)
             {
-                result = await dialogContext.BeginDialogAsync(this.Id).ConfigureAwait(false);
+                result = await dialogContext.BeginDialogAsync(this.Id, null, cancellationToken).ConfigureAwait(false);
             }
 
             return result;
+        }
+
+        protected abstract Task<DialogTurnResult> OnRunTurnAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken));
+
+        /// <summary>
+        /// The implementation just calls onContinueDialog()
+        /// We're overriding the components built in logic that wants to start the initial dialog.
+        /// We want the bots onRunTurn() implementation to always decide which dialog (if any) 
+        /// gets started.
+        /// </summary>
+        /// <param name="innerDC">The DialogContext scoped to this ComponnentDialog instance.</param>
+        /// <param name="options">(Optional) arguments that were passed to the dialog during `begin()` call that started the instance.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The result of running the OnRunAsync call.</returns>
+        protected override Task<DialogTurnResult> OnBeginDialogAsync(DialogContext innerDC, object options, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return OnContinueDialogAsync(innerDC, cancellationToken);
+        }
+
+        /// <summary>
+        /// The implementation calls onRunTurn()
+        /// We're overriding the components built in logic that calls innerDC.continueDialog().
+        /// We want the bots onRunTurn() implementation to decide what should happen for a turn.
+        /// </summary>
+        /// <param name="innerDC">The DialogContext scoped to this ComponnentDialog instance.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>The result of running the OnRunAsync call.</returns>
+        protected override Task<DialogTurnResult> OnContinueDialogAsync(DialogContext innerDC, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            return OnRunTurnAsync(innerDC, cancellationToken);
         }
     }
 }
