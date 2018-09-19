d8c51b46e5a24b173bb492903bb7d834dedb5088
Enzo Cano <enzo.cano@southworks.com>
2018-09-19 10:30:05 -0300

    Add coverage for pull-request

--- build/PublishCoverage.ps1
+++ build/PublishCoverage.ps1
@@ -7,12 +7,43 @@ $coverageAnalyzer = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterpr
 dotnet tool install coveralls.net --version 1.0.0 --tool-path tools
 $coverageUploader = ".\tools\csmacnz.Coveralls.exe"
 
-Write-Host Analyze coverage
+Write-Host "Analyze coverage [$coverageAnalyzer] with args:"
 $coverageFiles = Get-ChildItem -Path "$env:Build_SourcesDirectory\CodeCoverage" -Include "*.coverage" -Recurse | Select -Exp FullName
-Write-Host with files: $coverageFiles
-."$coverageAnalyzer" analyze /output:"$env:Build_SourcesDirectory\CodeCoverage\coverage.coveragexml" $coverageFiles
+$analyzeArgs = @(
+    "analyze",
+    "/output:""$env:Build_SourcesDirectory\CodeCoverage\coverage.coveragexml"""
+);
+$analyzeArgs += $coverageFiles
+Foreach ($i in $analyzeArgs) { Write-Host "  $i" }
+."$coverageAnalyzer" @analyzeArgs
 
-Write-Host Upload coverage
-$branchName = $env:Build_SourceBranch -replace "refs/heads/", ""
-Write-Host with args: --repoToken $coverallsToken --commitId $env:Build_SourceVersion --commitBranch $branchName --commitAuthor $env:Build_RequestedFor --commitEmail $env:Build_RequestedForEmail --commitMessage $env:Build_SourceVersionMessage --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory"
-."$coverageUploader" --dynamiccodecoverage -i "$env:Build_SourcesDirectory\CodeCoverage\coverage.coveragexml" --repoToken $coverallsToken --commitId $env:Build_SourceVersion --commitBranch "$branchName" --commitAuthor "$env:Build_RequestedFor" --commitEmail "$env:Build_RequestedForEmail" --commitMessage "$env:Build_SourceVersionMessage" --jobId $env:Build_BuildId --useRelativePaths --basePath "$env:Build_SourcesDirectory" -o "$env:Build_SourcesDirectory\CodeCoverage\coverage.json"
+Write-Host "Upload coverage [$coverageUploader] with args"
+if (Test-Path env:System_PullRequest_SourceBranch) {
+    $branchName = $env:System_PullRequest_SourceBranch -replace "refs/heads/", "" }
+$uploadArgs = @(
+    "--dynamiccodecoverage",
+    "-i ""$env:Build_SourcesDirectory\CodeCoverage\coverage.coveragexml""",
+    "-o ""$env:Build_SourcesDirectory\CodeCoverage\coverage.json"""
+    "--useRelativePaths",
+    "--basePath ""$env:Build_SourcesDirectory""",
+    "--repoToken ""$coverallsToken""",
+    "--jobId ""$env:Build_BuildId""",
+    "--commitId ""$env:Build_SourceVersion""",
+    "--commitAuthor ""$env:Build_RequestedFor""",
+    "--commitEmail ""$env:Build_RequestedForEmail""",
+    "--commitMessage ""$env:Build_SourceVersionMessage"""
+);
+if (Test-Path env:System_PullRequest_SourceBranch)
+{
+    $uploadArgs += "--commitBranch ""$($env:System_PullRequest_SourceBranch -replace ""refs/heads/"", """")"""
+}
+else
+{
+    $uploadArgs += "--commitBranch ""$($env:Build_SourceBranch -replace ""refs/heads/"", """")"""
+}
+if (Test-Path env:System_PullRequest_PullRequestId)
+{
+    $uploadArgs += "--pullRequest ""$env:System_PullRequest_PullRequestId"""
+}
+Foreach ($i in $uploadArgs) { Write-Host "  $i" }
+Start-Process $coverageUploader -ArgumentList $uploadArgs -NoNewWindow
