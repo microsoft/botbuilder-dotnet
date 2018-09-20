b994c48e91392d7c0f43ca937309d6abee1b045d
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2018-09-20 11:42:04 -0700

    Fix broken doc XML and some minor spelling and grammar issues.

--- libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.WebApi/ConfigurationChannelProvider.cs
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.WebApi/ConfigurationChannelProvider.cs
@@ -11,7 +11,7 @@ namespace Microsoft.Bot.Builder.BotFramework
     /// Channel provider which uses <see cref="ConfigurationManager.AppSettings"/> to lookup the channel service property.
     /// </summary>
     /// <remarks>
-    /// This will populate the <see cref="SimpleChannelProvider.Issuer"/> from a configuration entry with the key of <see cref="ChannelIssuerKey"/>.
+    /// This will populate the <see cref="SimpleChannelProvider.ChannelService"/> from a configuration entry with the key of <see cref="ChannelServiceKey"/>.
     ///
     /// NOTE: if the keys are not present, a <c>null</c> value will be used.
     /// </remarks>
