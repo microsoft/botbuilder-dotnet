65f98b1a39e288c7ef1d2c4716066e4695898224
Amr Elsehemy <amrelsehemy@live.com>
2018-09-23 15:46:28 +0200

    adding LanguageGenerationApplication Constructor tests

--- tests/Microsoft.Bot.Builder.AI.LanguageGeneration.Tests/LanguageGenerationApplicationTests.cs
+++ tests/Microsoft.Bot.Builder.AI.LanguageGeneration.Tests/LanguageGenerationApplicationTests.cs
@@ -1,14 +1,27 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.Bot.Builder.AI.LanguageGeneration.Resolver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Bot.Builder.AI.LanguageGeneration.Tests
 {
     [TestClass]
     public class LanguageGenerationApplicationTests
     {
+        private const string Endpoint = "https://platform.bing.com/speechdx/lg-dev/v1/lg";
 
-        [TestInitialize]
-        public void TestInitialize()
+        [TestMethod]
+        public void LanguageGenerationApplication_Construction()
         {
+
+            Assert.ThrowsException<ArgumentException>(() => new LanguageGenerationApplication(null, Guid.NewGuid().ToString(), Endpoint));
+            Assert.ThrowsException<ArgumentException>(() => new LanguageGenerationApplication(string.Empty, Guid.NewGuid().ToString(), Endpoint));
+            Assert.ThrowsException<ArgumentException>(() => new LanguageGenerationApplication(Guid.NewGuid().ToString(), null, Endpoint));
+            Assert.ThrowsException<ArgumentException>(() => new LanguageGenerationApplication(Guid.NewGuid().ToString(), string.Empty, Endpoint));
+            Assert.ThrowsException<ArgumentException>(() => new LanguageGenerationApplication(Guid.NewGuid().ToString(), "0000", Endpoint));
+            Assert.ThrowsException<ArgumentException>(() => new LanguageGenerationApplication(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), null));
+
+            var languageGenerationApplication = new LanguageGenerationApplication(Guid.NewGuid().ToString(), Guid.NewGuid().ToString(), Endpoint);
+            Assert.AreEqual(Endpoint, languageGenerationApplication.Endpoint);
         }
     }
 }
