e8ed685f2905767b42426fa7a0f87e31714a9fd5
Chris Mullins <cleemullins@users.noreply.github.com>
2018-10-09 16:03:01 -0700

    Updated to latest CosmosDB NuGet Package. Extracted Key Escaping into a dedicated class (with tests). Added CTor exception tests. Added additional parameter checking on methods. Plumbed through Cancellation Tokens to CosmosDB Calls Fixed race condition around Initialization

---
+++ tests/Microsoft.Bot.Builder.Azure.Tests/CosmosDBKeyEscapeTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Azure.Tests
+{
+    [TestClass]
+    [TestCategory("Storage")]
+    [TestCategory("Storage - CosmosDB")]
+    public class CosmosDBKeyEscapeTests
+    {
+        [TestMethod]
+        public void Sanitize_Key_Should_Fail_With_Null_Key()
+        {
+            // Null key should throw
+            Assert.ThrowsException<ArgumentNullException>(() => CosmosDBKeyEscape.EscapeKey(null));
+
+            // Empty string should throw
+            Assert.ThrowsException<ArgumentNullException>(() => CosmosDBKeyEscape.EscapeKey(string.Empty));
+
+            // Whitespace key should throw
+            Assert.ThrowsException<ArgumentNullException>(() => CosmosDBKeyEscape.EscapeKey("     "));
+        }
+
+        [TestMethod]
+        public void Sanitize_Key_Should_Not_Change_A_Valid_Key()
+        {
+            var validKey = "Abc12345";
+            var sanitizedKey = CosmosDBKeyEscape.EscapeKey(validKey);
+            Assert.AreEqual(validKey, sanitizedKey);
+        }
+
+        [TestMethod]
+        public void Sanitize_Key_Should_Escape_Illegal_Character()
+        {
+            // Ascii code of "?" is "3f".
+            var sanitizedKey = CosmosDBKeyEscape.EscapeKey("?test?");
+            Assert.AreEqual(sanitizedKey, "*3ftest*3f");
+
+            // Ascii code of "/" is "2f".
+            var sanitizedKey2 = CosmosDBKeyEscape.EscapeKey("/test/");
+            Assert.AreEqual(sanitizedKey2, "*2ftest*2f");
+
+            // Ascii code of "\" is "5c".
+            var sanitizedKey3 = CosmosDBKeyEscape.EscapeKey("\\test\\");
+            Assert.AreEqual(sanitizedKey3, "*5ctest*5c");
+
+            // Ascii code of "#" is "23".
+            var sanitizedKey4 = CosmosDBKeyEscape.EscapeKey("#test#");
+            Assert.AreEqual(sanitizedKey4, "*23test*23");
+
+            // Check a compound key
+            var compoundSanitizedKey = CosmosDBKeyEscape.EscapeKey("?#/");
+            Assert.AreEqual(compoundSanitizedKey, "*3f*23*2f");
+        }
+    }
+}
