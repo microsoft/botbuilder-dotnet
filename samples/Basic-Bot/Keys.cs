b8d37cde025d6ec0477a1cb2107a092de316f851
Amr Elsehemy <amrelsehemy@live.com>
2018-10-18 16:36:19 +0200

    -Adding basic bot to LG samples

---
+++ samples/Basic-Bot/Keys.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public static class Keys
+    {
+        public static string LanguageGenerationSubscriptionKey { get; set; } = "<YOUR_KEY_HERE>";
+    }
+}
---
+++ samples/Basic-Bot/TemplateResponses.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public static class TemplateResponses
+    {
+        // Templates
+        public static string ConfusionTemplate { get; } = "[confusion]";
+
+        public static string NameQuestionTemplate { get; } = "[nameQuestion]";
+
+        public static string CityQuestionTemplate { get; } = "[cityQuestion]";
+
+        public static string ShowAppreciationTemplate { get; } = "[showAppreciation]";
+
+        public static string WelcomeUserTemplate { get; } = "[welcomeUser]";
+
+        public static string OfferHelpTemplate { get; } = "[offerHelp]";
+    }
+}
