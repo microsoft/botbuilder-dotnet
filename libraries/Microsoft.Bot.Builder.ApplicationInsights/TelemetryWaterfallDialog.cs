23413859a91793dd21fbd274293c7335dd974e58
Dave Taniguchi <daveta@microsoft.com>
2018-11-12 17:07:12 -0800

    Safety checkin

---
+++ libraries/Microsoft.Bot.Builder.ApplicationInsights/TelemetryWaterfallDialog.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ApplicationInsights;
+using Microsoft.Bot.Builder.Dialogs;
+
+namespace Microsoft.Bot.Builder.ApplicationInsights
+{
+    class TelemetryWaterfallDialog : WaterfallDialog
+    {
+        private readonly IBotTelemetryClient _telemetryClient;
+
+        public TelemetryWaterfallDialog(string dialogId, IBotTelemetryClient telemetryClient, IEnumerable<WaterfallStep> steps = null)
+            : base(dialogId, steps)
+        {
+            if (telemetryClient == null)
+            {
+                throw new ArgumentNullException(nameof(telemetryClient));
+            }
+
+            _telemetryClient = telemetryClient;
+        }
+
+        protected virtual async Task<DialogTurnResult> OnStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            _telemetryClient.TrackWaterfallStep(stepContext, null);
+            return await base.OnStepAsync(stepContext, cancellationToken);
+        }
+
+    }
+}
