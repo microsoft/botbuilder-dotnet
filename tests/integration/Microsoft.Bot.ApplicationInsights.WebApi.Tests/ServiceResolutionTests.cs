94d236a7c008a7215e45f9dfa95fc58df650652e
Dave Taniguchi <daveta@microsoft.com>
2018-11-20 08:40:18 -0800

    Cleanup test references

--- tests/integration/Microsoft.Bot.ApplicationInsights.WebApi.Tests/ServiceResolutionTests.cs
+++ tests/integration/Microsoft.Bot.ApplicationInsights.WebApi.Tests/ServiceResolutionTests.cs
@@ -1,21 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System;
-using System.Linq;
-using Moq;
-using System.Threading.Tasks;
-using Microsoft.ApplicationInsights.AspNetCore.TelemetryInitializers;
-using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.Bot.Builder.ApplicationInsights.Core;
-using Microsoft.Bot.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Microsoft.AspNetCore.TestHost;
-using System.Net.Http;
-using System.IO;
 
 namespace Microsoft.Bot.ApplicationInsights.WebApi.Tests
 {
@@ -30,14 +16,14 @@ namespace Microsoft.Bot.ApplicationInsights.WebApi.Tests
                 //.UseStartup<Startup>());
                 //_client = _server.CreateClient();
             }
-            [TestMethod]
-            [ExpectedException(typeof(InvalidOperationException))]
-            public void AppSettings_NoAppSettings()
-            {
-                var server = new TestServer(new WebHostBuilder()
-                    .UseStartup<Startup>());
-            }
+            //[TestMethod]
+            //[ExpectedException(typeof(InvalidOperationException))]
+            //public void AppSettings_NoAppSettings()
+            //{
+            //    var server = new TestServer(new WebHostBuilder()
+            //        .UseStartup<Startup>());
+            //}
         }
 
-        }
-    }
+}
+
