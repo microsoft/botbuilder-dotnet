5b1801474194182bba5258d7792ed91592d0a229
Drew Marsh <drmarsh@microsoft.com>
2018-11-20 20:45:53 -0800

    Initial implementation of WebAPI extension APIs for AI
     * Deletes the `TelemetryBotInitializer` approach
     * Adds extensions for `BotFrameworkConfigurationBuilderExtensions` which configure the `BotActivityTelemetryMiddleware`
     * Adds the implementation of `BotActivityTelemetryMiddleware` by linking the .cs file from the other project (NOTE: this needs further consideration)

---
+++ libraries/integration/Microsoft.Bot.Builder.ApplicationInsights.WebApi/BotFrameworkConfigurationBuilderExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Bot.Builder.ApplicationInsights.Core;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.WebApi
+{
+    public static class BotFrameworkConfigurationBuilderExtensions
+    {
+        public static BotFrameworkConfigurationBuilder UseApplicationInsightsBotTelemetry(this BotFrameworkConfigurationBuilder builder, string instrumentationKey) =>
+            builder.UseApplicationInsightsBotTelemetry(tc => tc.InstrumentationKey = instrumentationKey);
+
+        public static BotFrameworkConfigurationBuilder UseApplicationInsightsBotTelemetry(this BotFrameworkConfigurationBuilder builder, Action<TelemetryConfiguration> configure)
+        {
+            if (builder == null)
+            {
+                throw new ArgumentNullException(nameof(builder));
+            }
+
+            return builder.UseMiddleware(middlewareSet =>
+            {
+                middlewareSet.Insert(0, new BotActivityTelemetryMiddleware(CreateApplicationInsightsTelemetryClient(configure)));
+            });
+        }
+
+        private static TelemetryClient CreateApplicationInsightsTelemetryClient(Action<TelemetryConfiguration> configure)
+        {
+            var telemetryConfiguration = new TelemetryConfiguration();
+
+            var telemetryInitializers = telemetryConfiguration.TelemetryInitializers;
+            telemetryInitializers.Add(new OperationCorrelationTelemetryInitializer());
+
+            configure?.Invoke(telemetryConfiguration);
+
+            return new TelemetryClient(telemetryConfiguration);
+        }
+    }
+}
