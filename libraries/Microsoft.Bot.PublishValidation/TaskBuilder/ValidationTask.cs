2839880835f76898ddd2f6eda8e368b453383db7
GasparAcevedoZainSouthworks <gaspar.acevedo@southworks.com>
2018-11-28 18:30:50 -0300

    Add Task Validation project to check the existence of '.bot' file in the root folder of the project which implements it.

---
+++ libraries/Microsoft.Bot.PublishValidation/TaskBuilder/ValidationTask.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Build.Framework;
+using TaskBuilder.Helpers;
+
+namespace TaskBuilder
+{
+    public class ValidationTask : Microsoft.Build.Utilities.Task
+    {
+        public string DirectoryPath { get; set; }
+        
+        public override bool Execute()
+        {
+            LoggerHelper logHelper = new LoggerHelper(Log.LogError, Log.LogWarning);
+            
+            string resultMsg = string.Empty;
+            int logType = 0;
+
+            // Validate if the Project directory path is valid
+            if (!DirectoryValidatorHelper.DirectoryIsValid(DirectoryPath, out resultMsg, out logType))
+            {
+                logHelper.Log(resultMsg, logType);
+                return false;
+            }
+            
+            // Validate if in the Project Directory exists any .bot file
+            if(!DirectoryValidatorHelper.FileExists(DirectoryPath, "*.bot", out resultMsg, out logType))
+            {
+                logHelper.Log(resultMsg, logType);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
