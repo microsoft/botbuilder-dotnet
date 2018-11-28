59dcb9209a471039002884a2c85a3dd45db648e1
GasparAcevedoZainSouthworks <gaspar.acevedo@southworks.com>
2018-11-28 18:30:50 -0300

    Add Task Validation project to check the existence of '.bot' file in the root folder of the project which implements it.

---
+++ libraries/Microsoft.Bot.PublishValidation/TaskTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace taskTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
