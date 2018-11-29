6f6b26d13bb947ffd7c85797a2fec2a6ddae3306
GasparAcevedoZainSouthworks <gaspar.acevedo@southworks.com>
2018-11-29 14:51:16 -0300

    First Nuget package version. Only validates the existence of Bot configuration file

--- libraries/Microsoft.Bot.PublishValidation/BotConfigCheckerTask.cs
+++ libraries/Microsoft.Bot.PublishValidation/BotConfigCheckerTask.cs
@@ -1,28 +1,28 @@
 ﻿using Microsoft.Build.Framework;
 using TaskBuilder.Helpers;
 
-namespace TaskBuilder
+namespace Microsoft.Bot.PublishValidation
 {
-    public class ValidationTask : Microsoft.Build.Utilities.Task
+    public class BotConfigCheckerTask : Microsoft.Build.Utilities.Task
     {
-        public string DirectoryPath { get; set; }
-        
+        public string ProjectPath { get; set; }
+
         public override bool Execute()
         {
             LoggerHelper logHelper = new LoggerHelper(Log.LogError, Log.LogWarning);
-            
+
             string resultMsg = string.Empty;
             int logType = 0;
 
             // Validate if the Project directory path is valid
-            if (!DirectoryValidatorHelper.DirectoryIsValid(DirectoryPath, out resultMsg, out logType))
+            if (!DirectoryValidatorHelper.DirectoryIsValid(ProjectPath, out resultMsg, out logType))
             {
                 logHelper.Log(resultMsg, logType);
                 return false;
             }
-            
-            // Validate if in the Project Directory exists any .bot file
-            if(!DirectoryValidatorHelper.FileExists(DirectoryPath, "*.bot", out resultMsg, out logType))
+
+            // Validate if there is any .bot file inside the Project Directory 
+            if (!DirectoryValidatorHelper.FileExists(ProjectPath, "*.bot", out resultMsg, out logType))
             {
                 logHelper.Log(resultMsg, logType);
                 return false;
