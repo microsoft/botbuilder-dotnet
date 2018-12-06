437805a19a3dbb97c88153b387d45c0e342a8c1e
GasparAcevedoZainSouthworks <gaspar.acevedo@southworks.com>
2018-12-06 17:10:16 -0300

    Applied linting rules

--- libraries/Microsoft.Bot.PublishValidation/Helpers/ConfigurationOptions.cs
+++ libraries/Microsoft.Bot.PublishValidation/Helpers/ConfigurationOptions.cs
@@ -8,11 +8,17 @@ namespace Microsoft.Bot.PublishValidation
     public class ConfigurationOptions
     {
         public string ProjectPath { get; set; }
+
         public bool ForbidSpacesInProjectName { get; set; }
+
         public bool RequireBotFile { get; set; }
+
         public IEnumerable<string> RequiredEndpoints { get; set; }
+
         public IEnumerable<string> ForbiddenEndpoints { get; set; }
+
         public bool RequireLuisKey { get; set; }
+
         public bool RequireQnAMakerKey { get; set; }
     }
 }
