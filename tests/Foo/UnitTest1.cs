0b74d22dd754bcddd2e9019fca7f66ffa34c8c6c
cleemullins <cleemullins@users.noreply.github.com>
2019-01-24 17:23:25 -0800

    Add in a very simple Test project with no deps.

---
+++ tests/Foo/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Foo
+{    
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.IsTrue(false);
+        }
+    }
+}
