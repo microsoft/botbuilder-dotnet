3b65b3d2afb602ddc7042bc2c8ee4e3e2a309415
Kyle Delaney <v-kydela@microsoft.com>
2019-01-29 14:31:18 -0800

    Include Cortana test

--- tests/Microsoft.Bot.Builder.Dialogs.Tests/ChoiceFactoryTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Tests/ChoiceFactoryTests.cs
@@ -3,6 +3,7 @@
 
 using System.Collections.Generic;
 using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Connector;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Bot.Builder.Dialogs.Tests
@@ -13,6 +14,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
     public class ChoiceFactoryTests
     {
         private static List<Choice> colorChoices = new List<Choice> { new Choice("red"), new Choice("green"), new Choice("blue") };
+        private static List<Choice> extraChoices = new List<Choice> { new Choice("red"), new Choice("green"), new Choice("blue"), new Choice("alpha") };
 
         [TestMethod]
         public void ShouldRenderChoicesInline()
@@ -56,7 +58,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
         [TestMethod]
         public void ShouldAutomaticallyChooseRenderStyleBasedOnChannelType()
         {
-            var activity = ChoiceFactory.ForChannel("emulator", colorChoices, "select from:");
+            var activity = ChoiceFactory.ForChannel(Channels.Emulator, colorChoices, "select from:");
             Assert.AreEqual("select from:", activity.Text);
             Assert.IsNotNull(activity.SuggestedActions);
             Assert.AreEqual(3, activity.SuggestedActions.Actions.Count);
@@ -69,6 +71,18 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
             Assert.AreEqual("imBack", activity.SuggestedActions.Actions[2].Type);
             Assert.AreEqual("blue", activity.SuggestedActions.Actions[2].Value);
             Assert.AreEqual("blue", activity.SuggestedActions.Actions[2].Title);
+        }
+
+        [TestMethod]
+        public void ShouldChooseCorrectStylesForCortana()
+        {
+            var activity = ChoiceFactory.ForChannel(Channels.Cortana, colorChoices, "select from:");
+            Assert.AreEqual("select from: (1) red, (2) green, or (3) blue", activity.Text);
+            Assert.IsNull(activity.SuggestedActions);
+
+            activity = ChoiceFactory.ForChannel(Channels.Cortana, extraChoices, "select from:");
+            Assert.AreEqual("select from:\n\n   1. red\n   2. green\n   3. blue\n   4. alpha", activity.Text);
+            Assert.IsNull(activity.SuggestedActions);
         }
     }
 }
