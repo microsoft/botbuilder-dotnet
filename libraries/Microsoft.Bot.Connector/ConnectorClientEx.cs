fe4f552a783d37e16206a0130bbb609403a65df2
Carlos Castro <ccastro@microsoft.com>
2019-02-14 10:51:15 -0800

    Proxy support: ConnectorClient accepts custom http client and BotFrameworkAdapter passes httpClient over to clients

--- libraries/Microsoft.Bot.Connector/ConnectorClientEx.cs
+++ libraries/Microsoft.Bot.Connector/ConnectorClientEx.cs
@@ -23,9 +23,10 @@ namespace Microsoft.Bot.Connector
         /// <param name="baseUri">Base URI for the Connector service</param>
         /// <param name="microsoftAppId">Optional. Your Microsoft app id. If null, this setting is read from settings["MicrosoftAppId"]</param>
         /// <param name="microsoftAppPassword">Optional. Your Microsoft app password. If null, this setting is read from settings["MicrosoftAppPassword"]</param>
+        /// <param name="httpClientHandler">Optional. <see cref="HttpClient"></see>/> to be used by the connector client.</param>
         /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
-        public ConnectorClient(Uri baseUri, string microsoftAppId = null, string microsoftAppPassword = null, params DelegatingHandler[] handlers)
-            : this(baseUri, new MicrosoftAppCredentials(microsoftAppId, microsoftAppPassword), handlers: handlers)
+        public ConnectorClient(Uri baseUri, string microsoftAppId = null, string microsoftAppPassword = null, HttpClient customHttpClient = null, params DelegatingHandler[] handlers)
+            : this(baseUri, new MicrosoftAppCredentials(microsoftAppId, microsoftAppPassword), customHttpClient: customHttpClient, handlers: handlers)
         {
         }
 
@@ -35,11 +36,16 @@ namespace Microsoft.Bot.Connector
         /// <param name="baseUri">Base URI for the Connector service</param>
         /// <param name="credentials">Credentials for the Connector service</param>
         /// <param name="addJwtTokenRefresher">(DEPRECATED)</param>
+        /// <param name="httpClientHandler">Optional. <see cref="HttpClient"></see>/> to be used by the connector client.</param>
         /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
-        public ConnectorClient(Uri baseUri, MicrosoftAppCredentials credentials, bool addJwtTokenRefresher = true, params DelegatingHandler[] handlers)
+        public ConnectorClient(Uri baseUri, MicrosoftAppCredentials credentials, bool addJwtTokenRefresher = true, HttpClient customHttpClient = null, params DelegatingHandler[] handlers)
             : this(baseUri, handlers)
         {
             this.Credentials = credentials;
+            if (customHttpClient != null)
+            {
+                this.HttpClient = customHttpClient;
+            }
         }
 
         /// <summary>
@@ -49,11 +55,16 @@ namespace Microsoft.Bot.Connector
         /// <param name="credentials">Credentials for the Connector service</param>
         /// <param name="httpClientHandler">The httpClientHandler used by http client</param>
         /// <param name="addJwtTokenRefresher">(DEPRECATED)</param>
+        /// <param name="httpClientHandler">Optional. <see cref="HttpClient"></see>/> to be used by the connector client.</param>
         /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
-        public ConnectorClient(Uri baseUri, MicrosoftAppCredentials credentials, HttpClientHandler httpClientHandler, bool addJwtTokenRefresher = true, params DelegatingHandler[] handlers)
+        public ConnectorClient(Uri baseUri, MicrosoftAppCredentials credentials, HttpClientHandler httpClientHandler, bool addJwtTokenRefresher = true, HttpClient customHttpClient = null, params DelegatingHandler[] handlers)
             : this(baseUri, httpClientHandler, handlers)
         {
             this.Credentials = credentials;
+            if (customHttpClient != null)
+            {
+                this.HttpClient = customHttpClient;
+            }
         }
 
         partial void CustomInitialize()
---
+++ tests/Microsoft.Bot.Connector.Tests/ConnectorClientTest.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Connector.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Bot.Connector;
+    using Microsoft.Bot.Connector.Authentication;
+    using Microsoft.Bot.Schema;
+    using Microsoft.Rest;
+    using Xunit;
+
+    public class ConnectorClientTest : BaseTest
+    {
+        [Fact]
+        public void ConnectorClientWithCustomHttpClient()
+        {
+            var baseUri = new Uri("https://test.coffee");
+            var customHttpClient = new HttpClient();
+
+            // Set a special base address so then we can make sure the connector client is honoring this http client
+            customHttpClient.BaseAddress = baseUri;
+            var connector = new ConnectorClient(new Uri("http://localhost/"), string.Empty, string.Empty, customHttpClient: customHttpClient);
+
+            Assert.Equal(connector.HttpClient.BaseAddress, baseUri);
+        }
+
+        [Fact]
+        public void ConnectorClientWithCustomHttpClientAndMicrosoftCredentials()
+        {
+            var baseUri = new Uri("https://test.coffee");
+            var customHttpClient = new HttpClient();
+
+            // Set a special base address so then we can make sure the connector client is honoring this http client
+            customHttpClient.BaseAddress = baseUri;
+            var connector = new ConnectorClient(new Uri("http://localhost/"), new MicrosoftAppCredentials(string.Empty, string.Empty), customHttpClient: customHttpClient);
+
+            Assert.Equal(connector.HttpClient.BaseAddress, baseUri);
+        }
+    }
+}
