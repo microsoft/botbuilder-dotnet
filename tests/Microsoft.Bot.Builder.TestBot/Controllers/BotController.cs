9fa044c640492a23b483e8641b990bc733f3058b
John Taylor <johtaylo@microsoft.com>
2019-02-20 11:40:24 -0800

    new Test bot based on Controllers and ActivityHandler

---
+++ tests/Microsoft.Bot.Builder.TestBot/Controllers/BotController.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace Microsoft.Bot.Builder.TestBot.Controllers
+{
+    [Route("bot")]
+    [ApiController]
+    public class BotController : ControllerBase
+    {
+        private IBotFrameworkHttpAdapter _adapter;
+        private IBot _bot;
+
+        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        {
+            _adapter = adapter;
+            _bot = bot;
+        }
+
+        [HttpPost]
+        public async Task PostAsync()
+        {
+            await _adapter.ProcessAsync(Request, Response, _bot);
+        }
+    }
+}
