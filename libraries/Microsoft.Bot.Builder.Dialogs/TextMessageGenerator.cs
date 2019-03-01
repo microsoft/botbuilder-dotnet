977214116fe8813b7ed5d6576b8dc45e382ed312
Tom Laird-McConnell <tomlm@microsoft.com>
2019-03-01 11:17:12 -0800

    extended TextMessageGenerator to support all of the card types chatdown does

--- libraries/Microsoft.Bot.Builder.Dialogs/TextMessageGenerator.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/TextMessageGenerator.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Bot.Builder.Dialogs
 {
@@ -46,67 +50,275 @@ namespace Microsoft.Bot.Builder.Dialogs
 
             var activity = Activity.CreateMessageActivity();
             activity.TextFormat = TextFormatTypes.Markdown;
+            var lines = result.Split('\n');
 
             // if result is multi line
-            if (result.IndexOf("\n") > 0)
+            for (int iLine = 0; iLine < lines.Length; iLine++)
             {
-                // TODO look for [herocard] attachment semantics
-                int start = result.IndexOf("[Herocard");
-                if (start >= 0)
+                var line = lines[iLine].Trim();
+                switch (line.ToLower())
                 {
-                    var end = result.IndexOf("]", start++);
-                    if (end > 0)
-                    {
+                    case "[herocard":
+                        iLine = AddGenericCardAtttachment(activity, HeroCard.ContentType, lines, ++iLine);
+                        break;
 
-                        var section = result.Substring(start, end - start).Trim('[', ']');
-                        var lines = section.Split('\n');
-                        var card = new HeroCard();
-                        foreach (var line in lines)
+                    case "[thumbnailcard":
+                        iLine = AddGenericCardAtttachment(activity, ThumbnailCard.ContentType, lines, ++iLine);
+                        break;
+
+                    case "[audiocard":
+                        iLine = AddGenericCardAtttachment(activity, AudioCard.ContentType, lines, ++iLine);
+                        break;
+
+                    case "[videocard":
+                        iLine = AddGenericCardAtttachment(activity, VideoCard.ContentType, lines, ++iLine);
+                        break;
+
+                    case "[animationcard":
+                        iLine = AddGenericCardAtttachment(activity, AnimationCard.ContentType, lines, ++iLine);
+                        break;
+
+                    case "[signincard":
+                        iLine = AddGenericCardAtttachment(activity, SigninCard.ContentType, lines, ++iLine);
+                        break;
+
+                    case "[oauthcard":
+                        iLine = AddGenericCardAtttachment(activity, OAuthCard.ContentType, lines, ++iLine);
+                        break;
+
+                    case "{":
+                        // json object
+                        iLine = AddJsonAttachment(activity, lines, iLine);
+                        break;
+
+                    default:
+                        if (line.StartsWith("[") && line.EndsWith("]"))
                         {
-                            var parts = line.Split('=');
-                            switch (parts[0].ToLower())
+                            var lowerLine = line.ToLower();
+                            if (lowerLine.StartsWith("[attachmentlayout="))
                             {
-                                case "title":
-                                    card.Title = line.Substring(parts[0].Length + 1);
-                                    break;
-                                case "subtitle":
-                                    card.Subtitle = line.Substring(parts[0].Length + 1);
-                                    break;
-                                case "text":
-                                    card.Text = line.Substring(parts[0].Length + 1);
-                                    break;
-                                //case "buttons":
-                                //    card.Buttons = line.Substring(parts[0].Length + 1).Split('|').Select(label => new CardAction(;
-                                default:
-                                    break;
+                                AddAttachmentLayout(activity, line);
+                                break;
+                            }
+                            else if (lowerLine.StartsWith("[suggestions="))
+                            {
+                                AddSuggestions(activity, line);
+                                break;
+                            }
+                            else if (lowerLine.StartsWith("[attachment="))
+                            {
+
+                                AddAttachment(activity, line);
+                                break;
                             }
                         }
 
-                        activity.Attachments.Add(card.ToAttachment());
-                        return activity;
-                    }
+                        if (string.IsNullOrEmpty(activity.Text))
+                        {
+                            var i = line.IndexOf("||");
+                            if (i > 0)
+                            {
+                                activity.Text = line.Substring(0, i).Trim();
+                                activity.Speak = line.Substring(i + 2).Trim();
+                            }
+                            else
+                            {
+                                activity.Text = activity.Speak = line.Trim();
+                            }
+                        }
+                        else
+                        {
+                            System.Diagnostics.Debug.WriteLine(string.Format("Card type {0} is not support!", line));
+                        }
+                        break;
                 }
-
-                // look to see if it has complex attachment semantics
-                activity.Text = result;
-                activity.Speak = result;
-                return activity;
             }
-            else
-            {
-                var i = result.IndexOf("||");
-                if (i > 0)
-                {
-                    activity.Text = result.Substring(0, i);
-                    activity.Speak = result.Substring(i + 2);
-                    return activity;
-                }
-            }
-
-            // return it simply as text
-            activity.Text = result;
-            activity.Speak = result;
             return activity;
         }
+
+        private static void AddAttachment(IMessageActivity activity, string line)
+        {
+            var parts = line.Split('=');
+            if (parts.Length == 1)
+            {
+                throw new ArgumentOutOfRangeException($"Missing = seperator in {line}");
+            }
+
+            var value = parts[1].TrimEnd(']').Trim();
+            var parts2 = value.Split(' ');
+            var attachment = new Attachment(contentUrl: parts2[0]);
+            if (parts2.Length == 2)
+            {
+                switch (parts2[1].ToLower())
+                {
+                    case "animation":
+                        attachment.ContentType = AnimationCard.ContentType;
+                        break;
+                    case "audio":
+                        attachment.ContentType = AudioCard.ContentType;
+                        break;
+                    case "hero":
+                        attachment.ContentType = HeroCard.ContentType;
+                        break;
+                    case "receipt":
+                        attachment.ContentType = ReceiptCard.ContentType;
+                        break;
+                    case "thumbnail":
+                        attachment.ContentType = ThumbnailCard.ContentType;
+                        break;
+                    case "signin":
+                        attachment.ContentType = SigninCard.ContentType;
+                        break;
+                    case "video":
+                        attachment.ContentType = VideoCard.ContentType;
+                        break;
+                    case "adaptivecard":
+                        attachment.ContentType = "application/vnd.microsoft.card.adaptive";
+                        break;
+                    default:
+                        attachment.ContentType = parts2[1].Trim();
+                        break;
+                }
+            }
+
+            activity.Attachments.Add(attachment);
+        }
+
+        private static int AddJsonAttachment(IMessageActivity activity, string[] lines, int iLine)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (; iLine < lines.Length; iLine++)
+            {
+                sb.AppendLine(lines[iLine]);
+            }
+
+            dynamic obj = JsonConvert.DeserializeObject(sb.ToString());
+            string contentType = "application/json";
+
+            if (obj.type == "AdaptiveCard")
+            {
+                contentType = "application/vnd.microsoft.card.adaptive";
+            }
+
+            var attachment = new Attachment(contentType, content: obj);
+            activity.Attachments.Add(attachment);
+            return iLine;
+        }
+
+        private static void AddSuggestions(IMessageActivity activity, string line)
+        {
+            var value = line.Split('=');
+            if (value.Length > 1)
+            {
+                var suggestions = value[1].Split('|');
+                activity.SuggestedActions = new SuggestedActions();
+                activity.SuggestedActions.Actions = suggestions.Select(s =>
+                {
+                    var text = s.TrimEnd(']').Trim();
+                    return new CardAction(type: ActionTypes.MessageBack, title: text, displayText: text, text: text);
+                }).ToList();
+            }
+        }
+
+        private static void AddAttachmentLayout(IMessageActivity activity, string line)
+        {
+            var value = line.Split('=');
+            if (value.Length > 1)
+            {
+                activity.AttachmentLayout = value[1].TrimEnd(']').Trim();
+            }
+        }
+
+        private static int AddGenericCardAtttachment(IMessageActivity activity, string type, string[] lines, int iLine)
+        {
+            var attachment = new Attachment(type, content: new JObject());
+            iLine = BuildGenericCard(attachment.Content, lines, iLine);
+            activity.Attachments.Add(attachment);
+            return iLine;
+        }
+
+        private static int BuildGenericCard(dynamic card, string[] lines, int iLine)
+        {
+            bool lastLine = false;
+
+            for (; !lastLine && iLine < lines.Length; iLine++)
+            {
+                var line = lines[iLine];
+                var start = line.IndexOf('=');
+                if (start > 0)
+                {
+                    var property = line.Substring(0, start).Trim().ToLower();
+                    var value = line.Substring(start + 1).Trim();
+                    if (value.EndsWith("]"))
+                    {
+                        value = value.TrimEnd(']');
+                        lastLine = true;
+                    }
+
+                    switch (property.ToLower())
+                    {
+                        case "title":
+                        case "subtitle":
+                        case "text":
+                        case "aspect":
+                        case "value":
+                        case "connectionName":
+                            card[property] = value;
+                            break;
+
+                        case "image":
+                            var urlObj = new JObject() { { "url", value } };
+                            card.Add(property, urlObj);
+                            break;
+
+                        case "images":
+                            if (card[property] == null)
+                            {
+                                card[property] = new JArray();
+                            }
+
+                            urlObj = new JObject() { { "url", value } };
+                            ((JArray)card[property]).Add(urlObj);
+                            break;
+
+                        case "media":
+                            if (card[property] == null)
+                            {
+                                card[property] = new JArray();
+                            }
+                            var mediaObj = new JObject() { { "url", value } };
+                            ((JArray)card[property]).Add(mediaObj);
+                            break;
+
+                        case "buttons":
+                            if (card[property] == null)
+                            {
+                                card[property] = new JArray();
+                            }
+
+                            foreach (var button in value.Split('|'))
+                            {
+                                var buttonObj = new JObject() { { "title", button.Trim() }, { "type", "imBack" }, { "value", button.Trim() } };
+                                ((JArray)card[property]).Add(buttonObj);
+                            }
+                            break;
+
+                        case "autostart":
+                        case "sharable":
+                        case "autoloop":
+                            card[property] = value.ToLower() == "true";
+                            break;
+                        case "":
+                            break;
+                        default:
+                            System.Diagnostics.Debug.WriteLine(string.Format("Skipping unknown card property {0}", property));
+                            break;
+                    }
+                }
+            }
+
+            return iLine;
+        }
+
     }
 }
---
+++ tests/Microsoft.Bot.Builder.Dialogs.Tests/TextMessageGeneratorTests.cs
@@ -0,0 +1,139 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.AI.LanguageGeneration;
+using Microsoft.Bot.Builder.Dialogs.Composition.Resources;
+using Microsoft.Bot.Schema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Bot.Builder.Dialogs.Tests
+{
+    public class MockLanguageGenator : ILanguageGenerator
+    {
+        public Task<string> Generate(string locale, string inlineTemplate, string id, object data, string[] types, string[] tags)
+        {
+            if (!String.IsNullOrEmpty(inlineTemplate))
+            {
+                return Task.FromResult(inlineTemplate);
+            }
+            else
+            {
+                return Task.FromResult(id);
+            }
+        }
+    }
+
+    [TestClass]
+    public class TextMessageGeneratorTests
+    {
+        public TestContext TestContext { get; set; }
+
+        private TextMessageActivityGenerator GetGenerator()
+        {
+            var rm = new BotResourceManager();
+            rm.AddFolderResources(GetLgFolder());
+            var lg = new LGLanguageGenerator(rm);
+            var mg = new TextMessageActivityGenerator(lg);
+            return mg;
+        }
+
+
+        private string GetLgFolder()
+        {
+            return AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin")) + "lg";
+        }
+
+        [TestMethod]
+        public async Task TestInline()
+        {
+            var lg = new MockLanguageGenator();
+            var mg = new TextMessageActivityGenerator(lg);
+            var activity = await mg.Generate("", "text", id: null, data: null, types: null, tags: null);
+            Assert.AreEqual(ActivityTypes.Message, activity.Type);
+            Assert.AreEqual("text", activity.Text);
+            Assert.AreEqual("text", activity.Speak);
+        }
+
+        [TestMethod]
+        public async Task TestSpeak()
+        {
+            var lg = new MockLanguageGenator();
+            var mg = new TextMessageActivityGenerator(lg);
+            var activity = await mg.Generate("", "text||speak", id: null, data: null, types: null, tags: null);
+            Assert.AreEqual(ActivityTypes.Message, activity.Type);
+            Assert.AreEqual("text", activity.Text);
+            Assert.AreEqual("speak", activity.Speak);
+        }
+
+        [TestMethod]
+        public async Task TestHerocard()
+        {
+            var mg = GetGenerator();
+
+            IMessageActivity activity = await mg.Generate("", "[HeroCardTemplate]", id: null, data: null, types: null, tags: null);
+            Assert.AreEqual(ActivityTypes.Message, activity.Type);
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Text));
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Speak));
+            Assert.AreEqual(1, activity.Attachments.Count);
+            Assert.AreEqual(HeroCard.ContentType, activity.Attachments[0].ContentType);
+            var card = ((JObject)activity.Attachments[0].Content).ToObject<HeroCard>();
+            Assert.IsNotNull(card, "should have herocard");
+            //title = Cheese gromit!
+            //subtitle = Hero Card
+            //text = This is some text describing the card, it's cool because it's cool
+            //images = https://memegenerator.net/img/instances/500x/73055378/cheese-gromit.jpg
+            //buttons = Option 1 | Option 2 | Option 3]
+            Assert.AreEqual("Cheese gromit!", card.Title, "card title should be set");
+            Assert.AreEqual("Hero Card", card.Subtitle, "card title should be set");
+            Assert.AreEqual("This is some text describing the card, it's cool because it's cool", card.Text, "card text should be set");
+            Assert.AreEqual("https://memegenerator.net/img/instances/500x/73055378/cheese-gromit.jpg", card.Images[0].Url, "image should be set");
+            Assert.AreEqual(3, card.Buttons.Count, "card buttons should be set");
+            for (int i = 0; i <= 2; i++)
+                Assert.AreEqual($"Option {i + 1}", card.Buttons[i].Title, "card buttons should be set");
+            // TODO add all of the other property types
+        }
+
+        [TestMethod]
+        public async Task TestAttachment()
+        {
+            var mg = GetGenerator();
+
+            IMessageActivity activity = await mg.Generate("", "[ImageAttachment]", id: null, data: null, types: null, tags: null);
+            Assert.AreEqual(ActivityTypes.Message, activity.Type);
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Text));
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Speak));
+            Assert.AreEqual(1, activity.Attachments.Count);
+            Assert.AreEqual(null, activity.Attachments[0].ContentType);
+            Assert.AreEqual(null, activity.AttachmentLayout);
+            Assert.AreEqual("https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png", activity.Attachments[0].ContentUrl);
+
+            activity = await mg.Generate("", "[ImageAttachment2]", id: null, data: null, types: null, tags: null);
+            Assert.AreEqual(ActivityTypes.Message, activity.Type);
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Text));
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Speak));
+            Assert.AreEqual(1, activity.Attachments.Count);
+            Assert.AreEqual("image/png", activity.Attachments[0].ContentType);
+            Assert.AreEqual(AttachmentLayoutTypes.List, activity.AttachmentLayout);
+            Assert.AreEqual("https://docs.microsoft.com/en-us/bot-framework/media/how-it-works/architecture-resize.png", activity.Attachments[0].ContentUrl);
+        }
+
+
+        [TestMethod]
+        public async Task TestAdaptiveCard()
+        {
+            var mg = GetGenerator();
+            dynamic data = new JObject();
+            data.adaptiveCardTitle = "test";
+            IMessageActivity activity = await mg.Generate("", "[adaptiveCardTemplate]", id: null, data: data, types: null, tags: null);
+            Assert.AreEqual(ActivityTypes.Message, activity.Type);
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Text));
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Speak));
+            Assert.AreEqual(1, activity.Attachments.Count);
+            Assert.AreEqual("application/vnd.microsoft.card.adaptive", activity.Attachments[0].ContentType);
+            Assert.AreEqual("test", (string)((dynamic)activity.Attachments[0].Content).body[0].text);
+        }
+    }
+}
