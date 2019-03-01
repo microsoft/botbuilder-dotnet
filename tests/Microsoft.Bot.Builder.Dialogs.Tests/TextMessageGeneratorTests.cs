240ed75b4c4dda70cd6d869d85f11bfa72398a82
Tom Laird-McConnell <tomlm@microsoft.com>
2019-03-01 14:16:37 -0800

    add unit test for direct create

--- tests/Microsoft.Bot.Builder.Dialogs.Tests/TextMessageGeneratorTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Tests/TextMessageGeneratorTests.cs
@@ -82,11 +82,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
             Assert.AreEqual(HeroCard.ContentType, activity.Attachments[0].ContentType);
             var card = ((JObject)activity.Attachments[0].Content).ToObject<HeroCard>();
             Assert.IsNotNull(card, "should have herocard");
-            //title = Cheese gromit!
-            //subtitle = Hero Card
-            //text = This is some text describing the card, it's cool because it's cool
-            //images = https://memegenerator.net/img/instances/500x/73055378/cheese-gromit.jpg
-            //buttons = Option 1 | Option 2 | Option 3]
             Assert.AreEqual("Cheese gromit!", card.Title, "card title should be set");
             Assert.AreEqual("herocard", card.Subtitle, "card subtitle should be data bound ");
             Assert.AreEqual("This is some text describing the card, it's cool because it's cool", card.Text, "card text should be set");
@@ -98,7 +93,36 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
         }
 
         [TestMethod]
-        public async Task TestAttachment()
+        public async Task TestCreateFromText()
+        {
+            string text = @"[Herocard 
+    title=Cheese gromit! 
+    subtitle=cheezy
+    text=This is some text describing the card, it's cool because it's cool 
+    images=https://memegenerator.net/img/instances/500x/73055378/cheese-gromit.jpg 
+    buttons=Option 1| Option 2| Option 3]";
+            var activity = TextMessageActivityGenerator.CreateActivityFromText(text);
+
+            Assert.AreEqual(ActivityTypes.Message, activity.Type);
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Text));
+            Assert.IsTrue(string.IsNullOrEmpty(activity.Speak));
+            Assert.AreEqual(1, activity.Attachments.Count);
+            Assert.AreEqual(HeroCard.ContentType, activity.Attachments[0].ContentType);
+            var card = ((JObject)activity.Attachments[0].Content).ToObject<HeroCard>();
+            Assert.IsNotNull(card, "should have herocard");
+            Assert.AreEqual("Cheese gromit!", card.Title, "card title should be set");
+            Assert.AreEqual("cheezy", card.Subtitle, "card subtitle should be set");
+            Assert.AreEqual("This is some text describing the card, it's cool because it's cool", card.Text, "card text should be set");
+            Assert.AreEqual("https://memegenerator.net/img/instances/500x/73055378/cheese-gromit.jpg", card.Images[0].Url, "image should be set");
+            Assert.AreEqual(3, card.Buttons.Count, "card buttons should be set");
+            for (int i = 0; i <= 2; i++)
+                Assert.AreEqual($"Option {i + 1}", card.Buttons[i].Title, "card buttons should be set");
+            // TODO add all of the other property types
+        }
+
+
+        [TestMethod]
+        public async Task TestImageAttachment()
         {
             var mg = GetGenerator();
 
@@ -155,5 +179,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
             Assert.AreEqual("http://images.fineartamerica.com/images-medium-large/good-morning-turtles-freund-gloria.jpg", (string)((dynamic)activity.Attachments[2].ContentUrl));
             Assert.AreEqual("http://4.bp.blogspot.com/--cFa6t-x4qY/UAqEgUvPd2I/AAAAAAAANIg/pMLE080Zjh4/s1600/turtle.jpg", (string)((dynamic)activity.Attachments[3].ContentUrl));
         }
+
+
     }
 }
