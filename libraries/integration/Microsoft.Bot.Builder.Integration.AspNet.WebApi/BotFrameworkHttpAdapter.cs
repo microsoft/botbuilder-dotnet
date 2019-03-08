9443216c3a3a3109c6790c29d57b31029999e29d
stevengum <14935595+stevengum@users.noreply.github.com>
2019-03-08 11:03:40 -0800

    update WebApi BFHttpAdapter, update WebApi tests csproj

--- libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.WebApi/BotFrameworkHttpAdapter.cs
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.WebApi/BotFrameworkHttpAdapter.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Rest.TransientFaultHandling;
 
 namespace Microsoft.Bot.Builder.Integration.AspNet.WebApi
 {
@@ -14,8 +16,20 @@ namespace Microsoft.Bot.Builder.Integration.AspNet.WebApi
     /// </summary>
     public class BotFrameworkHttpAdapter : BotFrameworkAdapter, IBotFrameworkHttpAdapter
     {
-        public BotFrameworkHttpAdapter(ICredentialProvider credentialProvider = null)
-            : base(credentialProvider ?? new SimpleCredentialProvider())
+        public BotFrameworkHttpAdapter(
+            ICredentialProvider credentialProvider = null,
+            IChannelProvider channelProvider = null,
+            RetryPolicy connectorClientRetryPolicy = null,
+            HttpClient customHttpClient = null,
+            IMiddleware middleware = null,
+            ILogger logger = null)
+            : base(
+                  credentialProvider ?? new SimpleCredentialProvider(),
+                  channelProvider,
+                  connectorClientRetryPolicy,
+                  customHttpClient,
+                  middleware,
+                  logger)
         {
         }
 
