0be70753ccf0e965029bdee9bc11696520f57a3e
aliandi <mica.andini@gmail.com>
2019-03-12 11:08:32 -0300

    [StyleCop] Fix all the warnings on TemplateManager - Add documentation for methods - Move classes to new file - Fix spacing

---
+++ libraries/Microsoft.Bot.Builder.TemplateManager/TemplateIdMap.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.TemplateManager
+{
+    /// <summary>
+    /// Map of Template Ids-> Template Function().
+    /// </summary>
+    public class TemplateIdMap : Dictionary<string, Func<ITurnContext, dynamic, object>>
+    {
+    }
+}
