786cf4b48b3966f98cc36e4dbb618c943111ba2e
Qi Kang <qika@microsoft.com>
2019-03-13 11:28:43 +0800

    Add list management

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Rules/Steps/ChangeList.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Dialogs.Rules.Steps
+{
+    public class ChangeList : DialogCommand
+    {
+        public enum ChangeListType
+        {
+            push,
+            pop,
+            take,
+            remove,
+            clear
+        }
+
+
+        public ChangeList()
+            : base()
+        {
+        }
+
+        protected override string OnComputeId()
+        {
+            return $"list[{changeType + ": " + listProperty}]";
+        }
+
+        public ChangeListType changeType;
+
+        public string listProperty;
+
+        public string itemProperty;
+
+        public ChangeList(ChangeListType changeType, string listProperty = null, string itemProperty = null)
+            : base()
+        {
+            this.changeType = changeType;
+
+            if (!string.IsNullOrEmpty(listProperty))
+            {
+                this.listProperty = listProperty;
+            }
+
+            if (!string.IsNullOrEmpty(itemProperty))
+            {
+                this.itemProperty = itemProperty;
+            }
+        }
+
+        protected override async Task<DialogTurnResult> OnRunCommandAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (string.IsNullOrEmpty(listProperty))
+            {
+                throw new Exception($"ChangeList: \"{ changeType }\" operation couldn't be performed because the listProperty wasn't specified.");
+            }
+
+            var list = dc.State.GetValue(listProperty, new List<object>());
+
+            object item = null;
+            string serialized = string.Empty;
+            object lastResult = null;
+
+            switch (changeType)
+            {
+                case ChangeListType.pop:
+                    item = list[list.Count - 1];
+                    list.RemoveAt(list.Count - 1);
+                    if (!string.IsNullOrEmpty(itemProperty))
+                    {
+                        dc.State.SetValue(itemProperty, item);
+                    }
+                    lastResult = item;
+                    break;
+                case ChangeListType.push:
+                    ensureItemProperty();
+                    item = dc.State.GetValue<object>(itemProperty);
+                    lastResult = item != null;
+                    if ((bool)lastResult)
+                    {
+                        list.Add(item);
+                    }
+                    break;
+                case ChangeListType.take:
+                    if (list.Count == 0)
+                    {
+                        break;
+                    }
+                    item = list[0];
+                    list.RemoveAt(0);
+                    if (!string.IsNullOrEmpty(itemProperty))
+                    {
+                        dc.State.SetValue(itemProperty, item);
+                    }
+                    lastResult = item;
+                    break;
+                case ChangeListType.remove:
+                    ensureItemProperty();
+                    item = dc.State.GetValue<object>(itemProperty);
+                    if (item != null)
+                    {
+                        lastResult = false;
+                        list.Remove(item);
+                    }
+                    break;
+                case ChangeListType.clear:
+                    lastResult = list.Count > 0;
+                    list.Clear();
+                    break;
+            }
+
+            dc.State.SetValue(listProperty, list);
+            dc.State.SetValue("dialog.lastResult", lastResult);
+            return await dc.EndDialogAsync();
+        }
+
+        private void ensureItemProperty()
+        {
+            if (string.IsNullOrEmpty(itemProperty))
+            {
+                throw new Exception($"ChangeList: \"{ changeType }\" operation couldn't be performed for list \"{listProperty}\" because an itemProperty wasn't specified.");
+            }
+        }
+
+    }
+}
---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Rules/Steps/SendList.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Dialogs.Rules.Steps
+{
+    public class SendList : DialogCommand
+    {
+        public SendList()
+            : base()
+        {
+        }
+
+        protected override string OnComputeId()
+        {
+            return $"sendList[{listProperty}]";
+        }
+
+        public string listProperty;
+
+        public string messageTemplate
+        {
+            set; get;
+        }
+        
+        public string itemTemplate
+        {
+            set; get;
+        }
+
+        public SendList(string listProperty, string messageTemplate = null, string itemTemplate = null)
+        {
+          
+            if (!string.IsNullOrEmpty(listProperty))
+            {
+                this.listProperty = listProperty;
+            }
+
+            if (!string.IsNullOrEmpty(messageTemplate))
+            {
+                this.messageTemplate = messageTemplate;
+            }
+
+            if (!string.IsNullOrEmpty(itemTemplate))
+            {
+                this.itemTemplate = itemTemplate;
+            }
+        }
+
+        protected override async Task<DialogTurnResult> OnRunCommandAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (string.IsNullOrEmpty(messageTemplate))
+            {
+                messageTemplate = "{list}";
+            }
+            else if (messageTemplate.IndexOf("{list") < 0)
+            {
+                messageTemplate += "\n\n{list}";
+            }
+
+            if (string.IsNullOrEmpty(itemTemplate))
+            {
+                itemTemplate = "- {item}\n";
+            }
+            else if (this.itemTemplate.IndexOf("{item") < 0)
+            {
+                itemTemplate += " {item}\n";
+            }
+
+            var list = string.Empty;
+            var value = dc.State.GetValue<List<object>>(listProperty);
+
+            foreach (var v in value)
+            {
+                list += itemTemplate.Replace("{item}", v.ToString());
+            }
+
+            var activity = messageTemplate.Replace("{list}", list);
+
+            if (!string.IsNullOrEmpty(activity))
+            {
+                var result = await dc.Context.SendActivityAsync(activity);
+                return await dc.EndDialogAsync(result);
+            }
+            return await dc.EndDialogAsync();
+        }
+
+    }
+}
