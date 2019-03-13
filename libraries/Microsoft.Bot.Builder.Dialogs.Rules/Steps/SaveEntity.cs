83eef45f9d7fd0865c1fa9c37aa5c41df607b0a0
Qi Kang <qika@microsoft.com>
2019-03-13 15:35:42 +0800

    Save Entity (TODO)

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Rules/Steps/SaveEntity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Dialogs.Rules.Steps
+{
+    public class SaveEntity : DialogCommand
+    {
+        public SaveEntity() : base()
+        { }
+
+        public SaveEntity(string entityName, string property)
+            : base()
+        {
+            if (!string.IsNullOrEmpty(entityName))
+            {
+                this.entityName = entityName;
+            }
+
+            if (!string.IsNullOrEmpty(property))
+            {
+                this.property = property;
+            }
+        }
+
+        public string entityName;
+
+        public string property;
+
+
+        protected override async Task<DialogTurnResult> OnRunCommandAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // TODO Get the entities from turn context and set to the user state.
+            return await dc.EndDialogAsync();
+        }
+    }
+}
