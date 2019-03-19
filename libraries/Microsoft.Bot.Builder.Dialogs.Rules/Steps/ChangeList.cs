4937f348b977c1a9fc46a2f3368b769cf721a5a5
Qi Kang <qika@microsoft.com>
2019-03-19 17:30:45 +0800

    Rename public properties.

--- libraries/Microsoft.Bot.Builder.Dialogs.Rules/Steps/ChangeList.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Rules/Steps/ChangeList.cs
@@ -25,58 +25,58 @@ namespace Microsoft.Bot.Builder.Dialogs.Rules.Steps
 
         protected override string OnComputeId()
         {
-            return $"list[{changeType + ": " + listProperty}]";
+            return $"list[{ChangeType + ": " + ListProperty}]";
         }
 
-        public ChangeListType changeType;
+        public ChangeListType ChangeType { get; set; }
 
-        public string listProperty;
+        public string ListProperty { get; set; }
 
-        public string itemProperty;
+        public string ItemProperty { get; set; }
 
         public ChangeList(ChangeListType changeType, string listProperty = null, string itemProperty = null)
             : base()
         {
-            this.changeType = changeType;
+            this.ChangeType = changeType;
 
             if (!string.IsNullOrEmpty(listProperty))
             {
-                this.listProperty = listProperty;
+                this.ListProperty = listProperty;
             }
 
             if (!string.IsNullOrEmpty(itemProperty))
             {
-                this.itemProperty = itemProperty;
+                this.ItemProperty = itemProperty;
             }
         }
 
         protected override async Task<DialogTurnResult> OnRunCommandAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
         {
-            if (string.IsNullOrEmpty(listProperty))
+            if (string.IsNullOrEmpty(ListProperty))
             {
-                throw new Exception($"ChangeList: \"{ changeType }\" operation couldn't be performed because the listProperty wasn't specified.");
+                throw new Exception($"ChangeList: \"{ ChangeType }\" operation couldn't be performed because the listProperty wasn't specified.");
             }
 
-            var list = dc.State.GetValue(listProperty, new List<object>());
+            var list = dc.State.GetValue(ListProperty, new List<object>());
 
             object item = null;
             string serialized = string.Empty;
             object lastResult = null;
 
-            switch (changeType)
+            switch (ChangeType)
             {
                 case ChangeListType.pop:
                     item = list[list.Count - 1];
                     list.RemoveAt(list.Count - 1);
-                    if (!string.IsNullOrEmpty(itemProperty))
+                    if (!string.IsNullOrEmpty(ItemProperty))
                     {
-                        dc.State.SetValue(itemProperty, item);
+                        dc.State.SetValue(ItemProperty, item);
                     }
                     lastResult = item;
                     break;
                 case ChangeListType.push:
-                    ensureItemProperty();
-                    item = dc.State.GetValue<object>(itemProperty);
+                    EnsureItemProperty();
+                    item = dc.State.GetValue<object>(ItemProperty);
                     lastResult = item != null;
                     if ((bool)lastResult)
                     {
@@ -90,15 +90,15 @@ namespace Microsoft.Bot.Builder.Dialogs.Rules.Steps
                     }
                     item = list[0];
                     list.RemoveAt(0);
-                    if (!string.IsNullOrEmpty(itemProperty))
+                    if (!string.IsNullOrEmpty(ItemProperty))
                     {
-                        dc.State.SetValue(itemProperty, item);
+                        dc.State.SetValue(ItemProperty, item);
                     }
                     lastResult = item;
                     break;
                 case ChangeListType.remove:
-                    ensureItemProperty();
-                    item = dc.State.GetValue<object>(itemProperty);
+                    EnsureItemProperty();
+                    item = dc.State.GetValue<object>(ItemProperty);
                     if (item != null)
                     {
                         lastResult = false;
@@ -111,16 +111,16 @@ namespace Microsoft.Bot.Builder.Dialogs.Rules.Steps
                     break;
             }
 
-            dc.State.SetValue(listProperty, list);
+            dc.State.SetValue(ListProperty, list);
             dc.State.SetValue("dialog.lastResult", lastResult);
             return await dc.EndDialogAsync();
         }
 
-        private void ensureItemProperty()
+        private void EnsureItemProperty()
         {
-            if (string.IsNullOrEmpty(itemProperty))
+            if (string.IsNullOrEmpty(ItemProperty))
             {
-                throw new Exception($"ChangeList: \"{ changeType }\" operation couldn't be performed for list \"{listProperty}\" because an itemProperty wasn't specified.");
+                throw new Exception($"ChangeList: \"{ ChangeType }\" operation couldn't be performed for list \"{ListProperty}\" because an itemProperty wasn't specified.");
             }
         }
 
