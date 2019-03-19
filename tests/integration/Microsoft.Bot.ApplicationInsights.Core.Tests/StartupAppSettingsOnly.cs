1dd67dddda4545cd8147a665502f8574e4610b9d
daveta <6182197+daveta@users.noreply.github.com>
2019-03-18 18:09:47 -0700

    Add option to init that skips the botfile and just uses appsettings.json (#1402)

---
+++ tests/integration/Microsoft.Bot.ApplicationInsights.Core.Tests/StartupAppSettingsOnly.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Configuration;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Bot.Builder.Integration.ApplicationInsights.Core.Tests
+{
+    internal class StartupAppSettingsOnly
+    {
+        public StartupAppSettingsOnly(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddBotApplicationInsights(this.Configuration);
+
+            // Adding IConfiguration in sample test server.  Otherwise this appears to be 
+            // registered.
+            services.AddSingleton<IConfiguration>(this.Configuration);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseBotApplicationInsights();
+        }
+
+    }
+}
