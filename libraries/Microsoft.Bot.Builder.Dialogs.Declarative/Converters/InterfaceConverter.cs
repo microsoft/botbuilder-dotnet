961a6c3bc41a76a9de3b06bc3bf521d810cce0ac
Will Portnoy <wportnoy@microsoft.com>
2019-03-27 10:23:35 -0700

    capture source information and add debugger steps

--- libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Converters/InterfaceConverter.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Converters/InterfaceConverter.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Collections.Generic;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Debugger;
 using Microsoft.Bot.Builder.Dialogs.Declarative.Resolvers;
 using Microsoft.Bot.Builder.Dialogs.Declarative.Types;
 using Newtonsoft.Json;
@@ -12,10 +14,14 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Converters
     public class InterfaceConverter<T> : JsonConverter where T : class
     {
         private readonly IRefResolver refResolver;
+        private readonly Source.IRegistry registry;
+        private readonly Stack<string> paths;
 
-        public InterfaceConverter(IRefResolver refResolver)
+        public InterfaceConverter(IRefResolver refResolver, Source.IRegistry registry, Stack<string> paths)
         {
             this.refResolver = refResolver ?? throw new ArgumentNullException(nameof(refResolver));
+            this.registry = registry ?? throw new ArgumentNullException(nameof(registry));
+            this.paths = paths ?? throw new ArgumentNullException(nameof(paths));
         }
 
         public override bool CanRead => true;
@@ -27,11 +33,11 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Converters
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            var jsonObject = JToken.Load(reader);
+            var jsonObject = Source.Point.Read(reader, JToken.Load, out var start, out var after);
 
             if (refResolver.IsRef(jsonObject))
             {
-                // We can't do this asynchornously as the Json.NET interface is synchronous
+                // We can't do this asynchronously as the Json.NET interface is synchronous
                 jsonObject = refResolver.ResolveAsync(jsonObject).GetAwaiter().GetResult();
             }
 
@@ -42,7 +48,23 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Converters
             {
                 throw new ArgumentNullException(JsonConvert.SerializeObject(jsonObject));
             }
+
+            var found = this.registry.TryGetValue(jsonObject, out var range);
+
+            if (found)
+            {
+                paths.Push(range.Path);
+            }
+
             T result = TypeFactory.Build<T>(typeName, jsonObject, serializer);
+
+            range = new Source.Range() { Path = paths.Peek(), Start = start, After = after };
+            this.registry.Add(result, range);
+
+            if (found)
+            {
+                paths.Pop();
+            }
 
             return result;
         }
