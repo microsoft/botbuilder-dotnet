664a523419e8002e99fd710452befb9ed8c9e1c6
Cecilia Avila <cecilia.avila@southworks.com>
2019-04-03 17:54:34 -0300

    Create new FunctionalTests project.

---
+++ Microsoft.Bot.Builder.FunctionalTest/FunctionalTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Bot.Builder.FunctionalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
