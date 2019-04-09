ae33b419c313634f4a8d1a8674846512ebe7874e
Cecilia Avila <cecilia.avila@southworks.com>
2019-04-09 11:55:22 -0300

    Move GetTokenRefreshTests to new project

--- tests/Microsoft.Bot.Builder.FunctionalTests/GetTokenRefreshTests.cs
+++ tests/Microsoft.Bot.Builder.FunctionalTests/GetTokenRefreshTests.cs
@@ -1,42 +1,37 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Bot.Connector.Authentication;
 using Xunit;
 
-namespace Microsoft.Bot.Connector.Tests
+namespace Microsoft.Bot.Builder.FunctionalTests
 {
-    /// <summary>
-    /// Description for UnitTest1.
-    /// </summary>
     public class GetTokenRefreshTests
     {
+        private string testAppId = null;
+        private string testPassword = null;
+
         public GetTokenRefreshTests()
         {
         }
 
-        //[Fact]
+        [Fact]
         public async Task TokenTests_GetCredentialsWorks()
         {
-            MicrosoftAppCredentials credentials = new MicrosoftAppCredentials("645cd89f-a83e-4af9-abb5-a454e917cbc4", "jvoMWRBA67:zjgePZ359_-_");
+            GetEnvironmentVarsTestAppIdPassword();
+            MicrosoftAppCredentials credentials = new MicrosoftAppCredentials(testAppId, testPassword);
             var result = await credentials.GetTokenAsync();
             Assert.NotNull(result);
         }
 
-        //[Fact]
-        public async Task TokenTests_GetCredentialsFromTenantWorks()
-        {
-            MicrosoftAppCredentials credentials = new MicrosoftAppCredentials("645cd89f-a83e-4af9-abb5-a454e917cbc4", "jvoMWRBA67:zjgePZ359_-_", "microsoft.com", null);
-            var result = await credentials.GetTokenAsync();
-            Assert.NotNull(result);
-        }
-
-        //[Fact]
+        [Fact]
         public async Task TokenTests_RefreshTokenWorks()
         {
-            MicrosoftAppCredentials credentials = new MicrosoftAppCredentials("12604f0f-bc92-4318-a6dd-aed704445ba4", "H_k}}7b75BEl+KY1");
+            GetEnvironmentVarsTestAppIdPassword();
+            MicrosoftAppCredentials credentials = new MicrosoftAppCredentials(testAppId, testPassword);
             var result = await credentials.GetTokenAsync();
             Assert.NotNull(result);
             var result2 = await credentials.GetTokenAsync();
@@ -46,10 +41,11 @@ namespace Microsoft.Bot.Connector.Tests
             Assert.NotEqual(result2, result3);
         }
 
-        //[Fact]
+        [Fact]
         public async Task TokenTests_RefreshTestLoad()
         {
-            MicrosoftAppCredentials credentials = new MicrosoftAppCredentials("12604f0f-bc92-4318-a6dd-aed704445ba4", "H_k}}7b75BEl+KY1");
+            GetEnvironmentVarsTestAppIdPassword();
+            MicrosoftAppCredentials credentials = new MicrosoftAppCredentials(testAppId, testPassword);
             List<Task<string>> tasks = new List<Task<string>>();
             for (int i = 0; i < 1000; i++)
             {
@@ -89,9 +85,9 @@ namespace Microsoft.Bot.Connector.Tests
                 if (i == 0)
                 {
                     results.Add(result);
+                    Assert.NotNull(result);
                 }
 
-                Assert.NotNull(result);
                 if (prevResult != null)
                 {
                     if (i % 100 == 50)
@@ -103,6 +99,25 @@ namespace Microsoft.Bot.Connector.Tests
                     {
                         Assert.Contains(result, results);
                     }
+                }
+            }
+        }
+
+        private void GetEnvironmentVarsTestAppIdPassword()
+        {
+            if (string.IsNullOrWhiteSpace(testAppId) || string.IsNullOrWhiteSpace(testPassword))
+            {
+                testAppId = Environment.GetEnvironmentVariable("TestAppId");
+                if (string.IsNullOrWhiteSpace(testAppId))
+                {
+                    throw new Exception("Environment variable 'TestAppId' not found.");
+                }
+
+                testPassword = Environment.GetEnvironmentVariable("TestPassword");
+
+                if (string.IsNullOrWhiteSpace(testPassword))
+                {
+                    throw new Exception("Environment variable 'TestPassword' not found.");
                 }
             }
         }
