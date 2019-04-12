c638f2e2d2785685e8adcbc0259e762e88b6d74c
Fei Chen <feich@microsoft.com>
2019-04-12 16:56:15 +0800

    add more bad expression tests

--- tests/Microsoft.Bot.Builder.Expressions.Tests/BadExpressionTests.cs
+++ tests/Microsoft.Bot.Builder.Expressions.Tests/BadExpressionTests.cs
@@ -57,6 +57,41 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
             Test("add()"), // arg count doesn't match
             Test("func()"), // no such func
             Test("add(five, six)"), // no such variables
+            Test("add(one)"), // add function need two variables
+            Test("items[3]"), // index out of range
+            Test("items[one+0.5]"), // index is not integer
+            Test("div(one, 0)"), // one cannot be divided by zero
+            Test("div(hello, one)"), // string hello cannot be divided
+            Test("and(one, hello, one < two)"), //one and hello are not bool type
+            Test("greater(one, hello)"), // string and integer are not comparable
+            Test("greater(one)"), // greater need two parameters
+            Test("less(one, hello)"), // string and integer are not comparable
+            Test("less(one)"), // less need two parameters
+            Test("pow(2, hello)"), // pow cannot accept parameter of string
+            Test("mod(one, 0)"), // mod cannot accept zero as the second parameter
+            Test("not(hello)"), // not can only accept bool parameter
+            Test("'string'&one"), // $ can only accept string parameter
+            Test("concat(one, hello)"), // concat can only accept string parameter
+            Test("length(one)"), // length can only accept string parameter
+            Test("replace(hello)"), // replace need three parameters
+            Test("replace(one, 'l', 'k')"), // replace only accept string parameter
+            Test("split(hello)"), // split need two parameters
+            Test("split(one, 'l')"), // split only accept string parameter
+            Test("substring(hello, 0.5)"), // the second parameter of substring must be integer
+            Test("substring(one, 0)"), // the first parameter of substring must be string
+            Test("substring(hello, 10)"), // the start index is out of the range of the string length
+            Test("substring(hello, 0, 10)"), // the length of substring is out of the range of the original string
+            Test("toLower(one)"), // the parameter of toLower must be string
+            Test("toUpper(one)"), // the parameter of toUpper must be string
+            Test("trim(one)"), // the parameter of trim must be string
+            Test("equals(one)"), // equals must accept two parameters
+            Test("if(hello, 'r1', 'r2')"), // the first parameter of the if must be bool
+            Test("if(!exists(one), one, hello)"), // the second and third parameters of if must the same type
+            Test("or(hello == 'hello')"), // or function needs two parameters
+            Test("or(hello, one)"), // or function only accept bool parameters
+            Test("max(hello, one)"), // max can only accept two parameters with same type of integer, float or string
+            Test("mul(hello, one)"), // mul can only accept two parameters of interger or float
+            Test("mul(one)"), // mul function need two parameters
         };
 
         [DataTestMethod]
