fe8afafc611907f56ec15cd3d4088f779dd247fe
Chris McConnell <chrimc@microsoft.com>
2019-04-12 14:45:24 -0700

    Add a debug break step.

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Steps/DebugBreak.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Steps
+{
+    public class DebugBreak : DialogCommand
+    {
+        protected override async Task<DialogTurnResult> OnRunCommandAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (Debugger.IsAttached)
+            {
+                Debugger.Break();
+            }
+            return await dc.EndDialogAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
