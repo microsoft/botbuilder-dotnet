afa2debee640268dc2c019dbfaa56dac040c61a6
Will Portnoy <wportnoy@microsoft.com>
2019-04-17 12:46:41 -0700

    implement "launch bot" debug configuration

--- libraries/Microsoft.Bot.Builder.Dialogs.Debugging/DebugTransport.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Debugging/DebugTransport.cs
@@ -37,6 +37,10 @@ namespace Microsoft.Bot.Builder.Dialogs.Debugging
             listener.Start();
             using (cancellationToken.Register(listener.Stop))
             {
+                var local = (IPEndPoint) listener.LocalEndpoint;
+                // output is parsed on launch by "vscode-dialog-debugger\src\ts\extension.ts"
+                Console.WriteLine($"{nameof(DebugTransport)}\t{local.Address}\t{local.Port}");
+
                 while (!cancellationToken.IsCancellationRequested)
                 {
                     try
