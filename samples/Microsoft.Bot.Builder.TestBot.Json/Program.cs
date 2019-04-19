99e0316baaec7f3f2d8a7da5edf330bf995f9b5e
Tom Laird-McConnell <tomlm@microsoft.com>
2019-04-18 20:01:44 -0700

    check in working models for development

--- samples/Microsoft.Bot.Builder.TestBot.Json/Program.cs
+++ samples/Microsoft.Bot.Builder.TestBot.Json/Program.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -21,7 +22,17 @@ namespace Microsoft.Bot.Builder.TestBot.Json
                 var env = hostingContext.HostingEnvironment;
                 config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                       .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+
+                foreach (var region in new string[] {  "westus", "westeurope", "australiaeast"})
+                {
+                    config.AddJsonFile($"luis.settings.{env.EnvironmentName}.{region}.json", optional: true, reloadOnChange: true);
+                }
+                foreach (var region in new string[] { "westus", "westeurope", "australiaeast" })
+                {
+                    config.AddJsonFile($"luis.settings.{Environment.UserName}.{region}.json", optional: true, reloadOnChange: true);
+                }
                 config.AddEnvironmentVariables();
+
                 config.AddCommandLine(args);
             }).UseStartup<Startup>()
             .Build();
