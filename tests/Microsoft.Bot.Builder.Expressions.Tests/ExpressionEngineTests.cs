33617773a7477fee79892e29fff34fdda504387a
hond <hond@microsoft.com>
2019-04-23 17:12:45 +0800

    Expression: Rename "property" function to "getProperty" (#1762)
    Expression: Rename property function to getProperty

--- tests/Microsoft.Bot.Builder.Expressions.Tests/ExpressionEngineTests.cs
+++ tests/Microsoft.Bot.Builder.Expressions.Tests/ExpressionEngineTests.cs
@@ -258,7 +258,6 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
             Test("min(4, 5) ", 4),
             Test("min(4) ", 4),
             Test("min(1.0, two) + max(one, 2.0)", 3.0, oneTwo),
-
             Test("sub(2, 1)", 1),
             Test("sub(2, 1, 1)", 0),
             Test("sub(2.0, 0.5)", 1.5),
@@ -357,15 +356,15 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
             # endregion
 
             # region  Memory access
-            Test("property(bag, concat('na','me'))","mybag"),
+            Test("getProperty(bag, concat('na','me'))","mybag"),
             Test("items[2]", "two", new HashSet<string> { "items[2]" }),
             Test("bag.list[bag.index - 2]", "blue", new HashSet<string> {"bag.list", "bag.index" }),
             Test("items[nestedItems[1].x]", "two", new HashSet<string> { "items","nestedItems[1].x" }),
             Test("bag['name']","mybag"),
             Test("bag[substring(concat('na','me','more'), 0, length('name'))]","mybag"),
             Test("items[1+1]","two"),
-            Test("property(null, 'p')", null),
-            Test("(property(null, 'p'))[1]", null),
+            Test("getProperty(null, 'p')", null),
+            Test("(getProperty(null, 'p'))[1]", null),
             # endregion
         };
 
