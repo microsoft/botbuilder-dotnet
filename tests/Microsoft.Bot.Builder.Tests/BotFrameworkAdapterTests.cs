8a3ce580d5d78d123b3715160652b6130f9724d8
Gabo Gilabert <ggilaber@microsoft.com>
2019-04-22 23:37:25 -0400

    Added TenantIdWorkaroundForTeamsMiddleware to BotFrameworkAdapter to workaround issue with MSTeams not sending the TenantId in Acivity.Conversation.TenantId.
    Replaced ArgumentNullException thrown by NullReferenceException (this is more apriopiate for the type of validatation that is being done).
    
    Fixed types and stylecop violations on BotFrameworkAdapter.
    
    Added unit tests for MSTeams workaround.

---
+++ tests/Microsoft.Bot.Builder.Tests/BotFrameworkAdapterTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Bot.Builder.Tests
+{
+    [TestClass]
+    public class BotFrameworkAdapterTests
+    {
+        [TestMethod]
+        public async Task TenantIdShouldShowInConversationForTeams()
+        {
+            var activity = await ProcessActivity(Channels.Msteams, "theTenantId", null);
+            Assert.AreEqual("theTenantId", activity.Conversation.TenantId);
+        }
+
+        [TestMethod]
+        public async Task TenantIdShouldNotShowInConversationForTeamsIfPresent()
+        {
+            var activity = await ProcessActivity(Channels.Msteams, "theTenantId", "shouldNotBeReplaced");
+            Assert.AreEqual("shouldNotBeReplaced", activity.Conversation.TenantId);
+        }
+
+        [TestMethod]
+        public async Task TenantIdShouldNotShowInConversationIfNotTeams()
+        {
+            var activity = await ProcessActivity(Channels.Directline, "theTenantId", null);
+            Assert.IsNull(activity.Conversation.TenantId);
+        }
+
+        private static async Task<IActivity> ProcessActivity(string channelId, string channelDataTenantId, string conversationTenantId)
+        {
+            IActivity activity = null;
+            var mockClaims = new Mock<ClaimsIdentity>();
+            var mockCredentialProvider = new Mock<ICredentialProvider>();
+
+            var sut = new BotFrameworkAdapter(mockCredentialProvider.Object);
+            await sut.ProcessActivityAsync(
+                mockClaims.Object,
+                new Activity("test")
+                {
+                    ChannelId = channelId,
+                    ServiceUrl = "https://smba.trafficmanager.net/amer/",
+                    ChannelData = new JObject
+                    {
+                        ["tenant"] = new JObject
+                            { ["id"] = channelDataTenantId },
+                    },
+                    Conversation = new ConversationAccount
+                        { TenantId = conversationTenantId },
+                },
+                (context, token) =>
+                {
+                    activity = context.Activity;
+                    return Task.CompletedTask;
+                },
+                CancellationToken.None);
+            return activity;
+        }
+    }
+}
