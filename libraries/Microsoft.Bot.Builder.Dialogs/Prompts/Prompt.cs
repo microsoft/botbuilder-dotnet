22a63dbdd3774023570853b92a4b9e89ec75c0f0
Gabo Gilabert <gabog@users.noreply.github.com>
2019-04-24 16:48:17 -0400

    Modifications to number of attempts property behavior (#1774)
    * First pass:
     - Removed NumberOfAttempts from PromptOptions
     - Added NumberOfAttempts getter to PromptValidatorContext
     - Implemented logic to keep internal count of number of attempts in the prompt state.
     - Updated unit test to pass
     - Added missing cancellationTokens where needed
    
    * Changed the counter to start at 1.
    Changed documentation for getter.
    Updated test to show the try count on succes.
    
    * Made PersistedState const private again (I don't need to see it from anywhere else).
    
    * Changed where we increment the retryCount.
    
    * Renamed property to AttemptCount
    
    * Reverted default added to options param for backward compat.
    
    * Update PromptValidatorContext.cs
    
    Touched to force a build

--- libraries/Microsoft.Bot.Builder.Dialogs/Prompts/Prompt.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/Prompts/Prompt.cs
@@ -18,9 +18,10 @@ namespace Microsoft.Bot.Builder.Dialogs
     /// <typeparam name="T">The type of the <see cref="Prompt{T}"/>.</typeparam>
     public abstract class Prompt<T> : Dialog
     {
+        internal const string AttemptCountKey = "AttemptCount";
+
         private const string PersistedOptions = "options";
         private const string PersistedState = "state";
-
         private readonly PromptValidator<T> _validator;
 
         public Prompt(string dialogId, PromptValidator<T> validator = null)
@@ -56,11 +57,14 @@ namespace Microsoft.Bot.Builder.Dialogs
             // Initialize prompt state
             var state = dc.ActiveDialog.State;
             state[PersistedOptions] = opt;
-            state[PersistedState] = new Dictionary<string, object>();
+            state[PersistedState] = new Dictionary<string, object>
+            {
+                { AttemptCountKey, 0 },
+            };
 
             // Send initial prompt
             await OnPromptAsync(dc.Context, (IDictionary<string, object>)state[PersistedState], (PromptOptions)state[PersistedOptions], false, cancellationToken).ConfigureAwait(false);
-            return Dialog.EndOfTurn;
+            return EndOfTurn;
         }
 
         public override async Task<DialogTurnResult> ContinueDialogAsync(DialogContext dc, CancellationToken cancellationToken = default(CancellationToken))
@@ -73,7 +77,7 @@ namespace Microsoft.Bot.Builder.Dialogs
             // Don't do anything for non-message activities
             if (dc.Context.Activity.Type != ActivityTypes.Message)
             {
-                return Dialog.EndOfTurn;
+                return EndOfTurn;
             }
 
             // Perform base recognition
@@ -82,13 +86,15 @@ namespace Microsoft.Bot.Builder.Dialogs
             var options = (PromptOptions)instance.State[PersistedOptions];
             var recognized = await OnRecognizeAsync(dc.Context, state, options, cancellationToken).ConfigureAwait(false);
 
+            // Increment attempt count
+            state[AttemptCountKey] = (int)state[AttemptCountKey] + 1;
+
             // Validate the return value
             var isValid = false;
             if (_validator != null)
             {
                 var promptContext = new PromptValidatorContext<T>(dc.Context, recognized, state, options);
                 isValid = await _validator(promptContext, cancellationToken).ConfigureAwait(false);
-                options.NumberOfAttempts++;
             }
             else if (recognized.Succeeded)
             {
@@ -98,17 +104,15 @@ namespace Microsoft.Bot.Builder.Dialogs
             // Return recognized value or re-prompt
             if (isValid)
             {
-                return await dc.EndDialogAsync(recognized.Value).ConfigureAwait(false);
+                return await dc.EndDialogAsync(recognized.Value, cancellationToken).ConfigureAwait(false);
             }
-            else
-            {
-                if (!dc.Context.Responded)
-                {
-                    await OnPromptAsync(dc.Context, state, options, true).ConfigureAwait(false);
-                }
 
-                return Dialog.EndOfTurn;
+            if (!dc.Context.Responded)
+            {
+                await OnPromptAsync(dc.Context, state, options, true, cancellationToken).ConfigureAwait(false);
             }
+
+            return EndOfTurn;
         }
 
         public override async Task<DialogTurnResult> ResumeDialogAsync(DialogContext dc, DialogReason reason, object result = null, CancellationToken cancellationToken = default(CancellationToken))
@@ -118,15 +122,15 @@ namespace Microsoft.Bot.Builder.Dialogs
             // dialogResume() when the pushed on dialog ends.
             // To avoid the prompt prematurely ending we need to implement this method and
             // simply re-prompt the user.
-            await RepromptDialogAsync(dc.Context, dc.ActiveDialog).ConfigureAwait(false);
-            return Dialog.EndOfTurn;
+            await RepromptDialogAsync(dc.Context, dc.ActiveDialog, cancellationToken).ConfigureAwait(false);
+            return EndOfTurn;
         }
 
         public override async Task RepromptDialogAsync(ITurnContext turnContext, DialogInstance instance, CancellationToken cancellationToken = default(CancellationToken))
         {
             var state = (IDictionary<string, object>)instance.State[PersistedState];
             var options = (PromptOptions)instance.State[PersistedOptions];
-            await OnPromptAsync(turnContext, state, options, false).ConfigureAwait(false);
+            await OnPromptAsync(turnContext, state, options, false, cancellationToken).ConfigureAwait(false);
         }
 
         protected abstract Task OnPromptAsync(ITurnContext turnContext, IDictionary<string, object> state, PromptOptions options, bool isRetry, CancellationToken cancellationToken = default(CancellationToken));
@@ -176,7 +180,7 @@ namespace Microsoft.Bot.Builder.Dialogs
 
                 prompt.Text = msg.Text;
 
-                if (msg.SuggestedActions != null && msg.SuggestedActions.Actions != null && msg.SuggestedActions.Actions.Count > 0)
+                if (msg.SuggestedActions?.Actions != null && msg.SuggestedActions.Actions.Count > 0)
                 {
                     prompt.SuggestedActions = msg.SuggestedActions;
                 }
--- tests/Microsoft.Bot.Builder.Dialogs.Tests/PromptValidatorContextTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Tests/PromptValidatorContextTests.cs
@@ -22,46 +22,43 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
 
             var dialogs = new DialogSet(dialogState);
 
-            dialogs.Add(new TextPrompt("namePrompt", (promptContext, cancellationToken) =>
-            {
-                return Task.FromResult(true);
-            }));
+            dialogs.Add(new TextPrompt("namePrompt", (promptContext, cancellationToken) => Task.FromResult(true)));
 
             var steps = new WaterfallStep[]
-                    {
-                        async (stepContext, cancellationToken) =>
-                        {
-                            return await stepContext.PromptAsync("namePrompt", new PromptOptions { Prompt = new Activity { Text = "Please type your name.", Type = ActivityTypes.Message } }, cancellationToken);
-                        },
-                        async (stepContext, cancellationToken) =>
-                        {
-                            var name = (string)stepContext.Result;
-                            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{name} is a great name!"), cancellationToken);
-                            return await stepContext.EndDialogAsync();
-                        },
-                    };
+            {
+                async (stepContext, cancellationToken) =>
+                {
+                    return await stepContext.PromptAsync("namePrompt", new PromptOptions { Prompt = new Activity { Text = "Please type your name.", Type = ActivityTypes.Message } }, cancellationToken);
+                },
+                async (stepContext, cancellationToken) =>
+                {
+                    var name = (string)stepContext.Result;
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{name} is a great name!"), cancellationToken);
+                    return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+                },
+            };
             dialogs.Add(new WaterfallDialog(
                 "nameDialog",
                 steps));
 
             await new TestFlow(adapter, async (turnContext, cancellationToken) =>
-            {
-                var dc = await dialogs.CreateContextAsync(turnContext);
-                await dc.ContinueDialogAsync();
-                if (!turnContext.Responded)
                 {
-                    await dc.BeginDialogAsync("nameDialog");
-                }
-            })
-            .Send("hello")
-            .AssertReply("Please type your name.")
-            .Send("John")
-            .AssertReply("John is a great name!")
-            .Send("Hi again")
-            .AssertReply("Please type your name.")
-            .Send("1")
-            .AssertReply("1 is a great name!")
-            .StartTestAsync();
+                    var dc = await dialogs.CreateContextAsync(turnContext, cancellationToken);
+                    await dc.ContinueDialogAsync(cancellationToken);
+                    if (!turnContext.Responded)
+                    {
+                        await dc.BeginDialogAsync("nameDialog", cancellationToken: cancellationToken);
+                    }
+                })
+                .Send("hello")
+                .AssertReply("Please type your name.")
+                .Send("John")
+                .AssertReply("John is a great name!")
+                .Send("Hi again")
+                .AssertReply("Please type your name.")
+                .Send("1")
+                .AssertReply("1 is a great name!")
+                .StartTestAsync();
         }
 
         [TestMethod]
@@ -70,15 +67,15 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
             var convoState = new ConversationState(new MemoryStorage());
             var dialogState = convoState.CreateProperty<DialogState>("dialogState");
 
-            TestAdapter adapter = new TestAdapter()
+            var adapter = new TestAdapter()
                 .Use(new AutoSaveStateMiddleware(convoState));
 
-            DialogSet dialogs = new DialogSet(dialogState);
+            var dialogs = new DialogSet(dialogState);
 
             // Create TextPrompt with dialogId "namePrompt" and custom validator
             dialogs.Add(new TextPrompt("namePrompt", async (promptContext, cancellationToken) =>
             {
-                string result = promptContext.Recognized.Value;
+                var result = promptContext.Recognized.Value;
                 if (result.Length > 3)
                 {
                     return true;
@@ -92,38 +89,38 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
             }));
 
             var steps = new WaterfallStep[]
-                    {
-                        async (stepContext, cancellationToken) =>
-                        {
-                            return await stepContext.PromptAsync("namePrompt", new PromptOptions { Prompt = new Activity { Text = "Please type your name.", Type = ActivityTypes.Message } }, cancellationToken);
-                        },
-                        async (stepContext, cancellationToken) =>
-                        {
-                            var name = (string)stepContext.Result;
-                            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{name} is a great name!"), cancellationToken);
-                            return await stepContext.EndDialogAsync();
-                        },
-                    };
+            {
+                async (stepContext, cancellationToken) =>
+                {
+                    return await stepContext.PromptAsync("namePrompt", new PromptOptions { Prompt = new Activity { Text = "Please type your name.", Type = ActivityTypes.Message } }, cancellationToken);
+                },
+                async (stepContext, cancellationToken) =>
+                {
+                    var name = (string)stepContext.Result;
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{name} is a great name!"), cancellationToken);
+                    return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+                },
+            };
             dialogs.Add(new WaterfallDialog(
                 "nameDialog",
                 steps));
 
             await new TestFlow(adapter, async (turnContext, cancellationToken) =>
-            {
-                var dc = await dialogs.CreateContextAsync(turnContext, cancellationToken);
-                await dc.ContinueDialogAsync(cancellationToken);
-                if (!turnContext.Responded)
                 {
-                    await dc.BeginDialogAsync("nameDialog", null, cancellationToken);
-                }
-            })
-            .Send("hello")
-            .AssertReply("Please type your name.")
-            .Send("hi")
-            .AssertReply("Please send a name that is longer than 3 characters.")
-            .Send("John")
-            .AssertReply("John is a great name!")
-            .StartTestAsync();
+                    var dc = await dialogs.CreateContextAsync(turnContext, cancellationToken);
+                    await dc.ContinueDialogAsync(cancellationToken);
+                    if (!turnContext.Responded)
+                    {
+                        await dc.BeginDialogAsync("nameDialog", null, cancellationToken);
+                    }
+                })
+                .Send("hello")
+                .AssertReply("Please type your name.")
+                .Send("hi")
+                .AssertReply("Please send a name that is longer than 3 characters.")
+                .Send("John")
+                .AssertReply("John is a great name!")
+                .StartTestAsync();
         }
 
         [TestMethod]
@@ -132,65 +129,66 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
             var convoState = new ConversationState(new MemoryStorage());
             var dialogState = convoState.CreateProperty<DialogState>("dialogState");
 
-            TestAdapter adapter = new TestAdapter()
+            var adapter = new TestAdapter()
                 .Use(new AutoSaveStateMiddleware(convoState));
 
-            DialogSet dialogs = new DialogSet(dialogState);
+            var dialogs = new DialogSet(dialogState);
 
             // Create TextPrompt with dialogId "namePrompt" and custom validator
             dialogs.Add(new TextPrompt("namePrompt", async (promptContext, cancellationToken) =>
             {
-                string result = promptContext.Recognized.Value;
+                var result = promptContext.Recognized.Value;
                 if (result.Length > 3)
                 {
+                    var succeededMessage = MessageFactory.Text($"You got it at the {promptContext.AttemptCount}th try!");
+                    await promptContext.Context.SendActivityAsync(succeededMessage, cancellationToken);
                     return true;
                 }
-                else
-                {
-                    var reply = MessageFactory.Text($"Please send a name that is longer than 3 characters. {promptContext.Options.NumberOfAttempts}");
-                    await promptContext.Context.SendActivityAsync(reply, cancellationToken);
-                }
+
+                var reply = MessageFactory.Text($"Please send a name that is longer than 3 characters. {promptContext.AttemptCount}");
+                await promptContext.Context.SendActivityAsync(reply, cancellationToken);
 
                 return false;
             }));
 
             var steps = new WaterfallStep[]
-                    {
-                        async (stepContext, cancellationToken) =>
-                        {
-                            return await stepContext.PromptAsync("namePrompt", new PromptOptions { Prompt = new Activity { Text = "Please type your name.", Type = ActivityTypes.Message } }, cancellationToken);
-                        },
-                        async (stepContext, cancellationToken) =>
-                        {
-                            var name = (string)stepContext.Result;
-                            await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{name} is a great name!"), cancellationToken);
-                            return await stepContext.EndDialogAsync();
-                        },
-                    };
+            {
+                async (stepContext, cancellationToken) =>
+                {
+                    return await stepContext.PromptAsync("namePrompt", new PromptOptions { Prompt = new Activity { Text = "Please type your name.", Type = ActivityTypes.Message } }, cancellationToken);
+                },
+                async (stepContext, cancellationToken) =>
+                {
+                    var name = (string)stepContext.Result;
+                    await stepContext.Context.SendActivityAsync(MessageFactory.Text($"{name} is a great name!"), cancellationToken);
+                    return await stepContext.EndDialogAsync(cancellationToken: cancellationToken);
+                },
+            };
             dialogs.Add(new WaterfallDialog(
                 "nameDialog",
                 steps));
 
             await new TestFlow(adapter, async (turnContext, cancellationToken) =>
-            {
-                var dc = await dialogs.CreateContextAsync(turnContext, cancellationToken);
-                await dc.ContinueDialogAsync(cancellationToken);
-                if (!turnContext.Responded)
                 {
-                    await dc.BeginDialogAsync("nameDialog", null, cancellationToken);
-                }
-            })
-            .Send("hello")
-            .AssertReply("Please type your name.")
-            .Send("hi")
-            .AssertReply("Please send a name that is longer than 3 characters. 0")
-            .Send("hi")
-            .AssertReply("Please send a name that is longer than 3 characters. 1")
-            .Send("hi")
-            .AssertReply("Please send a name that is longer than 3 characters. 2")
-            .Send("John")
-            .AssertReply("John is a great name!")
-            .StartTestAsync();
+                    var dc = await dialogs.CreateContextAsync(turnContext, cancellationToken);
+                    await dc.ContinueDialogAsync(cancellationToken);
+                    if (!turnContext.Responded)
+                    {
+                        await dc.BeginDialogAsync("nameDialog", null, cancellationToken);
+                    }
+                })
+                .Send("hello")
+                .AssertReply("Please type your name.")
+                .Send("hi")
+                .AssertReply("Please send a name that is longer than 3 characters. 1")
+                .Send("hi")
+                .AssertReply("Please send a name that is longer than 3 characters. 2")
+                .Send("hi")
+                .AssertReply("Please send a name that is longer than 3 characters. 3")
+                .Send("John")
+                .AssertReply("You got it at the 4th try!")
+                .AssertReply("John is a great name!")
+                .StartTestAsync();
         }
     }
 }
