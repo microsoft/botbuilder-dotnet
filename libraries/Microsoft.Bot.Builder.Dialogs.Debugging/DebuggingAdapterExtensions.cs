4f52b6f40da5bb3e9a2fed21e5c79816ab85cc47
Will Portnoy <wportnoy@microsoft.com>
2019-04-25 15:51:52 -0700

    factor out ICodeModel abstraction

--- libraries/Microsoft.Bot.Builder.Dialogs.Debugging/DebuggingAdapterExtensions.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Debugging/DebuggingAdapterExtensions.cs
@@ -16,10 +16,11 @@ namespace Microsoft.Bot.Builder.Dialogs.Debugging
         /// <param name="breakpoints">IBreakpoints to use (default will be SourceMap())</param>
         /// <param name="terminate">Termination function (Default is Environment.Exit()</param>
         /// <param name="logger">ILogger to use (Default is NullLogger)</param>
-        /// <param name="model">IDataModel to use (default is internal implmentation)</param>
-        /// <param name="coercion">ICoercion to use (default is internal implmentation)</param>
+        /// <param name="codeModel">ICodeModel to use (default is internal implementation)</param>
+        /// <param name="dataModel">IDataModel to use (default is internal implementation)</param>
+        /// <param name="coercion">ICoercion to use (default is internal implementation)</param>
         /// <returns></returns>
-        public static BotAdapter UseDebugger(this BotAdapter botAdapter, int port, Source.IRegistry registry = null, IBreakpoints breakpoints = null, Action terminate = null, IDataModel model = null, ILogger logger = null, ICoercion coercion = null)
+        public static BotAdapter UseDebugger(this BotAdapter botAdapter, int port, Source.IRegistry registry = null, IBreakpoints breakpoints = null, Action terminate = null, ICodeModel codeModel = null, IDataModel dataModel = null, ILogger logger = null, ICoercion coercion = null)
         {
             var sourceMap = new SourceMap();
             DebugSupport.SourceRegistry = registry ?? sourceMap;
@@ -27,7 +28,8 @@ namespace Microsoft.Bot.Builder.Dialogs.Debugging
                 registry: registry ?? sourceMap, 
                 breakpoints: breakpoints ?? registry as IBreakpoints ?? sourceMap, 
                 terminate: terminate, 
-                model: model, 
+                codeModel: codeModel,
+                dataModel: dataModel, 
                 logger: logger));
         }
 
