de6bfab5dac39a4238a20a4c36ffccd46dcd7187
Tom Laird-McConnell <tomlm@microsoft.com>
2019-04-26 02:53:29 -0700

    fixed some unit tests digressions from .dialog files

--- tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/JsonLoadTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/JsonLoadTests.cs
@@ -112,11 +112,13 @@ namespace Microsoft.Bot.Builder.Dialogs.Loader.Tests
         {
             await BuildTestFlow("RepeatDialog.main.dialog")
             .SendConversationUpdate()
+                .AssertReply("RepeatDialog.main.dialog starting")
                 .AssertReply("Hello, what is your name?")
             .Send("Carlos")
-                .AssertReply("Hello Carlos, nice to meet you!")
+                .AssertReply("Hello Carlos, nice to meet you! (type cancel to end this)")
             .Send("hi")
-                .AssertReply("Hello Carlos, nice to meet you!")
+                .AssertReply("RepeatDialog.main.dialog starting")
+                .AssertReply("Hello Carlos, nice to meet you! (type cancel to end this)")
             .StartTestAsync();
         }
 
@@ -145,6 +147,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Loader.Tests
                 .AssertReply("Hello, I'm Zoidberg. What is your name?")
             .Send("Carlos")
                 .AssertReply("Hello Carlos, nice to talk to you!")
+                .AssertReply("Hey, I can tell you a joke, or tell your fortune")
             .Send("Do you know a joke?")
                 .AssertReply("Why did the chicken cross the road?")
             .Send("Why?")
@@ -165,6 +168,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Loader.Tests
                 .AssertReply("Hello, I'm Zoidberg. What is your name?")
             .Send("Carlos")
                 .AssertReply("Hello Carlos, nice to talk to you!")
+                .AssertReply("Hey, I can tell you a joke, or tell your fortune")
             .Send("Do you know a joke?")
                 .AssertReply("Why did the chicken cross the road?")
             .Send("Why?")
