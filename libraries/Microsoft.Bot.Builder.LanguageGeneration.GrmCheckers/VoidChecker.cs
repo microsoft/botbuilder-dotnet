a9e1fc4f0ceee6bdf1295101d1fdaf2d2e05ba5d
Dong Lei <donglei@microsoft.com>
2019-05-05 16:34:38 +0800

    init lg middle ware

---
+++ libraries/Microsoft.Bot.Builder.LanguageGeneration.GrmCheckers/VoidChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Bot.Builder.LanguageGeneration;
+
+namespace Microsoft.Bot.Builder.LanguageGeneration.GrmCheckers
+{
+    public class VoidChecker : ITemplateEngineMiddleware
+    {
+        // a VoidChecker don't do anything, just return the rawResult
+        public string OnReplace(string rawResult, List<Tuple<int, int>> replacements)
+        {
+            return rawResult;
+        }
+    }
+}
---
+++ libraries/Microsoft.Bot.Builder.LanguageGeneration/ITemplateEngineMiddleware.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.LanguageGeneration
+{
+    public interface ITemplateEngineMiddleware
+    {
+        string OnReplace(string rawResult, List<Tuple<int, int>> replacements);
+    }
+
+}
