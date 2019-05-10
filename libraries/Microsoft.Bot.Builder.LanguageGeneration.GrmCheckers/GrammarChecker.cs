28297a37601d68080da3cd256bf2486eaa8c1105
Zongyang Ma <zoma@microsoft.com>
2019-05-10 15:00:10 +0800

    update grammar checker

---
+++ libraries/Microsoft.Bot.Builder.LanguageGeneration.GrmCheckers/GrammarChecker.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Bot.Builder.LanguageGeneration;
+using SurfaceRealizationV2;
+
+namespace Microsoft.Bot.Builder.LanguageGeneration.GrmCheckers
+{
+    public class GrammarChecker : IOutputTransformer
+    {
+        public SurfaceRealizerENUS surfaceRealizer = null;
+
+        public void Init() {
+            surfaceRealizer = new SurfaceRealizerENUS();
+        }
+
+        public string Transform(string previous, OutputTransformationContext context)
+        {
+            var result = surfaceRealizer.CheckSentence(previous);
+            return result;
+        }
+    }
+}
--- tests/Microsoft.Bot.Builder.LanguageGeneration.Tests/GrammarCheckerTests.cs
+++ tests/Microsoft.Bot.Builder.LanguageGeneration.Tests/GrammarCheckerTests.cs
@@ -12,21 +12,59 @@ namespace Microsoft.Bot.Builder.LanguageGeneration.Tests
     [TestClass]
     public class GrammarCheckerTests
     {
-        private string GetExampleFilePath(string fileName)
+        public GrammarChecker gc  = null;
+        public OutputTransformationContext context = null;
+
+        [TestInitialize]
+        public void TestInitialization()
         {
-            return Path.Combine(AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin")), "Examples", fileName);
+            gc = new GrammarChecker();
+            gc.Init();
+            context = new OutputTransformationContext();
+        }
+
+        //private string GetExampleFilePath(string fileName)
+        //{
+        //    return Path.Combine(AppContext.BaseDirectory.Substring(0, AppContext.BaseDirectory.IndexOf("bin")), "Examples", fileName);
+        //}
+
+        //[TestMethod]
+        //public void TestVoidGrammarChecker()
+        //{
+        //    var engine = TemplateEngine.FromFiles(GetExampleFilePath("2.lg"));
+        //    engine.OutputTransformers.Add(new VoidChecker());
+
+        //    var evaled = engine.EvaluateTemplate("wPhrase", null);
+        //    var options = new List<string> { "Hi", "Hello", "Hiya " };
+
+        //    Assert.IsTrue(options.Contains(evaled), $"The result `{evaled}` is not in those options [{string.Join(",", options)}]");
+        //}
+
+        [TestMethod]
+        public void TestNumNounGrammarChecker()
+        {
+            string orgin_sent = "It's about 12 mile away";
+            string truth_sent = "It's about 12 miles away";
+            string result = gc.Transform(orgin_sent, context);
+            Assert.AreEqual(truth_sent, result);
         }
 
         [TestMethod]
-        public void TestVoidGrammarChecker()
+        public void TestAOrAnGrammarChecker()
         {
-            var engine = TemplateEngine.FromFiles(GetExampleFilePath("2.lg"));
-            engine.OutputTransformers.Add(new VoidChecker());
+            string orgin_sent = "An useful tool"; 
+            string truth_sent = "A useful tool";
+            string result = gc.Transform(orgin_sent, context);
+            Assert.AreEqual(truth_sent, result);
+        }
 
-            var evaled = engine.EvaluateTemplate("wPhrase", null);
-            var options = new List<string> { "Hi", "Hello", "Hiya " };
-
-            Assert.IsTrue(options.Contains(evaled), $"The result `{evaled}` is not in those options [{string.Join(",", options)}]");
+        [TestMethod]
+        public void TestSubjectVerbConsistencyGrammarChecker()
+        {
+            string orgin_sent = "there is 54 cheap restaurants.";
+            string truth_sent = "there are 54 cheap restaurants.";
+            string result = gc.Transform(orgin_sent, context);
+            Assert.AreEqual(truth_sent, result);
         }
     }
 }
