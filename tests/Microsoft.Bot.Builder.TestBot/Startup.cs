b713026c2dfbec61469928923de2a47258880d96
VictorGrycuk <victor@grycuk.net>
2019-05-24 12:23:27 -0300

    Add routing-based bot assignation using 'Func'

--- tests/Microsoft.Bot.Builder.TestBot/Startup.cs
+++ tests/Microsoft.Bot.Builder.TestBot/Startup.cs
@@ -49,10 +49,28 @@ namespace Microsoft.BotBuilderSamples
             services.AddSingleton<MainDialog>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddScoped<MyBot>();
+            services.AddScoped<DialogBot<MainDialog>>();
+            services.AddScoped<DialogAndWelcomeBot<MainDialog>>();
 
             // We can also run the inspection at a different endpoint. Just uncomment these lines.
             // services.AddSingleton<DebugAdapter>();
             // services.AddTransient<DebugBot>();
+            services.AddTransient<Func<string, IBot>>(serviceProvider => key =>
+            {
+                switch (key)
+                {
+                    case "mybot":
+                        return serviceProvider.GetService<MyBot>();
+                    case "dialogbot":
+                        return serviceProvider.GetService<DialogBot<MainDialog>>();
+                    case "messages":
+                    case "dialogandwelcomebot":
+                        return serviceProvider.GetService<DialogAndWelcomeBot<MainDialog>>();
+                    default:
+                        return null;
+                }
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,10 +89,12 @@ namespace Microsoft.BotBuilderSamples
             app.UseStaticFiles();
 
             // app.UseHttpsRedirection();
-            app.UseMvc();
-        }
-
+            app.UseMvc(routes =>
             {
+                routes.MapRoute(
+                    name: "default",
+                    template: "api/{controller}");
+            });
         }
     }
 }
