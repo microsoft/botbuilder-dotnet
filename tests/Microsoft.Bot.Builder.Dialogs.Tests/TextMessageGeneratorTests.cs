7b90dae82be1fb9354e19debd755d05d82ccac62
Tom Laird-McConnell <tomlm@microsoft.com>
2019-06-02 12:19:49 -0700

    fix build break in unit test

--- tests/Microsoft.Bot.Builder.Dialogs.Tests/TextMessageGeneratorTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Tests/TextMessageGeneratorTests.cs
@@ -66,7 +66,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
 
             var context = new TurnContext(new TestAdapter(), new Activity());
             var lgText = await resourceExplorer.GetResource(lgFile).ReadTextAsync();
-            context.TurnState.Add<ILanguageGenerator>(new TemplateEngineLanguageGenerator(lgText));
+            context.TurnState.Add<ILanguageGenerator>(new TemplateEngineLanguageGenerator("test", lgText));
             return context;
         }
 
