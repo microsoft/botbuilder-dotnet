883562aab055b68f855fc0cf7a10c9f6554d14bb
Hongyang Du <hond@microsoft.com>
2019-06-19 15:24:09 +0800

    add LRUCache

--- libraries/Microsoft.Bot.Builder.Expressions/CommonRegex.cs
+++ libraries/Microsoft.Bot.Builder.Expressions/CommonRegex.cs
@@ -8,52 +8,25 @@ using Antlr4.Runtime.Tree;
 
 namespace Microsoft.Bot.Builder.Expressions
 {
-    internal class RegexLRUEntity
-    {
-        public RegexLRUEntity(long timestamp, Regex regex)
-        {
-            this.Timestamp = timestamp;
-            this.Regex = regex;
-        }
-
-        /// <summary>
-        /// Gets time when this item inserted.
-        /// </summary>
-        /// <value>
-        /// Time when this item inserted.
-        /// </value>
-        public long Timestamp { get; }
-
-        /// <summary>
-        /// Gets regex entity of cache.
-        /// </summary>
-        /// <value>
-        /// Regex entity of cache.
-        /// </value>
-        public Regex Regex { get; }
-    }
-
     public class CommonRegex
     {
-        private const int CacheCapacity = 15;
-        private static Dictionary<string, RegexLRUEntity> lruCacheMap = new Dictionary<string, RegexLRUEntity>();
-        private static readonly object LockObj = new object();
+        private static LRUCache<string, Regex> regexCache = new LRUCache<string, Regex>(15);
 
         public static Regex CreateRegex(string pattern)
         {
+            if (!string.IsNullOrEmpty(pattern) && regexCache.TryGet(pattern, out var regex))
+            {
+                return regex;
+            }
+
             if (string.IsNullOrEmpty(pattern) || !IsCommonRegex(pattern))
             {
                 throw new ArgumentException("A regular expression parsing error occurred.");
             }
 
-            var regex = GetCacheValue(pattern);
-            if (regex == null)
-            {
-                regex = new Regex(pattern);
-                SetChacheValue(pattern, regex);
-            }
-
-            return regex as Regex;
+            regex = new Regex(pattern);
+            regexCache.Set(pattern, regex);
+            return regex;
         }
 
         private static bool IsCommonRegex(string pattern)
@@ -79,48 +52,6 @@ namespace Microsoft.Bot.Builder.Expressions
             parser.AddErrorListener(new ErrorListener());
             parser.BuildParseTree = true;
             return parser.parse();
-        }
-
-        private static Regex GetCacheValue(string key)
-        {
-            if (key != null && lruCacheMap.TryGetValue(key, out var val))
-            {
-                // update timestamp
-                SetChacheValue(key, val.Regex);
-                return val.Regex;
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-        private static void SetChacheValue(string key, Regex value)
-        {
-            lock (LockObj)
-            {
-                if (key != null)
-                {
-                    if (lruCacheMap.ContainsKey(key))
-                    {
-                        // update timestamp
-                        lruCacheMap[key] = new RegexLRUEntity(DateTime.Now.Ticks, value);
-                    }
-                    else
-                    {
-                        // delete least recently used objects
-                        if (lruCacheMap.Count > CacheCapacity)
-                        {
-                            var deleteKeys = lruCacheMap
-                                .OrderBy(item => item.Value.Timestamp)
-                                .Take(lruCacheMap.Count - CacheCapacity)
-                                .Select(u => u.Key).ToList();
-                            deleteKeys.ForEach(u => lruCacheMap.Remove(u));
-                        }
-                        lruCacheMap.Add(key, new RegexLRUEntity(DateTime.Now.Ticks, value));
-                    }
-                }
-            }
         }
     }
 
---
+++ libraries/Microsoft.Bot.Builder.Expressions/LRUCache.cs
@@ -0,0 +1,183 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace Microsoft.Bot.Builder.Expressions
+{
+    public class LRUCache<TKey, TValue>
+    {
+        private const int DefaultCapacity = 255;
+        private int capacity;
+        private ReaderWriterLockSlim locker;
+        private IDictionary<TKey, TValue> dictionary;
+        private LinkedList<TKey> linkedList;
+
+        public LRUCache()
+            : this(DefaultCapacity)
+        {
+        }
+
+        public LRUCache(int capacity)
+        {
+            locker = new ReaderWriterLockSlim();
+            this.capacity = capacity > 0 ? capacity : DefaultCapacity;
+            dictionary = new Dictionary<TKey, TValue>();
+            linkedList = new LinkedList<TKey>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                locker.EnterReadLock();
+                try
+                {
+                    return dictionary.Count;
+                }
+                finally
+                {
+                    locker.ExitReadLock();
+                }
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                locker.EnterReadLock();
+                try
+                {
+                    return capacity;
+                }
+                finally
+                {
+                    locker.ExitReadLock();
+                }
+            }
+
+            set
+            {
+                locker.EnterUpgradeableReadLock();
+                try
+                {
+                    if (value > 0 && capacity != value)
+                    {
+                        locker.EnterWriteLock();
+                        try
+                        {
+                            capacity = value;
+                            while (linkedList.Count > capacity)
+                            {
+                                linkedList.RemoveLast();
+                            }
+                        }
+                        finally
+                        {
+                            locker.ExitWriteLock();
+                        }
+                    }
+                }
+                finally
+                {
+                    locker.ExitUpgradeableReadLock();
+                }
+            }
+        }
+
+        public ICollection<TKey> Keys
+        {
+            get
+            {
+                locker.EnterReadLock();
+                try
+                {
+                    return dictionary.Keys;
+                }
+                finally
+                {
+                    locker.ExitReadLock();
+                }
+            }
+        }
+
+        public ICollection<TValue> Values
+        {
+            get
+            {
+                locker.EnterReadLock();
+                try
+                {
+                    return dictionary.Values;
+                }
+                finally
+                {
+                    locker.ExitReadLock();
+                }
+            }
+        }
+
+        public void Set(TKey key, TValue value)
+        {
+            locker.EnterWriteLock();
+            try
+            {
+                dictionary[key] = value;
+                linkedList.Remove(key);
+                linkedList.AddFirst(key);
+                if (linkedList.Count > capacity)
+                {
+                    dictionary.Remove(linkedList.Last.Value);
+                    linkedList.RemoveLast();
+                }
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+
+        public bool TryGet(TKey key, out TValue value)
+        {
+            locker.EnterUpgradeableReadLock();
+            try
+            {
+                var getSuccess = dictionary.TryGetValue(key, out value);
+                if (getSuccess)
+                {
+                    locker.EnterWriteLock();
+                    try
+                    {
+                        linkedList.Remove(key);
+                        linkedList.AddFirst(key);
+                    }
+                    finally
+                    {
+                        locker.ExitWriteLock();
+                    }
+                }
+                return getSuccess;
+            }
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                locker.ExitUpgradeableReadLock();
+            }
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            locker.EnterReadLock();
+            try
+            {
+                return dictionary.ContainsKey(key);
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+    }
+}
