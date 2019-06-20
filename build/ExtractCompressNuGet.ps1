92c94c92daca06417f8ee445b96e75f43b2170fb
BruceHaley <some@one.com>
2019-06-20 16:32:49 -0700

    Message tweak

--- build/ExtractCompressNuGet.ps1
+++ build/ExtractCompressNuGet.ps1
@@ -17,7 +17,7 @@ pushd $path
 # Ensure Powershell.Archive minimum version 1.2.3.0 is installed. That fixes a path separator issue on macOS/Linux. 
 $ver = (Get-Command -Module Microsoft.PowerShell.Archive | Select-Object -Property version -First 1).Version.ToString()
 if ($ver -lt '1.2.3.0') { 
-    Write-Host "Installing Microsoft.Powershell.Archive 1.2.3.0"
+    Write-Host "Installing Microsoft.Powershell.Archive 1.2.3.0 (fix for Linux path separator bug)"
     Install-Module -Name Microsoft.PowerShell.Archive -MinimumVersion '1.2.3.0' -AllowClobber -Force -AcceptLicense 
 } else { 
     Write-Host "Already installed: Microsoft.Powershell.Archive $ver"
