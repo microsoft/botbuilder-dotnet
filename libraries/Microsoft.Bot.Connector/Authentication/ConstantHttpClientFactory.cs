3adc26cc2a7036f0c6e1bb2d7b5b197aad9ba7ca
Carlos Castro <ccastro@microsoft.com>
2019-06-24 09:33:57 -0700

    Authentication: Update ADAL and pass custom HttpClients through HttpClientFactory for AcquireToken

---
+++ libraries/Microsoft.Bot.Connector/Authentication/ConstantHttpClientFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace Microsoft.Bot.Connector.Authentication
+{
+    internal class ConstantHttpClientFactory : IHttpClientFactory
+    {
+        private readonly HttpClient httpClient;
+
+        public ConstantHttpClientFactory(HttpClient client)
+        {
+            httpClient = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public HttpClient GetHttpClient()
+        {
+            return httpClient;
+        }
+    }
+}
