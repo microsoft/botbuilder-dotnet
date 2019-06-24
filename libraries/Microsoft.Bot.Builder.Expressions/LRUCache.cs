be5902124a98b281686ef00036acda22e0d3cf90
Hongyang Du <hond@microsoft.com>
2019-06-24 18:41:36 +0800

    update LRUCache

--- libraries/Microsoft.Bot.Builder.Expressions/LRUCache.cs
+++ libraries/Microsoft.Bot.Builder.Expressions/LRUCache.cs
@@ -3,13 +3,14 @@ using System.Threading;
 
 namespace Microsoft.Bot.Builder.Expressions
 {
-    public class LRUCache<TKey, TValue>
+    public sealed class LRUCache<TKey, TValue>
     {
         private const int DefaultCapacity = 255;
+
+        private readonly object lockObj = new object();
         private readonly int capacity;
-        private ReaderWriterLockSlim locker;
-        private IDictionary<TKey, TValue> dictionary;
-        private LinkedList<TKey> linkedList;
+        private readonly Dictionary<TKey, Entry> cacheMap;
+        private readonly LinkedList<TKey> cacheList;
 
         public LRUCache()
             : this(DefaultCapacity)
@@ -18,61 +19,74 @@ namespace Microsoft.Bot.Builder.Expressions
 
         public LRUCache(int capacity)
         {
-            locker = new ReaderWriterLockSlim();
             this.capacity = capacity > 0 ? capacity : DefaultCapacity;
-            dictionary = new Dictionary<TKey, TValue>();
-            linkedList = new LinkedList<TKey>();
-        }
-
-        public void Set(TKey key, TValue value)
-        {
-            locker.EnterWriteLock();
-            try
-            {
-                dictionary[key] = value;
-                linkedList.Remove(key);
-                linkedList.AddFirst(key);
-                if (linkedList.Count > capacity)
-                {
-                    dictionary.Remove(linkedList.Last.Value);
-                    linkedList.RemoveLast();
-                }
-            }
-            finally
-            {
-                locker.ExitWriteLock();
-            }
+            this.cacheMap = new Dictionary<TKey, Entry>();
+            this.cacheList = new LinkedList<TKey>();
         }
 
         public bool TryGet(TKey key, out TValue value)
         {
-            locker.EnterUpgradeableReadLock();
-            try
+            lock (lockObj)
             {
-                var keyExist = dictionary.TryGetValue(key, out value);
-                if (keyExist)
+                if (this.cacheMap.TryGetValue(key, out var entry))
                 {
-                    locker.EnterWriteLock();
-                    try
-                    {
-                        linkedList.Remove(key);
-                        linkedList.AddFirst(key);
-                    }
-                    finally
-                    {
-                        locker.ExitWriteLock();
-                    }
+                    Touch(entry.Node);
+                    value = entry.Value;
+                    return true;
                 }
+            }
+            value = default(TValue);
+            return false;
+        }
 
-                return keyExist;
-            }
-            catch
+        public void Set(TKey key, TValue value)
+        {
+            lock (lockObj)
             {
-                throw;
+                if (!this.cacheMap.TryGetValue(key, out var entry))
+                {
+                    LinkedListNode<TKey> node;
+                    if (this.cacheMap.Count >= this.capacity)
+                    {
+                        node = this.cacheList.Last;
+                        this.cacheMap.Remove(node.Value);
+                        this.cacheList.RemoveLast();
+                        node.Value = key;
+                    }
+                    else
+                    {
+                        node = new LinkedListNode<TKey>(key);
+                    }
+                    this.cacheList.AddFirst(node);
+                    this.cacheMap.Add(key, new Entry(node, value));
+                }
+                else
+                {
+                    entry.Value = value;
+                    this.cacheMap[key] = entry;
+                    Touch(entry.Node);
+                }
             }
-            finally
+        }
+
+        private void Touch(LinkedListNode<TKey> node)
+        {
+            if (node != this.cacheList.First)
             {
-                locker.ExitUpgradeableReadLock();
+                this.cacheList.Remove(node);
+                this.cacheList.AddFirst(node);
+            }
+        }
+
+        private struct Entry
+        {
+            public LinkedListNode<TKey> Node;
+            public TValue Value;
+
+            public Entry(LinkedListNode<TKey> node, TValue value)
+            {
+                this.Node = node;
+                this.Value = value;
             }
         }
     }
