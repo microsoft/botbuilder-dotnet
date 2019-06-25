5460315f41326739e22191f4a1f3ed96926821f4
Shuai Wang <shuwan@microsoft.com>
2019-06-25 09:24:42 +0800

    add test for lrucache

--- tests/Microsoft.Bot.Builder.Expressions.Tests/LRUCacheTest.cs
+++ tests/Microsoft.Bot.Builder.Expressions.Tests/LRUCacheTest.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.Cryptography.X509Certificates;
+using Antlr4.Runtime.Atn;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Bot.Builder.Expressions.Tests
 {
@@ -36,5 +40,54 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
             Assert.AreEqual(result, "num3");
         }
 
+        //public int Fibonacci(LRUCache<int, int> cache, int n)
+        //{
+        //
+        //}
+            
+ 
+
+        [TestMethod]
+        public void TestDPMemorySmall()
+        {
+            var cache = new LRUCache<int, int>(2);
+            cache.Set(0, 1);
+            cache.Set(1, 1);
+            for (int i = 2; i <= 1000; i++){
+                cache.TryGet(i - 2,  out var prev2);
+                cache.TryGet(i - 1, out var prev1);
+                cache.Set(i, prev1 + prev2);
+            }
+
+            Assert.IsFalse(cache.TryGet(998, out var result));
+
+            Assert.IsTrue(cache.TryGet(999, out result));
+            Assert.AreEqual(result, 1556111435);
+
+            Assert.IsTrue(cache.TryGet(1000, out result));
+            Assert.AreEqual(result, 1318412525);
+        }
+
+        [TestMethod]
+        public void TestDPMemoryLarge()
+        {
+            var cache = new LRUCache<int, int>(500);
+            cache.Set(0, 1);
+            cache.Set(1, 1);
+            for (int i = 2; i <= 1000; i++)
+            {
+                cache.TryGet(i - 2, out var prev2);
+                cache.TryGet(i - 1, out var prev1);
+                cache.Set(i, prev1 + prev2);
+            }
+
+            Assert.IsFalse(cache.TryGet(1, out var result));
+
+            Assert.IsTrue(cache.TryGet(999, out result));
+            Assert.AreEqual(result, 1556111435);
+
+            Assert.IsTrue(cache.TryGet(1000, out result));
+            Assert.AreEqual(result, 1318412525);
+        }
     }
 }
