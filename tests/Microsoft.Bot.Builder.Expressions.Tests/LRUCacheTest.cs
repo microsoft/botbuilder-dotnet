2409f1c71523dd4c00270450641beb40c09bca74
Shuai Wang <shuwan@microsoft.com>
2019-06-25 14:36:35 +0800

    Add multi tasks test for LRUCache

--- tests/Microsoft.Bot.Builder.Expressions.Tests/LRUCacheTest.cs
+++ tests/Microsoft.Bot.Builder.Expressions.Tests/LRUCacheTest.cs
@@ -1,6 +1,9 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.ComTypes;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
 using Antlr4.Runtime.Atn;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -40,41 +43,46 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
             Assert.AreEqual(result, "num3");
         }
 
-        //public int Fibonacci(LRUCache<int, int> cache, int n)
-        //{
-        //
-        //}
-            
- 
-
         [TestMethod]
+        /*
+         * The average time of this test is about 2ms. 
+         */
         public void TestDPMemorySmall()
         {
             var cache = new LRUCache<int, int>(2);
             cache.Set(0, 1);
             cache.Set(1, 1);
-            for (int i = 2; i <= 1000; i++){
+            var fib9999 = 1242044891;
+            var fib100000 = 2132534333;
+            var maxIdx = 10000;
+            for (int i = 2; i <= maxIdx; i++){
                 cache.TryGet(i - 2,  out var prev2);
                 cache.TryGet(i - 1, out var prev1);
                 cache.Set(i, prev1 + prev2);
             }
 
-            Assert.IsFalse(cache.TryGet(998, out var result));
+            Assert.IsFalse(cache.TryGet(9998, out var result));
 
-            Assert.IsTrue(cache.TryGet(999, out result));
-            Assert.AreEqual(result, 1556111435);
+            Assert.IsTrue(cache.TryGet(maxIdx-1, out result));
+            Assert.AreEqual(result, fib9999);
 
-            Assert.IsTrue(cache.TryGet(1000, out result));
-            Assert.AreEqual(result, 1318412525);
+            Assert.IsTrue(cache.TryGet(maxIdx, out result));
+            Assert.AreEqual(result, fib100000);
         }
 
         [TestMethod]
+        /*
+         * The average time of this test is about 3ms. 
+         */
         public void TestDPMemoryLarge()
         {
             var cache = new LRUCache<int, int>(500);
             cache.Set(0, 1);
             cache.Set(1, 1);
-            for (int i = 2; i <= 1000; i++)
+            var fib9999 = 1242044891;
+            var fib100000 = 2132534333;
+            var maxIdx = 10000; 
+            for (int i = 2; i <= 10000; i++)
             {
                 cache.TryGet(i - 2, out var prev2);
                 cache.TryGet(i - 1, out var prev1);
@@ -83,11 +91,43 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
 
             Assert.IsFalse(cache.TryGet(1, out var result));
 
-            Assert.IsTrue(cache.TryGet(999, out result));
-            Assert.AreEqual(result, 1556111435);
+            Assert.IsTrue(cache.TryGet(maxIdx - 1, out result));
+            Assert.AreEqual(result, fib9999);
 
-            Assert.IsTrue(cache.TryGet(1000, out result));
-            Assert.AreEqual(result, 1318412525);
+            Assert.IsTrue(cache.TryGet(maxIdx, out result));
+            Assert.AreEqual(result, fib100000);
+        }
+
+        [TestMethod]
+        /*
+         * The average time of this test is about 13ms(without the loop of Assert statements). 
+         */
+        public async Task TestMultiThreadingAsync()
+        {
+            var cache = new LRUCache<int, int>(10);
+            var tasks = new List<Task>();
+            var numOfThreads = 10;
+            var numOfOps = 1000;
+            for (var i = 0; i < numOfThreads; i++)
+            {
+                tasks.Add(Task.Run(() => storeElement(cache, numOfOps, i)));
+            }
+            await Task.WhenAll(tasks);
+
+            for (var i = numOfOps - numOfThreads; i < numOfOps; i++)
+            {
+               Assert.IsTrue(cache.TryGet(i, out var result));
+            }
+        }
+
+        public void storeElement(LRUCache<int,int> cache, int numOfOps, int idx)
+        {
+            for(int i = 0; i < numOfOps; i++)
+            {
+                var key =  i;
+                var value = i;
+                cache.Set(key, value);
+            }
         }
     }
 }
