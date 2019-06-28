56c124f3e583daab3a5cc683780b8d0e75aafcf0
Cecilia Avila <cecilia.avila@southworks.com>
2019-06-28 12:47:31 -0300

    Add Botkit Adapter Slack Project

---
+++ libraries/Adapters/Microsoft.BotKit.Adapters.Slack/ActivityResourceResponse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.BotKit.Adapters.Slack
+{
+    /// <summary>
+    /// Extends ResourceResponse with ActivityID and Conversation properties.
+    /// </summary>
+    public class ActivityResourceResponse : ResourceResponse
+    {
+        /// <summary>
+        /// Gets or sets the Activity ID.
+        /// </summary>
+        /// <value>
+        /// The Activity ID.
+        /// </value>
+        public string ActivityID { get; set; }
+
+        /// <summary>
+        /// Gets or sets a Conversation Account.
+        /// </summary>
+        /// <value>
+        /// Conversation Account.
+        /// </value>
+        public ConversationAccount Conversation { get; set; }
+    }
+}
