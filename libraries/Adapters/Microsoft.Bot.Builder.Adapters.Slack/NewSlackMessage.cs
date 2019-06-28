5cddadaa255d877b94fa4a514469affc7c880d52
Mai <mailen.havela@southworks.com>
2019-06-28 14:57:05 -0300

    Add missing Copyright

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Slack/NewSlackMessage.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Slack/NewSlackMessage.cs
@@ -1,4 +1,7 @@
-﻿using SlackAPI.WebSocketMessages;
+﻿// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+using SlackAPI.WebSocketMessages;
 
 namespace Microsoft.BotBuilder.Adapters.Slack
 {
