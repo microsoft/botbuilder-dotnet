a70d5e4ef2f4d56b8191a52d3b9b652f9d888eb6
Daniel Evans <dev@microsoft.com>
2019-07-02 15:28:53 -0700

    BB integration files only, no library.

---
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/BotFrameworkApplicationBuilderExtensions.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.Core
+{
+    /// <summary>
+    /// Maps various endpoint handlers for the <see cref="ServiceCollectionExtensions.AddBot{TBot}(IServiceCollection, Action{BotFrameworkOptions})">registered bot</see> into the request execution pipeline using the V4 protocol.
+    /// </summary>
+    public static class BotFrameworkApplicationBuilderExtensions
+    {
+        /// <summary>
+        /// Maps various endpoint handlers for the <see cref="ServiceCollectionExtensions.AddBot{TBot}(IServiceCollection, Action{BotFrameworkOptions})">
+        /// registered bot</see> into the request execution pipeline using the V4 protocol.
+        /// Throws <see cref="ArgumentNullException"/> if application is null.
+        /// </summary>
+        /// <param name="applicationBuilder">The application builder that defines the bot's pipeline.<see cref="IApplicationBuilder"/>.</param>
+        /// <param name="middlewareSet">The set of middleware the bot executes on each turn. <see cref="MiddlewareSet"/>.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IApplicationBuilder UseBotFrameworkNamedPipe(this IApplicationBuilder applicationBuilder, IList<IMiddleware> middlewareSet = null)
+        {
+            if (applicationBuilder == null)
+            {
+                throw new ArgumentNullException(nameof(applicationBuilder));
+            }
+
+            var applicationServices = applicationBuilder.ApplicationServices;
+            var bot = applicationServices.GetRequiredService<IBot>();
+            var connector = applicationServices.GetRequiredService<NamedPipeConnector>();
+            connector.InitializeNamedPipeServer(bot, middlewareSet);
+
+            return applicationBuilder;
+        }
+    }
+}
---
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.WebApi/WebSocketConnector.cs
@@ -0,0 +1,138 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.WebSockets;
+using Microsoft.Bot.Connector.Authentication;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.WebApi.StreamingExtensions
+{
+    internal class WebSocketConnector
+    {
+        // These headers are used to send the required values for validation of an incoming connection request from an ABS channel.
+        // TODO: We must document this somewhere, right? Find it and put a reference link here.
+        private const string AuthHeaderName = "authorization";
+        private const string ChannelIdHeaderName = "channelid";
+        private readonly IChannelProvider _channelProvider;
+        private readonly ICredentialProvider _credentialProvider;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="WebSocketConnector"/> class.
+        /// Constructor for use when establishing a connection with a WebSocket server.
+        /// </summary>
+        /// <param name="credentialProvider">Used for validating channel credential authentication information.</param>
+        /// <param name="channelProvider">Used for validating channel authentication information.</param>
+        internal WebSocketConnector(ICredentialProvider credentialProvider, IChannelProvider channelProvider = null)
+        {
+            _credentialProvider = credentialProvider;
+            _channelProvider = channelProvider;
+        }
+
+        /// <summary>
+        /// Process the initial request to establish a long lived connection via a streaming server.
+        /// </summary>
+        /// <param name="onTurnError">Logic to execute on encountering turn errors.</param>
+        /// <param name="middlewareSet">The set of middleware to perform on each turn.</param>
+        /// <param name="httpRequest">The connection request.</param>
+        /// <param name="httpResponse">The response sent on error or connection termination.</param>
+        /// <param name="bot">The bot that is communicating over this connection.</param>
+        /// <param name="cancellationToken">Optional cancellation token.</param>
+        /// <returns>Returns on task completion.</returns>
+        internal async Task ProcessAsync(Func<ITurnContext, Exception, Task> onTurnError, List<IMiddleware> middlewareSet, HttpRequestMessage httpRequest, HttpResponseMessage httpResponse, IBot bot = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (httpRequest == null)
+            {
+                throw new ArgumentNullException(nameof(httpRequest));
+            }
+
+            if (httpResponse == null)
+            {
+                throw new ArgumentNullException(nameof(httpResponse));
+            }
+
+            if (!System.Web.HttpContext.Current.IsWebSocketRequest)
+            {
+                httpResponse.Content = new StringContent("Upgrade to web socket is required");
+                httpResponse.StatusCode = HttpStatusCode.BadRequest;
+
+                return;
+            }
+
+            try
+            {
+                if (!await _credentialProvider.IsAuthenticationDisabledAsync().ConfigureAwait(false))
+                {
+                    var authHeader = httpRequest.Headers.Where(x => x.Key.ToLower() == AuthHeaderName).FirstOrDefault().Value?.FirstOrDefault();
+                    var channelId = httpRequest.Headers.Where(x => x.Key.ToLower() == ChannelIdHeaderName).FirstOrDefault().Value?.FirstOrDefault();
+
+                    if (string.IsNullOrWhiteSpace(authHeader))
+                    {
+                        MissingAuthHeaderHelper(AuthHeaderName, httpResponse);
+
+                        return;
+                    }
+
+                    if (string.IsNullOrWhiteSpace(channelId))
+                    {
+                        MissingAuthHeaderHelper(ChannelIdHeaderName, httpResponse);
+
+                        return;
+                    }
+
+                    var claimsIdentity = await JwtTokenValidation.ValidateAuthHeader(authHeader, _credentialProvider, _channelProvider, channelId).ConfigureAwait(false);
+                    if (!claimsIdentity.IsAuthenticated)
+                    {
+                        httpResponse.StatusCode = HttpStatusCode.Unauthorized;
+
+                        return;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                httpResponse.StatusCode = HttpStatusCode.InternalServerError;
+                httpResponse.Content = new StringContent("Error while attempting to authorize connection.");
+
+                throw ex;
+            }
+
+            CreateStreamingServerConnection(onTurnError, middlewareSet, System.Web.HttpContext.Current, httpResponse, httpRequest, bot);
+        }
+
+        private void MissingAuthHeaderHelper(string headerName, HttpResponseMessage httpResponseMessage)
+        {
+            httpResponseMessage.StatusCode = HttpStatusCode.Unauthorized;
+            httpResponseMessage.Content = new StringContent($"Unable to authentiate. Missing header: {headerName}");
+        }
+
+        private void CreateStreamingServerConnection(Func<ITurnContext, Exception, Task> onTurnError, List<Builder.IMiddleware> middlewareSet, System.Web.HttpContext httpContext, HttpResponseMessage httpResponse, HttpRequestMessage httpRequest, IBot bot = null)
+        {
+            var handler = new StreamingRequestHandler(onTurnError, bot ?? httpRequest.GetDependencyScope().GetService(typeof(IBot)) as IBot, middlewareSet);
+
+            Func<AspNetWebSocketContext, Task> processWebSocketSessionFunc = async (context) =>
+            {
+                try
+                {
+                    await handler.StartAsync(context.WebSocket).ConfigureAwait(false);
+                }
+                catch (Exception ex)
+                {
+                    httpResponse.StatusCode = HttpStatusCode.InternalServerError;
+                    httpResponse.Content = new StringContent("Unable to create transport server.");
+
+                    throw ex;
+                }
+            };
+
+            // set the status code first , since the next call might have a chance to block the thread.
+            httpResponse.StatusCode = HttpStatusCode.SwitchingProtocols;
+            httpContext.AcceptWebSocketRequest(processWebSocketSessionFunc);
+        }
+    }
+}
