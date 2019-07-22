892438e44f48b761ee9d0344a75b0c55f5555f90
Jonathan Fingold <35047199+JonathanFingold@users.noreply.github.com>
2019-07-22 11:52:56 -0700

    wip

--- libraries/Microsoft.Bot.Connector/ConnectorClientEx.cs
+++ libraries/Microsoft.Bot.Connector/ConnectorClientEx.cs
@@ -12,18 +12,17 @@ using Newtonsoft.Json.Serialization;
 
 namespace Microsoft.Bot.Connector
 {
-    /// <summary>
-    /// ConnectorClient extension.
-    /// </summary>
     public partial class ConnectorClient
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="ConnectorClient"/> class.
         /// </summary>
         /// <param name="baseUri">Base URI for the Connector service.</param>
-        /// <param name="microsoftAppId">Optional. Your Microsoft app id. If null, this setting is read from settings["MicrosoftAppId"].</param>
-        /// <param name="microsoftAppPassword">Optional. Your Microsoft app password. If null, this setting is read from settings["MicrosoftAppPassword"].</param>
-        /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
+        /// <param name="microsoftAppId">Optional, the Microsoft app for the bot resource.
+        /// If null, this setting is read from the `MicrosoftAppId` setting for the bot's application resource.</param>
+        /// <param name="microsoftAppPassword">Optional, the Microsoft app password for the bot.
+        /// If null, this setting is read from the `MicrosoftAppPassword` setting for the bot's application resource.</param>
+        /// <param name="handlers">Optional, <see cref="DelegatingHandler"/> objects to add to the HTTP client pipeline.</param>
         public ConnectorClient(Uri baseUri, string microsoftAppId = null, string microsoftAppPassword = null, params DelegatingHandler[] handlers)
             : this(baseUri, new MicrosoftAppCredentials(microsoftAppId, microsoftAppPassword), handlers: handlers)
         {
@@ -34,9 +33,10 @@ namespace Microsoft.Bot.Connector
         /// Initializes a new instance of the <see cref="ConnectorClient"/> class.
         /// </summary>
         /// <param name="baseUri">Base URI for the Connector service.</param>
-        /// <param name="credentials">Credentials for the Connector service.</param>
-        /// <param name="addJwtTokenRefresher">(DEPRECATED).</param>
-        /// <param name="handlers">Optional. The delegating handlers to add to the http client pipeline.</param>
+        /// <param name="credentials">A <see cref="MicrosoftAppCredentials"/> object that contains the
+        /// Microsoft app ID and password for your bot resource.</param>
+        /// <param name="addJwtTokenRefresher">Optional.</param>
+        /// <param name="handlers">Optional, <see cref="DelegatingHandler"/> objects to add to the HTTP client pipeline.</param>
         public ConnectorClient(Uri baseUri, MicrosoftAppCredentials credentials, bool addJwtTokenRefresher = true, params DelegatingHandler[] handlers)
             : this(baseUri, credentials, null, addJwtTokenRefresher, handlers)
         {
