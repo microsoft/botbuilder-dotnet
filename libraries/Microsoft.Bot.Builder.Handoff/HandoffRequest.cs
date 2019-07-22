f56e24d4c711f743fa5d8ee867482974b1707c9a
Artur Laksberg <arturl@microsoft.com>
2019-07-22 16:09:26 -0700

    Change completion string

--- libraries/Microsoft.Bot.Builder.Handoff/HandoffRequest.cs
+++ libraries/Microsoft.Bot.Builder.Handoff/HandoffRequest.cs
@@ -28,7 +28,7 @@ namespace Microsoft.Bot.Builder
             try
             {
                 var result = await HandoffHttpSupport.GetHandoffStatusWithHttpMessagesAsync((IServiceOperations<ConnectorClient>)conversations, conversationId).ConfigureAwait(false);
-                return result.Body == "completed";
+                return result.Body == "Completed";
             }
             catch
             {
