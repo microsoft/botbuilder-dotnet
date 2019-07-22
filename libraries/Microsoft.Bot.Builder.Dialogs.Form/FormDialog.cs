7213b17436ca6973ba1b3574b013ef50dac2b993
Chris McConnell <chrimc@microsoft.com>
2019-07-22 10:59:01 -0700

    Initial project creation.

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Form/FormDialog.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Bot.Builder.Dialogs.Adaptive;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Bot.Builder.Dialogs.Form
+{
+    public class FormDialog : AdaptiveDialog
+    {
+        public FormSchema Schema { get; set; }
+
+        protected void GenerateRules()
+        { }
+    }
+}
---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Form/FormSchema.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Bot.Builder.Dialogs.Form
+{
+    public class FormSchema : JObject
+    {
+        public FormSchema(JObject schema)
+            : base(schema)
+        {
+        }
+
+        public FormSchema(string schema)
+            : base(JsonConvert.DeserializeObject(schema))
+        {
+        }
+    }
+}
