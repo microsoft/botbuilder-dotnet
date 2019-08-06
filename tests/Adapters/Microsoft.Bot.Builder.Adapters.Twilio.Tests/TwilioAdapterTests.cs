d1c30719d9f1c0f399b31f9534ec04f8dd8d2d3b
matiasroldan6 <matias.roldan@southworks.com>
2019-08-06 14:17:17 -0300

    Add tests project to Twilio adapter

---
+++ tests/Adapters/Microsoft.Bot.Builder.Adapters.Twilio.Tests/TwilioAdapterTests.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio.Tests
+{
+    public class TwilioAdapterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
