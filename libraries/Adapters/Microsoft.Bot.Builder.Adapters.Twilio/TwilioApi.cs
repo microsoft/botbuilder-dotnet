e7b64d195a2912be4be983cb03b553a4fd1c9ba1
VictorGrycuk <victor@grycuk.net>
2019-08-09 17:19:25 -0300

    Add documentation comments to TwilioApi

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioApi.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioApi.cs
@@ -9,11 +9,21 @@ namespace Microsoft.Bot.Builder.Adapters.Twilio
 {
     public class TwilioApi : ITwilioClient
     {
+        /// <summary>
+        /// Initialize the Twilio client by supplying the username and password.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The password.</param>
         public void LogIn(string username, string password)
         {
             TwilioClient.Init(username, password);
         }
 
+        /// <summary>
+        /// Returns the resource ID from a message.
+        /// </summary>
+        /// <param name="messageOptions">Object that represents the Twilio message options.</param>
+        /// <returns>ID from Twilio message.</returns>
         public async Task<string> GetResourceIdentifier(object messageOptions)
         {
             var messageResource = await MessageResource.CreateAsync((CreateMessageOptions)messageOptions).ConfigureAwait(false);
