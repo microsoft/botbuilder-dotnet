599d023c4a95d3a652e47f3a61d010ac208c57b3
VictorGrycuk <victor@grycuk.net>
2019-08-09 16:10:28 -0300

    Extract Twilio API to a class and interface

---
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/ITwilioClient.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
+{
+    public interface ITwilioClient
+    {
+        void LogIn(string username, string password);
+
+        Task<string> GetResourceIdentifier(object messageOptions);
+    }
+}
---
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioApi.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Microsoft.Bot.Builder.Adapters.Twilio
+{
+    public class TwilioApi : ITwilioClient
+    {
+        public void LogIn(string username, string password)
+        {
+            TwilioClient.Init(username, password);
+        }
+
+        public async Task<string> GetResourceIdentifier(object messageOptions)
+        {
+            var messageResource = await MessageResource.CreateAsync((CreateMessageOptions)messageOptions).ConfigureAwait(false);
+            return messageResource.Sid;
+        }
+    }
+}
