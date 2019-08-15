33f3faa1c8c443cfd102cda8de5e8493f1eeb1d7
Dong Lei <donglei@microsoft.com>
2019-08-15 14:58:45 +0800

    Add tests for expression (#2402)
    * Add tests for expression
    
    * Add more
    
    * Add more

--- tests/Microsoft.Bot.Builder.Expressions.Tests/ExpressionEngineTests.cs
+++ tests/Microsoft.Bot.Builder.Expressions.Tests/ExpressionEngineTests.cs
@@ -378,6 +378,11 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
             Test("if(0, 'r1', 'r2')", "r1"),//true
             Test("if(bool('true'), 'r1', 'r2')", "r1"),//true
             Test("if(istrue, 'r1', 'r2')", "r1"),//true
+            Test("if(bag.name == null, \"hello\",  bag.name)", "mybag"),
+            Test("if(user.name == null, \"hello\",  user.name)", "hello"), // user.name don't exist
+            Test("if(user.name == null, '',  user.name)", ""), // user.name don't exist
+            Test("if(one > 0, one, two)", 1),
+            Test("if(one < 0, one, two)", 2),
             Test("exists(one)", true),
             Test("exists(xxx)", false),
             Test("exists(one.xxx)", false),
