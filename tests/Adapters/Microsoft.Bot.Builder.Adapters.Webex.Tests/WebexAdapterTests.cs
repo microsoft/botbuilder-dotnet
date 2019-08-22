baa004bc529f705580ed9a8307a814807a06d0df
matiasroldan6 <matias.roldan@southworks.com>
2019-08-22 10:14:08 -0300

    Add unit tests
    Added tests for
    ContinueConversation
    ProcessAsync
    UpdateActivityAsync

--- tests/Adapters/Microsoft.Bot.Builder.Adapters.Webex.Tests/WebexAdapterTests.cs
+++ tests/Adapters/Microsoft.Bot.Builder.Adapters.Webex.Tests/WebexAdapterTests.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Schema;
 using Moq;
 using Xunit;
 
@@ -32,6 +36,147 @@ namespace Microsoft.Bot.Builder.Adapters.Webex.Tests
             options.Object.PublicAddress = null;
 
             Assert.Throws<Exception>(() => { new WebexAdapter(options.Object); });
+        }
+
+        [Fact]
+        public async void ContinueConversationAsync_Should_Fail_With_Null_ConversationReference()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+
+            Task BotsLogic(ITurnContext turnContext, CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => { await webexAdapter.ContinueConversationAsync(null, BotsLogic, default); });
+        }
+
+        [Fact]
+        public async void ContinueConversationAsync_Should_Fail_With_Null_Logic()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+            var conversationReference = new ConversationReference();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => { await webexAdapter.ContinueConversationAsync(conversationReference, null, default); });
+        }
+
+        [Fact]
+        public async void ContinueConversationAsync_Should_Succeed()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+            var conversationReference = new ConversationReference();
+            Task BotsLogic(ITurnContext turnContext, CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+
+            await webexAdapter.ContinueConversationAsync(conversationReference, BotsLogic, default);
+        }
+
+        [Fact]
+        public async void ProcessAsync_Should_Fail_With_Null_HttpRequest()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+            var httpResponse = new Mock<HttpResponse>();
+            var bot = new Mock<IBot>();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await webexAdapter.ProcessAsync(null, httpResponse.Object, bot.Object, default(CancellationToken));
+            });
+        }
+
+        [Fact]
+        public async void ProcessAsync_Should_Fail_With_Null_HttpResponse()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+            var httpRequest = new Mock<HttpRequest>();
+            var bot = new Mock<IBot>();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await webexAdapter.ProcessAsync(httpRequest.Object, null, default(IBot), default(CancellationToken));
+            });
+        }
+
+        [Fact]
+        public async void ProcessAsync_Should_Fail_With_Null_Bot()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+            var httpRequest = new Mock<HttpRequest>();
+            var httpResponse = new Mock<HttpResponse>();
+
+            await Assert.ThrowsAsync<ArgumentNullException>(async () =>
+            {
+                await webexAdapter.ProcessAsync(httpRequest.Object, httpResponse.Object, null, default(CancellationToken));
+            });
+        }
+
+        [Fact(Skip = "Can't mock extension methods")]
+        public async void ProcessAsync_Should_Succeed()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+            var httpRequest = new Mock<HttpRequest>();
+            var httpResponse = new Mock<HttpResponse>();
+            var bot = new Mock<IBot>();
+            httpResponse
+                .Setup(e => e.WriteAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.CompletedTask);
+
+            await webexAdapter.ProcessAsync(httpRequest.Object, httpResponse.Object, bot.Object, default(CancellationToken));
+        }
+
+        [Fact]
+        public async void UpdateActivityAsync_Should_Throw_NotSupportedException()
+        {
+            var options = new Mock<IWebexAdapterOptions>();
+            options.SetupAllProperties();
+            options.Object.AccessToken = "Test";
+            options.Object.PublicAddress = "http://contoso.com/api/messages";
+
+            var webexAdapter = new WebexAdapter(options.Object);
+            var activity = new Activity();
+            var turnContext = new TurnContext(webexAdapter, activity);
+
+            await Assert.ThrowsAsync<NotSupportedException>(async () =>
+            {
+                await webexAdapter.UpdateActivityAsync(turnContext, activity, default(CancellationToken));
+            });
         }
     }
 }
