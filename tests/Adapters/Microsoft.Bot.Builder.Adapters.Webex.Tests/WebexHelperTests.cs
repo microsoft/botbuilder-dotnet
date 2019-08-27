3daf3d2adaead64446fd590f9c40b8ccac546c98
Cecilia Avila <cecilia.avila@southworks.com>
2019-08-27 10:22:30 -0300

    Add unit tests for DecryptedMessageToActivity

--- tests/Adapters/Microsoft.Bot.Builder.Adapters.Webex.Tests/WebexHelperTests.cs
+++ tests/Adapters/Microsoft.Bot.Builder.Adapters.Webex.Tests/WebexHelperTests.cs
@@ -3,9 +3,12 @@
 
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Schema;
 using Moq;
 using Newtonsoft.Json;
+using Thrzn41.WebexTeams;
 using Thrzn41.WebexTeams.Version1;
 using Xunit;
 
@@ -22,7 +25,8 @@ namespace Microsoft.Bot.Builder.Adapters.Webex.Tests
         [Fact]
         public void PayloadToActivity_Should_Return_Activity()
         {
-            var seralizedPayload = "{\"id\":\"id\",\"name\":\"name\",\"resource\":\"messages\",\"event\":\"created\",\"filter\":null,\"orgId\":\"orgId\",\"createdBy\":\"creator_id\",\"appId\":\"app_id\",\"ownedBy\":\"creator\",\"status\":\"active\",\"actorId\":\"actor_id\",\"targetUrl\":\"https://contoso.com/api/messages\",\"created\":\"2019-01-01T00:00:00.096Z\",\"data\":{\"id\":\"id\",\"roomId\":\"room_id\",\"roomType\":\"direct\",\"personId\":\"person_id\",\"personEmail\":\"person@email.com\",\"created\":\"2019-01-01T00:00:00.534Z\"}}";
+            var seralizedPayload =
+                "{\"id\":\"id\",\"name\":\"name\",\"resource\":\"messages\",\"event\":\"created\",\"filter\":null,\"orgId\":\"orgId\",\"createdBy\":\"creator_id\",\"appId\":\"app_id\",\"ownedBy\":\"creator\",\"status\":\"active\",\"actorId\":\"actor_id\",\"targetUrl\":\"https://contoso.com/api/messages\",\"created\":\"2019-01-01T00:00:00.096Z\",\"data\":{\"id\":\"id\",\"roomId\":\"room_id\",\"roomType\":\"direct\",\"personId\":\"person_id\",\"personEmail\":\"person@email.com\",\"created\":\"2019-01-01T00:00:00.534Z\"}}";
             var payload = JsonConvert.DeserializeObject<WebhookEventData>(seralizedPayload);
             var serializedPerson = "{\"id\":\"person_id\"}";
             WebexHelper.Identity = JsonConvert.DeserializeObject<Person>(serializedPerson);
@@ -56,9 +60,31 @@ namespace Microsoft.Bot.Builder.Adapters.Webex.Tests
         }
 
         [Fact]
-        public async void DecryptedMessageToActivityAsync_Should_Return_Null_With_Null_Payload()
+        public async void GetDecryptedMessage_Should_Return_Null_With_Null_Payload()
         {
-            Assert.Null(await WebexHelper.DecryptedMessageToActivityAsync(null, null));
+            Assert.Null(await WebexHelper.GetDecryptedMessage(null, null));
+        }
+
+        [Fact]
+        public void DecryptedMessageToActivityAsync_Should_Return_Null_With_Null_Message()
+        {
+            Assert.Null(WebexHelper.DecryptedMessageToActivity(null));
+        }
+
+        [Fact]
+        public void DecryptedMessageToActivity_Should_Return_Activity_Type_SelfMessage()
+        {
+            var serializedPerson = "{\"id\":\"person_id\"}";
+            WebexHelper.Identity = JsonConvert.DeserializeObject<Person>(serializedPerson);
+
+            var message =
+                JsonConvert.DeserializeObject<Message>(
+                    File.ReadAllText(Directory.GetCurrentDirectory() + @"\Files\Message.json"));
+
+            var activity = WebexHelper.DecryptedMessageToActivity(message);
+
+            Assert.Equal(message.Id, activity.Id);
+            Assert.Equal(ActivityTypes.Event, activity.Type);
         }
     }
 }
