ecd7075b222f3b88febb099160958bdcbcf0ee07
Dong Lei <donglei@microsoft.com>
2019-08-30 18:44:48 +0800

    Initial commit

---
+++ libraries/Microsoft.Bot.Builder.Expressions/DefaultMemoryScopeManager.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Bot.Builder.Expressions
+{
+    internal class DefaultMemoryScopeManager : IMemoryScopeManager
+    {
+        private readonly Stack<object> scopes = new Stack<object>();
+
+        public DefaultMemoryScopeManager(object initialScope)
+        {
+            scopes.Push(initialScope);
+        }
+
+        public object GetValue(string path)
+        {
+            var (value, error) = AccessProperty(scopes.Peek(), path);
+            return value;
+        }
+
+        public void PopScope()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PushScope(object scope)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetValue(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        private (object value, string error) AccessProperty(object instance, string property)
+        {
+            // NOTE: This returns null rather than an error if property is not present
+            if (instance == null)
+            {
+                return (null, null);
+            }
+
+            object value = null;
+            string error = null;
+            property = property.ToLower();
+
+            // NOTE: what about other type of TKey, TValue?
+            if (instance is IDictionary<string, object> idict)
+            {
+                if (!idict.TryGetValue(property, out value))
+                {
+                    // fall back to case insensitive
+                    var prop = idict.Keys.Where(k => k.ToLower() == property).SingleOrDefault();
+                    if (prop != null)
+                    {
+                        idict.TryGetValue(prop, out value);
+                    }
+                }
+            }
+            else if (instance is IDictionary dict)
+            {
+                foreach (var p in dict.Keys)
+                {
+                    value = dict[property];
+                }
+            }
+            else if (instance is JObject jobj)
+            {
+                value = jobj.GetValue(property, StringComparison.CurrentCultureIgnoreCase);
+            }
+            else
+            {
+                // Use reflection
+                var type = instance.GetType();
+                var prop = type.GetProperties().Where(p => p.Name.ToLower() == property).SingleOrDefault();
+                if (prop != null)
+                {
+                    value = prop.GetValue(instance);
+                }
+            }
+
+            value = ResolveValue(value);
+
+            return (value, error);
+        }
+
+        private object ResolveValue(object obj)
+        {
+            object value;
+            if (!(obj is JValue jval))
+            {
+                value = obj;
+            }
+            else
+            {
+                value = jval.Value;
+                if (jval.Type == JTokenType.Integer)
+                {
+                    value = jval.ToObject<int>();
+                }
+                else if (jval.Type == JTokenType.String)
+                {
+                    value = jval.ToObject<string>();
+                }
+                else if (jval.Type == JTokenType.Boolean)
+                {
+                    value = jval.ToObject<bool>();
+                }
+                else if (jval.Type == JTokenType.Float)
+                {
+                    value = jval.ToObject<float>();
+                }
+            }
+            return value;
+        }
+    }
+}
---
+++ libraries/Microsoft.Bot.Builder.Expressions/IMemoryScopeManager.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Microsoft.Bot.Builder.Expressions
+{
+    // the memory scope manager for the evaluation process an expression
+    // which controls the memory scope access and update
+    public interface IMemoryScopeManager
+    {
+        // resolve a value for a given path, the path can be a simple indenfiter like "a"
+        // or a combined path like "a.b", "a.b[2]", "a.b[2].c"
+        object GetValue(string path);
+
+        // set a value to a given path
+        void SetValue(string path);
+
+        // push a new scope when a sub-evaluation
+        void PushScope(object scope);
+
+        // pop scope when sub-evaluation finished
+        void PopScope();
+    }
+}
