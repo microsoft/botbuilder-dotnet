0082b6586e95a80335329357eabbff22ca75d362
stevengum <14935595+stevengum@users.noreply.github.com>
2019-09-18 15:30:22 -0700

    add handlers for ActionBasedMessagingExtensionBot

--- tests/Teams/ActionBasedMessagingExtension/Bots/ActionBasedMessagingExtensionBot.cs
+++ tests/Teams/ActionBasedMessagingExtension/Bots/ActionBasedMessagingExtensionBot.cs
@@ -8,6 +8,7 @@ using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Teams;
 using Microsoft.Bot.Schema;
 using Microsoft.Bot.Schema.Teams;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
@@ -16,6 +17,56 @@ namespace Microsoft.BotBuilderSamples.Bots
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
             await turnContext.SendActivityAsync(MessageFactory.Text($"echo: {turnContext.Activity.Text}"), cancellationToken);
+        }
+
+        protected override async Task<MessagingExtensionActionResponse> OnTeamsMessagingExtensionSubmitActionAsync(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action, CancellationToken cancellationToken)
+        {
+            var response = action;
+            var t = "hello";
+            // Need to convert action.data to a base class that has Id:
+            // 
+            switch (action.CommandId)
+            {
+                case "createCard":
+                    return HandleCreateCardCommands(turnContext, action);
+
+                case "shareMessage":
+                    return HandleShareMessageCommand(turnContext, action);
+                default:
+                    return new MessagingExtensionActionResponse();
+            }
+        }
+
+        private MessagingExtensionActionResponse HandleCreateCardCommands(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action)
+        {
+            var actionData = (action.Data as JObject).ToObject<CreateCardData>();
+            var response = new MessagingExtensionActionResponse
+            {
+                ComposeExtension = new MessagingExtensionResult
+                {
+                    AttachmentLayout = "list",
+                    Type = "list",
+                },
+            };
+
+            var card = new HeroCard();
+
+            var attachments = new List<MessagingExtensionAttachment>();
+            attachments.Add(new MessagingExtensionAttachment
+            {
+                Content = card,
+                ContentType = HeroCard.ContentType,
+                Preview = card.ToAttachment(),
+            });
+
+            return response;
+        }
+
+        private MessagingExtensionActionResponse HandleShareMessageCommand(ITurnContext<IInvokeActivity> turnContext, MessagingExtensionAction action)
+        {
+            var response = new MessagingExtensionActionResponse();
+
+            return response;
         }
     }
 }
