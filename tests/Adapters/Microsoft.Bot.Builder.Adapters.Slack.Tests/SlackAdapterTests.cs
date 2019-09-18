5c9866fb0a553c2d06a043548262bea122e424cd
Gaspar Acevedo <gaspar.acevedo@southworks.com>
2019-09-18 12:36:24 -0300

    Slack Adapter: Add Unit Test project
        - Create SlackAdapter Tests project.
    
        - Add SlackAdapterTests file.
    
        - Add first test to SlackAdapterTests.

---
+++ tests/Adapters/Microsoft.Bot.Builder.Adapters.Slack.Tests/SlackAdapterTests.cs
@@ -0,0 +1,17 @@
+﻿// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.Adapters.Slack.Tests
+{
+    public class SlackAdapterTests
+    {
+        [Fact]
+        public void Constructor_Should_Fail_With_Null_Options()
+        {
+            Assert.Throws<ArgumentNullException>(() => new SlackAdapter(null));
+        }
+    }
+}
