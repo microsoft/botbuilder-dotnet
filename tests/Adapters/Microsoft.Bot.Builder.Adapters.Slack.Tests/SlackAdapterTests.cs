3307633d14c77da6ee0807837a4290f1030877e1
Gaspar Acevedo <gaspar.acevedo@southworks.com>
2019-09-18 14:06:24 -0300

    SlackAdapter: Add constructor tests.

--- tests/Adapters/Microsoft.Bot.Builder.Adapters.Slack.Tests/SlackAdapterTests.cs
+++ tests/Adapters/Microsoft.Bot.Builder.Adapters.Slack.Tests/SlackAdapterTests.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using Moq;
 using Xunit;
 
 namespace Microsoft.Bot.Builder.Adapters.Slack.Tests
@@ -12,6 +13,29 @@ namespace Microsoft.Bot.Builder.Adapters.Slack.Tests
         public void Constructor_Should_Fail_With_Null_Options()
         {
             Assert.Throws<ArgumentNullException>(() => new SlackAdapter(null));
+        }
+
+        [Fact]
+        public void Constructor_Should_Fail_With_Null_Security_Mechanisms()
+        {
+            var slackAdapterOptions = new SlackAdapterOptions()
+            {
+                VerificationToken = null,
+                ClientSigningSecret = null,
+            };
+
+            Assert.Throws<Exception>(() => new SlackAdapter(slackAdapterOptions));
+        }
+
+        [Fact]
+        public void Constructor_Succeeds()
+        {
+            var options = new Mock<SlackAdapterOptions>();
+            options.Object.VerificationToken = "VerificationToken";
+            options.Object.ClientSigningSecret = "ClientSigningSecret";
+            options.Object.BotToken = "BotToken";
+
+            Assert.NotNull(new SlackAdapter(options.Object));
         }
     }
 }
