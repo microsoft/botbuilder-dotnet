70c1be73d208fde149935c87e6ccf23c5b8aff87
virtual-josh <ratliff.joshua@gmail.com>
2019-09-19 09:24:35 -0700

    adding proactive message bot

---
+++ tests/Teams/ProactiveMessages/Bots/ProactiveMessageBot.cs
@@ -0,0 +1,95 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class ProactiveMessageBot : TeamsActivityHandler
+    {
+        private string _appId;
+
+        public ProactiveMessageBot(IConfiguration configuration) 
+        {
+            _appId = configuration["MicrosoftAppId"];
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var connectorClient = turnContext.TurnState.Get<IConnectorClient>();
+            var connector = new ConnectorClient(connectorClient.Credentials);
+
+             connector.BaseUri = new Uri(turnContext.Activity.ServiceUrl);
+
+            var parameters = new ConversationParameters
+            {
+                Bot = turnContext.Activity.From,
+                Members = new ChannelAccount[] { turnContext.Activity.From },
+                ChannelData = new TeamsChannelData
+                {
+                    Tenant = new TenantInfo
+                    {
+                        Id = turnContext.Activity.Conversation.TenantId,
+                    },
+                },
+            };
+
+            var converationReference = await connector.Conversations.CreateConversationAsync(parameters);
+            var proactiveMessage = MessageFactory.Text($"Hello {turnContext.Activity.From.Name}. You sent me a message. This is a proactive responsive message.");
+            proactiveMessage.From = turnContext.Activity.From;
+            proactiveMessage.Conversation = new ConversationAccount
+            {
+                Id = converationReference.Id.ToString(),
+            };
+
+            await connector.Conversations.SendToConversationAsync(proactiveMessage, cancellationToken);
+
+            await turnContext.Adapter.ContinueConversationAsync(_appId, turnContext.Activity.GetConversationReference(), BotOnTurn, cancellationToken);
+        }
+
+        private async Task BotOnTurn (ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync("Proactive response to the thread.");
+        }
+
+        protected override async Task OnTeamsMembersAddedAsync(IList<ChannelAccount> membersAdded, TeamInfo teamInfo, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                var replyActivity = MessageFactory.Text($"{member.Id} was added to the team");
+                replyActivity.ApplyConversationReference(turnContext.Activity.GetConversationReference());
+
+                var channelId = turnContext.Activity.Conversation.Id.Split(";")[0];
+                replyActivity.Conversation.Id = channelId;
+                var resourceResponse = await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+            }
+        }
+
+        protected override async Task OnTeamsMembersRemovedAsync(IList<ChannelAccount> membersAdded, TeamInfo teamInfo, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                var replyActivity = MessageFactory.Text($"{member.Id} was removed to the team");
+                replyActivity.ApplyConversationReference(turnContext.Activity.GetConversationReference());
+
+                var channelId = turnContext.Activity.Conversation.Id.Split(";")[0];
+                replyActivity.Conversation.Id = channelId;
+                var resourceResponse = await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+            }
+        }
+    }
+}
