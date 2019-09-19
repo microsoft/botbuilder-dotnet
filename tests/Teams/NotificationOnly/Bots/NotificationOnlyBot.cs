ae239f6ad590f6b2ecb32b651c2c1f38bf9986c8
virtual-josh <ratliff.joshua@gmail.com>
2019-09-19 13:34:46 -0700

    updating bot file

--- tests/Teams/NotificationOnly/Bots/NotificationOnlyBot.cs
+++ tests/Teams/NotificationOnly/Bots/NotificationOnlyBot.cs
@@ -39,5 +39,31 @@ namespace Microsoft.BotBuilderSamples.Bots
                 var resourceResponse = await turnContext.SendActivityAsync(replyActivity, cancellationToken);
             }
         }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersAdded)
+            {
+                var replyActivity = MessageFactory.Text($"{member.Id} was added to the team");
+                replyActivity.ApplyConversationReference(turnContext.Activity.GetConversationReference());
+
+                var channelId = turnContext.Activity.Conversation.Id.Split(";")[0];
+                replyActivity.Conversation.Id = channelId;
+                var resourceResponse = await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+            }
+        }
+
+        protected override async Task OnMembersRemovedAsync(IList<ChannelAccount> membersRemoved, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in membersRemoved)
+            {
+                var replyActivity = MessageFactory.Text($"{member.Id} was removed to the team");
+                replyActivity.ApplyConversationReference(turnContext.Activity.GetConversationReference());
+
+                var channelId = turnContext.Activity.Conversation.Id.Split(";")[0];
+                replyActivity.Conversation.Id = channelId;
+                var resourceResponse = await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+            }
+        }
     }
 }
