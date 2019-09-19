46552d168329baa2eaee52bbd08d700acadd6f4d
Steven Ickman <stevenic@microsoft.com>
2019-09-19 15:53:39 -0700

    Dropped turn.stepCount in favor of turn.interrupted

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/AdaptiveDialog.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/AdaptiveDialog.cs
@@ -259,6 +259,15 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
                             handled = await this.ProcessEventAsync(sequenceContext, dialogEvent: recognizedIntentEvent, preBubble: true, cancellationToken: cancellationToken).ConfigureAwait(false);
                         }
 
+                        // Has an interruption occured?
+                        // - Setting this value to true causes any running inputs to re-prompt when they're
+                        //   continued.  The developer can clear this flag if they want the input to instead
+                        //   process the users uterrance when its continued.
+                        if (handled)
+                        {
+                            sequenceContext.State.SetValue(TurnPath.INTERRUPTED, true);
+                        }
+
                         break;
                 }
             }
@@ -289,6 +298,15 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
                             {
                                 handled = false;
                             }
+                        }
+
+                        // Has an interruption occured?
+                        // - Setting this value to true causes any running inputs to re-prompt when they're
+                        //   continued.  The developer can clear this flag if they want the input to instead
+                        //   process the users uterrance when its continued.
+                        if (handled)
+                        {
+                            sequenceContext.State.SetValue(TurnPath.INTERRUPTED, true);
                         }
 
                         break;
@@ -347,12 +365,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
                     // Call begin dialog on our next step, passing the effective options we computed
                     result = await action.BeginDialogAsync(nextAction.DialogId, effectiveOptions, cancellationToken).ConfigureAwait(false);
                 }
-
-                // Increment turns step count
-                // This helps dialogs being resumed from an interruption to determine if they
-                // should re-prompt or not.
-                var stepCount = sequenceContext.State.GetValue<int>(TurnPath.STEPCOUNT, () => 0);
-                sequenceContext.State.SetValue(TurnPath.STEPCOUNT, stepCount + 1);
 
                 // Is the step waiting for input or were we cancelled?
                 if (result.Status == DialogTurnStatus.Waiting || this.GetUniqueInstanceId(sequenceContext) != instanceId)
