6bba2c55abdd09b064d4c764624df2e9403efb6c
virtual-josh <ratliff.joshua@gmail.com>
2019-09-20 09:12:17 -0700

    adding example when the Sample field isn't populated

--- tests/Teams/OneOnOneAlertBot/Bots/OneOnOneAlertBot.cs
+++ tests/Teams/OneOnOneAlertBot/Bots/OneOnOneAlertBot.cs
@@ -14,24 +14,46 @@ namespace Microsoft.BotBuilderSamples.Bots
     using Microsoft.Bot.Connector.Teams;
     using Microsoft.Bot.Schema;
     using Microsoft.Bot.Schema.Teams;
- 
+    using Microsoft.EntityFrameworkCore.Migrations;
 
     public class OneOnOneAlertBot : TeamsActivityHandler
     {
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var heroCard = new HeroCard(text: "You will receive a notification in your Activity feed.");
-            var msg = MessageFactory.Attachment(heroCard.ToAttachment());
-            msg.Summary = "This text will only show if the activty has a card attachment";
-            msg.ChannelData = new TeamsChannelData
+            if (turnContext.Activity.Text == "card")
             {
-                Notification = new NotificationInfo
+                var heroCard = new HeroCard(text: "You will receive a notification in your Activity feed.");
+                var msg = MessageFactory.Attachment(heroCard.ToAttachment());
+                msg.Summary = "This text will only show if the activty has a card attachment";
+                msg.ChannelData = new TeamsChannelData
                 {
-                    Alert = true,
-                },
-            };
+                    Notification = new NotificationInfo
+                    {
+                        Alert = true,
+                    },
+                };
 
-            await turnContext.SendActivityAsync(msg, cancellationToken);
+                await turnContext.SendActivityAsync(msg, cancellationToken);
+            }
+            else if (turnContext.Activity.Text == "message")
+            {
+                var msg = MessageFactory.Text("you will receive a notification in your Activity feed.");
+                msg.Summary = "You won't see this text";
+                msg.ChannelData = new TeamsChannelData
+                {
+                    Notification = new NotificationInfo
+                    {
+                        Alert = true,
+                    },
+                };
+
+                await turnContext.SendActivityAsync(msg, cancellationToken);
+            }
+            else
+            {
+                var msg = MessageFactory.Text("Send me \"card\" to receive a card. Send me \"message\" to receive a message");
+                await turnContext.SendActivityAsync(msg, cancellationToken);
+            }
         }
     }
 }
