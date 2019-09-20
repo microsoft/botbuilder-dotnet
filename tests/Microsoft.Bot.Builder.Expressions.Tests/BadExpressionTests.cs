95c75a8b576b048a21b9bde0ed7842675ac9c57e
hond <hond@microsoft.com>
2019-09-20 16:11:15 +0800

    revert

--- tests/Microsoft.Bot.Builder.Expressions.Tests/BadExpressionTests.cs
+++ tests/Microsoft.Bot.Builder.Expressions.Tests/BadExpressionTests.cs
@@ -359,7 +359,7 @@ namespace Microsoft.Bot.Builder.Expressions.Tests
 
             #region SetPathToValue tests
             Test("setPathToValue(2+3, 4)"), // Not a real path
-            Test("setPathToValue(a)"), // Missing value
+            Test("setPathToValue(a)") // Missing value
             #endregion
         };
 
