5ca424540a7e9e884bfeb69df751eb235cc1c631
virtual-josh <ratliff.joshua@gmail.com>
2019-09-21 10:19:59 -0700

    adding Teams helpers

---
+++ tests/Teams/Helpers/Bots/HelperBot.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Helpers;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class HelperBot : TeamsActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            turnContext.Activity.RemoveRecipientMention();
+            if (turnContext.Activity.Text == "notify")
+            {
+                var msg = MessageFactory.Text("This message will contain a notification");
+                msg.NotifyUser();
+                msg.ApplyConversationReference(turnContext.Activity.GetConversationReference());
+
+                await turnContext.SendActivityAsync(msg, cancellationToken);
+            }
+            else if (turnContext.Activity.Text == "get general channel")
+            {
+                var msg = MessageFactory.Text($"The ID of the general channel is {turnContext.Activity.GetGeneralChannel().Id}");
+                msg.ApplyConversationReference(turnContext.Activity.GetConversationReference());
+                await turnContext.SendActivityAsync(msg, cancellationToken);
+            }
+            else if (turnContext.Activity.Text == "send msg to general channel")
+            {
+                var msg = MessageFactory.Text("This message will appear in the teams general channel");
+                msg.ApplyConversationReference(turnContext.Activity.GetConversationReference());
+                turnContext.Activity.AddressMessageToTeamsGeneralChannel();
+                msg.ChannelData = turnContext.Activity.ChannelData;
+                await turnContext.SendActivityAsync(msg, cancellationToken);
+            }
+            else
+            {
+                var msg = MessageFactory.Text("Send me \"notify\" to get a notificaiton. Send me \"get general channel\" to get the ID of the general channel." +
+                    "Send me \"send msg to general channel\" to send a message to the general channel");
+                await turnContext.SendActivityAsync(msg, cancellationToken);
+            }
+        }
+    }
+}
---
+++ tests/Teams/Helpers/TeamsActivityExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+
+namespace Helpers
+{
+    public static class TeamsActivityExtensions
+    {
+        public static IMessageActivity NotifyUser(this IMessageActivity activity)
+        {
+            if (activity.ChannelData == null)
+            {
+                TeamsChannelData channelData = new TeamsChannelData
+                {
+                    Notification = new NotificationInfo
+                    {
+                        Alert = true,
+                    },
+                };
+
+                activity.ChannelData = channelData;
+            }
+            else
+            {
+                TeamsChannelData channelData = activity.GetChannelData<TeamsChannelData>();
+                channelData.Notification = new NotificationInfo
+                {
+                    Alert = true,
+                };
+                activity.ChannelData = channelData;
+            }
+
+            return activity;
+        }
+
+        public static ChannelInfo GetGeneralChannel(this IMessageActivity activity)
+        {
+            var channelData = activity.GetChannelData<TeamsChannelData>();
+
+            if (channelData == null)
+            {
+                throw new ArgumentNullException("The Teams ChannelData object is null.");
+            }
+
+            if (channelData.Team == null)
+            {
+                throw new ArgumentNullException("The Team proprty in the ChannelData object is null");
+            }
+
+            return new ChannelInfo { Id = channelData.Team.Id };
+
+        }
+
+        public static IMessageActivity AddressMessageToTeamsGeneralChannel(this IMessageActivity activity)
+        {
+            var channelData = activity.GetChannelData<TeamsChannelData>();
+            if (channelData == null)
+            {
+                throw new ArgumentNullException("The Teams ChannelData object is null.");
+            }
+
+            if (channelData.Team == null)
+            {
+                throw new ArgumentNullException("The Teams ChannelData Team properity is null.");
+            }
+
+            channelData.Channel = activity.GetGeneralChannel();
+            activity.Conversation.Id = channelData.Team.Id;
+
+            return activity;
+        }
+    }
+}
