15ab68a04a9ce211a444b3f1a28dc0c55e2a06f1
John Taylor <johtaylo@microsoft.com>
2019-09-24 15:53:43 -0700

    add teams extensions

---
+++ libraries/Microsoft.Bot.Builder.Teams/TeamsActivityExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+
+namespace Microsoft.Bot.Builder.Teams
+{
+    public static class TeamsActivityExtensions
+    {
+        public static void NotifyUser(this Activity activity)
+        {
+            var teamsChannelData = activity.ChannelData as TeamsChannelData;
+            if (teamsChannelData == null)
+            {
+                teamsChannelData = new TeamsChannelData();
+                activity.ChannelData = teamsChannelData;
+            }
+
+            teamsChannelData.Notification = new NotificationInfo
+            {
+                Alert = true,
+            };
+        }
+    }
+}
