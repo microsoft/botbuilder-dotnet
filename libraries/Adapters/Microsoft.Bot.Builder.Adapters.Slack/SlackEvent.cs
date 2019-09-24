f7468bdce3b5625a7977893943af61841fd8ad54
Cecilia Avila <cecilia.avila@southworks.com>
2019-09-24 10:26:23 -0300

    Create classes to deserialize the request's body

---
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Slack/SlackEvent.cs
@@ -0,0 +1,65 @@
+﻿// Copyright(c) Microsoft Corporation.All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Adapters.Slack
+{
+    public class SlackEvent
+    {
+        [JsonProperty(PropertyName = "client_msg_id")]
+        public string ClientMsgId { get; set; }
+
+        [JsonProperty(PropertyName = "type")]
+        public string Type { get; set; }
+
+        [JsonProperty(PropertyName = "subtype")]
+        public string SubType { get; set; }
+
+        [JsonProperty(PropertyName = "text")]
+        public string Text { get; set; }
+
+        [JsonProperty(PropertyName = "ts")]
+        public string TimeStamp { get; set; }
+
+        [JsonProperty(PropertyName = "team")]
+        public string Team { get; set; }
+
+        [JsonProperty(PropertyName = "channel")]
+        public string Channel { get; set; }
+
+        [JsonProperty(PropertyName = "channel_id")]
+        public string ChannelId { get; set; }
+
+        [JsonProperty(PropertyName = "event_ts")]
+        public string EventTS { get; set; }
+
+        [JsonProperty(PropertyName = "channel_type")]
+        public string ChannelType { get; set; }
+
+        [JsonProperty(PropertyName = "thread_ts")]
+        public string ThreadTS { get; set; }
+
+        [JsonProperty(PropertyName = "team_id")]
+        public string TeamId { get; set; }
+
+        [JsonProperty(PropertyName = "user")]
+        public string User { get; set; }
+
+        [JsonProperty(PropertyName = "user_id")]
+        public string UserId { get; set; }
+
+        [JsonProperty(PropertyName = "bot_id")]
+        public string BotId { get; set; }
+
+        [JsonProperty(PropertyName = "actions")]
+        public List<string> Actions { get; set; }
+
+        [JsonProperty(PropertyName = "item")]
+        public string Item { get; set; }
+
+        [JsonProperty(PropertyName = "item_channel")]
+        public string ItemChannel { get; set; }
+    }
+}
