a6b6e092320e2bbc5708904a3be565da0654194c
Daniel Evans <dev@microsoft.com>
2019-09-26 12:56:44 -0700

    Add test projects

---
+++ Microsoft.Bot.Builder.StreamingExtensions.tests/StreamingRequestHandlerTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.StreamingExtensions.Tests
+{
+    public class StreamingRequestHandlerTests
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
