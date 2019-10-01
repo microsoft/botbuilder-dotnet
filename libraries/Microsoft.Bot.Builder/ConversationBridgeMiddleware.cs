e1a3b1da74b702a1878b98a241f7c8b1e655d4bb
Gabo Gilabert <ggilaber@microsoft.com>
2019-10-01 12:35:23 -0400

    Ported Skills Stuff from steve

---
+++ libraries/Microsoft.Bot.Builder/ConversationBridgeMiddleware.cs
@@ -0,0 +1,157 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder
+{
+    public delegate Task<ConversationBridgeActions> ConversationBridgeFilter(ITurnContext turnContext);
+
+    public delegate Task<InvokeResponse> ConversationBridgeCommand(ITurnContext turnContext, object args, CancellationToken cancellationToken);
+
+    public enum ConversationBridgeActions
+    {
+        /// <summary>
+        /// Rats.
+        /// </summary>
+        Block,
+
+        /// <summary>
+        /// Pigs.
+        /// </summary>
+        Forward,
+
+        /// <summary>
+        /// Cats.
+        /// </summary>
+        PassThrough,
+    }
+
+    public class ConversationBridgeMiddleware : IMiddleware
+    {
+        private static readonly ConversationBridgeFilter _defaultFilter = context =>
+        {
+            var activity = context.Activity;
+            if (activity.RelatesTo != null && activity.Type != ActivityTypes.EndOfConversation)
+            {
+                return Task.FromResult(ConversationBridgeActions.Forward);
+            }
+
+            return Task.FromResult(ConversationBridgeActions.PassThrough);
+        };
+
+        private readonly Dictionary<string, ConversationBridgeCommand> _commands = new Dictionary<string, ConversationBridgeCommand>();
+        private readonly ConversationBridgeFilter _filter;
+
+        public ConversationBridgeMiddleware()
+            : this(_defaultFilter)
+        {
+        }
+
+        public ConversationBridgeMiddleware(ConversationBridgeFilter filter, bool allowAdapterCalls = true)
+        {
+            _filter = filter;
+
+            // Add adapter commands
+            if (allowAdapterCalls)
+            {
+                AddCommand("BotAdapter.sendActivities", async (context, args, cancellationToken) =>
+                {
+                    // TODO: find a better solution for (Dictionary<string, object>)args.
+                    var argsDictionary = (Dictionary<string, object>)args;
+                    var responses = await context.Adapter.SendActivitiesAsync(context, (Activity[])argsDictionary["activities"], cancellationToken).ConfigureAwait(false);
+                    return new InvokeResponse()
+                    {
+                        Status = 200,
+                        Body = responses,
+                    };
+                });
+                AddCommand("BotAdapter.updateActivity", async (context, args, cancellationToken) =>
+                {
+                    var argsDictionary = (Dictionary<string, object>)args;
+                    await context.Adapter.UpdateActivityAsync(context, (Activity)argsDictionary["activity"], cancellationToken).ConfigureAwait(false);
+                    return new InvokeResponse
+                    {
+                        Status = 200,
+                    };
+                });
+                AddCommand("BotAdapter.deleteActivity", async (context, args, cancellationToken) =>
+                {
+                    var argsDictionary = (Dictionary<string, object>)args;
+                    await context.Adapter.DeleteActivityAsync(context, (ConversationReference)argsDictionary["reference"], cancellationToken).ConfigureAwait(false);
+                    return new InvokeResponse
+                    {
+                        Status = 200,
+                    };
+                });
+            }
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default)
+        {
+            // Filter incoming activity
+            var conversationBridgeActions = await _filter(turnContext).ConfigureAwait(false);
+            switch (conversationBridgeActions)
+            {
+                case ConversationBridgeActions.Forward:
+                    if (turnContext.Activity.Type == ActivityTypes.Invoke)
+                    {
+                        // Invoke command
+                        await OnInvokeCommand(turnContext, cancellationToken).ConfigureAwait(false);
+                    }
+                    else
+                    {
+                        // Forward activity
+                        await OnForwardActivity(turnContext, cancellationToken).ConfigureAwait(false);
+                    }
+
+                    break;
+                case ConversationBridgeActions.PassThrough:
+                    await next(cancellationToken).ConfigureAwait(false);
+                    break;
+            }
+        }
+
+        private async Task OnForwardActivity(ITurnContext context, CancellationToken cancellationToken)
+        {
+            // Clone activity and re-address
+            var clone = JsonConvert.DeserializeObject<Activity>(JsonConvert.SerializeObject(context.Activity));
+
+            // TODO: Not sure how to remap this JS code to C#, need to check with steve.
+            // clone.ApplyConversationReference(new ConversationReference())
+            // Clone activity and re-address
+            // const clone = Object.assign({}, context.activity);
+            // TurnContext.applyConversationReference(clone, clone.relatesTo);
+            // delete clone.relatesTo;
+
+            // Forward to adapter for delivery
+            await context.Adapter.SendActivitiesAsync(context, new Activity[] { clone }, cancellationToken).ConfigureAwait(false);
+        }
+
+        private async Task OnInvokeCommand(ITurnContext context, CancellationToken cancellationToken)
+        {
+            // Lookup and invoke command
+            // - Commands are always processed as 'invoke' activities
+            var activity = context.Activity;
+            var command = _commands[activity.Name];
+            if (command != null)
+            {
+                var response = await command(context, activity.Value, cancellationToken).ConfigureAwait(false);
+                await context.SendActivityAsync(new Activity(type: "invokeResponse", value: response), cancellationToken).ConfigureAwait(false);
+            }
+            else
+            {
+                await context.SendActivityAsync(new Activity(type: "invokeResponse", value: new InvokeResponse { Status = 404 }), cancellationToken).ConfigureAwait(false);
+            }
+        }
+
+        private void AddCommand(string name, ConversationBridgeCommand command)
+        {
+            _commands.Add(name, command);
+        }
+    }
+}
---
+++ libraries/Microsoft.Bot.Builder/MultiAdapter.cs
@@ -0,0 +1,91 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Builder
+{
+    public class MultiAdapter : BotAdapter
+    {
+        private readonly Dictionary<string, BotAdapter> _adapterMap = new Dictionary<string, BotAdapter>();
+
+        public BotAdapter AddAdapter(BotAdapter adapter)
+        {
+            return AddAdapter("*", adapter);
+        }
+
+        public BotAdapter AddAdapter(string adapterId, BotAdapter adapter)
+        {
+            // ensure unique and add
+            if (_adapterMap.ContainsKey(adapterId))
+            {
+                throw new Exception($"MultiAdapter.AddAdapter(): an adapter with a channel id of '{adapterId}' has already been added.");
+            }
+
+            _adapterMap.Add(adapterId, adapter);
+
+            // Chain middleware stacks together
+            // TODO: we don't have MiddlewareHandler in C# (JS code from steve below
+            // adapter.use(async (context: TurnContext, next: () => Promise<void>) => {
+            //     await this.middleware.run(context, next)
+            // });
+            return this;
+        }
+
+        public BotAdapter GetAdapter(string channelId)
+        {
+            if (_adapterMap.ContainsKey(channelId))
+            {
+                return _adapterMap[channelId];
+            }
+
+            if (_adapterMap.ContainsKey("*"))
+            {
+                return _adapterMap[channelId];
+            }
+
+            throw new Exception($"MultiAdapter.GetAdapter(): an adapter with a channel id of '{channelId}' not found.");
+        }
+
+        public override async Task<ResourceResponse[]> SendActivitiesAsync(ITurnContext turnContext, Activity[] activities, CancellationToken cancellationToken)
+        {
+            var responses = new List<ResourceResponse>();
+            foreach (var activity in activities)
+            {
+                // Route to appropriate adapter
+                var adapter = GetAdapter(activity.ChannelId);
+                var ar = await adapter.SendActivitiesAsync(turnContext, activities, cancellationToken).ConfigureAwait(false);
+
+                // TODO: not sure if this is equivalent to the logic steve has in JS
+                responses.AddRange(ar);
+            }
+
+            return responses.ToArray();
+        }
+
+        public override async Task<ResourceResponse> UpdateActivityAsync(ITurnContext turnContext, Activity activity, CancellationToken cancellationToken)
+        {
+            // Route to appropriate adapter
+            var adapter = GetAdapter(activity.ChannelId);
+            return await adapter.UpdateActivityAsync(turnContext, activity, cancellationToken).ConfigureAwait(false);
+        }
+
+        public override async Task DeleteActivityAsync(ITurnContext turnContext, ConversationReference reference, CancellationToken cancellationToken)
+        {
+            // Route to appropriate adapter
+            var adapter = GetAdapter(reference.ChannelId);
+            await adapter.DeleteActivityAsync(turnContext, reference, cancellationToken).ConfigureAwait(false);
+        }
+
+        public override async Task ContinueConversationAsync(string botId, ConversationReference reference, BotCallbackHandler callback, CancellationToken cancellationToken)
+        {
+            // Route to appropriate adapter
+            var adapter = GetAdapter(reference.ChannelId);
+            await adapter.ContinueConversationAsync(botId, reference, callback, cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
