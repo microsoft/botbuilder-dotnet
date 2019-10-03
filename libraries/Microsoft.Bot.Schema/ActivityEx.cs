0fdf67713a8c27b68b4d44d7cbf4b7e97477b80b
bothosting.runtime@bf.ai <bothosting.runtime@bf.ai>
2019-10-03 13:18:35 -0700

    Lint

--- libraries/Microsoft.Bot.Schema/ActivityEx.cs
+++ libraries/Microsoft.Bot.Schema/ActivityEx.cs
@@ -511,7 +511,7 @@ namespace Microsoft.Bot.Schema
         /// Determine if the Activity was sent via an Http/Https connection or Streaming
         /// This can be determined by looking at the ServiceUrl property:
         /// (1) All channels that send messages via http/https are not streaming
-        /// (2) Channels that send messages via streaming have a ServiceUrl that does not begin with http/https
+        /// (2) Channels that send messages via streaming have a ServiceUrl that does not begin with http/https.
         /// </summary>
         /// <returns>True if the Activity originated from a streaming connection.</returns>
         public bool IsFromStreamingConnection()
