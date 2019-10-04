81ef1d1acd755a80e0b861f374aff54fe1a7d9e2
Daniel Evans <dev@microsoft.com>
2019-10-03 17:31:19 -0700

    some more tests

---
+++ tests/Microsoft.Bot.Streaming.Tests/Utilities/MockBot.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Streaming;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Streaming.Tests.Utilities
+{
+    public class MockBot : IBot
+    {
+        private readonly DirectLineAdapter _adapter;
+        private readonly Func<Schema.Activity, Task<InvokeResponse>> _processActivityAsync;
+
+        public MockBot(Func<Schema.Activity, Task<InvokeResponse>> processActivityAsync, string pipeName = "testPipes", DirectLineAdapter adapter = null)
+        {
+            _processActivityAsync = processActivityAsync;
+            _adapter = adapter ?? new DirectLineAdapter(null, this, null);
+            _adapter.AddNamedPipeConnection(pipeName, this);
+        }
+
+        public List<Schema.Activity> ReceivedActivities { get; private set; } = new List<Schema.Activity>();
+
+        public List<Schema.Activity> SentActivities { get; private set; } = new List<Schema.Activity>();
+
+        public async Task<Schema.ResourceResponse> SendActivityAsync(Schema.Activity activity, List<AttachmentStream> attachmentStreams = null)
+        {
+            SentActivities.Add(activity);
+
+            var requestPath = $"/v3/conversations/{activity.Conversation?.Id}/activities/{activity.Id}";
+            var request = StreamingRequest.CreatePost(requestPath);
+            request.SetBody(activity);
+            attachmentStreams?.ForEach(a =>
+            {
+                var streamContent = new StreamContent(a.ContentStream);
+                streamContent.Headers.TryAddWithoutValidation(HeaderNames.ContentType, a.ContentType);
+                request.AddStream(streamContent);
+            });
+
+            var serverResponse = await _adapter.ProcessActivityForStreamingChannelAsync(activity, CancellationToken.None).ConfigureAwait(false);
+
+            if (serverResponse.Status == (int)HttpStatusCode.OK)
+            {
+                return JsonConvert.DeserializeObject<Schema.ResourceResponse>(serverResponse.Body.ToString());
+            }
+
+            throw new Exception("SendActivityAsync failed");
+        }
+
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default)
+        {
+            turnContext.SendActivityAsync(MessageFactory.Text($"Echo: {turnContext.Activity.Text}"), cancellationToken);
+
+            return;
+
+            // await SendActivityAsync(turnContext.Activity);
+        }
+
+        public Task<InvokeResponse> ProcessActivityAsync(Schema.Activity activity)
+        {
+            ReceivedActivities.Add(activity);
+
+            return _processActivityAsync(activity);
+        }
+    }
+}
---
+++ tests/Microsoft.Bot.Streaming.Tests/WebSocketTransportTests.cs
@@ -0,0 +1,141 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Streaming;
+using Microsoft.Bot.Streaming.Transport.NamedPipes;
+using Microsoft.Bot.Streaming.Transport.WebSockets;
+using Xunit;
+
+namespace Microsoft.Bot.Streaming.UnitTests
+{
+    public class WebSocketTransportTests
+    {
+        [Fact]
+        public async Task WebSocketServer_Connects()
+        {
+            var sock = new FauxSock();
+            var writer = new WebSocketServer(sock, new StreamingRequestHandler(null, new DirectLineAdapter(), sock));
+
+            writer.StartAsync();
+            Assert.True(writer.IsConnected);
+        }
+
+        [Fact]
+        public async Task WebSocketServer_BackAndForth()
+        {
+            var sock = new FauxSock();
+            var writer = new WebSocketServer(sock, new StreamingRequestHandler(null, new DirectLineAdapter(), sock));
+
+            writer.StartAsync();
+        }
+
+        [Fact]
+        public async Task WebSocketClient_ThrowsOnEmptyUrl()
+        {
+            Exception result = null;
+
+            try
+            {
+                var reader = new WebSocketClient(string.Empty);
+            }
+            catch (Exception ex)
+            {
+                result = ex;
+            }
+
+            Assert.IsType<ArgumentNullException>(result);
+        }
+
+        [Fact]
+        public async Task WebSocketClient_AcceptsAnyUrl()
+        {
+            Exception result = null;
+            WebSocketClient reader = null;
+            try
+            {
+                var webSocketClient = new WebSocketClient("fakeurl");
+                reader = webSocketClient;
+            }
+            catch (Exception ex)
+            {
+                result = ex;
+            }
+
+            reader.Dispose();
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public async Task WebSocketClient_ConnectThrowsIfUrlIsBad()
+        {
+            Exception result = null;
+            WebSocketClient reader = null;
+            IDictionary<string, string> fakeHeaders = new Dictionary<string, string>();
+            fakeHeaders.Add("authorization", "totally");
+            fakeHeaders.Add("channelId", "mtv");
+            try
+            {
+                var webSocketClient = new WebSocketClient("fakeurl");
+                reader = webSocketClient;
+                await reader.ConnectAsync(fakeHeaders);
+            }
+            catch (Exception ex)
+            {
+                result = ex;
+            }
+
+            reader.Dispose();
+            Assert.IsType<UriFormatException>(result);
+        }
+
+        private class FauxSock : WebSocket
+        {
+            public ArraySegment<byte> SentArray { get; set; }
+
+            public override WebSocketCloseStatus? CloseStatus => throw new NotImplementedException();
+
+            public override string CloseStatusDescription => throw new NotImplementedException();
+
+            public override WebSocketState State => throw new NotImplementedException();
+
+            public override string SubProtocol => throw new NotImplementedException();
+
+            public override void Abort()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override Task CloseAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override Task CloseOutputAsync(WebSocketCloseStatus closeStatus, string statusDescription, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void Dispose()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override Task<WebSocketReceiveResult> ReceiveAsync(ArraySegment<byte> buffer, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override Task SendAsync(ArraySegment<byte> buffer, WebSocketMessageType messageType, bool endOfMessage, CancellationToken cancellationToken)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
