e4bc9343fa7c61e14ef3f52b455a6e122f718d9e
Eric Dahlvang <v-eridah@microsoft.com>
2019-10-07 10:29:19 -0700

    ReplyToChannel cleanup

--- tests/Teams/ReplyToChannel/Bots/ReplyToChannel.cs
+++ tests/Teams/ReplyToChannel/Bots/ReplyToChannel.cs
@@ -1,24 +1,20 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Teams;
-using Microsoft.Bot.Connector;
-using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
-using Microsoft.Bot.Schema.Teams;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.BotBuilderSamples.Bots
 {
-    public class ActivityUpdateAndDeleteBot : ActivityHandler
+    public class ReplyToChannelBot : ActivityHandler
     {
         private string _botId;
 
-        public ActivityUpdateAndDeleteBot(IConfiguration configuration)
+        public ReplyToChannelBot(IConfiguration configuration)
         {
             _botId = configuration["MicrosoftAppId"];
         }
