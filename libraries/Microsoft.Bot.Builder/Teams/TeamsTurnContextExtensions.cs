88f5b590ab4ae95ee1ac8420641d1186eff94c62
John Taylor <johtaylo@microsoft.com>
2019-10-07 17:34:42 -0700

    add unit tests and remove dead code

--- libraries/Microsoft.Bot.Builder/Teams/TeamsTurnContextExtensions.cs
+++ libraries/Microsoft.Bot.Builder/Teams/TeamsTurnContextExtensions.cs
@@ -12,21 +12,6 @@ namespace Microsoft.Bot.Builder.Teams
 {
     public static class TeamsTurnContextExtensions
     {
-        public static async Task<ResourceResponse> TeamsSendToChannelAsync(this ITurnContext turnContext, string teamsChannelId, IActivity activity, CancellationToken cancellationToken = default)
-        {
-            var originalConversationId = turnContext.Activity.Conversation.Id;
-            try
-            {
-                turnContext.Activity.Conversation.Id = teamsChannelId;
-                var result = await turnContext.SendActivityAsync(activity, cancellationToken).ConfigureAwait(false);
-                return result;
-            }
-            finally
-            {
-                turnContext.Activity.Conversation.Id = originalConversationId;
-            }
-        }
-
         public static async Task<(ConversationReference conversationReference, string activityId)> TeamsCreateConversationAsync(this ITurnContext turnContext, string teamsChannelId, IActivity message, CancellationToken cancellationToken = default)
         {
             if (string.IsNullOrEmpty(teamsChannelId))
@@ -37,13 +22,6 @@ namespace Microsoft.Bot.Builder.Teams
             if (message == null)
             {
                 throw new ArgumentNullException(nameof(message));
-            }
-
-            var channelData = turnContext.Activity.GetChannelData<TeamsChannelData>();
-
-            if (channelData == null)
-            {
-                throw new InvalidOperationException("This method is only valid within the scope of MS Teams TurnContext.");
             }
 
             // TODO: do we need to add the tenantId into the TeamsChannelData
@@ -66,7 +44,7 @@ namespace Microsoft.Bot.Builder.Teams
             return (conversationReference, conversationResourceResponse.ActivityId);
         }
 
-        public static Task<ResourceResponse> TeamsSendToGeneralChannelAsync(this ITurnContext turnContext, IActivity activity, CancellationToken cancellationToken = default)
+        public static Task<(ConversationReference conversationReference, string activityId)> TeamsSendToGeneralChannelAsync(this ITurnContext turnContext, IActivity activity, CancellationToken cancellationToken = default)
         {
             // The Team Id is also the Id of the general channel
             var teamId = turnContext.Activity.TeamsGetTeamId();
@@ -76,7 +54,7 @@ namespace Microsoft.Bot.Builder.Teams
                 throw new Exception("The current Activity was not sent from a Teams Team.");
             }
 
-            return turnContext.TeamsSendToChannelAsync(teamId, activity, cancellationToken);
+            return turnContext.TeamsCreateConversationAsync(teamId, activity, cancellationToken);
         }
     }
 }
