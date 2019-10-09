833c641d31087c328f510aee071aab3ccde125f8
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-10-09 15:46:19 -0300

    Add FacebookWrapperTests class

---
+++ tests/Adapters/Microsoft.Bot.Builder.Adapters.Facebook.Tests/FacebookWrapperTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.Adapters.Facebook.Tests
+{
+    public class FacebookWrapperTests
+    {
+    }
+}
