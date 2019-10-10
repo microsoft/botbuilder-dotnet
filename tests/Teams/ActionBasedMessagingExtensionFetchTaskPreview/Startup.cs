9cc07c70d14c53236889fe8b819648b0eb3b317a
John Taylor <johtaylo@microsoft.com>
2019-10-10 12:49:48 -0700

    some renaming and removal

--- tests/Teams/ActionBasedMessagingExtensionFetchTaskPreview/Startup.cs
+++ tests/Teams/ActionBasedMessagingExtensionFetchTaskPreview/Startup.cs
@@ -26,11 +26,14 @@ namespace Microsoft.BotBuilderSamples
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
             // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-            services.AddTransient<IBot, HelperBot>();
+            services.AddTransient<IBot, ActionBasedMessagingExtensionFetchTaskBotPreview>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
