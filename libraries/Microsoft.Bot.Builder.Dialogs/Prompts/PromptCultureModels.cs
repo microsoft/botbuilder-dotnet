7561e31578012a4f25310359a408f22b7d901be6
mdrichardson <v-micric@microsoft.com>
2019-10-15 09:42:51 -0700

    touch

--- libraries/Microsoft.Bot.Builder.Dialogs/Prompts/PromptCultureModels.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/Prompts/PromptCultureModels.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.using System;
+// Licensed under the MIT License.
 
+using System;
 using System.Globalization;
 using System.Linq;
 using Microsoft.Recognizers.Text;
