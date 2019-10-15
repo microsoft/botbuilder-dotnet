15356fbf77665e541ea0198e0addc324b0dff54c
Steven Ickman <stevenic@microsoft.com>
2019-10-14 19:48:39 -0700

    Reworked InputDialog's core processing flow
    - Removed SetProperty hack to clear dialog stack after running.
    - InputDialogs recognize method now processes input in the cirrect order.
    - Tweak to the way interruptions are delt with
    - Updated unit tests.

--- tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/AdaptiveDialogTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/AdaptiveDialogTests.cs
@@ -1208,8 +1208,8 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
                             // request the active input step to re-process user input. 
                             new SetProperty()
                             {
-                                Property = "turn.processInput",
-                                Value = "true"
+                                Property = "turn.interrupted",
+                                Value = "false"
                             }
                         }
                     },
@@ -1304,8 +1304,8 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
                             // request the active input step to re-process user input. 
                             new SetProperty()
                             {
-                                Property = "turn.processInput",
-                                Value = "true"
+                                Property = "turn.interrupted",
+                                Value = "false"
                             }
                         }
                     },
@@ -1369,8 +1369,8 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
                             // request the active input step to re-process user input. 
                             new SetProperty()
                             {
-                                Property = "turn.processInput",
-                                Value = "true"
+                                Property = "turn.interrupted",
+                                Value = "false"
                             }
                         }
                     },
@@ -1382,8 +1382,8 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
                             new SendActivity("You said {turn.activity.text}"),
                             new SetProperty()
                             {
-                                Property = "turn.processInput",
-                                Value = "true"
+                                Property = "turn.interrupted",
+                                Value = "false"
                             }
                         }
                     }
@@ -1450,8 +1450,8 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
                             // request the active input step to re-process user input. 
                             new SetProperty()
                             {
-                                Property = "turn.processInput",
-                                Value = "true"
+                                Property = "turn.interrupted",
+                                Value = "false"
                             }
                         }
                     },
