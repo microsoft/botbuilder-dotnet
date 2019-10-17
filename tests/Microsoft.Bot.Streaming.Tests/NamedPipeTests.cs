ab05cc96c451ab0cb3f138c84e2be4d481c82ca3
Daniel Evans <dev@microsoft.com>
2019-10-17 13:54:55 -0700

    Massive overhaul to make test run on Linux.

--- tests/Microsoft.Bot.Streaming.Tests/NamedPipeTests.cs
+++ tests/Microsoft.Bot.Streaming.Tests/NamedPipeTests.cs
@@ -21,7 +21,7 @@ namespace Microsoft.Bot.Streaming.UnitTests
         {
             var pipeName = Guid.NewGuid().ToString();
             var readStream = new NamedPipeServerStream(pipeName, PipeDirection.In, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte, PipeOptions.WriteThrough | PipeOptions.Asynchronous);
-            var writeStream = new NamedPipeClientStream(Guid.NewGuid().ToString(), pipeName, PipeDirection.Out, PipeOptions.WriteThrough | PipeOptions.Asynchronous);
+            var writeStream = new NamedPipeClientStream(".", pipeName, PipeDirection.Out, PipeOptions.WriteThrough | PipeOptions.Asynchronous);
             new StreamingRequestHandler(new Microsoft.Bot.Streaming.UnitTests.Mocks.MockBot(), new BotFrameworkHttpAdapter(), pipeName);
             var reader = new NamedPipeClient(pipeName);
             var writer = new NamedPipeServer(pipeName, new StreamingRequestHandler(new MockBot(), new BotFrameworkHttpAdapter(), pipeName));
