a12f27e03be3d2e35ebacbbea520cbb820b8897a
IgorHrabrov <18480638+IgorHrabrov@users.noreply.github.com>
2019-11-19 16:20:31 +0200

    Fixed typos

--- libraries/Microsoft.Bot.Builder/PathUtils.cs
+++ libraries/Microsoft.Bot.Builder/PathUtils.cs
@@ -9,22 +9,22 @@ namespace Microsoft.Bot.Builder
         /// </summary>
         /// <remarks>
         /// path is from authored content which doesn't know what OS it is running on.
-        /// This method treats / and \ both as seperators regardless of OS, for windows that means / -> \ and for linux/mac \ -> /.
-        /// This allows author to use ../foo.lg or ..\foo.lg as equivelents for importing.
+        /// This method treats / and \ both as separators regardless of OS, for windows that means / -> \ and for linux/mac \ -> /.
+        /// This allows author to use ../foo.lg or ..\foo.lg as equivalents for importing.
         /// </remarks>
-        /// <param name="ambigiousPath">authoredPath.</param>
+        /// <param name="ambiguousPath">authoredPath.</param>
         /// <returns>path expressed as OS path.</returns>
-        public static string NormalizePath(string ambigiousPath)
+        public static string NormalizePath(string ambiguousPath)
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 // map linux/mac sep -> windows
-                return ambigiousPath.Replace("/", "\\");
+                return ambiguousPath.Replace("/", "\\");
             }
             else
             {
                 // map windows sep -> linux/mac
-                return ambigiousPath.Replace("\\", "/");
+                return ambiguousPath.Replace("\\", "/");
             }
         }
     }
