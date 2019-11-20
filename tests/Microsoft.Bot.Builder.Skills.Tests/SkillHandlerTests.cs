d5b331f9a3fb71e52551e3725f64790c897847d2
Gabo Gilabert <ggilaber@microsoft.com>
2019-11-20 08:57:52 -0500

    Fixed unit tests so the compile and run.

--- tests/Microsoft.Bot.Builder.Skills.Tests/SkillHandlerTests.cs
+++ tests/Microsoft.Bot.Builder.Skills.Tests/SkillHandlerTests.cs
@@ -12,11 +12,14 @@ using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Logging;
+using Moq;
 using Xunit;
 using Xunit.Sdk;
 
 namespace Microsoft.Bot.Builder.Skills.Tests
 {
+    // TODO: need to simplify this tests
     public class SkillHandlerTests
     {
         [Fact(Skip = "TODO: I moved this logic to ChannelServiceHandler.InvokeChannelApiAsync, rewrite this test once i finish with refactoring. Gabo")]
@@ -24,7 +27,7 @@ namespace Microsoft.Bot.Builder.Skills.Tests
         {
             var botAdapter = CreateAdapter("TestSkillAdapterInjectsMiddleware");
 
-            var skillClient = new SkillHandler(botAdapter, new CallbackBot());
+            var skillClient = new SkillHandler(botAdapter, new CallbackBot(), new Mock<ICredentialProvider>().Object, new AuthenticationConfiguration());
 
             Assert.Equal(1, botAdapter.MiddlewareSet.Count(s => s is ChannelApiMiddleware));
         }
@@ -42,7 +45,7 @@ namespace Microsoft.Bot.Builder.Skills.Tests
             var middleware = new AssertInvokeMiddleware(botAdapter, activityId);
             botAdapter.Use(middleware);
             var bot = new CallbackBot();
-            var skillClient = new SkillHandler(botAdapter, bot);
+            var skillClient = new SkillHandlerInstanceForTests(botAdapter, bot, new Mock<ICredentialProvider>().Object, new AuthenticationConfiguration());
 
             var sc = new SkillConversation
             {
@@ -55,35 +58,35 @@ namespace Microsoft.Bot.Builder.Skills.Tests
             claimsIdentity.AddClaim(new Claim(AuthenticationConstants.AppIdClaim, botId));
             claimsIdentity.AddClaim(new Claim(AuthenticationConstants.ServiceUrlClaim, botAdapter.Conversation.ServiceUrl));
 
-            object result = await skillClient.OnCreateConversationAsync(claimsIdentity, skillConversationId, new ConversationParameters());
+            object result = await skillClient.TestOnCreateConversationAsync(claimsIdentity, skillConversationId, new ConversationParameters());
             Assert.IsType<ConversationResourceResponse>(result);
             Assert.Equal(middleware.NewResourceId, ((ConversationResourceResponse)result).Id);
 
-            await skillClient.OnDeleteActivityAsync(claimsIdentity, skillConversationId, activityId);
+            await skillClient.TestOnDeleteActivityAsync(claimsIdentity, skillConversationId, activityId);
 
-            await skillClient.OnDeleteConversationMemberAsync(claimsIdentity, skillConversationId, "user2");
+            await skillClient.TestOnDeleteConversationMemberAsync(claimsIdentity, skillConversationId, "user2");
 
-            result = await skillClient.OnGetActivityMembersAsync(claimsIdentity, skillConversationId, activityId);
+            result = await skillClient.TestOnGetActivityMembersAsync(claimsIdentity, skillConversationId, activityId);
             Assert.IsAssignableFrom<IList<ChannelAccount>>(result);
 
-            result = await skillClient.OnGetConversationMembersAsync(claimsIdentity, skillConversationId);
+            result = await skillClient.TestOnGetConversationMembersAsync(claimsIdentity, skillConversationId);
             Assert.IsAssignableFrom<IList<ChannelAccount>>(result);
 
-            result = await skillClient.OnGetConversationPagedMembersAsync(claimsIdentity, skillConversationId);
+            result = await skillClient.TestOnGetConversationPagedMembersAsync(claimsIdentity, skillConversationId);
             Assert.IsType<PagedMembersResult>(result);
 
-            result = await skillClient.OnGetConversationPagedMembersAsync(claimsIdentity, skillConversationId, 10);
+            result = await skillClient.TestOnGetConversationPagedMembersAsync(claimsIdentity, skillConversationId, 10);
             Assert.IsType<PagedMembersResult>(result);
 
             var pagedMembersResult = (PagedMembersResult)result;
-            result = await skillClient.OnGetConversationPagedMembersAsync(claimsIdentity, skillConversationId, continuationToken: pagedMembersResult.ContinuationToken);
+            result = await skillClient.TestOnGetConversationPagedMembersAsync(claimsIdentity, skillConversationId, continuationToken: pagedMembersResult.ContinuationToken);
             Assert.IsType<PagedMembersResult>(result);
 
-            result = await skillClient.OnGetConversationsAsync(claimsIdentity, skillConversationId);
+            result = await skillClient.TestOnGetConversationsAsync(claimsIdentity, skillConversationId);
             Assert.IsType<ConversationsResult>(result);
 
             var conversationsResult = (ConversationsResult)result;
-            result = await skillClient.OnGetConversationsAsync(claimsIdentity, skillConversationId, continuationToken: conversationsResult.ContinuationToken);
+            result = await skillClient.TestOnGetConversationsAsync(claimsIdentity, skillConversationId, continuationToken: conversationsResult.ContinuationToken);
             Assert.IsType<ConversationsResult>(result);
 
             var msgActivity = Activity.CreateMessageActivity();
@@ -92,24 +95,24 @@ namespace Microsoft.Bot.Builder.Skills.Tests
             msgActivity.Recipient = botAdapter.Conversation.User;
             msgActivity.Text = "yo";
 
-            result = await skillClient.OnSendToConversationAsync(claimsIdentity, skillConversationId, (Activity)msgActivity);
+            result = await skillClient.TestOnSendToConversationAsync(claimsIdentity, skillConversationId, (Activity)msgActivity);
             Assert.IsType<ResourceResponse>(result);
             Assert.Equal(middleware.NewResourceId, ((ResourceResponse)result).Id);
             msgActivity.Id = ((ResourceResponse)result).Id;
 
-            result = await skillClient.OnReplyToActivityAsync(claimsIdentity, skillConversationId, activityId, (Activity)msgActivity);
+            result = await skillClient.TestOnReplyToActivityAsync(claimsIdentity, skillConversationId, activityId, (Activity)msgActivity);
             Assert.IsType<ResourceResponse>(result);
             Assert.Equal(middleware.NewResourceId, ((ResourceResponse)result).Id);
 
-            result = await skillClient.OnSendConversationHistoryAsync(claimsIdentity, skillConversationId, new Transcript());
+            result = await skillClient.TestOnSendConversationHistoryAsync(claimsIdentity, skillConversationId, new Transcript());
             Assert.IsType<ResourceResponse>(result);
             Assert.Equal(middleware.NewResourceId, ((ResourceResponse)result).Id);
 
-            result = await skillClient.OnUpdateActivityAsync(claimsIdentity, skillConversationId, activityId, (Activity)msgActivity);
+            result = await skillClient.TestOnUpdateActivityAsync(claimsIdentity, skillConversationId, activityId, (Activity)msgActivity);
             Assert.IsType<ResourceResponse>(result);
             Assert.Equal(middleware.NewResourceId, ((ResourceResponse)result).Id);
 
-            result = await skillClient.OnUploadAttachmentAsync(claimsIdentity, skillConversationId, new AttachmentData());
+            result = await skillClient.TestOnUploadAttachmentAsync(claimsIdentity, skillConversationId, new AttachmentData());
             Assert.IsType<ResourceResponse>(result);
             Assert.Equal(middleware.NewResourceId, ((ResourceResponse)result).Id);
         }
@@ -285,6 +288,77 @@ namespace Microsoft.Bot.Builder.Skills.Tests
 
                 // return next(cancellationToken);
                 return Task.CompletedTask;
+            }
+        }
+
+        /// <summary>
+        /// A helper class that provides public methods that allow us to test protected methods in <see cref="SkillHandler"/>.
+        /// </summary>
+        private class SkillHandlerInstanceForTests : SkillHandler
+        {
+            public SkillHandlerInstanceForTests(BotAdapter adapter, IBot bot, ICredentialProvider credentialProvider, AuthenticationConfiguration authConfig, IChannelProvider channelProvider = null, ILogger logger = null)
+                : base(adapter, bot, credentialProvider, authConfig, channelProvider, logger)
+            {
+            }
+
+            public async Task<ResourceResponse> TestOnSendToConversationAsync(ClaimsIdentity claimsIdentity, string conversationId, Activity activity, CancellationToken cancellationToken = default)
+            {
+                return await OnSendToConversationAsync(claimsIdentity, conversationId, activity, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<ResourceResponse> TestOnReplyToActivityAsync(ClaimsIdentity claimsIdentity, string conversationId, string activityId, Activity activity, CancellationToken cancellationToken = default)
+            {
+                return await OnReplyToActivityAsync(claimsIdentity, conversationId, activityId, activity, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<ResourceResponse> TestOnUpdateActivityAsync(ClaimsIdentity claimsIdentity, string conversationId, string activityId, Activity activity, CancellationToken cancellationToken = default)
+            {
+                return await OnUpdateActivityAsync(claimsIdentity, conversationId, activityId, activity, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task TestOnDeleteActivityAsync(ClaimsIdentity claimsIdentity, string conversationId, string activityId, CancellationToken cancellationToken = default)
+            {
+                await OnDeleteActivityAsync(claimsIdentity, conversationId, activityId, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<IList<ChannelAccount>> TestOnGetActivityMembersAsync(ClaimsIdentity claimsIdentity, string conversationId, string activityId, CancellationToken cancellationToken = default)
+            {
+                return await OnGetActivityMembersAsync(claimsIdentity, conversationId, activityId, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<ConversationResourceResponse> TestOnCreateConversationAsync(ClaimsIdentity claimsIdentity, string conversationId, ConversationParameters parameters, CancellationToken cancellationToken = default)
+            {
+                return await OnCreateConversationAsync(claimsIdentity, conversationId, parameters, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<ConversationsResult> TestOnGetConversationsAsync(ClaimsIdentity claimsIdentity, string conversationId, string continuationToken = default, CancellationToken cancellationToken = default)
+            {
+                return await OnGetConversationsAsync(claimsIdentity, conversationId, continuationToken, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<IList<ChannelAccount>> TestOnGetConversationMembersAsync(ClaimsIdentity claimsIdentity, string conversationId, CancellationToken cancellationToken = default)
+            {
+                return await OnGetConversationMembersAsync(claimsIdentity, conversationId, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<PagedMembersResult> TestOnGetConversationPagedMembersAsync(ClaimsIdentity claimsIdentity, string conversationId, int? pageSize = default, string continuationToken = default, CancellationToken cancellationToken = default)
+            {
+                return await OnGetConversationPagedMembersAsync(claimsIdentity, conversationId, pageSize, continuationToken, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task TestOnDeleteConversationMemberAsync(ClaimsIdentity claimsIdentity, string conversationId, string memberId, CancellationToken cancellationToken = default)
+            {
+                await OnDeleteConversationMemberAsync(claimsIdentity, conversationId, memberId, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<ResourceResponse> TestOnSendConversationHistoryAsync(ClaimsIdentity claimsIdentity, string conversationId, Transcript transcript, CancellationToken cancellationToken = default)
+            {
+                return await OnSendConversationHistoryAsync(claimsIdentity, conversationId, transcript, cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task<ResourceResponse> TestOnUploadAttachmentAsync(ClaimsIdentity claimsIdentity, string conversationId, AttachmentData attachmentUpload, CancellationToken cancellationToken = default)
+            {
+                return await OnUploadAttachmentAsync(claimsIdentity, conversationId, attachmentUpload, cancellationToken).ConfigureAwait(false);
             }
         }
     }
