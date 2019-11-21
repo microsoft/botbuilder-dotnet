a697d4f12a34ed294e2dfcd941671b6a864f937e
Gabo Gilabert <ggilaber@microsoft.com>
2019-11-21 13:17:44 -0500

    Added some tests for BotFrameworkHttpClient

---
+++ tests/integration/Microsoft.Bot.Builder.Integration.AspNet.Core.Tests/BotFrameworkHttpClientTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Bot.Connector.Authentication;
+using Moq;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.Core.Tests
+{
+    public class BotFrameworkHttpClientTests
+    {
+        [Fact]
+        public void ConstructorValidations()
+        {
+            var mockHttpClient = new Mock<HttpClient>();
+            var mockCredentialProvider = new Mock<ICredentialProvider>();
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var client = new BotFrameworkHttpClient(null, mockCredentialProvider.Object);
+            });
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                var client = new BotFrameworkHttpClient(mockHttpClient.Object, null);
+            });
+        }
+
+        [Fact]
+        public void ConstructorAddsHttpClientHeaders()
+        {
+            var httpClient = new HttpClient();
+            var mockCredentialProvider = new Mock<ICredentialProvider>();
+
+            Assert.False(httpClient.DefaultRequestHeaders.Any());
+            var client = new BotFrameworkHttpClient(httpClient, mockCredentialProvider.Object);
+            Assert.True(httpClient.DefaultRequestHeaders.Any());
+        }
+    }
+}
