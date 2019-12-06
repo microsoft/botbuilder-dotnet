1ad7c2b9f556e5116fd79e43a08bff1ef03685e7
Luis Sergio <37625424+LCarbonellRod@users.noreply.github.com>
2019-12-06 16:31:58 -0300

    Add Slack Functional tests

---
+++ FunctionalTests/Microsoft.Bot.Builder.FunctionalTests/Payloads/SlackHistoryRetrieve.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Bot.Builder.Adapters.Slack;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.FunctionalTests.Payloads
+{
+    public class SlackHistoryRetrieve
+    {
+        [JsonProperty(PropertyName = "ok")]
+        public string Ok { get; set; }
+
+        [JsonProperty(PropertyName = "messages")]
+        public List<SlackMessage> Messages { get; } = new List<SlackMessage>();
+    }
+}
