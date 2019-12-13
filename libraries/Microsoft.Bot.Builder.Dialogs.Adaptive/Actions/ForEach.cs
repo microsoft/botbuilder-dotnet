7624a3c8fc3dab4a35ba8898029445fc53134b57
Tom Laird-McConnell <tomlm@microsoft.com>
2019-12-13 12:38:40 -0800

    Add ActionScope to support Goto commands (#3095)
    * remove explicit resource id from test scripts
    
    * goto works, ifCondition, SwitchCondition, Foreach work
    added $schema to all test dialog files
    fixed bad data in test.dialog files (AutoEndDialog instead of autoEndDialog)
    added schema definitions for new actions
    
    * added unit tests and fixes for breakloop and continueloop
    
    * restore async awaits
    
    * remove uneeded content files
    
    * add copyright headers and comments to new classes
    
    * fix bad merge

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Actions/ForEach.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Actions/ForEach.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Actions
     /// <summary>
     /// Executes a set of actions once for each item in an in-memory list or collection.
     /// </summary>
-    public class Foreach : Dialog, IDialogDependencies
+    public class Foreach : ActionScope
     {
         [JsonProperty("$kind")]
         public const string DeclarativeType = "Microsoft.Foreach";
@@ -39,20 +39,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Actions
         [JsonProperty("itemsProperty")]
         public string ItemsProperty { get; set; }
 
-        /// <summary>
-        /// Gets or sets the actions to be run for each of items.
-        /// </summary>
-        /// <value>
-        /// The actions to be run for each of items.
-        /// </value>
-        [JsonProperty("actions")]
-        public List<Dialog> Actions { get; set; } = new List<Dialog>();
-
-        public virtual IEnumerable<Dialog> GetDependencies()
-        {
-            return this.Actions;
-        }
-
         public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
         {
             if (options is CancellationToken)
@@ -60,89 +46,53 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Actions
                 throw new ArgumentException($"{nameof(options)} cannot be a cancellation token");
             }
 
-            // Ensure planning context
-            if (dc is SequenceContext sc)
+            dc.GetState().SetValue(INDEX, -1);
+            return await this.NextItemAsync(dc, cancellationToken).ConfigureAwait(false);
+        }
+
+        protected override async Task<DialogTurnResult> OnBreakLoopAsync(DialogContext dc, ActionScopeResult actionScopeResult, CancellationToken cancellationToken = default)
+        {
+            return await dc.EndDialogAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+        }
+
+        protected override async Task<DialogTurnResult> OnContinueLoopAsync(DialogContext dc, ActionScopeResult actionScopeResult, CancellationToken cancellationToken = default)
+        {
+            return await this.NextItemAsync(dc, cancellationToken).ConfigureAwait(false);
+        }
+
+        protected override async Task<DialogTurnResult> OnEndOfActionsAsync(DialogContext dc, object result = null, CancellationToken cancellationToken = default)
+        {
+            return await this.NextItemAsync(dc, cancellationToken).ConfigureAwait(false);
+        }
+
+        protected virtual async Task<DialogTurnResult> NextItemAsync(DialogContext dc, CancellationToken cancellationToken = default)
+        {
+            // Get list information
+            var itemsProperty = new ExpressionEngine().Parse(this.ItemsProperty);
+            var (itemList, error) = itemsProperty.TryEvaluate(dc.GetState());
+            var list = JArray.FromObject(itemList);
+            var index = dc.GetState().GetIntValue(INDEX);
+
+            // Next item
+            if (++index < list.Count)
             {
-                Expression itemsProperty = null;
-                int offset = 0;
-                if (options != null && options is ForeachOptions)
-                {
-                    var opt = options as ForeachOptions;
-                    if (!string.IsNullOrEmpty(opt.List))
-                    {
-                        itemsProperty = new ExpressionEngine().Parse(opt.List);
-                    }
+                // Persist index and value
+                dc.GetState().SetValue(VALUE, list[index]);
+                dc.GetState().SetValue(INDEX, index);
 
-                    offset = opt.Offset;
-                }
-
-                itemsProperty = new ExpressionEngine().Parse(this.ItemsProperty);
-                var (itemList, error) = itemsProperty.TryEvaluate(dc.GetState());
-
-                if (error == null)
-                {
-                    var item = this.GetItem(itemList, offset);
-                    if (item != null)
-                    {
-                        dc.GetState().SetValue(VALUE, item);
-                        dc.GetState().SetValue(INDEX, offset);
-                        var changes = new ActionChangeList()
-                        {
-                            ChangeType = ActionChangeType.InsertActions,
-                            Actions = new List<ActionState>()
-                        };
-                        this.Actions.ForEach(step => changes.Actions.Add(new ActionState(step.Id)));
-
-                        changes.Actions.Add(new ActionState()
-                        {
-                            DialogStack = new List<DialogInstance>(),
-                            DialogId = this.Id,
-                            Options = new ForeachOptions()
-                            {
-                                List = ItemsProperty,
-                                Offset = offset + 1
-                            }
-                        });
-                        sc.QueueChanges(changes);
-                    }
-                }
-
-                return await sc.EndDialogAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+                // Start loop
+                return await this.BeginActionAsync(dc, 0, cancellationToken).ConfigureAwait(false);
             }
             else
             {
-                throw new Exception("`Foreach` should only be used in the context of an adaptive dialog.");
+                // End of list has been reached
+                return await dc.EndDialogAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
             }
         }
 
         protected override string OnComputeId()
         {
             return $"{this.GetType().Name}({this.ItemsProperty})";
-        }
-
-        private object GetItem(object list, int index)
-        {
-            JToken result = null;
-            if (list != null && list.GetType() == typeof(JArray))
-            {
-                if (index < JArray.FromObject(list).Count)
-                {
-                    result = JArray.FromObject(list)[index];
-                }
-            }
-            else if (list != null && list is JObject)
-            {
-                result = ((JObject)list).SelectToken(index.ToString());
-            }
-
-            return result;
-        }
-
-        public class ForeachOptions
-        {
-            public string List { get; set; }
-
-            public int Offset { get; set; }
         }
     }
 }
