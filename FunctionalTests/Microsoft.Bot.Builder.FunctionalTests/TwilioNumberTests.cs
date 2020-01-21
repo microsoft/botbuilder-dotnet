4d54178423c4a816c371413c7a89e5d9f79dfac1
Santiago Grangetto <santiagograngetto@gmail.com>
2020-01-21 12:30:35 -0300

    Add Twilio functional test

---
+++ FunctionalTests/Microsoft.Bot.Builder.FunctionalTests/TwilioNumberTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace Microsoft.Bot.Builder.FunctionalTests
+{
+    [TestClass]
+    [TestCategory("FunctionalTests")]
+    public class TwilioNumberTests
+    {
+        private string _botEndpoint;
+        private string _senderNumber;
+        private string _twilioNumber;
+        private string _twilioAuthToken;
+        private string _twilioAccountSid;
+
+        [TestMethod]
+        public async Task SendAndReceiveSmsShouldSucceed()
+        {
+            GetEnvironmentVars();
+            TwilioClient.Init(_twilioAccountSid, _twilioAuthToken);
+            var echoGuid = Guid.NewGuid().ToString();
+            await SendMessage(echoGuid);
+            var response = RetrieveMessage();
+
+            Assert.IsTrue(response.Contains($"Echo: {echoGuid}"));
+        }
+
+        private async Task SendMessage(string message)
+        {
+            var parameters = GetParameters(message);
+            var signature = ComputeSignature(_botEndpoint, parameters);
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("X-Twilio-Signature", signature);
+
+            await client.PostAsync(_botEndpoint, new FormUrlEncodedContent(parameters));
+        }
+
+        private string ComputeSignature(string url, IDictionary<string, string> parameters)
+        {
+            var toEncode = new StringBuilder(url);
+            
+            if (parameters != null)
+            {
+                var sortedKeys = new List<string>(parameters.Keys);
+                sortedKeys.Sort(StringComparer.Ordinal);
+
+                foreach (var key in sortedKeys)
+                {
+                    toEncode.Append(key).Append(parameters[key] ?? string.Empty);
+                }
+            }
+
+            var hmac = new HMACSHA1(Encoding.UTF8.GetBytes(_twilioAuthToken));
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(toEncode.ToString()));
+            
+            return Convert.ToBase64String(hash);
+        }
+
+        private string RetrieveMessage()
+        {
+            System.Threading.Thread.Sleep(60000);
+            var lastMessage = MessageResource.Read(limit: 10).First().Body;
+                
+            if (lastMessage.Contains("Echo: "))
+            {
+                return lastMessage;
+            }
+
+            throw new Exception("Echo message not found");
+        }
+
+        private Dictionary<string, string> GetParameters(string message)
+        {
+            var parameters = new Dictionary<string, string>
+            {
+                { "Body", message },
+                { "From", this._senderNumber }
+            };
+
+            return parameters;
+        }
+
+        private void GetEnvironmentVars()
+        {
+            if (string.IsNullOrWhiteSpace(_twilioNumber) || string.IsNullOrWhiteSpace(_twilioAuthToken) || string.IsNullOrWhiteSpace(_twilioAccountSid) || string.IsNullOrWhiteSpace(_botEndpoint) || string.IsNullOrWhiteSpace(_senderNumber))
+            {
+                _twilioNumber = Environment.GetEnvironmentVariable("TwilioNumber");
+                if (string.IsNullOrWhiteSpace(_twilioNumber))
+                {
+                    throw new Exception("Environment variable 'TwilioNumber' not found.");
+                }
+
+                _twilioAuthToken = Environment.GetEnvironmentVariable("TwilioAuthToken");
+                if (string.IsNullOrWhiteSpace(_twilioAuthToken))
+                {
+                    throw new Exception("Environment variable 'TwilioAuthToken' not found.");
+                }
+
+                _twilioAccountSid = Environment.GetEnvironmentVariable("TwilioAccountSid");
+                if (string.IsNullOrWhiteSpace(_twilioAccountSid))
+                {
+                    throw new Exception("Environment variable 'TwilioAccountSid' not found.");
+                }
+
+                _senderNumber = Environment.GetEnvironmentVariable("SenderNumber");
+                if (string.IsNullOrWhiteSpace(_senderNumber))
+                {
+                    throw new Exception("Environment variable 'SenderNumber' not found.");
+                }
+
+                _botEndpoint = Environment.GetEnvironmentVariable("TwilioValidationUrl");
+                if (string.IsNullOrWhiteSpace(_botEndpoint))
+                {
+                    throw new Exception("Environment variable 'TwilioValidationUrl' not found.");
+                }
+            }
+        }
+    }
+}
