96cbf65ac4e0b0b34f17c72678c5c07255f4bb0e
Gary Pretty <gary@garypretty.co.uk>
2020-01-27 15:49:45 +0000

    Updated Slack Test Bot project to .NET Core 3.1

--- tests/Adapters/Microsoft.Bot.Builder.Adapters.Slack.TestBot/Startup.cs
+++ tests/Adapters/Microsoft.Bot.Builder.Adapters.Slack.TestBot/Startup.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Bot.Builder.Adapters.Slack.TestBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(); //.SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc();
 
             // Create the Bot Framework Slack Adapter.
             services.AddSingleton<IBotFrameworkHttpAdapter, SlackAdapter>();
@@ -39,8 +39,14 @@ namespace Microsoft.Bot.Builder.Adapters.Slack.TestBot
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            // app.UseHttpsRedirection();
-            app.UseMvc();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
