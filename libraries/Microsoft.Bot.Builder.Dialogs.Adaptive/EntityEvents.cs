2ec13ad8b03f7e9d5d1d32891cf29dab82c2e19b
Chris McConnell <chrimc@microsoft.com>
2020-02-03 17:53:53 -0800

    Do unexpected assigns before expected assigns. (#3334)
    Add HasEvents flag to entity event queues.

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/EntityEvents.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/EntityEvents.cs
@@ -1,6 +1,8 @@
 ﻿// Licensed under the MIT License.
 // Copyright (c) Microsoft Corporation. All rights reserved.
 using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
 
 namespace Microsoft.Bot.Builder.Dialogs.Adaptive
 {
@@ -19,25 +21,32 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
         /// Gets mappings where a property is ready to be set to a specific entity.
         /// </summary>
         /// <value>List of entities to use when changeing properties.</value>
+        [JsonProperty("assignEntities")]
         public List<EntityAssignment> AssignEntities { get; } = new List<EntityAssignment>();
 
         /// <summary>
         /// Gets mappings where the entity is ambiguous.
         /// </summary>
         /// <value>List of ambiguous entities and the property they should be assigned to.</value>
+        [JsonProperty("chooseEntities")]
         public List<EntityAssignment> ChooseEntities { get; } = new List<EntityAssignment>();
 
         /// <summary>
         /// Gets entity that can be consumed by more than one property.
         /// </summary>
         /// <value>List of choices between entity and property assignments.</value>
+        [JsonProperty("chooseProperties")]
         public List<List<EntityAssignment>> ChooseProperties { get; } = new List<List<EntityAssignment>>();
 
         /// <summary>
         /// Gets list of properties to clear.
         /// </summary>
         /// <value>List of properties to clear.</value>
+        [JsonProperty("clearProperties")]
         public List<string> ClearProperties { get; } = new List<string>();
+
+        [JsonProperty("hasEvents")]
+        public bool HasEvents => AssignEntities.Any() || ChooseEntities.Any() || ChooseProperties.Any() || ClearProperties.Any();
 
         /// <summary>
         /// Read event queues from memory.
