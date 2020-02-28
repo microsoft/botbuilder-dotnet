45a83c19e3ba275822665c6fd8127252dfcde4bc
Chris McConnell <chrimc@microsoft.com>
2020-02-27 16:58:32 -0800

    Update generated tests to use ${} and latest naming (#3455)
    * Update tests to use ${} for expressions and latest naming pattern from generator.
    
    * Update generated files.

--- tests/Microsoft.Bot.Builder.TestBot.Json/buildluis.cmd
+++ tests/Microsoft.Bot.Builder.TestBot.Json/buildluis.cmd
@@ -1,2 +1,2 @@
 call bf luis:build --authoringKey %1 --botName TestBot --dialog --suffix %USERNAME% --in samples\ToDoLuisBot\ToDoLuis.lu
-call bf luis:build --authoringKey %1 --botName TestBot --dialog --suffix %USERNAME% --in Samples\GeneratedForm\luis\sandwich.en-us.lu
+call bf luis:build --authoringKey %1 --botName TestBot --dialog --suffix %USERNAME% --in Samples\sandwich\luis\sandwich.en-us.lu
