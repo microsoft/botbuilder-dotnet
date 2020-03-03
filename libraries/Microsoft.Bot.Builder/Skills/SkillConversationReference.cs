c42dcde28bae9d12cffb666b6323b0830bd3122f
Eric Dahlvang <v-eridah@microsoft.com>
2020-03-03 15:16:10 -0800

    Add originatingAudience to SkillHttpClient.PostActivityAsync (#3477)
    * Add originatingAudience to SkillHttpClient.PostActivityAsync
    
    * Deprecated old methods in SkillCOnversationIdFactoryBase and added new ones.
    Updated related classes to use the new methods.
    
    * clone ConversationReference for FunctionalTest's SkillConversationIdFactory
    
    * change skill conversation.id in SimpleRootBot example
    
    * Skill Unit test updates
    
    Co-authored-by: Gabo Gilabert <gabog@users.noreply.github.com>

---
+++ libraries/Microsoft.Bot.Builder/Skills/SkillConversationReference.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema;
+
+namespace Microsoft.Bot.Builder.Skills
+{
+    public class SkillConversationReference
+    {
+        public ConversationReference ConversationReference { get; set; }
+
+        public string OAuthScope { get; set; }
+    }
+}
