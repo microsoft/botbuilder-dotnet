2ceaf6599c9d9367bdcaf0895221b47a1141b743
Swagat Mishra <swagatm@microsoft.com>
2020-03-03 19:24:59 -0800

    Support SSO on parent and child bots for manual testing purposes

--- tests/Skills/Child/ChildBot.cs
+++ tests/Skills/Child/ChildBot.cs
@@ -4,18 +4,63 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
 
 namespace Microsoft.BotBuilderSamples
 {
     public class ChildBot : ActivityHandler
     {
+        private readonly Dialog _dialog;
+        private readonly ConversationState _conversationState;
+        private readonly UserState _userState;
+
+        public ChildBot(MainDialog dialog, ConversationState conversationState, UserState userState)
+        {
+            _dialog = dialog;
+            _conversationState = conversationState;
+            _userState = userState;
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            await _conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await _userState.SaveChangesAsync(turnContext, false, cancellationToken);
+        }
+
+        protected override async Task OnSignInInvokeAsync(ITurnContext<IInvokeActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await _conversationState.LoadAsync(turnContext, true, cancellationToken);
+            await _userState.LoadAsync(turnContext, true, cancellationToken);
+            await _dialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+        }
+
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            await turnContext.SendActivityAsync(MessageFactory.Text("child: activity (1)"), cancellationToken);
-            await turnContext.SendActivityAsync(MessageFactory.Text("child: activity (2)"), cancellationToken);
-            await turnContext.SendActivityAsync(MessageFactory.Text("child: activity (3)"), cancellationToken);
-            await turnContext.SendActivityAsync(MessageFactory.Text($"child: {turnContext.Activity.Text}"), cancellationToken);
+            if (turnContext.Activity.ChannelId == "emulator")
+            {
+                if (turnContext.Activity.Text == "skill login")
+                {
+                    await _conversationState.LoadAsync(turnContext, true, cancellationToken);
+                    await _userState.LoadAsync(turnContext, true, cancellationToken);
+                    await _dialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+                    return;
+                }
+                else if (turnContext.Activity.Text == "skill logout")
+                {
+                    var adapter = turnContext.Adapter as IExtendedUserTokenProvider;
+                    await adapter.SignOutUserAsync(turnContext, "SkillApp", turnContext.Activity.From.Id, cancellationToken);
+                }
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text("child: activity (1)"), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text("child: activity (2)"), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text("child: activity (3)"), cancellationToken);
+                await turnContext.SendActivityAsync(MessageFactory.Text($"child: {turnContext.Activity.Text}"), cancellationToken);
+            }
         }
     }
 }
--- tests/Skills/Parent/ParentBot.cs
+++ tests/Skills/Parent/ParentBot.cs
@@ -2,28 +2,101 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Mime;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.Skills;
 using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
 
 namespace Microsoft.BotBuilderSamples
 {
     public class ParentBot : ActivityHandler
     {
         private BotFrameworkHttpClient _client;
+        private readonly Dialog _dialog;
+        private readonly ConversationState _conversationState;
+        private readonly UserState _userState;
+        private readonly string _toBotId;
+        private readonly string _fromBotId;
+        private readonly string _connectionName;
 
-        public ParentBot(BotFrameworkHttpClient client)
+        // regex to check if code supplied is a 6 digit numerical code (hence, a magic code).
+        private readonly Regex _magicCodeRegex = new Regex(@"(\d{6})");
+
+        public ParentBot(BotFrameworkHttpClient client, IConfiguration configuration, MainDialog dialog, ConversationState conversationState, UserState userState)
         {
             _client = client;
+            _dialog = dialog;
+            _conversationState = conversationState;
+            _userState = userState;
+            _fromBotId = configuration.GetSection("MicrosoftAppId")?.Value;
+            _toBotId = configuration.GetSection("SkillMicrosoftAppId")?.Value;
+            _connectionName = configuration.GetSection("ConnectionName")?.Value;
+        }
+
+        public override async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await base.OnTurnAsync(turnContext, cancellationToken);
+
+            await _conversationState.SaveChangesAsync(turnContext, false, cancellationToken);
+            await _userState.SaveChangesAsync(turnContext, false, cancellationToken);
         }
 
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
+            // for signin, just use an oauth prompt to get the exchangeable token
+            // also ensure that the channelId is not emulator
+            if (turnContext.Activity.ChannelId != "emulator")
+            {
+                if (_magicCodeRegex.IsMatch(turnContext.Activity.Text) || turnContext.Activity.Text == "login")
+                {
+                    // start an oauth prompt
+                    await _conversationState.LoadAsync(turnContext, true, cancellationToken);
+                    await _userState.LoadAsync(turnContext, true, cancellationToken);
+                    await _dialog.RunAsync(turnContext, _conversationState.CreateProperty<DialogState>(nameof(DialogState)), cancellationToken);
+                }
+                else if (turnContext.Activity.Text == "logout")
+                {
+                    var adapter = turnContext.Adapter as IExtendedUserTokenProvider;
+                    await adapter.SignOutUserAsync(turnContext, _connectionName, turnContext.Activity.From.Id, cancellationToken);
+                }
+                else if (turnContext.Activity.Text == "skill login" || turnContext.Activity.Text == "skill logout")
+                {
+                    // incoming activity needs to be cloned for buffered replies
+                    var cloneActivity = MessageFactory.Text(turnContext.Activity.Text);
+                    cloneActivity.ApplyConversationReference(turnContext.Activity.GetConversationReference(), true);
+                    cloneActivity.DeliveryMode = DeliveryModes.BufferedReplies;
+                    var response1 = await _client.PostActivityAsync<Activity[]>(
+                        _fromBotId,
+                        _toBotId,
+                        new Uri("http://localhost:2303/api/messages"),
+                        new Uri("http://tempuri.org/whatever"),
+                        turnContext.Activity.Conversation.Id,
+                        cloneActivity,
+                        cancellationToken);
+
+                    if (response1.Status == (int)HttpStatusCode.OK)
+                    {
+                        if (!(await InterceptOAuthCards(response1.Body, turnContext, cancellationToken)))
+                        {
+                            await turnContext.SendActivitiesAsync(response1.Body, cancellationToken);
+                        }
+                    }
+                }
+
+                return;
+            }
+
             await turnContext.SendActivityAsync(MessageFactory.Text("parent: before child"), cancellationToken);
 
             var activity = MessageFactory.Text("parent to child");
@@ -31,9 +104,9 @@ namespace Microsoft.BotBuilderSamples
             activity.DeliveryMode = DeliveryModes.BufferedReplies;
 
             var response = await _client.PostActivityAsync<Activity[]>(
-                null,
-                "toBotId",
-                new Uri("http://localhost:3979/api/messages"),
+                _fromBotId,
+                _toBotId,
+                new Uri("http://localhost:2303/api/messages"),
                 new Uri("http://tempuri.org/whatever"),
                 Guid.NewGuid().ToString(),
                 activity,
@@ -45,6 +118,67 @@ namespace Microsoft.BotBuilderSamples
             }
 
             await turnContext.SendActivityAsync(MessageFactory.Text("parent: after child"), cancellationToken);
+        }
+
+        private async Task<bool> InterceptOAuthCards(Activity[] activities, ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            if (activities.Length == 0)
+            {
+                return false;
+            }
+
+            var activity = activities[0];
+            if (activity.Attachments != null)
+            {
+                foreach (var attachment in activity.Attachments.Where(a => a?.ContentType == OAuthCard.ContentType))
+                {
+                    var oauthCard = ((JObject)attachment.Content).ToObject<OAuthCard>();
+                    if (oauthCard.TokenExchangeResource != null)
+                    {
+                        // AAD token exchange
+                        var tokenExchangeProvider = turnContext.Adapter as IExtendedUserTokenProvider;
+                        var result = await tokenExchangeProvider.ExchangeTokenAsync(
+                            turnContext,
+                            _connectionName,
+                            turnContext.Activity.From.Id,
+                            new TokenExchangeRequest(oauthCard.TokenExchangeResource.Uri));
+
+                        if (!string.IsNullOrWhiteSpace(result.Token))
+                        {
+                            // Send an invoke back to the skill
+                            return await SendTokenExchangeInvokeToSkill(activity, oauthCard.TokenExchangeResource.Id, result.Token, "SkillApp", cancellationToken);
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private async Task<bool> SendTokenExchangeInvokeToSkill(Activity incomingActivity, string id, string token, string connectionName, CancellationToken cancellationToken)
+        {
+            var activity = incomingActivity.CreateReply() as Activity;
+            activity.Type = ActivityTypes.Invoke;
+            activity.Name = "signin/tokenExchange";
+            activity.Value = new TokenExchangeInvokeRequest()
+            {
+                Id = id,
+                Token = token,
+                ConnectionName = connectionName,
+            };
+
+            // route the activity to the skill
+            var response = await _client.PostActivityAsync(
+                _fromBotId,
+                _toBotId,
+                new Uri("http://localhost:2303/api/messages"),
+                new Uri("http://tempuri.org/whatever"),
+                incomingActivity.Conversation.Id,
+                activity,
+                cancellationToken);
+
+            // Check response status: true if success, false if failure
+            return response.Status >= 200 && response.Status <= 299;
         }
     }
 }
