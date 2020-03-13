fd607d7fbe1514512b804333f36c792f8053592a
Will Portnoy <wportnoy@microsoft.com>
2020-03-13 14:08:17 -0700

    add JValue data model

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Debugging/DataModels/JValueDataModel.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Bot.Builder.Dialogs.Debugging
+{
+    public sealed class JValueDataModel : IDataModel
+    {
+        public static readonly IDataModel Instance = new JValueDataModel();
+
+        private JValueDataModel()
+        {
+        }
+
+        int IDataModel.Rank => 1;
+
+        object IDataModel.this[object context, object name] { get => throw new NotSupportedException(); set => throw new NotSupportedException(); }
+
+        bool IDataModel.IsScalar(object context) => true;
+
+        IEnumerable<object> IDataModel.Names(object context) => Enumerable.Empty<object>();
+
+        string IDataModel.ToString(object context) => context.ToString();
+    }
+}
