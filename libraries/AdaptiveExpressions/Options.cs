6188caaccd72835721765fefa33a428ea49dcb54
Dong Lei <donglei@microsoft.com>
2020-03-27 11:40:37 +0000

    expand the interface expression evaluation to take an extra option

---
+++ libraries/AdaptiveExpressions/Options.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdaptiveExpressions
+{
+    /// <summary>
+    /// Options used to define evaluation behaviors.
+    /// </summary>
+    public class Options
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether we allow the memory interface to substitue the value when value is null.
+        /// </summary>
+        /// <value>
+        /// The flag.
+        /// </value>
+        public bool AllowSubstitution { get; set; } = true;
+    }
+}
