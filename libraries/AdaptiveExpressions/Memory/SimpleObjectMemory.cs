b6f747dd7da3dca44e88cc8b1ef4fd646f7a6954
Dong Lei <donglei@microsoft.com>
2020-03-30 08:25:54 +0000

    fix doc

--- libraries/AdaptiveExpressions/Memory/SimpleObjectMemory.cs
+++ libraries/AdaptiveExpressions/Memory/SimpleObjectMemory.cs
@@ -33,7 +33,7 @@ namespace AdaptiveExpressions.Memory
         /// </summary>
         /// <param name="path">Given path.</param>
         /// <param name="value">Resolved value.</param>
-        /// <param name="allowSubstitution">allow substitution at null or not.</param>
+        /// <param name="allowSubstitution">Allow substitution on null or not.</param>
         /// <returns>True if the memory contains an element with the specified key; otherwise, false.</returns>
         public bool TryGetValue(string path, out object value, bool allowSubstitution = true)
         {
