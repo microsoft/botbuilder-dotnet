9bfdff97ee7d61eb8398992f1f357724efdfb337
Shuai Wang <shuwan@microsoft.com>
2020-04-02 15:31:23 +0800

    Fix local culture info changes the result of parsing number and datetime (#3631)
    * fix culture info changes the result of parsing number and datetime
    
    * extract culture invariant todouble as a new function
    
    * add two testmethod for different culture info
    
    * retrigger
    
    * add switch back to original culture
    
    * retrigger
    
    * reuse code
    
    * retrigger

--- tests/AdaptiveExpressions.Tests/ExpressionParserTests.cs
+++ tests/AdaptiveExpressions.Tests/ExpressionParserTests.cs
@@ -6,6 +6,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 using AdaptiveExpressions.Memory;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
@@ -677,10 +679,10 @@ namespace AdaptiveExpressions.Tests
             Test("getFutureTime(1,'Month','MM-dd-yy')", DateTime.Now.AddMonths(1).ToString("MM-dd-yy")),
             Test("getFutureTime(1,'Week','MM-dd-yy')", DateTime.Now.AddDays(7).ToString("MM-dd-yy")),
             Test("getFutureTime(1,'Day','MM-dd-yy')", DateTime.Now.AddDays(1).ToString("MM-dd-yy")),
-            Test("convertFromUTC('2018-01-02T02:00:00.000Z', 'Pacific Standard Time', 'D')", "Monday, January 1, 2018"),
-            Test("convertFromUTC('2018-01-02T01:00:00.000Z', 'America/Los_Angeles', 'D')", "Monday, January 1, 2018"),
+            Test("convertFromUTC('2018-01-02T02:00:00.000Z', 'Pacific Standard Time', 'D')", "Monday, 01 January 2018"),
+            Test("convertFromUTC('2018-01-02T01:00:00.000Z', 'America/Los_Angeles', 'D')", "Monday, 01 January 2018"),
             Test("convertToUTC('01/01/2018 00:00:00', 'Pacific Standard Time')", "2018-01-01T08:00:00.000Z"),
-            Test("addToTime('2018-01-01T08:00:00.000Z', 1, 'Day', 'D')", "Tuesday, January 2, 2018"),
+            Test("addToTime('2018-01-01T08:00:00.000Z', 1, 'Day', 'D')", "Tuesday, 02 January 2018"),
             Test("addToTime('2018-01-01T00:00:00.000Z', 1, 'Week')", "2018-01-08T00:00:00.000Z"),
             Test("startOfDay('2018-03-15T13:30:30.000Z')", "2018-03-15T00:00:00.000Z"),
             Test("startOfHour('2018-03-15T13:30:30.000Z')", "2018-03-15T13:00:00.000Z"),
@@ -900,6 +902,35 @@ namespace AdaptiveExpressions.Tests
 
         [DataTestMethod]
         [DynamicData(nameof(Data))]
+        public void EvaluateInOtherCultures(string input, object expected, HashSet<string> expectedRefs)
+        { 
+            var cultureList = new List<string>() { "de-DE", "fr-FR", "es-ES" };
+            foreach (var newCultureInfo in cultureList)
+            {
+                var originalCuture = Thread.CurrentThread.CurrentCulture;
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(newCultureInfo);
+                var parsed = Expression.Parse(input);
+                Assert.IsNotNull(parsed);
+                var (actual, msg) = parsed.TryEvaluate(scope);
+                Assert.AreEqual(null, msg);
+                AssertObjectEquals(expected, actual);
+                if (expectedRefs != null)
+                {
+                    var actualRefs = parsed.References();
+                    Assert.IsTrue(expectedRefs.SetEquals(actualRefs), $"References do not match, expected: {string.Join(',', expectedRefs)} acutal: {string.Join(',', actualRefs)}");
+                }
+
+                // ToString re-parse
+                var newExpression = Expression.Parse(parsed.ToString());
+                var newActual = newExpression.TryEvaluate(scope).value;
+                AssertObjectEquals(actual, newActual);
+
+                Thread.CurrentThread.CurrentCulture = originalCuture;
+            } 
+        }
+
+        [DataTestMethod]
+        [DynamicData(nameof(Data))]
         public void EvaluateJson(string input, object expected, HashSet<string> expectedRefs)
         {
             var jsonScope = JToken.FromObject(scope);
@@ -975,8 +1006,8 @@ namespace AdaptiveExpressions.Tests
             AssertResult<ushort>(ushort.MaxValue.ToString(), ushort.MaxValue);
             AssertResult<uint>(uint.MaxValue.ToString(), uint.MaxValue);
             AssertResult<ulong>(uint.MaxValue.ToString(), uint.MaxValue);
-            AssertResult<float>(15.32322F.ToString(), 15.32322F);
-            AssertResult<double>(15.32322.ToString(), 15.32322);
+            AssertResult<float>(15.32322F.ToString(CultureInfo.InvariantCulture), 15.32322F);
+            AssertResult<double>(15.32322.ToString(CultureInfo.InvariantCulture), 15.32322);
         }
 
         [TestMethod]
