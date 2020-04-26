eb47d678b0eb4a0b330661920813526f138288f9
Tom Laird-McConnell <tomlm@microsoft.com>
2020-04-26 09:18:14 -0700

    Replace interface with abstract base classes for resourceExplorer Add method to add new resourceType (and reload resources) Properly modeled event using EventHandler<T>

--- libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/FileResource.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/FileResource.cs
@@ -7,7 +7,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Resources
     /// <summary>
     /// Class which represents a file as a resource.
     /// </summary>
-    public class FileResource : IResource
+    public class FileResource : Resource
     {
         private Task<byte[]> contentTask;
         private Task<string> textTask;
@@ -23,14 +23,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Resources
         }
 
         /// <summary>
-        /// Gets resource Id for the resource.
-        /// </summary>
-        /// <value>
-        /// ResourceId.
-        /// </value>
-        public string Id { get; }
-
-        /// <summary>
         /// Gets the resource path.
         /// </summary>
         /// <value>
@@ -42,7 +34,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Resources
         /// Open a stream to the resource.
         /// </summary>
         /// <returns>Stream for accesssing the content of the resource.</returns>
-        public async Task<Stream> OpenStreamAsync()
+        public override async Task<Stream> OpenStreamAsync()
         {
             if (contentTask == null)
             {
@@ -70,25 +62,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Resources
 
             var content = await contentTask.ConfigureAwait(false);
             return new MemoryStream(content);
-        }
-
-        /// <summary>
-        /// Get resource as a text.
-        /// </summary>
-        /// <returns>A <see cref="Task"/> with the string.</returns>
-        public Task<string> ReadTextAsync()
-        {
-            if (this.textTask == null)
-            {
-                this.textTask = Task.Run(async () =>
-                {
-                    var stream = await OpenStreamAsync().ConfigureAwait(false);
-                    TextReader textReader = new StreamReader(stream);
-                    return await textReader.ReadToEndAsync().ConfigureAwait(false);
-                });
-            }
-
-            return this.textTask;
         }
 
         public override string ToString()
