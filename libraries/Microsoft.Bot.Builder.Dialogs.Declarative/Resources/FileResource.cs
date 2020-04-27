b6e7d936086deb4c4870fd1fa6e6b7f588369a96
Tom Laird-McConnell <tomlm@microsoft.com>
2020-04-27 13:50:50 -0700

    add copyright headers

--- libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/FileResource.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/FileResource.cs
@@ -1,4 +1,7 @@
-﻿using System.Diagnostics;
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System.Diagnostics;
 using System.IO;
 using System.Threading.Tasks;
 
