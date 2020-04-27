59d7a3f84c2a5abe26bed3fae16e7b92e441f6eb
Tom Laird-McConnell <tomlm@microsoft.com>
2020-04-27 13:50:50 -0700

    add copyright headers

--- libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/Resource.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/Resource.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Builder.Dialogs.Declarative.Resources
--- libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/ResourceProvider.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Resources/ResourceProvider.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System;
 using System.Collections.Generic;
 
 namespace Microsoft.Bot.Builder.Dialogs.Declarative.Resources
