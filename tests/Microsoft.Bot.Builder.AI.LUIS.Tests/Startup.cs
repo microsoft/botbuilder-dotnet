d223d58286f9271514b98f3962998026bc5e162c
Tom Laird-McConnell <tomlm@microsoft.com>
2020-04-30 14:14:50 -0700

    add copyright to tests/Microsoft.Bot.Builder.AI.LUIS.Tests/Startup.cs
    Co-authored-by: Carlos Castro <carlosscastro@users.noreply.github.com>

--- tests/Microsoft.Bot.Builder.AI.LUIS.Tests/Startup.cs
+++ tests/Microsoft.Bot.Builder.AI.LUIS.Tests/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Bot.Builder.AI.Luis;
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder.AI.Luis;
 using Microsoft.Bot.Builder.Dialogs;
 
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
