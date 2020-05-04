3b1cf37b61b578dd0b8688793d4f14968b87aadd
Tom Laird-McConnell <tomlm@microsoft.com>
2020-05-04 12:49:08 -0700

    changed method signatures to be backward compatible. Added obselete to UseState() helper.

--- libraries/Microsoft.Bot.Builder/AdapterExtensions.cs
+++ libraries/Microsoft.Bot.Builder/AdapterExtensions.cs
@@ -44,5 +44,48 @@ namespace Microsoft.Bot.Builder
 
             return botAdapter;
         }
+
+        /// <summary>
+        /// Registers user and conversation state objects with the adapter. These objects will be available via the turn context's
+        /// <see cref="TurnContext.TurnState"/>.<see cref="TurnContextStateCollection.Get{T}()"/> method.
+        /// </summary>
+        /// <param name="botAdapter">The <see cref="BotAdapter"/> on which to register the storage object.</param>
+        /// <param name="userState">The <see cref="UserState"/> object to register.</param>
+        /// <param name="conversationState">The <see cref="ConversationState"/> object to register.</param>
+        /// <param name="auto">`true` to automatically persist state each turn; otherwise, `false`.
+        /// When false, it is your responsibility to persist state each turn.</param>
+        /// <returns>The updated adapter.</returns>
+        /// <remarks>
+        /// This adds <see cref="IMiddleware"/> to register the user and conversation state management objects.
+        /// If <paramref name="auto"/> is true, this also adds middleware to automatically persist state before each turn ends.
+        /// </remarks>
+        [Obsolete("This method is deprecated in 4.9.  You should use the method .UseBotState() instead.")]
+        public static BotAdapter UseState(this BotAdapter botAdapter, UserState userState, ConversationState conversationState, bool auto = true)
+        {
+            if (botAdapter == null)
+            {
+                throw new ArgumentNullException(nameof(botAdapter));
+            }
+
+            if (userState == null)
+            {
+                throw new ArgumentNullException(nameof(userState));
+            }
+
+            if (conversationState == null)
+            {
+                throw new ArgumentNullException(nameof(conversationState));
+            }
+
+            botAdapter.Use(new RegisterClassMiddleware<UserState>(userState));
+            botAdapter.Use(new RegisterClassMiddleware<ConversationState>(conversationState));
+
+            if (auto)
+            {
+                return botAdapter.Use(new AutoSaveStateMiddleware(userState, conversationState));
+            }
+
+            return botAdapter;
+        }
     }
 }
