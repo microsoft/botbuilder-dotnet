a82c9fa0031acc11482c69a8839c7ea12a34e012
Vijaya Peketi <vipeketi@microsoft.com>
2020-05-14 13:27:30 +0530

    AnswerSpanRequest property in Request.

---
+++ libraries/Microsoft.Bot.Builder.AI.QnA/AnswerSpanRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Bot.Builder.AI.QnA
+{
+    public class  AnswerSpanRequest
+    {
+        public boolean enable { get; set; }
+
+        public int topAnswersWithSpan { get; set; }
+    }
+}
