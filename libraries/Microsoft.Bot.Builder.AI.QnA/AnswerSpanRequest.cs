e131a74315594ec1bb34f75eaa05e2060c7944c5
Vijaya Peketi <vipeketi@microsoft.com>
2020-05-21 01:20:58 +0530

    Mock changes

--- libraries/Microsoft.Bot.Builder.AI.QnA/AnswerSpanRequest.cs
+++ libraries/Microsoft.Bot.Builder.AI.QnA/AnswerSpanRequest.cs
@@ -8,21 +8,12 @@ namespace Microsoft.Bot.Builder.AI.QnA
     public class AnswerSpanRequest
     {
         /// <summary>
-        /// Gets or sets the enablet.
+        /// Gets or sets a value indicating whether gets or sets the enablet.
         /// </summary>
         /// <value>
         /// The answer text.
         /// </value>
         [JsonProperty("enable")]
         public bool Enable { get; set; }
-
-        /// <summary>
-        /// Gets or sets the TopAnswersWithSpan.
-        /// </summary>
-        /// <value>
-        /// The answer TopAnswersWithSpan.
-        /// </value>
-        [JsonProperty("topAnswersWithSpan")]
-        public int TopAnswersWithSpan { get; set; }
     }
 }
