e6bb473afaa11454b82df3eecfb5dc76a77fa3cd
Hongyang Du <1990512dhy@163.com>
2020-06-08 15:11:52 +0800

    add test

--- tests/Microsoft.Bot.Builder.LanguageGeneration.Tests/TemplatesParseTreeTest.cs
+++ tests/Microsoft.Bot.Builder.LanguageGeneration.Tests/TemplatesParseTreeTest.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Text;
 using Antlr4.Runtime;
-using Antlr4.Runtime.Tree;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using static Microsoft.Bot.Builder.LanguageGeneration.LGTemplateParser;
 
@@ -13,33 +10,82 @@ namespace Microsoft.Bot.Builder.LanguageGeneration.Tests
     public class TemplatesParseTreeTest
     {
         [TestMethod]
-        public void TestBasic()
+        public void ParseTreeTest()
         {
             var templates = Templates.ParseFile(GetExampleFilePath("ParseTreeTest.lg"));
 
             Assert.AreEqual(4, templates.Count);
 
             // Normal template body
-            Assert.AreEqual(1, templates[0].TemplateBodyParseTree.children.Count);
-            var body = templates[0].TemplateBodyParseTree.children[0] as NormalBodyContext;
+            var normalTemplateBody = templates[0].TemplateBodyParseTree as NormalBodyContext;
 
-            Assert.AreEqual("-hi", body.GetText());
-            Assert.AreEqual(1, body.Start.Line);
-            Assert.AreEqual(0, body.Start.Column);
-            Assert.AreEqual(1, body.Stop.Line);
-            Assert.AreEqual(3, body.Stop.Column);
+            // - ${welcomeword} ${name}
+            Assert.AreEqual("${welcomeword} ${name}", normalTemplateBody.GetText());
+            var templateStrings = normalTemplateBody.normalTemplateBody().templateString();
+            Assert.AreEqual(2, templateStrings.Length);
+            var expression1 = templateStrings[0].normalTemplateString().expression()[0].GetText();
+            Assert.AreEqual("${welcomeword}", expression1);
+            var expression2 = templateStrings[1].normalTemplateString().expression()[0].GetText();
+            Assert.AreEqual("${name}", expression2);
 
             // Condition template body
-            Assert.AreEqual(1, templates[1].TemplateBodyParseTree.children.Count);
-            var conditionBody = templates[1].TemplateBodyParseTree.children[0] as IfElseBodyContext;
-            Assert.AreEqual(3, body.children.Count);
+            var conditionalBody = templates[1].TemplateBodyParseTree as IfElseBodyContext;
+            var rules = conditionalBody.ifElseTemplateBody().ifConditionRule();
+            Assert.AreEqual(3, rules.Length);
 
             // - IF:${a > 0}
             // -positve
-            var ifCondition = body.children[0] as ParserRuleContext;
-            Assert.AreEqual(2, ifCondition.children.Count);
+            var ifCondition = rules[0].ifCondition();
+            Assert.AreEqual("-IF:${a > 0}", ifCondition.GetText());
+            var expressionContext = ifCondition.expression()[0];
+            Assert.AreEqual("${a > 0}", expressionContext.GetText());
 
-            //var ifStatement = ifCondition.
+            //-ELSEIF: ${ a == 0}
+            //-equals to 0
+            var elseIfCondition = rules[1].ifCondition();
+            Assert.AreEqual("-ELSEIF:${a == 0}", elseIfCondition.GetText());
+            expressionContext = elseIfCondition.expression()[0];
+            Assert.AreEqual("${a == 0}", expressionContext.GetText());
+
+            // - ELSE:
+            // - negative
+            var elseCondition = rules[2].ifCondition();
+            Assert.AreEqual("-ELSE:", elseCondition.GetText());
+
+            // switch/case template body
+            var switchBody = templates[2].TemplateBodyParseTree as SwitchCaseBodyContext;
+            var caseRules = switchBody.switchCaseTemplateBody().switchCaseRule();
+            Assert.AreEqual(4, caseRules.Length);
+
+            // -SWITCH:${day}
+            var switchStat = caseRules[0].switchCaseStat();
+            Assert.AreEqual("-SWITCH:${day}", switchStat.GetText());
+            expressionContext = switchStat.expression()[0];
+            Assert.AreEqual("${day}", expressionContext.GetText());
+
+            //-CASE: ${'Saturday'}
+            //-Happy Saturday!
+            var caseStat = caseRules[1].switchCaseStat();
+            Assert.AreEqual("-CASE:${'Saturday'}", caseStat.GetText());
+            expressionContext = caseStat.expression()[0];
+            Assert.AreEqual("${'Saturday'}", expressionContext.GetText());
+
+            //-DEFAULT:
+            var defaultStat = caseRules[3].switchCaseStat();
+            Assert.AreEqual("-DEFAULT:", defaultStat.GetText());
+
+            // structure
+            var structureBody = templates[3].TemplateBodyParseTree as StructuredBodyContext;
+            var nameLine = structureBody.structuredTemplateBody().structuredBodyNameLine();
+            Assert.AreEqual("MyStruct", nameLine.STRUCTURE_NAME().GetText());
+            var bodyLines = structureBody.structuredTemplateBody().structuredBodyContentLine();
+            Assert.AreEqual(1, bodyLines.Length);
+
+            //body =${ body}
+            var contentLine = bodyLines[0].keyValueStructureLine();
+            Assert.AreEqual("body", contentLine.STRUCTURE_IDENTIFIER().GetText());
+            Assert.AreEqual(1, contentLine.keyValueStructureValue().Length);
+            Assert.AreEqual("${body}", contentLine.keyValueStructureValue()[0].expressionInStructure()[0].GetText());
         }
 
         private string GetExampleFilePath(string fileName)
