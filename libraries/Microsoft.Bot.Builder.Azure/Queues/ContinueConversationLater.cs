cc3a235784868ec467436c549e95e0de9d4fcfd9
Tom Laird-McConnell <tomlm@microsoft.com>
2020-06-16 10:53:30 -0700

    changes based on PR feedback, copyright and disposing of response

--- libraries/Microsoft.Bot.Builder.Azure/Queues/ContinueConversationLater.cs
+++ libraries/Microsoft.Bot.Builder.Azure/Queues/ContinueConversationLater.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
