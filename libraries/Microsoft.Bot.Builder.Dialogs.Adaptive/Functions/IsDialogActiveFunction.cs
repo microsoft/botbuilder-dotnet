6d9e973274b98bb51087128468240cb5bde02b5e
Tom Laird-McConnell <tomlm@microsoft.com>
2020-06-18 14:22:54 -0700

    added custom isDialogActive function to adaptive assembly.

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Functions/IsDialogActiveFunction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AdaptiveExpressions;
+using AdaptiveExpressions.Memory;
+using Microsoft.Bot.Builder.Adapters;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Functions
+{
+    public class IsDialogActiveFunction : ExpressionEvaluator
+    {
+        public const string Name = "isDialogActive";
+
+        public IsDialogActiveFunction()
+            : base(ExpressionType.Lambda, Evaluate, ReturnType.Boolean)
+        {
+        }
+
+        public static (object value, string error) Evaluate(Expression expression, IMemory state, Options options)
+        {
+            if (state.TryGetValue("dialogcontext.stack", out object stackArray))
+            {
+                List<string> args = new List<string>();
+                foreach (var child in expression.Children)
+                {
+                    var (value, error) = child.TryEvaluate<string>(state, options);
+                    if (error != null)
+                    {
+                        throw new Exception(error);
+                    }
+
+                    args.Add(value);
+                }
+
+                foreach (var dlg in ((JArray)stackArray).ToObject<List<string>>())
+                {
+                    if (args.Contains(dlg))
+                    {
+                        // one of the dialogs was found in the stack.
+                        return (true, null);
+                    }
+                }
+
+                // none of the dialogs was found in the stack.
+                return (false, null);
+            }
+
+            return (null, "DialogContext.Stack not found");
+        }
+    }
+}
