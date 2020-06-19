5ce95aecad62def985e1465226f537784d65ef60
Hongyang Du (hond) <hond@microsoft.com>
2020-06-19 13:48:20 +0800

    fix expandText return type error (#4132)
    * fix expandText return type error
    
    * fix

--- libraries/Microsoft.Bot.Builder.LanguageGeneration/Expander.cs
+++ libraries/Microsoft.Bot.Builder.LanguageGeneration/Expander.cs
@@ -541,7 +541,7 @@ namespace Microsoft.Bot.Builder.LanguageGeneration
 
             if (name.Equals(expandText))
             {
-                return new ExpressionEvaluator(expandText, ExpressionFunctions.Apply(this.ExpandText()), ReturnType.Boolean, ExpressionFunctions.ValidateUnaryString);
+                return new ExpressionEvaluator(expandText, ExpressionFunctions.Apply(this.ExpandText()), ReturnType.Object, ExpressionFunctions.ValidateUnaryString);
             }
 
             return null;
@@ -551,7 +551,7 @@ namespace Microsoft.Bot.Builder.LanguageGeneration
        => (IReadOnlyList<object> args) =>
        {
            var stringContent = args[0].ToString();
-           var newScope = CurrentTarget().Scope;
+           var newScope = evaluationTargetStack.Count == 0 ? null : CurrentTarget().Scope;
            var newTemplates = new Templates(templates: Templates, expressionParser: expressionParser);
            return newTemplates.EvaluateText(stringContent, newScope, lgOptions);
        };
@@ -678,7 +678,7 @@ namespace Microsoft.Bot.Builder.LanguageGeneration
            var resourcePath = GetResourcePath(filePath);
            var stringContent = File.ReadAllText(resourcePath);
 
-           var newScope = CurrentTarget().Scope;
+           var newScope = evaluationTargetStack.Count == 0 ? null : CurrentTarget().Scope;
            var newTemplates = new Templates(templates: Templates, expressionParser: expressionParser);
            return newTemplates.EvaluateText(stringContent, newScope, lgOptions);
        };
