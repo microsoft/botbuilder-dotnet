6e2078278b7acca8fe5465bbced3cc5554f5110c
Shuai Wang <shuwan@microsoft.com>
2020-06-24 17:11:17 +0800

    remove unused imports

--- libraries/AdaptiveExpressions/Options.cs
+++ libraries/AdaptiveExpressions/Options.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Globalization;
-using System.Threading;
 
 namespace AdaptiveExpressions
 {
--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Actions/ActionScope.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Actions/ActionScope.cs
@@ -5,11 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Actions
