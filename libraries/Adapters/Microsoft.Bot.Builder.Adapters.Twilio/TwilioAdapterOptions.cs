d7920977c2ec2a6c27544f6b5c30bf0c4fe881b0
Gary Pretty <gary@garypretty.co.uk>
2020-06-29 18:42:57 +0100

    Add missing code docs

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioAdapterOptions.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioAdapterOptions.cs
@@ -4,8 +4,17 @@ using System.Text;
 
 namespace Microsoft.Bot.Builder.Adapters.Twilio
 {
+    /// <summary>
+    /// Options for the <see cref="TwilioAdapter"/>.
+    /// </summary>
     public class TwilioAdapterOptions
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether incoming requests should be validated as coming from Twilio.
+        /// </summary>
+        /// <value>
+        /// A value indicating whether incoming requests should be validated as coming from Twilio.
+        /// </value>
         public bool ValidateIncomingRequests { get; set; } = true;
     }
 }
--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioMessageOptions.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioMessageOptions.cs
@@ -4,23 +4,49 @@ using System.Text;
 
 namespace Microsoft.Bot.Builder.Adapters.Twilio
 {
+    /// <summary>
+    /// Represents an outgoing message content and options for Twilio.
+    /// </summary>
     public class TwilioMessageOptions
     {
-        /// <summary>The destination phone number.</summary>
+        /// <summary>
+        /// Gets or sets the destination phone number.
+        /// </summary>
+        /// <value>
+        /// The destination phone number.
+        /// </value>
         public string To { get; set;  }
 
-        /// <summary>The phone number that initiated the message.</summary>
+        /// <summary>
+        /// Gets or sets the phone number that initiated the message.
+        /// </summary>
+        /// <value>
+        /// The phone number that initiated the message.
+        /// </value>
         public string From { get; set; }
 
         /// <summary>
-        /// The text of the message you want to send. Can be up to 1,600 characters in length.
+        /// Gets or sets the text of the message you want to send. Can be up to 1,600 characters in length.
         /// </summary>
+        /// <value>
+        /// The text of the message you want to send. Can be up to 1,600 characters in length.
+        /// </value>
         public string Body { get; set; }
 
-        /// <summary>The URL of the media to send with the message.</summary>
+        /// <summary>
+        /// Gets the URL of the media to send with the message.
+        /// </summary>
+        /// <value>
+        /// The URL of the media to send with the message.
+        /// </value>
         public List<Uri> MediaUrl { get; } = new List<Uri>();
 
-        /// <summary>The application to use for callbacks.</summary>
+        /// <summary>
+        /// Gets or sets the application to use for callbacks.
+        /// </summary>
+        /// <value>
+        /// The application to use for callbacks.
+        /// </value>
         public string ApplicationSid { get; set; }
     }
 }
