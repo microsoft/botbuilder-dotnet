a6a1e9265b2963e08bf7034feb2d59f1942e8d2f
Gary Pretty <gary@garypretty.co.uk>
2020-07-01 15:48:45 +0100

    Update Facebook, Slack, Webex adapters (#4214)
    * Update Facebook, Slack, Webex adapters to follow pattern from Twilio adapter
    
    * Tweak exception messages

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Webex/WebexAdapterOptions.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Webex/WebexAdapterOptions.cs
@@ -6,47 +6,16 @@ using System;
 namespace Microsoft.Bot.Builder.Adapters.Webex
 {
     /// <summary>
-    /// Defines implementation of the WebexAdapter Options.
+    /// Options class for the <see cref="WebexAdapter" />.
     /// </summary>
     public class WebexAdapterOptions
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="WebexAdapterOptions"/> class.
+        /// Gets or sets a value indicating whether the signature on incoming requests should be validated as originating from Webex.
         /// </summary>
-        /// <param name="webexAccessToken">An access token for the bot.</param>
-        /// <param name="webexPublicAddress">The root URL of the bot application.</param>
-        /// <param name="webexSecret">The secret used to validate incoming webhooks.</param>
-        /// <param name="webexWebhookName">A name for the webhook subscription.</param>
-        public WebexAdapterOptions(string webexAccessToken, Uri webexPublicAddress, string webexSecret, string webexWebhookName = null)
-        {
-            WebexAccessToken = webexAccessToken;
-            WebexPublicAddress = webexPublicAddress;
-            WebexSecret = webexSecret;
-            WebexWebhookName = webexWebhookName;
-        }
-
-        /// <summary>
-        /// Gets or sets an access token for the bot.
-        /// </summary>
-        /// <value>An access token for the bot. Get one from 'https://developer.webex.com/'.</value>
-        public string WebexAccessToken { get; set; }
-
-        /// <summary>
-        /// Gets or sets the secret used to validate incoming webhooks.
-        /// </summary>
-        /// <value>The secret used to validate incoming webhooks. You can define this yourself.</value>
-        public string WebexSecret { get; set; }
-
-        /// <summary>
-        /// Gets or sets the root URI of your bot application. Something like 'https://mybot.com/'.
-        /// </summary>
-        /// <value>the root URI of your bot application.</value>
-        public Uri WebexPublicAddress { get; set; }
-
-        /// <summary>
-        /// Gets or sets a name for the webhook subscription that will be created to tell Webex to send your bot webhooks.
-        /// </summary>
-        /// <value>A name for the webhook subscription.</value>
-        public string WebexWebhookName { get; set; }
+        /// <value>
+        /// A value indicating if the signature on incoming requests should be validated as originating from Webex.
+        /// </value>
+        public bool ValidateIncomingRequests { get; set; } = true;
     }
 }
