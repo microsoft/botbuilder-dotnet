e16793685f57782135138ab6c71f6847609dcef4
Zongyang Ma <zoma@microsoft.com>
2020-07-06 17:16:36 +0800

    add tests for qna setting

--- tests/Microsoft.Bot.Builder.AI.QnA.Tests/QnAMakerSettingTests.cs
+++ tests/Microsoft.Bot.Builder.AI.QnA.Tests/QnAMakerSettingTests.cs
@@ -19,11 +19,12 @@ namespace Microsoft.Bot.Builder.AI.QnA.Tests
             var config = builder.Build();
             var botRoot = config.GetValue<string>("root") ?? ".";
             var region = config.GetValue<string>("region") ?? "westus";
-            var environment = config.GetValue<string>("environment") ?? Environment.UserName;
+            var environment = config.GetValue<string>("environment") ?? "development";
 
             builder.UseQnAMakerSettings(botRoot, region, environment);
-  
-            Assert.IsNotNull(builder);
+
+            var source = builder.Sources[1] as Microsoft.Extensions.Configuration.Json.JsonConfigurationSource;
+            Assert.AreEqual(source.Path, "qnamaker.settings.development.westus.json");
         }
     }
 }
