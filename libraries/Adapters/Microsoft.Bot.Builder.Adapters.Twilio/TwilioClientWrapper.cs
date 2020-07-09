56d1e8a9b916bd1f4625dad3eeee976bd7beec0b
Gary Pretty <gary@garypretty.co.uk>
2020-07-09 17:25:21 +0100

    Update Twilio Adapter to better separate client / adapter (#4188)
    * Update Twilio Adapter to take adapter options in constructor instead of client.
    
    * Split adapter and client options apart.
    
    * Add missing code docs
    
    * Update following feedback
    
    * Remove US specific comment
    
    * Add copyright headers

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioClientWrapper.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Twilio/TwilioClientWrapper.cs
@@ -24,35 +24,20 @@ namespace Microsoft.Bot.Builder.Adapters.Twilio
         /// Initializes a new instance of the <see cref="TwilioClientWrapper"/> class.
         /// </summary>
         /// <param name="options">An object containing API credentials, a webhook verification token and other options.</param>
-        public TwilioClientWrapper(TwilioAdapterOptions options)
+        public TwilioClientWrapper(TwilioClientWrapperOptions options)
         {
             Options = options ?? throw new ArgumentNullException(nameof(options));
-
-            if (string.IsNullOrWhiteSpace(options.TwilioNumber))
-            {
-                throw new ArgumentException(nameof(options.TwilioNumber));
-            }
-
-            if (string.IsNullOrWhiteSpace(options.TwilioAccountSid))
-            {
-                throw new ArgumentException(nameof(options.TwilioAccountSid));
-            }
-
-            if (string.IsNullOrWhiteSpace(options.TwilioAuthToken))
-            {
-                throw new ArgumentException(nameof(options.TwilioAuthToken));
-            }
-
+            
             TwilioClient.Init(Options.TwilioAccountSid, Options.TwilioAuthToken);
         }
 
         /// <summary>
-        /// Gets the <see cref="TwilioAdapterOptions"/> for the wrapper. 
+        /// Gets the <see cref="TwilioClientWrapperOptions"/> for the wrapper. 
         /// </summary>
         /// <value>
-        /// The <see cref="TwilioAdapterOptions"/> for the wrapper.
+        /// The <see cref="TwilioClientWrapperOptions"/> for the wrapper.
         /// </value>
-        public TwilioAdapterOptions Options { get; }
+        public TwilioClientWrapperOptions Options { get; }
 
         /// <summary>
         /// Sends a Twilio SMS message.
@@ -60,9 +45,17 @@ namespace Microsoft.Bot.Builder.Adapters.Twilio
         /// <param name="messageOptions">An object containing the parameters for the message to send.</param>
         /// <param name="cancellationToken">A cancellation token for the task.</param>
         /// <returns>The SID of the Twilio message sent.</returns>
-        public virtual async Task<string> SendMessageAsync(CreateMessageOptions messageOptions, CancellationToken cancellationToken)
+        public virtual async Task<string> SendMessageAsync(TwilioMessageOptions messageOptions, CancellationToken cancellationToken)
         {
-            var messageResource = await MessageResource.CreateAsync(messageOptions).ConfigureAwait(false);
+            var createMessageOptions = new CreateMessageOptions(messageOptions.To)
+            {
+                ApplicationSid = messageOptions.ApplicationSid,
+                MediaUrl = messageOptions.MediaUrl,
+                Body = messageOptions.Body,
+                From = messageOptions.From
+            };
+
+            var messageResource = await MessageResource.CreateAsync(createMessageOptions).ConfigureAwait(false);
             return messageResource.Sid;
         }
 
