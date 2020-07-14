833df768cbabd96f36149871ccd9732d5d201d90
Gary Pretty <gary@garypretty.co.uk>
2020-07-14 13:29:02 +0100

    Added code docs

--- libraries/Microsoft.Bot.Builder.Dialogs/DialogContainer.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/DialogContainer.cs
@@ -50,6 +50,13 @@ namespace Microsoft.Bot.Builder.Dialogs
             return this.Dialogs.Find(dialogId);
         }
 
+        /// <summary>
+        /// Called when an event has been raised, using `DialogContext.emitEvent()`, by either the current dialog or a dialog that the current dialog started.
+        /// </summary>
+        /// <param name="dc">The dialog context for the current turn of conversation.</param>
+        /// <param name="e">The event being raised.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>True if the event is handled by the current dialog and bubbling should stop.</returns>
         public override async Task<bool> OnDialogEventAsync(DialogContext dc, DialogEvent e, CancellationToken cancellationToken)
         {
             var handled = await base.OnDialogEventAsync(dc, e, cancellationToken).ConfigureAwait(false);
