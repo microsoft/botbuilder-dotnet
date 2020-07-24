716512943172df1863ab97a022c9895082e00d7d
xieofxie <xieofxie@126.com>
2020-07-25 00:17:37 +0800

    use new HttpContent each time for HttpRequestMock (#4199)
    * use new HttpContent each time
    
    * update test
    
    * add comments and update SequenceResponseManager
    
    * fix
    
    * trigger pr validation
    
    * add comments
    
    * fix and update schema

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Testing/HttpRequestMocks/HttpRequestMock.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Testing/HttpRequestMocks/HttpRequestMock.cs
@@ -6,8 +6,15 @@ using RichardSzalay.MockHttp;
 
 namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Testing.HttpRequestMocks
 {
+    /// <summary>
+    /// Base class for all http request mocks.
+    /// </summary>
     public abstract class HttpRequestMock
     {
+        /// <summary>
+        /// In the constructor of MockHttpRequestMiddleware, this function will be called to setup a global handler.
+        /// </summary>
+        /// <param name="handler">The global handler.</param>
         public abstract void Setup(MockHttpMessageHandler handler);
 
         protected void RegisterSourcePath(string path, int line)
