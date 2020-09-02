489cac293b3639c535e59bf5be8fa3ab3a941df5
CeciliaAvila <cecilia.avila@southworks.com>
2020-09-02 17:01:37 -0300

    Migrate Startup class to xUnit

--- tests/Microsoft.Bot.Builder.AI.QnA.Tests/Startup.cs
+++ tests/Microsoft.Bot.Builder.AI.QnA.Tests/Startup.cs
@@ -1,17 +1,13 @@
 ﻿using Microsoft.Bot.Builder.AI.QnA;
-using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Testing;
 using Microsoft.Bot.Builder.Dialogs.Declarative;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Microsoft.Bot.Builder
 {
-    [TestClass]
     public class Startup
     {
-        [AssemblyInitialize]
-        public static void Initialize(TestContext testContext)
+        public static void Initialize()
         {
             ComponentRegistration.Add(new DeclarativeComponentRegistration());
             ComponentRegistration.Add(new AdaptiveComponentRegistration());
