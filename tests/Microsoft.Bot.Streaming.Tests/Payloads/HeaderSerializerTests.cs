03edd7ca9fc0c3c191d1a2a3d3b06e6b262afd8d
Denise Scollo <dens.scollo@gmail.com>
2020-09-10 15:20:42 -0300

    Migrate Payloads folder's tests to Xunit (#4590)
    Co-authored-by: CeciliaAvila <cecilia.avila@southworks.com>

--- tests/Microsoft.Bot.Streaming.Tests/Payloads/HeaderSerializerTests.cs
+++ tests/Microsoft.Bot.Streaming.Tests/Payloads/HeaderSerializerTests.cs
@@ -5,14 +5,13 @@ using System;
 using System.IO;
 using System.Text;
 using Microsoft.Bot.Streaming.Payloads;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 
 namespace Microsoft.Bot.Streaming.UnitTests.Payloads
 {
-    [TestClass]
     public class HeaderSerializerTests
     {
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_CanRoundTrip()
         {
             var header = new Header()
@@ -24,19 +23,19 @@ namespace Microsoft.Bot.Streaming.UnitTests.Payloads
             };
 
             var buffer = new byte[1024];
-            var offset = 0;
+            const int offset = 0;
 
             var length = HeaderSerializer.Serialize(header, buffer, offset);
 
             var result = HeaderSerializer.Deserialize(buffer, 0, length);
 
-            Assert.AreEqual(header.Type, result.Type);
-            Assert.AreEqual(header.PayloadLength, result.PayloadLength);
-            Assert.AreEqual(header.Id, result.Id);
-            Assert.AreEqual(header.End, result.End);
+            Assert.Equal(header.Type, result.Type);
+            Assert.Equal(header.PayloadLength, result.PayloadLength);
+            Assert.Equal(header.Id, result.Id);
+            Assert.Equal(header.End, result.End);
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_SerializesToAscii()
         {
             var header = new Header()
@@ -48,146 +47,146 @@ namespace Microsoft.Bot.Streaming.UnitTests.Payloads
             };
 
             var buffer = new byte[1024];
-            var offset = 0;
+            const int offset = 0;
 
             var length = HeaderSerializer.Serialize(header, buffer, offset);
 
             var str = Encoding.ASCII.GetString(buffer, offset, length);
 
-            Assert.AreEqual("A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n", str);
+            Assert.Equal("A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n", str);
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_DeserializesFromAscii()
         {
-            var header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
             var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
 
-            Assert.AreEqual('A', result.Type);
-            Assert.AreEqual(168, result.PayloadLength);
-            Assert.AreEqual(Guid.Parse("68e999ca-a651-40f4-ad8f-3aaf781862b4"), result.Id);
-            Assert.AreEqual(true, result.End);
+            Assert.Equal('A', result.Type);
+            Assert.Equal(168, result.PayloadLength);
+            Assert.Equal(Guid.Parse("68e999ca-a651-40f4-ad8f-3aaf781862b4"), result.Id);
+            Assert.True(result.End);
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_DeserializeUnknownType()
         {
-            var header = "Z.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "Z.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
             var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
 
-            Assert.AreEqual('Z', result.Type);
-            Assert.AreEqual(168, result.PayloadLength);
+            Assert.Equal('Z', result.Type);
+            Assert.Equal(168, result.PayloadLength);
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_LengthTooShort_Throws()
         {
-            var header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<ArgumentException>(() =>
+            Assert.Throws<ArgumentException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, 5);
+                HeaderSerializer.Deserialize(bytes, 0, 5);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_LengthTooLong_Throws()
         {
-            var header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<ArgumentException>(() =>
+            Assert.Throws<ArgumentException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, 55);
+                HeaderSerializer.Deserialize(bytes, 0, 55);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_BadTypeDelimeter_Throws()
         {
-            var header = "Ax000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "Ax000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<InvalidDataException>(() =>
+            Assert.Throws<InvalidDataException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
+                HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_BadLengthDelimeter_Throws()
         {
-            var header = "A.000168x68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "A.000168x68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<InvalidDataException>(() =>
+            Assert.Throws<InvalidDataException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
+                HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_BadIdDelimeter_Throws()
         {
-            var header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4x1\n";
+            const string header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4x1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<InvalidDataException>(() =>
+            Assert.Throws<InvalidDataException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
+                HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_BadTerminator_Throws()
         {
-            var header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1c";
+            const string header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1c";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<InvalidDataException>(() =>
+            Assert.Throws<InvalidDataException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
+                HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_BadLength_Throws()
         {
-            var header = "A.00p168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "A.00p168.68e999ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<InvalidDataException>(() =>
+            Assert.Throws<InvalidDataException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
+                HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_BadId_Throws()
         {
-            var header = "A.000168.68e9p9ca-a651-40f4-ad8f-3aaf781862b4.1\n";
+            const string header = "A.000168.68e9p9ca-a651-40f4-ad8f-3aaf781862b4.1\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<InvalidDataException>(() =>
+            Assert.Throws<InvalidDataException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
+                HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
             });
         }
 
-        [TestMethod]
+        [Fact]
         public void HeaderSerializer_Deserialize_BadEnd_Throws()
         {
-            var header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.z\n";
+            const string header = "A.000168.68e999ca-a651-40f4-ad8f-3aaf781862b4.z\n";
             var bytes = Encoding.ASCII.GetBytes(header);
 
-            Assert.ThrowsException<InvalidDataException>(() =>
+            Assert.Throws<InvalidDataException>(() =>
             {
-                var result = HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
+                HeaderSerializer.Deserialize(bytes, 0, bytes.Length);
             });
         }
     }
