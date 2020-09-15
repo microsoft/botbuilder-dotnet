48612e666146b5f6592859c68e0e0c9cea038074
Zongyang Ma <zoma@microsoft.com>
2020-09-15 17:51:38 +0800

    add more unittests

---
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/AssignEntityTests.cs
@@ -0,0 +1,50 @@
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.Luis.Testing;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core.Tests;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
+{
+    [TestClass]
+    public class AssignEntityTests
+    {
+        public static ResourceExplorer ResourceExplorer { get; set; }
+
+        public TestContext TestContext { get; set; }
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+            string assignEntityTestsDirectory = PathUtils.NormalizePath(@"..\..\..\..\..\tests\Microsoft.Bot.Builder.Dialogs.Adaptive.Tests\Tests\AssignEntityTests\");
+
+            var config = new ConfigurationBuilder()
+           .UseMockLuisSettings(assignEntityTestsDirectory, "TestBot")
+           .Build();
+
+            var path = Path.Combine(TestUtils.GetProjectPath(), "Tests", nameof(AssignEntityTests));
+
+            ResourceExplorer = new ResourceExplorer()
+                .AddFolder(path, monitorChanges: false)
+                .RegisterType(LuisAdaptiveRecognizer.Kind, typeof(MockLuisRecognizer), new MockLuisLoader(config));
+        }
+
+        [TestMethod]
+        public async Task AddEntity()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer);
+        }
+
+        [TestMethod]
+        public async Task ShowEntity()
+        {
+            await TestUtils.RunTestScript(ResourceExplorer);
+        }
+    }
+}
---
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/ChoosePropertyTests.cs
@@ -0,0 +1,36 @@
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Bot.Builder.AI.Luis.Testing;
+using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
+using Microsoft.Bot.Builder.Integration.ApplicationInsights.Core.Tests;
+using Microsoft.Extensions.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
+{
+    [TestClass]
+    public class ChoosePropertyTests
+    {
+        private readonly string choosePropertyTestsDirectory = PathUtils.NormalizePath(@"..\..\..\..\..\tests\Microsoft.Bot.Builder.Dialogs.Adaptive.Tests\Tests\ChoosePropertyTests\");
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        public async Task ChooseProperty()
+        {
+            var config = new ConfigurationBuilder()
+                .UseMockLuisSettings(choosePropertyTestsDirectory, "TestBot")
+                .Build();
+
+            var resourceExplorer = new ResourceExplorer()
+                .AddFolder(Path.Combine(TestUtils.GetProjectPath(), "Tests", nameof(ChoosePropertyTests)), monitorChanges: false)
+                .RegisterType(LuisAdaptiveRecognizer.Kind, typeof(MockLuisRecognizer), new MockLuisLoader(config));
+
+            await TestUtils.RunTestScript(resourceExplorer, configuration: config);
+        }
+    }
+}
