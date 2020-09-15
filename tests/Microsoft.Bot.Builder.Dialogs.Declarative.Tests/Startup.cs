0ccb16edcd1d69395700bbc0f1b4f8941cb99382
Denise Scollo <dens.scollo@gmail.com>
2020-09-15 13:20:22 -0300

    [#4095] Migrate Microsoft.Bot.Builder.Dialogs.Declarative.Tests project to xUnit (#4622)
    * Migrate Dialogs.Declarative.Tests to xUnit
    
    * Make SchemaMergeTests constructor static
    
    * Implement IClassFixture instead of ClassInitialize
    
    * Apply feedback
    
    Co-authored-by: CeciliaAvila <cecilia.avila@southworks.com>
    Co-authored-by: Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>

--- tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/Startup.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/Startup.cs
@@ -1,16 +1,18 @@
 ﻿using Microsoft.Bot.Builder.AI.QnA;
-using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
 using Microsoft.Bot.Builder.Dialogs.Declarative;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+[assembly: TestFramework("Microsoft.Bot.Builder.Startup", "Microsoft.Bot.Builder.Dialogs.Declarative.Tests")]
 
 namespace Microsoft.Bot.Builder
 {
-    [TestClass]
-    public class Startup
+    public class Startup : XunitTestFramework
     {
-        [AssemblyInitialize]
-        public static void Initialize(TestContext testContext)
+        public Startup(IMessageSink messageSink)
+            : base(messageSink)
         {
             ComponentRegistration.Add(new DeclarativeComponentRegistration());
             ComponentRegistration.Add(new AdaptiveComponentRegistration());
