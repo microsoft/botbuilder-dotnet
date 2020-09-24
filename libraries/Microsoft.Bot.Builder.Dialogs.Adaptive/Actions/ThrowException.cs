9ab0460e3b8962b4f25c76972d7da4ccd90cbf5d
Fei Chen <43032123+feich-ms@users.noreply.github.com>
2020-09-24 13:56:18 +0800

    Add ThrowException action to pass exception out for string error message (#4656)
    * add ThrowException action
    
    * update test schema
    
    * refactoring to throw wrapped exception directly
    
    * update schema

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Actions/ThrowException.cs
@@ -0,0 +1,102 @@
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Actions
+{
+    /// <summary>
+    /// Action which throws an exception declaratively.
+    /// </summary>
+    public class ThrowException : Dialog
+    {
+        /// <summary>
+        /// Class identifier.
+        /// </summary>
+        [JsonProperty("$kind")]
+        public const string Kind = "Microsoft.ThrowException";
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ThrowException"/> class.
+        /// </summary>
+        /// <param name="errorValue">Memory property path to use to get the error value to throw.</param>
+        /// <param name="callerPath">Optional, source file full path.</param>
+        /// <param name="callerLine">Optional, line number in source file.</param>
+        [JsonConstructor]
+        public ThrowException(object errorValue = null, [CallerFilePath] string callerPath = "", [CallerLineNumber] int callerLine = 0)
+            : base()
+        {
+            this.RegisterSourceLocation(callerPath, callerLine);
+
+            if (errorValue != null)
+            {
+                this.ErrorValue = new ValueExpression(errorValue);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets an optional expression which if is true will disable this action.
+        /// </summary>
+        /// <example>
+        /// "user.age > 18".
+        /// </example>
+        /// <value>
+        /// A boolean expression. 
+        /// </value>
+        [JsonProperty("disabled")]
+        public BoolExpression Disabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the memory property path to use to get the error value to throw.
+        /// </summary>
+        /// <value>
+        /// The memory property path to use to get the error value to throw.
+        /// </value>
+        [JsonProperty("errorValue")]
+        public ValueExpression ErrorValue { get; set; }
+
+        /// <summary>
+        /// Called when the dialog is started and pushed onto the dialog stack.
+        /// </summary>
+        /// <param name="dc">The <see cref="DialogContext"/> for the current turn of conversation.</param>
+        /// <param name="options">Optional, initial information to pass to the dialog.</param>
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects
+        /// or threads to receive notice of cancellation.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (options is CancellationToken)
+            {
+                throw new ArgumentException($"{nameof(options)} cannot be a cancellation token");
+            }
+
+            if (this.Disabled != null && this.Disabled.GetValue(dc.State) == true)
+            {
+                return await dc.EndDialogAsync(cancellationToken: cancellationToken).ConfigureAwait(false);
+            }
+
+            object value = null;
+            
+            if (ErrorValue != null)
+            {
+                value = this.ErrorValue.GetValue(dc.State);
+            }
+
+            throw new Exception(value?.ToString());
+        }
+
+        /// <summary>
+        /// Builds the compute Id for the dialog.
+        /// </summary>
+        /// <returns>A string representing the compute Id.</returns>
+        protected override string OnComputeId()
+        {
+            return $"{this.GetType().Name}[{DialogEvents.Error}]";
+        }
+    }
+}
