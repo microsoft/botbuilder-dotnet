df8c07a09a4fc98861a7be9d792c01eedfa6445c
Chris McConnell <chrimc@microsoft.com>
2020-09-25 10:58:32 -0700

    Reset retries on new generation events.

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/AdaptiveDialog.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/AdaptiveDialog.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
@@ -794,6 +793,12 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
                 if (nextAssignment.Alternative != null)
                 {
                     val = nextAssignment.Alternatives.ToList();
+                }
+
+                if (nextAssignment.RaisedCount++ == 0)
+                {
+                    // Reset retries when new form event is first issued
+                    actionContext.State.RemoveValue(DialogPath.Retries);
                 }
 
                 evt = new DialogEvent() { Name = nextAssignment.Event, Value = val, Bubble = false };
