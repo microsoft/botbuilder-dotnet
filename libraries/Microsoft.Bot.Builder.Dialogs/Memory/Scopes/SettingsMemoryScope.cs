2a3e3603671104c47e056bf0fb48675837a71142
hond <hond@microsoft.com>
2020-09-25 16:48:31 +0800

    change the appsetting parse logic

--- libraries/Microsoft.Bot.Builder.Dialogs/Memory/Scopes/SettingsMemoryScope.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/Memory/Scopes/SettingsMemoryScope.cs
@@ -73,91 +73,104 @@ namespace Microsoft.Bot.Builder.Dialogs.Memory.Scopes
             if (configuration != null)
             {
                 // load configuration into settings dictionary
-                foreach (var child in configuration.AsEnumerable())
+                var root = ConvertFlattenSettingToNode(configuration.AsEnumerable().ToList());
+                var result = new Dictionary<string, object>();
+                foreach (var child in root.Children)
                 {
-                    var keys = child.Key.Split(':');
-
-                    // initialize all parts of path up to the key/value we are attempting to set
-                    dynamic parent = null;
-                    var parentKey = string.Empty;
-                    dynamic node = settings;
-                    for (var i = 0; i < keys.Length - 1; i++)
-                    {
-                        var key = keys[i];
-                        if (!node.ContainsKey(key))
-                        {
-                            node[key] = new Dictionary<string, object>();
-                        }
-
-                        parent = node;
-                        parentKey = key;
-                        node = node[key];
-                    }
-
-                    if (child.Value != null)
-                    {
-                        var lastKey = keys.Last();
-                        var hasIndex = int.TryParse(lastKey, out var index);
-                        if (hasIndex)
-                        {
-                            // if node is dictionary, and has zero elements then convert to array
-                            var dict = node as IDictionary<string, object>;
-                            if (dict != null && dict.Count == 0)
-                            {
-                                // replace parent with array
-                                parent[parentKey] = new object[index + 1];
-                                node = parent[parentKey];
-                            }
-
-                            // if it's still a dictionary that's because we have a non-number value in it
-                            if (dict != null && dict.Count > 0)
-                            {
-                                // store using key as string
-                                node[lastKey] = child.Value;
-                            }
-                            else
-                            {
-                                // it's an array
-                                var arr = (object[])node;
-                                if (arr.Length < index + 1)
-                                {
-                                    // auto-resize to be bigger if we need to 
-                                    // NOTE: keys seem to be reverse indexed, which means we should have already "right-sized" the array
-                                    Array.Resize(ref arr, index + 1);
-                                    parent[parentKey] = arr;
-                                    node = arr;
-                                }
-
-                                // store using keyValue as an index
-                                arr[index] = child.Value;
-                            }
-                        }
-                        else
-                        {
-                            if (node is object[] arr)
-                            {
-                                // we got a non-number key but we have building an array, convert the array to dictionary
-                                // NOTE: keys is sorting reverse which means we should we always start out with a dictionary for nonnumberic keys
-                                var dict = new Dictionary<string, object>();
-                                for (var i = 0; i < arr.Length; i++)
-                                {
-                                    if (arr[i] != null)
-                                    {
-                                        dict[i.ToString(CultureInfo.InvariantCulture)] = arr[i];
-                                    }
-                                }
-
-                                parent[parentKey] = dict;
-                                node = dict;
-                            }
-
-                            node[lastKey] = child.Value;
-                        }
-                    }
+                    result.Add(child.Value, ConvertNodeToObject(child));
                 }
+
+                return result;
             }
 
             return settings;
+        }
+
+        private static Node ConvertFlattenSettingToNode(List<KeyValuePair<string, string>> kvs)
+        {
+            var root = new Node(null);
+            foreach (var child in kvs)
+            {
+                var key = child.Key;
+                var value = child.Value;
+                var keyChain = key.Split(':');
+                var currentNode = root;
+                foreach (var item in keyChain)
+                {
+                    var matchIten = currentNode.Children.FirstOrDefault(u => u?.Value == item);
+                    if (matchIten == null)
+                    {
+                        // Remove all the leaf children
+                        currentNode.Children.RemoveAll(u => u.Children.Count == 0);
+
+                        // Append new child into current node
+                        var node = new Node(item);
+                        currentNode.Children.Add(node);
+                        currentNode = node;
+                    }
+                    else
+                    {
+                        currentNode = matchIten;
+                    }
+                }
+
+                currentNode.Children.Add(new Node(value));
+            }
+
+            return root;
+        }
+
+        private static object ConvertNodeToObject(Node node)
+        {
+            if (node.Children.Count == 1 && node.Children[0].Children.Count == 0)
+            {
+                return node.Children[0].Value;
+            }
+
+            if (node.Children.All(u => int.TryParse(u.Value, out _)))
+            {
+                // all children are int number
+                var pairs = new List<Tuple<int, object>>();
+                node.Children.ForEach(u => pairs.Add(new Tuple<int, object>(int.Parse(u.Value, CultureInfo.InvariantCulture), ConvertNodeToObject(u))));
+                var maxIndex = pairs.Select(u => u.Item1).Max();
+                var list = new object[maxIndex + 1];
+                foreach (var pair in pairs)
+                {
+                    list[pair.Item1] = pair.Item2;
+                }
+
+                return list;
+            }
+
+            // convert all children into dictionary
+            var result = new Dictionary<string, object>();
+            foreach (var child in node.Children)
+            {
+                result.Add(child.Value, ConvertNodeToObject(child));
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// The setting node.
+        /// </summary>
+        private class Node
+        {
+            public Node(string value)
+            {
+                Value = value;
+            }
+
+            /// <summary>
+            /// Gets or sets value of the node. If the node is not leaf, value presents the current path.
+            /// </summary>
+            public string Value { get; set; }
+
+            /// <summary>
+            /// Gets or sets children of the node.
+            /// </summary>
+            public List<Node> Children { get; set; } = new List<Node>();
         }
     }
 }
