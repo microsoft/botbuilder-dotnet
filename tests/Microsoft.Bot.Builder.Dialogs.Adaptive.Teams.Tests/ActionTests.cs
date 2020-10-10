e55d8fdbb667c8c6192ea921f0a5ffe59a791c13
Eric Dahlvang <ericdahlvang@gmail.com>
2020-10-09 19:35:01 -0700

    Add tests for GetMeetingParticipant. (also update TestUtils to enable a custom HttpMessageHandler for connector client call results)

---
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Tests/ActionTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Testing;
+using Microsoft.Bot.Builder.Dialogs.Declarative;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Tests
+{
+    [CollectionDefinition("Dialogs.Adaptive")]
+    public class ActionTests : IClassFixture<ResourceExplorerFixture>
+    {
+        private readonly ResourceExplorerFixture _resourceExplorerFixture;
+
+        public ActionTests(ResourceExplorerFixture resourceExplorerFixture)
+        {
+            ComponentRegistration.Add(new DeclarativeComponentRegistration());
+            ComponentRegistration.Add(new DialogsComponentRegistration());
+            ComponentRegistration.Add(new AdaptiveComponentRegistration());
+            ComponentRegistration.Add(new LanguageGenerationComponentRegistration());
+            ComponentRegistration.Add(new AdaptiveTestingComponentRegistration());
+            ComponentRegistration.Add(new TeamsComponentRegistration());
+
+            _resourceExplorerFixture = resourceExplorerFixture.Initialize(nameof(ActionTests));
+        }
+
+        [Fact]
+        public async Task Action_GetMeetingParticipantError()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_GetMeetingParticipantMockedResults()
+        {
+            var participantResult = new JObject
+                        {
+                            new JProperty("meetingRole", "Organizer"),
+                            new JProperty("userPrincipalName", "userPrincipalName-1"),
+                            new JProperty("conversation", new JObject(new JProperty("Id", "meetigConversationId-1"))),
+                        };
+
+            var messageHandler = new TestsHttpMessageHandler("/v1/meetings/meeting-id-1/participants/participant-aad-id-1?tenantId=tenant-id-1", participantResult.ToString());
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer, testHttpClientMessageHandler: messageHandler);
+        }
+
+        private class TestsHttpMessageHandler : HttpMessageHandler
+        {
+            private Dictionary<string, string> _uriToContent;
+
+            public TestsHttpMessageHandler(string url, string content)
+                : this(new Dictionary<string, string>() { { url, content } })
+            {
+            }
+
+            public TestsHttpMessageHandler(Dictionary<string, string> uriToContent)
+            {
+                _uriToContent = uriToContent;
+            }
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.OK);
+
+                var path = request.RequestUri.PathAndQuery;
+                foreach (var urlAndContent in _uriToContent)
+                {
+                    if (urlAndContent.Key.Contains(path, System.StringComparison.OrdinalIgnoreCase))
+                    {
+                        response.Content = new StringContent(urlAndContent.Value);
+                    }
+                }
+
+                return Task.FromResult(response);
+            }
+        }
+    }
+}
--- tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Tests/TestUtils.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Tests/TestUtils.cs
@@ -5,12 +5,15 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Adapters;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Testing;
 using Microsoft.Bot.Builder.Dialogs.Declarative.Resources;
 using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 
 namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Tests
@@ -27,14 +30,24 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Tests
             return Directory.EnumerateFiles(testFolder, "*.test.dialog", SearchOption.AllDirectories).Select(s => new object[] { Path.GetFileName(s) }).ToArray();
         }
 
-        public static async Task RunTestScript(ResourceExplorer resourceExplorer, string resourceId = null, IConfiguration configuration = null, [CallerMemberName] string testName = null)
+        public static async Task RunTestScript(ResourceExplorer resourceExplorer, string resourceId = null, IConfiguration configuration = null, [CallerMemberName] string testName = null, HttpMessageHandler testHttpClientMessageHandler = null)
         {
             var storage = new MemoryStorage();
             var convoState = new ConversationState(storage);
             var userState = new UserState(storage);
 
-            var adapter = (TestAdapter)new TestAdapter(Channels.Msteams)
-                .Use(new RegisterClassMiddleware<IConfiguration>(DefaultConfiguration))
+            var adapter = (TestAdapter)new TestAdapter(Channels.Msteams);
+            if (testHttpClientMessageHandler != null)
+            {
+                var testHttpClient = new HttpClient(testHttpClientMessageHandler); 
+
+                // Set a special base address so then we can make sure the connector client is honoring this http client
+                testHttpClient.BaseAddress = new Uri("https://localhost.coffee");
+                var testConnectorClient = new ConnectorClient(new Uri("http://localhost.coffee/"), new MicrosoftAppCredentials(string.Empty, string.Empty), testHttpClient);
+                adapter.Use(new TestConnectorClientMiddleware(testConnectorClient));
+            }
+
+            adapter.Use(new RegisterClassMiddleware<IConfiguration>(DefaultConfiguration))
                 .UseStorage(storage)
                 .UseBotState(userState, convoState)
                 .Use(new TranscriptLoggerMiddleware(new TraceTranscriptLogger(traceActivity: false)));
@@ -61,6 +74,23 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Tests
             }
 
             return parent;
+        }
+
+        private class TestConnectorClientMiddleware : IMiddleware
+        {
+            private IConnectorClient _connectorClient;
+
+            public TestConnectorClientMiddleware(IConnectorClient connectorClient)
+            {
+                _connectorClient = connectorClient;
+            }
+
+            public Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default)
+            {
+                // turnContext.TurnState.Get<IConnectorClient>();
+                turnContext.TurnState.Add<IConnectorClient>(_connectorClient);
+                return next(cancellationToken);
+            }
         }
     }
 }
