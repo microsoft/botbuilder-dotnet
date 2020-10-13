37aa8a8a5aa666f5a45c8102732c5a237e1334f6
Hongyang Du (hond) <hond@microsoft.com>
2020-10-13 11:25:25 +0800

    Make languagepolicy in Recognizer align with the policy in UseLanguagePolicy (#4784)

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Recognizers/MultiLanguageRecognizer.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Recognizers/MultiLanguageRecognizer.cs
@@ -41,7 +41,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers
         /// </value>
         [JsonProperty("languagePolicy")]
 #pragma warning disable CA2227 // Collection properties should be read only (we can't change this without breaking binary compat)
-        public LanguagePolicy LanguagePolicy { get; set; } = new LanguagePolicy();
+        public LanguagePolicy LanguagePolicy { get; set; }
 #pragma warning restore CA2227 // Collection properties should be read only
 
         /// <summary>
@@ -66,13 +66,17 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers
         /// <returns>Analysis of utterance.</returns>
         public override async Task<RecognizerResult> RecognizeAsync(DialogContext dialogContext, Activity activity, CancellationToken cancellationToken = default, Dictionary<string, string> telemetryProperties = null, Dictionary<string, double> telemetryMetrics = null)
         {
+            var languagePolicy = LanguagePolicy ??
+                    dialogContext.Services.Get<LanguagePolicy>() ??
+                    new LanguagePolicy();
+
             var policy = new List<string>();
-            if (activity.Locale != null && LanguagePolicy.TryGetValue(activity.Locale, out string[] targetpolicy))
+            if (activity.Locale != null && languagePolicy.TryGetValue(activity.Locale, out string[] targetpolicy))
             {
                 policy.AddRange(targetpolicy);
             }
 
-            if (LanguagePolicy.TryGetValue(string.Empty, out string[] defaultPolicy))
+            if (languagePolicy.TryGetValue(string.Empty, out string[] defaultPolicy))
             {
                 // we now explictly add defaultPolicy instead of coding that into target's policy
                 policy.AddRange(defaultPolicy);
