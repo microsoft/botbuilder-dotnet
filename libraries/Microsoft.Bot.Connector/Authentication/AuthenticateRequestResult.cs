02d4c71005130635f728b30faba418123943d8be
johnataylor <johtaylo@microsoft.com>
2020-10-23 15:57:19 -0700

    Johtaylo/cloudadapter proactive (#4844)
    * initial implementation of cloud adapter proactive
    
    * add proactive support to cloud adapter

--- libraries/Microsoft.Bot.Connector/Authentication/AuthenticateRequestResult.cs
+++ libraries/Microsoft.Bot.Connector/Authentication/AuthenticateRequestResult.cs
@@ -2,14 +2,13 @@
 // Licensed under the MIT License.
 
 using System.Security.Claims;
-using Microsoft.Rest;
 
 namespace Microsoft.Bot.Connector.Authentication
 {
     /// <summary>
     /// The result from a call to authenticate a Bot Framework Protocol request.
     /// </summary>
-    public class AuthenticateRequestResult
+    public class AuthenticateRequestResult : ProactiveCredentialsResult
     {
         /// <summary>
         /// Gets or sets a value for the ClaimsIdentity.
@@ -18,22 +17,6 @@ namespace Microsoft.Bot.Connector.Authentication
         /// A value for the ClaimsIdentity.
         /// </value>
         public ClaimsIdentity ClaimsIdentity { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value for the Credentials.
-        /// </summary>
-        /// <value>
-        /// A value for the Credentials.
-        /// </value>
-        public ServiceClientCredentials Credentials { get; set; }
-
-        /// <summary>
-        /// Gets or sets a value for the Scope.
-        /// </summary>
-        /// <value>
-        /// A value for the Scope.
-        /// </value>
-        public string Scope { get; set; }
 
         /// <summary>
         /// Gets or sets a value for the CallerId.
