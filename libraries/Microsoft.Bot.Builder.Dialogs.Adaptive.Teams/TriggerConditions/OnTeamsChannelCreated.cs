07554cd1745017e0ace91786429b2a064b543a84
Tom Laird-McConnell <tomlm@microsoft.com>
2020-11-10 13:39:57 -0800

    OnCondition is not correctly caching expressions causing constant expression.Parse calls (#4935)
    * GetExpression() was only caching the root expresion parsing, all child conditions which overrode GetExpression() were callin Expression.Parse() on EVERY call, causing us to parse and allocate Expression obejcts every time someone asked for the expression.  This creates a  huge overhead for evaluating triggers.  This was an architectural regression introduced in refactoring in 2019 via commit fe543724, and fixes the problem by having GetExpression() clearly documented as returning a CACHED expression, and moving all creation logic to protected virtual method CreateExpression().  This should be backward compatible with classes which do override this method but there shouldn't be because custom triggers is not something our community has been active in.
    
    * add unit test
    
    * double registerSourceLocation causes exception in OnContinueConversation
    
    * Fix unit test

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Teams/TriggerConditions/OnTeamsChannelCreated.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Teams/TriggerConditions/OnTeamsChannelCreated.cs
@@ -27,10 +27,11 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Teams
         {
         }
 
-        public override Expression GetExpression()
+        /// <inheritdoc/>
+        protected override Expression CreateExpression()
         {
             // if teams channel and eventType == 'channelCreated'
-            return Expression.AndExpression(Expression.Parse($"{TurnPath.Activity}.ChannelId == '{Channels.Msteams}' && {TurnPath.Activity}.channelData.eventType == 'channelCreated'"), base.GetExpression());
+            return Expression.AndExpression(Expression.Parse($"{TurnPath.Activity}.ChannelId == '{Channels.Msteams}' && {TurnPath.Activity}.channelData.eventType == 'channelCreated'"), base.CreateExpression());
         }
     }
 }
