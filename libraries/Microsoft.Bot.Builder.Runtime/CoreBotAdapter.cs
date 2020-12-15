2e409c37d48eb3411bce1ee947d3d85e01ec031a
tracyboehrer <tracyboehrer@users.noreply.github.com>
2020-12-15 08:33:00 -0600

    Runtime: Refactored CoreBotAdapter to allow more DI options

--- libraries/Microsoft.Bot.Builder.Runtime/CoreBotAdapter.cs
+++ libraries/Microsoft.Bot.Builder.Runtime/CoreBotAdapter.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Net.Http;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.Runtime.Builders.Middleware;
 using Microsoft.Bot.Builder.Runtime.Settings;
@@ -10,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.Rest.TransientFaultHandling;
 
 namespace Microsoft.Bot.Builder.Runtime
 {
@@ -22,17 +24,25 @@ namespace Microsoft.Bot.Builder.Runtime
         /// Initializes a new instance of the <see cref="CoreBotAdapter"/> class.
         /// </summary>
         /// <param name="services">Services registered with the application.</param>
-        /// <param name="configuration">Application configuration.</param>
         /// <param name="options">Configured options for the <see cref="CoreBotAdapter"/> instance.</param>
+        /// <param name="configuration">Application configuration.</param>
+        /// <param name="credentialProvider">The credential provider.</param>
+        /// <param name="authConfig">The authentication configuration.</param>
+        /// <param name="channelProvider">The channel provider.</param>
+        /// <param name="connectorClientRetryPolicy">Retry policy for retrying HTTP operations.</param>
+        /// <param name="customHttpClient">The HTTP client.</param>
+        /// <param name="logger">The ILogger implementation this adapter should use.</param>
         public CoreBotAdapter(
             IServiceProvider services,
+            IOptions<CoreBotAdapterOptions> options,
             IConfiguration configuration,
-            IOptions<CoreBotAdapterOptions> options)
-            : base(
-                services.GetService<ICredentialProvider>(),
-                services.GetService<AuthenticationConfiguration>(),
-                services.GetService<IChannelProvider>(),
-                logger: services.GetService<ILogger<BotFrameworkHttpAdapter>>())
+            ICredentialProvider credentialProvider = null,
+            AuthenticationConfiguration authConfig = null,
+            IChannelProvider channelProvider = null,
+            RetryPolicy connectorClientRetryPolicy = null,
+            HttpClient customHttpClient = null,
+            ILogger logger = null)
+            : base(configuration, credentialProvider: credentialProvider, authConfig: authConfig, channelProvider: channelProvider, connectorClientRetryPolicy: connectorClientRetryPolicy, customHttpClient: customHttpClient, logger: logger)
         {
             var conversationState = services.GetService<ConversationState>();
             var userState = services.GetService<UserState>();
