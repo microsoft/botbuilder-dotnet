d2e13178be06a2246500468c0bb1b357b3991a0f
Ashley Finafrock <v-asho@microsoft.com>
2020-12-23 10:54:40 -0800

    Created AdaptiveRecognizer class; ValueRecognizer now derives from AdaptiveRecognizer

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/Recognizers/AdaptiveRecognizer.cs
@@ -0,0 +1,87 @@
+﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers
+{
+    /// <summary>
+    /// Recognizer implementation, intended to be a base class for adaptive recognizers.
+    /// </summary>
+    public class AdaptiveRecognizer : Recognizer
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AdaptiveRecognizer"/> class.
+        /// </summary>
+        /// <param name="callerPath">Optional, source file full path.</param>
+        /// <param name="callerLine">Optional, line number in source file.</param>
+        [JsonConstructor]
+        public AdaptiveRecognizer([CallerFilePath] string callerPath = "", [CallerLineNumber] int callerLine = 0)
+            : base(callerPath, callerLine)
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the flag to determine if personal information should be logged in telemetry.
+        /// </summary>
+        /// <value>
+        /// The flag to indicate in personal information should be logged in telemetry.
+        /// </value>
+        [JsonProperty("logPersonalInformation")]
+        public BoolExpression LogPersonalInformation { get; set; } = "=settings.telemetry.logPersonalInformation";
+
+        /// <summary>
+        /// Uses the RecognizerResult to create a list of propeties to be included when tracking the result in telemetry.
+        /// </summary>
+        /// <param name="recognizerResult">Recognizer Result.</param>
+        /// <param name="telemetryProperties">A list of properties to append or override the properties created using the RecognizerResult.</param>
+        /// <param name="dialogContext">Dialog Context.</param>
+        /// <returns>A dictionary that can be included when calling the TrackEvent method on the TelemetryClient.</returns>
+        protected override Dictionary<string, string> FillRecognizerResultTelemetryProperties(RecognizerResult recognizerResult, Dictionary<string, string> telemetryProperties, DialogContext dialogContext = null)
+        {
+            if (dialogContext == null)
+            {
+                throw new ArgumentNullException(nameof(dialogContext), "DialogContext needed for state in AdaptiveRecognizer.FillRecognizerResultTelemetryProperties method.");
+            }
+
+            var properties = new Dictionary<string, string>
+            {
+                { "AlteredText", recognizerResult.AlteredText },
+                { "TopIntent", recognizerResult.Intents.Any() ? recognizerResult.Intents.First().Key : null },
+                { "TopIntentScore", recognizerResult.Intents.Any() ? recognizerResult.Intents.First().Value?.ToString() : null },
+                { "Intents", recognizerResult.Intents.Any() ? JsonConvert.SerializeObject(recognizerResult.Intents) : null },
+                { "Entities", recognizerResult.Entities != null ? recognizerResult.Entities.ToString() : null },
+                { "AdditionalProperties", recognizerResult.Properties.Any() ? JsonConvert.SerializeObject(recognizerResult.Properties) : null },
+            };
+
+            var (logPersonalInfo, error) = this.LogPersonalInformation.TryGetValue(dialogContext.State);
+            
+            if (logPersonalInfo && !string.IsNullOrEmpty(dialogContext.Context.Activity.Text))
+            {
+                properties.Add("Text", dialogContext.Context.Activity.Text);
+            }
+
+            // Additional Properties can override "stock" properties.
+            if (telemetryProperties != null)
+            {
+                return telemetryProperties.Concat(properties)
+                    .GroupBy(kv => kv.Key)
+                    .ToDictionary(g => g.Key, g => g.First().Value);
+            }
+
+            return properties;
+        }        
+    }
+}
