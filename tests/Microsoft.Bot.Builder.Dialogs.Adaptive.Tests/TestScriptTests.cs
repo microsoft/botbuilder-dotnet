a80ed935ed916885ee7eebeb479a613ce94c142e
Michael Richardson <v-micric@microsoft.com>
2021-01-13 16:20:39 -0800

    add UserActivity test

--- tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/TestScriptTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/TestScriptTests.cs
@@ -140,6 +140,12 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
         }
 
         [Fact]
+        public async Task TestScriptTests_UserActivity()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
         public async Task TestScriptTests_UserConversationUpdate()
         {
             await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
