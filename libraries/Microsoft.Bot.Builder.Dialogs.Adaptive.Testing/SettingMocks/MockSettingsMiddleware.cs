e90ba0b131c0e24d82e2c9ca2cb4d6fce272db33
xieofxie <xieofxie@126.com>
2021-01-16 01:32:01 +0800

    use logic as SetProperties; handle settings specially; update & fix test (#4720)
    * use logic as SetProperties; handle settings specially; update & fix test
    
    * add SettingMocks
    
    * updates and schema
    
    * update test
    
    * update schema by TestDialogResourcesAreValidForSchema
    
    * update schema by TestDialogResourcesAreValidForSchema
    
    * update schema by TestDialogResourcesAreValidForSchema
    
    * update schema by TestDialogResourcesAreValidForSchema
    
    * use ConcurrentDictionary for SourceMap
    
    * update schema by TestDialogResourcesAreValidForSchema
    
    * update schema by TestDialogResourcesAreValidForSchema
    
    * update schema by TestDialogResourcesAreValidForSchema
    
    why does it always change a lot?
    
    * remove schemas
    
    * add back using @microsoft/botframework-cli@next
    
    Or tests in Mac won't pass
    
    * should bf plugins:install @microsoft/bf-dialog directly
    
    now 4.11.1

---
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Testing/SettingMocks/MockSettingsMiddleware.cs
@@ -0,0 +1,79 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Testing.SettingMocks
+{
+    /// <summary>
+    /// Middleware which injests mocked settings properties.
+    /// </summary>
+    public class MockSettingsMiddleware : IMiddleware
+    {
+        private readonly Dictionary<string, string> mockData = new Dictionary<string, string>();
+        private bool configured = false;
+        private IConfiguration configuredConfiguration = null;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MockSettingsMiddleware"/> class.
+        /// </summary>
+        /// <param name="settingMocks">Settings to mock.</param>
+        public MockSettingsMiddleware(List<SettingMock> settingMocks)
+        {
+            foreach (var property in settingMocks)
+            {
+                if (property is SettingStringMock mock)
+                {
+                    foreach (var assignment in mock.Assignments)
+                    {
+                        // Note that settings use : as separator in ConfigurationBuilder.
+                        var newProperty = assignment.Property.Replace('.', ':');
+                        if (mockData.ContainsKey(assignment.Property))
+                        {
+                            mockData[newProperty] = assignment.Value;
+                        }
+                        else
+                        {
+                            mockData.Add(newProperty, assignment.Value);
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task OnTurnAsync(ITurnContext turnContext, NextDelegate next, CancellationToken cancellationToken = default)
+        {
+            if (!configured)
+            {
+                if (mockData.Count != 0)
+                {
+                    IConfigurationBuilder configBuilder = new ConfigurationBuilder();
+
+                    // We assume bot will use TurnState to store settings' configuration.
+                    var previousConfig = turnContext.TurnState.Get<IConfiguration>();
+                    if (previousConfig != null)
+                    {
+                        configBuilder = configBuilder.AddConfiguration(previousConfig);
+                    }
+
+                    configBuilder.AddInMemoryCollection(mockData);
+                    configuredConfiguration = configBuilder.Build();
+                    mockData.Clear();
+                }
+
+                configured = true;
+            }
+
+            if (configuredConfiguration != null)
+            {
+                turnContext.TurnState.Set<IConfiguration>(configuredConfiguration);
+            }
+
+            await next(cancellationToken).ConfigureAwait(false);
+        }
+    }
+}
--- libraries/Microsoft.Bot.Builder.Dialogs/Debugging/SourceMap.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/Debugging/SourceMap.cs
@@ -1,18 +1,24 @@
 ﻿// Licensed under the MIT License.
 // Copyright (c) Microsoft Corporation. All rights reserved.
 
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Microsoft.Bot.Builder.Dialogs.Debugging
 {
     /// <summary>
     /// A simple ISourceMap of objects -> SourceRange.
     /// </summary>
-    public class SourceMap : Dictionary<object, SourceRange>, ISourceMap
+    public class SourceMap : ConcurrentDictionary<object, SourceRange>, ISourceMap
     {
         /// <summary>
         /// Initializes a read-only new instance of the <see cref="SourceMap"/>.
         /// </summary>
         public static readonly SourceMap Instance = new SourceMap();
+
+        /// <inheritdoc/>
+        public void Add(object item, SourceRange range)
+        {
+            this.AddOrUpdate(item, (key) => range, (key, prev) => range);
+        }
     }
 }
