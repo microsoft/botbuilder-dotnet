3cd0577ecb1b5854f59022931bdb4c4183248b40
Eric Dahlvang <erdahlva@microsoft.com>
2021-01-19 09:42:01 -0800

    Update ExpressionExtensions.cs

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Teams/Actions/ExpressionExtensions.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Teams/Actions/ExpressionExtensions.cs
@@ -9,24 +9,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Teams.Actions
 {
     internal static class ExpressionExtensions
     {
-//#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
-//        internal static T? GetValueOrNull<T>(this ExpressionProperty<T> expression, DialogStateManager state)
-//#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
-//        {
-//            if (expression != null)
-//            {
-//                var (value, valueError) = expression.TryGetValue(state);
-//                if (valueError != null)
-//                {
-//                    throw new Exception($"Expression evaluation resulted in an error. Expression: {expression.ExpressionText}. Error: {valueError}");
-//                }
-
-//                return value;
-//            }
-
-//            return default(T);
-//        }
-
         internal static int? GetValueOrNull(this IntExpression expression, DialogStateManager state)
         {
             if (expression != null)
