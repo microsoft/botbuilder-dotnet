e66dff1d230cc241bb28dd6c3f06791332aa5844
Ashley Finafrock <v-asho@microsoft.com>
2021-01-25 14:46:20 -0800

    Merged EntityEx --> Entity

--- libraries/Microsoft.Bot.Schema/Entity.cs
+++ libraries/Microsoft.Bot.Schema/Entity.cs
@@ -3,8 +3,10 @@
 
 namespace Microsoft.Bot.Schema
 {
+    using System;
     using System.Linq;
     using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
 
     /// <summary>
     /// Metadata object pertaining to an activity.
@@ -32,6 +34,90 @@ namespace Microsoft.Bot.Schema
         [JsonProperty(PropertyName = "type")]
         public string Type { get; set; }
 
+        /// <summary>
+        /// Gets or sets properties that are not otherwise defined by the <see cref="Entity"/> type but that
+        /// might appear in the REST JSON object.
+        /// </summary>
+        /// <value>The extended properties for the object.</value>
+        /// <remarks>With this, properties not represented in the defined type are not dropped when
+        /// the JSON object is deserialized, but are instead stored in this property. Such properties
+        /// will be written to a JSON object when the instance is serialized.</remarks>
+        [JsonExtensionData(ReadData = true, WriteData = true)]
+#pragma warning disable CA2227 // Collection properties should be read only (we can't change this without breaking binary compat)
+        public JObject Properties { get; set; } = new JObject();
+#pragma warning restore CA2227 // Collection properties should be read only
+
+        /// <summary>
+        /// Retrieve internal payload.
+        /// </summary>
+        /// <typeparam name="T">T.</typeparam>
+        /// <returns>T as T.</returns>
+        public T GetAs<T>()
+        {
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(this));
+        }
+
+        /// <summary>
+        /// Set internal payload.
+        /// </summary>
+        /// <typeparam name="T">T.</typeparam>
+        /// <param name="obj">obj.</param>
+        public void SetAs<T>(T obj)
+        {
+            var entity = JsonConvert.DeserializeObject<Entity>(JsonConvert.SerializeObject(obj));
+            this.Type = entity.Type;
+            this.Properties = entity.Properties;
+        }
+
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">The other object to compair against.</param>
+        /// <returns>true if the current object is equal to the other parameter, otherwise false.</returns>
+        public bool Equals(Entity other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return JsonConvert.SerializeObject(this).Equals(JsonConvert.SerializeObject(other), StringComparison.Ordinal);
+        }
+
+        /// <summary>
+        /// Determines whether the specifid object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The other object to compair against.</param>
+        /// <returns>true if the current object is equal to the obj parameter, otherwise false.</returns>
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj))
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            return Equals(obj as Entity);
+        }
+
+        /// <summary>
+        /// Hash function that generates a hash code for the current object.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+        
         /// <summary>
         /// An initialization method that performs custom operations like setting defaults.
         /// </summary>
