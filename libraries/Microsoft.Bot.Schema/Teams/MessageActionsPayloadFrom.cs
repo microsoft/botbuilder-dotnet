c6f306b9e20547e6a51147384f6f5f8b9458e3a0
Ashley Finafrock <35248895+Zerryth@users.noreply.github.com>
2021-01-25 08:54:56 -0800

    (dotnet) Remove comments pertaining to auto-generation in Schema and Connector (#5002)
    * Removed comments regarding auto-generation in Schema & Connector
    
    * Removed misleading Microsoft.Bot.Schema/Teams/Generated folder; *.cs files there moved to Teams/
    
    * Fixed some style cop errors (spacing, periods in docs, using statement reorder, etc.)
    
    * Fixed more stylecop errors (spacing, standard texts, etc.)
    
    * Disable CA2227 warnings (collection props should be readonly)
    
    * Attachment and AttachmentData prop. doc <value>
    
    * AttachmentInfo, AttachmentView, AudioCard <value>'s added
    
    * Added <value>'s in ChannelAccount, ConvoAccount, ConvoMembers
    
    * Added <value>'s for ConversationParameter and ConversationReference
    
    * <value>'s added for AnimationCard, ConvoRef, ConvoResourceRes, ConvoRes
    
    * <value>'s added in: CardImage, Entity, Error, ErrorRes, ExpectedReplies, Fact, GeoCoord, HeroCard
    
    * More <value>'s added to Schema
    
    * Added <value>'s to PaymentItem, PaymentMethodData, PaymentOptions, PaymentRequest
    
    * Added <value>'s to PaymentReqComplete, PayReqCompleteRes, PaymentReqUpdate, PaymentReqUpdateRes, PaymentResponse
    
    * Added <value>'s to PaymentShipppingOption, Place, and ReceiptCard
    
    * Added <value>'s to ReceiptItem, ResourceResponse, SemanticAction, & SigninCard
    
    * Added <value>'s to SignInResource, SuggestedActions, & AppBasedLinkQuery
    
    * Added <value>'s to ChannelInfo, ConvoList, and FileConsentCardRes
    
    * Added <value>'s to FileDownloadInfo, FileInfoCard, FileUploadInfo
    
    * Added <value>'s to MessageActionPayload, MessageActionsPayloadApp, MessageActionsPayloadAttachment
    
    * Added <value>'s to MessageActionPayload -Body, -Conversation, -From, and -Mention
    
    * Added <value>'s to MessageActionPayload -Reactions and -User, and MessageExtensionAction
    
    * Added <values>'s to MessagingExtension -ActionResponse, -Attachment, -Parameter, -Query, -QueryOptions, -Response, -Result, -SuggestedAction
    
    * Added <value>'s to NotificationInfo
    
    * Added <value>'s to O365ConnectorCard
    
    * <value>'s for O365ConnectorCard classes including: -ActionCard, -ActionQuery, -DateInput, -Fact, -HttpPOST, -CardImage, -InputBase, and more
    
    * <value>'s for SigninStateVerificationQuery, TaskModuleContinueReesponse, TaskModuleMessageResponse
    
    * <value>'s added to TaskModule: -Request, -RequestContext, -Response, -ResponseBase, -Info
    
    * Finished adding remaining <value>'s missing in Schema
    
    * Corrected some styles in: Attachments, AttachmentsExtensions, BotSignIn, BotSignInExtensions, ConnectorClient, Transcript, VideoCard
    
    * Corrected styles for Conversations.cs
    
    * Corrected styles for ConversationsExtensions.cs
    
    * Corrected styles for ErrorResponseException, OAuthClient, ITeamsConnectorClient, ITeamsOperations
    
    * Corrected styles for Teams -ConnectorClient, -Operations, -OperationExtensions
    
    * Corrected styles for UserToken & TeamsConnectorClient
    
    * Corrected styles for Attachemnts, Conversations, and ITeamsConnectorClient
    
    * Specified IFormatProvider & StringComparison for Attachemnts, BotSignIn, and Conversations
    
    * Added IFormatProvider to string.Format in BotSignIn, Conversations, OAuthClient, UserToken, TeamsOperations
    
    * Updated ValidationError doc -> removed disabled pragma SA1625

---
+++ libraries/Microsoft.Bot.Schema/Teams/MessageActionsPayloadFrom.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Schema.Teams
+{
+    using System.Linq;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Represents a user, application, or conversation type that either sent
+    /// or was referenced in a message.
+    /// </summary>
+    public partial class MessageActionsPayloadFrom
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageActionsPayloadFrom"/> class.
+        /// </summary>
+        public MessageActionsPayloadFrom()
+        {
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessageActionsPayloadFrom"/> class.
+        /// </summary>
+        /// <param name="user">Represents details of the user.</param>
+        /// <param name="application">Represents details of the app.</param>
+        /// <param name="conversation">Represents details of the
+        /// converesation.</param>
+        public MessageActionsPayloadFrom(MessageActionsPayloadUser user = default(MessageActionsPayloadUser), MessageActionsPayloadApp application = default(MessageActionsPayloadApp), MessageActionsPayloadConversation conversation = default(MessageActionsPayloadConversation))
+        {
+            User = user;
+            Application = application;
+            Conversation = conversation;
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Gets or sets represents details of the user.
+        /// </summary>
+        /// <value>The user details.</value>
+        [JsonProperty(PropertyName = "user")]
+        public MessageActionsPayloadUser User { get; set; }
+
+        /// <summary>
+        /// Gets or sets represents details of the app.
+        /// </summary>
+        /// <value>The application details.</value>
+        [JsonProperty(PropertyName = "application")]
+        public MessageActionsPayloadApp Application { get; set; }
+
+        /// <summary>
+        /// Gets or sets represents details of the converesation.
+        /// </summary>
+        /// <value>The conversation details.</value>
+        [JsonProperty(PropertyName = "conversation")]
+        public MessageActionsPayloadConversation Conversation { get; set; }
+
+        /// <summary>
+        /// An initialization method that performs custom operations like setting defaults.
+        /// </summary>
+        partial void CustomInit();
+    }
+}
---
+++ libraries/Microsoft.Bot.Schema/Teams/MessagingExtensionAction.cs
@@ -0,0 +1,102 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.Bot.Schema.Teams
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Newtonsoft.Json;
+
+    /// <summary>
+    /// Messaging extension action.
+    /// </summary>
+    public partial class MessagingExtensionAction : TaskModuleRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessagingExtensionAction"/> class.
+        /// </summary>
+        public MessagingExtensionAction()
+        {
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MessagingExtensionAction"/> class.
+        /// </summary>
+        /// <param name="data">User input data. Free payload with key-value
+        /// pairs.</param>
+        /// <param name="context">Current user context, i.e., the current
+        /// theme.</param>
+        /// <param name="commandId">Id of the command assigned by Bot.</param>
+        /// <param name="commandContext">The context from which the command
+        /// originates. Possible values include: 'message', 'compose',
+        /// 'commandbox'.</param>
+        /// <param name="botMessagePreviewAction">Bot message preview action
+        /// taken by user. Possible values include: 'edit', 'send'.</param>
+        /// <param name="botActivityPreview">A collection of bot activities.</param>
+        /// <param name="messagePayload">Message content sent as part of the
+        /// command request.</param>
+        public MessagingExtensionAction(object data = default(object), TaskModuleRequestContext context = default(TaskModuleRequestContext), string commandId = default(string), string commandContext = default(string), string botMessagePreviewAction = default(string), IList<Activity> botActivityPreview = default(IList<Activity>), MessageActionsPayload messagePayload = default(MessageActionsPayload))
+            : base(data, context)
+        {
+            CommandId = commandId;
+            CommandContext = commandContext;
+            BotMessagePreviewAction = botMessagePreviewAction;
+            BotActivityPreview = botActivityPreview;
+            MessagePayload = messagePayload;
+            CustomInit();
+        }
+
+        /// <summary>
+        /// Gets or sets ID of the command assigned by Bot.
+        /// </summary>
+        /// <value>The ID of the command assigned by the Bot.</value>
+        [JsonProperty(PropertyName = "commandId")]
+        public string CommandId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the context from which the command originates.
+        /// Possible values include: 'message', 'compose', 'commandbox'.
+        /// </summary>
+        /// <value>The context from which the command originates.</value>
+        [JsonProperty(PropertyName = "commandContext")]
+        public string CommandContext { get; set; }
+
+        /// <summary>
+        /// Gets or sets bot message preview action taken by user. Possible
+        /// values include: 'edit', 'send'.
+        /// </summary>
+        /// <value>The bot message preview action taken by the user.</value>
+        [JsonProperty(PropertyName = "botMessagePreviewAction")]
+        public string BotMessagePreviewAction { get; set; }
+
+        /// <summary>
+        /// Gets or sets the bot activity preview.
+        /// </summary>
+        /// <value>The bot activity preview.</value>
+        [JsonProperty(PropertyName = "botActivityPreview")]
+#pragma warning disable CA2227 // Collection properties should be read only (we can't change this without breaking compat).
+        public IList<Activity> BotActivityPreview { get; set; }
+#pragma warning restore CA2227 // Collection properties should be read only
+
+        /// <summary>
+        /// Gets or sets message content sent as part of the command request.
+        /// </summary>
+        /// <value>The message content sent as part of the command request.</value>
+        [JsonProperty(PropertyName = "messagePayload")]
+        public MessageActionsPayload MessagePayload { get; set; }
+
+        /// <summary>
+        /// Gets or sets state parameter passed back to the bot after authentication flow.
+        /// </summary>
+        /// <value>The state parameter passed back to the bot after authentication flow.</value>
+        [JsonProperty(PropertyName = "state")]
+        public string State { get; set; }
+
+        /// <summary>
+        /// An initialization method that performs custom operations like setting defaults.
+        /// </summary>
+        partial void CustomInit();
+    }
+}
