e91d950da39cb7edc616aea4e396a3d9532e78fa
hond <hond@microsoft.com>
2021-01-29 10:32:09 +0800

    remove solution 2

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/LanguageGeneratorExtensions.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/LanguageGeneratorExtensions.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Runtime.Caching;
 using System.Runtime.CompilerServices;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Generators;
 using Microsoft.Bot.Builder.Dialogs.Declarative;
@@ -16,14 +14,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
     /// </summary>
     public static class LanguageGeneratorExtensions
     {
-        //------------Solution1------------------------
         private static ConditionalWeakTable<ResourceExplorer, LanguageGeneratorManager> languageGeneratorManagers = new ConditionalWeakTable<ResourceExplorer, LanguageGeneratorManager>();
-
-        //---------------Solution2-------------------------//
-        //private static ObjectCache cache = MemoryCache.Default;
-        //
-        // //Set the expireTime to 1 minutes.
-        //private static readonly int expireMinutes = 1;
 
         /// <summary>
         /// Register default LG file as language generation.
@@ -64,7 +55,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
         {
             var resourceExplorer = dialogManager.InitialTurnState.Get<ResourceExplorer>();
 
-            //-------------------Solution1----------------------
             lock (languageGeneratorManagers)
             {
                 if (!languageGeneratorManagers.TryGetValue(resourceExplorer ?? throw new InvalidOperationException($"Unable to get an instance of {nameof(resourceExplorer)}."), out var lgm))
@@ -78,31 +68,6 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
 
                 return dialogManager;
             }
-
-            //--------------------------------------------------
-
-            //-------------------Solution2----------------------
-            //LanguageGeneratorManager lgm;
-            //var cacheItem = cache.Get(resourceExplorer.GetHashCode() + string.Empty);
-            //if (cacheItem != null)
-            //{
-            //    lgm = new LanguageGeneratorManager(resourceExplorer);
-            //    cache.Add(resourceExplorer.GetHashCode() + string.Empty, lgm, new CacheItemPolicy
-            //    {
-            //        AbsoluteExpiration = new DateTimeOffset(DateTime.Now.AddMinutes(expireMinutes))
-            //    });
-            //}
-            //else
-            //{
-            //    lgm = cacheItem as LanguageGeneratorManager;
-            //}
-
-            //dialogManager.InitialTurnState.Add<LanguageGeneratorManager>(lgm);
-            //dialogManager.InitialTurnState.Add<LanguageGenerator>(languageGenerator ?? throw new ArgumentNullException(nameof(languageGenerator)));
-
-            //return dialogManager;
-
-            //--------------------------------------------------
         }
 
         /// <summary>
