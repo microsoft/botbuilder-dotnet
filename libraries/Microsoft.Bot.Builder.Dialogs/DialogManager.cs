a163e05c62bf387e8b5064925050d4cb11d9b1b0
Gabo Gilabert <gabog@users.noreply.github.com>
2021-02-04 17:16:37 -0500

    Updated DialogManager to return EoC code (as RunAsync does) and updated tests (#5164)

--- libraries/Microsoft.Bot.Builder.Dialogs/DialogManager.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs/DialogManager.cs
@@ -355,10 +355,12 @@ namespace Microsoft.Bot.Builder.Dialogs
             if (ShouldSendEndOfConversationToParent(turnContext, turnResult))
             {
                 // Send End of conversation at the end.
+                var code = turnResult.Status == DialogTurnStatus.Complete ? EndOfConversationCodes.CompletedSuccessfully : EndOfConversationCodes.UserCancelled;
                 var activity = new Activity(ActivityTypes.EndOfConversation)
                 {
                     Value = turnResult.Result,
-                    Locale = turnContext.Activity.Locale
+                    Locale = turnContext.Activity.Locale,
+                    Code = code
                 };
                 await turnContext.SendActivityAsync(activity, cancellationToken).ConfigureAwait(false);
             }
