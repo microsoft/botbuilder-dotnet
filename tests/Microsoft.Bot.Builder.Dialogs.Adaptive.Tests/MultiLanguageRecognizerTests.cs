acf0a032cc1b5320193ce46be97d5179bec77496
Ashley Finafrock <v-asho@microsoft.com>
2021-02-09 10:29:08 -0800

    Converted MultiLanguageRecognizer tests to Theory

--- tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/MultiLanguageRecognizerTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/MultiLanguageRecognizerTests.cs
@@ -17,50 +17,47 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers.Tests
     [CollectionDefinition("Dialogs.Adaptive.Recognizers")]
     public class MultiLanguageRecognizerTests : IClassFixture<ResourceExplorerFixture>
     {
-        private static readonly Lazy<MultiLanguageRecognizer> Recognizer = new Lazy<MultiLanguageRecognizer>(() =>
+        private static readonly MultiLanguageRecognizer Recognizer = new MultiLanguageRecognizer()
         {
-            return new MultiLanguageRecognizer()
-            {
-                Recognizers = new ConcurrentDictionary<string, Recognizer>(
-                    new Dictionary<string, Recognizer>()
+            Recognizers = new ConcurrentDictionary<string, Recognizer>(
+                new Dictionary<string, Recognizer>()
+                {
                     {
+                        "en-us",
+                        new RegexRecognizer()
                         {
-                            "en-us",
-                            new RegexRecognizer()
+                            Intents = new List<IntentPattern>()
                             {
-                                Intents = new List<IntentPattern>()
-                                {
-                                    new IntentPattern("Greeting", "(?i)howdy"),
-                                    new IntentPattern("Goodbye", "(?i)bye")
-                                }
-                            }
-                        },
-                        {
-                            "en-gb",
-                            new RegexRecognizer()
-                            {
-                                Intents = new List<IntentPattern>()
-                                {
-                                    new IntentPattern("Greeting", "(?i)hiya"),
-                                    new IntentPattern("Goodbye", "(?i)cheerio")
-                                }
-                            }
-                        },
-                        {
-                            "en",
-                            new RegexRecognizer()
-                            {
-                                Intents = new List<IntentPattern>()
-                                {
-                                    new IntentPattern("Greeting", "(?i)hello"),
-                                    new IntentPattern("Goodbye", "(?i)goodbye")
-                                }
+                                new IntentPattern("Greeting", "(?i)howdy"),
+                                new IntentPattern("Goodbye", "(?i)bye")
                             }
                         }
-                    })
-            };
-        });
-        
+                    },
+                    {
+                        "en-gb",
+                        new RegexRecognizer()
+                        {
+                            Intents = new List<IntentPattern>()
+                            {
+                                new IntentPattern("Greeting", "(?i)hiya"),
+                                new IntentPattern("Goodbye", "(?i)cheerio")
+                            }
+                        }
+                    },
+                    {
+                        "en",
+                        new RegexRecognizer()
+                        {
+                            Intents = new List<IntentPattern>()
+                            {
+                                new IntentPattern("Greeting", "(?i)hello"),
+                                new IntentPattern("Goodbye", "(?i)goodbye")
+                            }
+                        }
+                    }
+                })
+        };
+
         private readonly ResourceExplorerFixture _resourceExplorerFixture;
 
         public MultiLanguageRecognizerTests(ResourceExplorerFixture resourceExplorerFixture)
@@ -104,33 +101,24 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Recognizers.Tests
             await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
         }
 
-        [Fact]
-        public async Task MultiLanguageRecognizerTest_Telemetry_LogsPii_WhenTrue()
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task MultiLanguageRecognizerTest_LogsTelemetry(bool logPersonalInformation)
         {
             var telemetryClient = new Mock<IBotTelemetryClient>();
-            var recognizer = Recognizer.Value;
+            var recognizer = Recognizer;
             recognizer.TelemetryClient = telemetryClient.Object;
-            recognizer.LogPersonalInformation = true;
-
-            await RecognizeIntentAndValidateTelemetry_WithCustomActivity(GreetingIntentTextEnUs, recognizer, telemetryClient, callCount: 1);
-        }
-        
-        [Fact]
-        public async Task MultiLanguageRecognizerTest_Telemetry_DoesNotLogPii_WhenFalse()
-        {
-            var telemetryClient = new Mock<IBotTelemetryClient>();
-            var recognizer = Recognizer.Value;
-            recognizer.TelemetryClient = telemetryClient.Object;
-            recognizer.LogPersonalInformation = false;
+            recognizer.LogPersonalInformation = logPersonalInformation;
 
             await RecognizeIntentAndValidateTelemetry_WithCustomActivity(GreetingIntentTextEnUs, recognizer, telemetryClient, callCount: 1);
         }
 
         [Fact]
-        public async Task MultiLanguageRecognizerTest_LogPii_IsFalseByDefault()
+        public async Task MultiLanguageRecognizerTest_LogPiiIsFalseByDefault()
         {
             var telemetryClient = new Mock<IBotTelemetryClient>();
-            var recognizer = Recognizer.Value;
+            var recognizer = Recognizer;
             recognizer.TelemetryClient = telemetryClient.Object;
             
             var dc = TestUtils.CreateContext(GreetingIntentTextEnUs);
