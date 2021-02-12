8bfaf91f125e49470593659f8f8addc4f7c42e71
Eric Dahlvang <erdahlva@microsoft.com>
2021-02-11 20:03:37 -0800

    Exclude more schema tests

--- tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/SchemaMergeTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/SchemaMergeTests.cs
@@ -96,6 +96,10 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Tests
                 "Action_SendTaskModuleCardResponseError.test.dialog",
                 "Action_SendAppBasedLinkQueryResponseError.test.dialog",
                 "Action_SendTabCardResponseError.test.dialog",
+                "Action_SendMessagingExtensionAuthResponseError.test.dialog",
+                "Action_SendMessagingExtensionSelectItemResponseError.test.dialog",
+                "Action_SendMessagingExtensionAuthResponseErrorWithAdapter.test.dialog",
+                "Action_SendMessagingExtensionMessageResponseError.test.dialog",
                 "TestScriptTests_OAuthInputLG.test.dialog"
             };
             if (omit.Any(e => fileResource.FullName.Contains(e)))
