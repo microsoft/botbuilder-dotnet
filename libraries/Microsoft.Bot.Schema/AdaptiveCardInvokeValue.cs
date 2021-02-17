c17b77a0fcb731df26a12f217b91954d4fd858a0
Eric Dahlvang <erdahlva@microsoft.com>
2021-02-16 17:01:10 -0800

    Add OnAdaptiveCardInvokeAsync to ActivityHandler (#5227)
    * Add OnAdaptiveCardInvokeAsync to ActivityHandler
    
    * Add TestOnAdaptiveCardInvokeAsync
    
    * Add state to AdaptiveCardInvokeValue

---
+++ libraries/Microsoft.Bot.Schema/AdaptiveCardInvokeValue.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Schema
+{
+    /// <summary>
+    /// Defines the structure that arrives in the Activity.Value for Invoke activity with Name of 'adaptiveCard/action'.
+    /// </summary>
+    public class AdaptiveCardInvokeValue
+    {
+        /// <summary>
+        /// Gets or sets the action of this adaptive card action invoke value.
+        /// </summary>
+        /// <value>
+        /// The action of this adaptive card invoke action value.
+        /// </value>
+        [JsonProperty("action")]
+        public AdaptiveCardInvokeAction Action { get; set; }
+
+        /// <summary>
+        /// Gets or sets the <see cref="AdaptiveCardAuthentication"/> for this adaptive card invoke action value.
+        /// </summary>
+        /// <value>
+        /// The <see cref="AdaptiveCardAuthentication"/> for this adaptive card invoke action value.
+        /// </value>
+        [JsonProperty("authentication")]
+        public AdaptiveCardAuthentication Authentication { get; set; }
+
+        /// <summary>
+        /// Gets or sets the 'state' or magic code for an OAuth flow.
+        /// </summary>
+        /// <value>
+        /// The 'state' or magic code for an OAuth flow.
+        /// </value>
+        [JsonProperty("state")]
+        public string State { get; set; }
+    }
+}
