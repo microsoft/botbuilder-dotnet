fbb4eb8b98ccce17ebcfc8e061237def33a68305
Eric Dahlvang <erdahlva@microsoft.com>
2021-03-12 14:37:34 -0800

    Cherry-Pick to 4.12: Change ChoiceSet to support LG (#5309) (#5312)
    * Change ChoiceSet to support LG (#5309)
    
    * Make ChoiceSet  implement ITemplate<ChoiceSet>
    Fix ExpressionProperty Throw in Try method that shouldn 't throw
    Updated ConfirmInput and ChoiceInput to use ITemplate>.BindData() if expression is template reference.
    Added unit tests
    
    * update schema
    
    * fix schema definition for choices
    
    * Fix warnings as errors (#5274)
    
    Co-authored-by: Tom Laird-McConnell <tomlm@microsoft.com>

--- tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/ActionTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/ActionTests.cs
@@ -114,6 +114,30 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
         }
 
         [Fact]
+        public async Task Action_ChoiceInput_SimpleTemplate_en()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_ChoiceInput_SimpleTemplate_es()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_ChoiceInput_ComplexTemplate_en()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_ChoiceInput_ComplexTemplate_es()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
         public async Task Action_ChoicesInMemory()
         {
             await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
@@ -127,6 +151,30 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
 
         [Fact]
         public async Task Action_ConfirmInput()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_ConfirmInput_SimpleTemplate_en()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_ConfirmInput_ComplexTemplate_en()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_ConfirmInput_SimpleTemplate_es()
+        {
+            await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+        }
+
+        [Fact]
+        public async Task Action_ConfirmInput_ComplexTemplate_es()
         {
             await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
         }
