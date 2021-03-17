a66cccbe5b199a4e82342d4a27f4decf2f428557
Eric Dahlvang <erdahlva@microsoft.com>
2021-03-16 20:09:11 -0700

    Add Command and CommandResult activity types

---
+++ libraries/Microsoft.Bot.Schema/CommandResultValue.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Newtonsoft.Json;
+
+namespace Microsoft.Bot.Schema
+{
+    /// <summary>
+    /// The value field of a <see cref="ICommandResultActivity"/> contains metadata related to a command result.
+    /// An optional extensible data payload may be included if defined by the command result activity name. 
+    /// The presence of an error field indicates that the original command failed to complete.
+    /// </summary>
+    public class CommandResultValue
+    {
+        /// <summary>
+        /// Gets or sets the id of the command.
+        /// </summary>
+        /// <value>
+        /// Id of the command.
+        /// </value>
+        [JsonProperty(PropertyName = "commandId")]
+        public string CommandId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data field containing optional parameters specific to this command result activity,
+        /// as defined by the name. The value of the data field is a complex type.
+        /// </summary>
+        /// <value>
+        /// Open-ended value.
+        /// </value>
+        [JsonProperty(PropertyName = "data")]
+        public object Data { get; set; }
+
+        /// <summary>
+        /// Gets or sets the optional error, if the command result indicates a failure.
+        /// </summary>
+        /// <value>
+        /// Error which occurred during processing of the command.
+        /// </value>
+        [JsonProperty(PropertyName = "error")]
+        public Error Error { get; set; }
+    }
+}
