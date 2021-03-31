6c42c794a8da6557233ab0ed221c6baa5a9d8a9d
carlosscastro <ccastro@microsoft.com>
2021-03-31 08:58:16 -0700

    Settings scope: support backward compat setting scope, remove ComponentRegistration in DialogStateManager

--- tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/Startup.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Declarative.Tests/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Bot.Builder.AI.QnA;
+using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
 using Microsoft.Bot.Builder.Dialogs.Declarative;
 using Xunit;
@@ -14,6 +15,7 @@ namespace Microsoft.Bot.Builder
         public Startup(IMessageSink messageSink)
             : base(messageSink)
         {
+            ComponentRegistration.Add(new DialogsComponentRegistration());
             ComponentRegistration.Add(new DeclarativeComponentRegistration());
             ComponentRegistration.Add(new AdaptiveComponentRegistration());
             ComponentRegistration.Add(new LanguageGenerationComponentRegistration());
