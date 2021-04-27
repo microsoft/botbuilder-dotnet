66c08e9c03dc25b77e2b8c64df0e538b8df35f69
Gary Pretty <gary@garypretty.co.uk>
2021-04-27 21:15:26 +0100

    Update Adaptive Runtime to add telemetry client to the TurnState (#5509)
    * Update Adaptive Runtime to add telemetry client to the TurnState.  Also amend the AdaptiveDialogAction  telemetry event to use correct dialog Id and log dialog instance Id
    
    * Remove instanceId implementation and tracking for adaptive dialogs

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/AdaptiveDialog.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive/AdaptiveDialog.cs
@@ -210,7 +210,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive
                 Value = options,
                 Bubble = false
             };
-
+            
             var properties = new Dictionary<string, string>()
                 {
                     { "DialogId", Id },
