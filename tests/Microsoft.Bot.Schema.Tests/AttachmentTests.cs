f8021bd630739c807b0d4fd1a6902c48e8031839
Ashley Finafrock <v-asho@microsoft.com>
2021-04-29 15:13:30 -0700

    Attachment: simply default vals + completed code coverage

---
+++ tests/Microsoft.Bot.Schema.Tests/AttachmentTests.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests
+{
+    public class AttachmentTests
+    {
+        [Fact]
+        public void SuccessfullyInitAttachment()
+        {
+            var contentType = "contentType";
+            var contentUrl = "contentUrl";
+            var content = new { };
+            var name = "name";
+            var thumbnailUrl = "thumbnailUrl";
+            var properties = new JObject();
+
+            var attachment = new Attachment(contentType, contentUrl, content, name, thumbnailUrl)
+            {
+                Properties = properties
+            };
+
+            Assert.NotNull(attachment);
+            Assert.IsType<Attachment>(attachment);
+            Assert.Equal(contentType, attachment.ContentType);
+            Assert.Equal(contentUrl, attachment.ContentUrl);
+            Assert.Equal(content, attachment.Content);
+            Assert.Equal(name, attachment.Name);
+            Assert.Equal(thumbnailUrl, attachment.ThumbnailUrl);
+            Assert.Equal(properties, attachment.Properties);
+        }
+    }
+}
