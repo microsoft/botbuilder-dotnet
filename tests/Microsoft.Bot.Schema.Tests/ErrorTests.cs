73c8e7ed0660b5e30f3bf9fa3303d305c4ac3032
Ashley Finafrock <v-asho@microsoft.com>
2021-05-03 13:23:31 -0700

    Error: simplified default vals + completed code coverage

---
+++ tests/Microsoft.Bot.Schema.Tests/ErrorTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests
+{
+    public class ErrorTests
+    {
+        [Fact]
+        public void ErrorInits()
+        {
+            // no
+            var code = "errCode";
+            var message = "errMessage";
+            var innerHttpError = new InnerHttpError();
+
+            var error = new Error(code, message, innerHttpError);
+
+            Assert.NotNull(error);
+            Assert.IsType<Error>(error);
+            Assert.Equal(code, error.Code);
+            Assert.Equal(message, error.Message);
+            Assert.Equal(innerHttpError, error.InnerHttpError);
+        }
+
+        [Fact]
+        public void ErrorInitsWithNoArgs()
+        {
+            var error = new Error();
+
+            Assert.NotNull(error);
+            Assert.IsType<Error>(error);
+        }
+    }
+}
