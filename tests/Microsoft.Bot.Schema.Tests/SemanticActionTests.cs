9a8a02ed24a7eaf60d6c063216fa837c8f2504d9
Ashley Finafrock <v-asho@microsoft.com>
2021-05-26 14:57:51 -0700

    Brought to 100% coverage in Microsoft.Bot.Schema

---
+++ tests/Microsoft.Bot.Schema.Tests/SemanticActionTests.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests
+{
+    public class SemanticActionTests
+    {
+        [Fact]
+        public void SemanticActionInits()
+        {
+            var id = "id";
+            var entities = new Dictionary<string, Entity>() { { "entityKey", new Entity() } };
+            var state = "done";
+
+            var semanticAction = new SemanticAction(id, entities)
+            {
+                State = state
+            };
+
+            Assert.NotNull(semanticAction);
+            Assert.IsType<SemanticAction>(semanticAction);
+            Assert.Equal(id, semanticAction.Id);
+            Assert.Equal(entities, semanticAction.Entities);
+            Assert.Equal(state, semanticAction.State);
+        }
+
+        [Fact]
+        public void SemanticActionInitsWithNoArgs()
+        {
+            var semanticAction = new SemanticAction();
+
+            Assert.NotNull(semanticAction);
+            Assert.IsType<SemanticAction>(semanticAction);
+        }
+    }
+}
