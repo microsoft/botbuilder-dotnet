b7dcea700a808bfe98ae33b97ac973519df44b8a
Ashley Finafrock <v-asho@microsoft.com>
2021-06-09 11:59:47 -0700

    MessagingExtension -ActionResponse, -Attachment: simplified default vals. + completed code coverage

--- tests/Microsoft.Bot.Schema.Tests/Teams/MessagingExtensionActionResponseTests.cs
+++ tests/Microsoft.Bot.Schema.Tests/Teams/MessagingExtensionActionResponseTests.cs
@@ -9,5 +9,32 @@ namespace Microsoft.Bot.Schema.Tests.Teams
 {
     public class MessagingExtensionActionResponseTests
     {
+        [Fact]
+        public void MessagingExtensionActionResponseInits()
+        {
+            var task = new TaskModuleResponseBase("message");
+            var composeExtension = new MessagingExtensionResult("list", "message", null, null, "with a personality like sunshine");
+            var cacheInfo = new CacheInfo();
+
+            var msgExtActionResponse = new MessagingExtensionActionResponse(task, composeExtension)
+            { 
+                CacheInfo = cacheInfo
+            };
+
+            Assert.NotNull(msgExtActionResponse);
+            Assert.IsType<MessagingExtensionActionResponse>(msgExtActionResponse);
+            Assert.Equal(task, msgExtActionResponse.Task);
+            Assert.Equal(composeExtension, msgExtActionResponse.ComposeExtension);
+            Assert.Equal(cacheInfo, msgExtActionResponse.CacheInfo);
+        }
+        
+        [Fact]
+        public void MessagingExtensionActionResponseInitsWithNoArgs()
+        {
+            var msgExtActionResponse = new MessagingExtensionActionResponse();
+
+            Assert.NotNull(msgExtActionResponse);
+            Assert.IsType<MessagingExtensionActionResponse>(msgExtActionResponse);
+        }
     }
 }
