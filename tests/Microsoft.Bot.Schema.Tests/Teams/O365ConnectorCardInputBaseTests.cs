0e2f8be5693f8734568ffad998a65578a9103636
Ashley Finafrock <v-asho@microsoft.com>
2021-06-11 11:19:45 -0700

    O365ConnectorCard -InputBase, -MultichoiceInput, -MultichoiceInputChoice: tests

---
+++ tests/Microsoft.Bot.Schema.Tests/Teams/O365ConnectorCardInputBaseTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema.Teams;
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests.Teams
+{
+    public class O365ConnectorCardInputBaseTests
+    {
+        [Fact]
+        public void O365ConnectorCardInputBaseInits()
+        {
+            var type = "textInput";
+            var id = "firstName";
+            var isRequired = true;
+            var title = "Profile";
+            var value = "First Name";
+
+            var inputBase = new O365ConnectorCardInputBase(type, id, isRequired, title, value);
+
+            Assert.NotNull(inputBase);
+            Assert.IsType<O365ConnectorCardInputBase>(inputBase);
+            Assert.Equal(type, inputBase.Type);
+            Assert.Equal(id, inputBase.Id);
+            Assert.Equal(isRequired, inputBase.IsRequired);
+            Assert.Equal(title, inputBase.Title);
+            Assert.Equal(value, inputBase.Value);
+        }
+        
+        [Fact]
+        public void O365ConnectorCardInputBaseInitsWithNoArgs()
+        {
+            var inputBase = new O365ConnectorCardInputBase();
+
+            Assert.NotNull(inputBase);
+            Assert.IsType<O365ConnectorCardInputBase>(inputBase);
+        }
+    }
+}
