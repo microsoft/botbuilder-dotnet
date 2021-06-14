8645d054dfc7bd56a6c61c282ab7317eb7f95792
Hongyang Du (hond) <hond@microsoft.com>
2021-06-15 04:20:25 +0800

    cherry-pick (4.11): Fix double evaluation and filter sensitive settings (#5641) (#5651)
    * Fix double evaluation and filter sensitive settings
    
    * fix conflict
    
    * fix conflict
    
    * Fix warning as errors in branch 4.11 (#5667)
    
    * Fix warnings as errors.
    
    * Fix exception error.
    
    Co-authored-by: Monica Rivera <44449640+mrivera-ms@users.noreply.github.com>

--- tests/Microsoft.Bot.Builder.Dialogs.Tests/MemoryScopeTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Tests/MemoryScopeTests.cs
@@ -455,6 +455,14 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
                 .AssertReply("three")
             .Send("settings.fakeArray.zzz")
                 .AssertReply("cat")
+            .Send("settings.MicrosoftAppPassword") // simple variable
+                .AssertReply("null")
+            .Send("settings.runtimeSettings.telemetry.options.connectionString") // nested setting
+                .AssertReply("null")
+            .Send("settings.BlobsStorage.CONNECTIONSTRING") // case in-sensitive 
+                .AssertReply("null")
+            .Send("settings.BlobsStorage.connectionString")
+                .AssertReply("null")
             .StartTestAsync();
         }
 
@@ -540,7 +548,8 @@ namespace Microsoft.Bot.Builder.Dialogs.Tests
     {
         public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
         {
-            await dc.Context.SendActivityAsync(dc.State.GetValue<string>(dc.Context.Activity.Text));
+            var value = dc.State.GetValue<string>(dc.Context.Activity.Text) ?? "null";
+            await dc.Context.SendActivityAsync(value);
             return await dc.EndDialogAsync();
         }
     }
