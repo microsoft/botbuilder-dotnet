1eed5fd91bd85bac2942124372458fe7656d63a6
Bruce Haley <v-brucehaley@microsoft.com>
2021-06-15 19:07:37 -0700

    Add FixLicenseInNuspecFiles.ps1.

---
+++ build/FixLicenseInNuspecFiles.ps1
@@ -0,0 +1,22 @@
+#
+# Replace "licenseUrl>" with "license>" in .nuspec files. 
+# Designed to be run after unpacking and before signing .nupkg file contents.
+# It eliminates "WARNING: The <licenseUrl> element is deprecated" when pushing to 
+# nuget.org. It compensates for a bug in Visual Studio's .nuspec file generation 
+# dating from when <licenseUrl> was deprecated in 2018. 6/15/2021
+#
+$path = "$(System.DefaultWorkingDirectory)/signing/*/*.nuspec";
+$find = "licenseUrl>";
+$replace = "license>";
+
+function GoReplace() {
+    Get-ChildItem -Path "$path" | % {
+        $_.FullName; 
+        $content = Get-Content -Raw $_.FullName;
+
+        $content -Replace "$find", "$replace" | Set-Content $_.FullName;
+        '-------------'; get-content $_.FullName; '===================';
+    }
+}
+
+GoReplace;
