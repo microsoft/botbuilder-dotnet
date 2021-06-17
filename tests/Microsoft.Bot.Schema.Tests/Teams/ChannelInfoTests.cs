8fcea717baae019e079f02c5e93865695716ccb0
Ashley Finafrock <35248895+Zerryth@users.noreply.github.com>
2021-06-16 21:37:19 -0700

    [Part 1] Increase Microsoft.Bot.Schema.Teams code coverage (#5573)
    * AppBasedLinkQuery: simplified default val., fixed doc typo, completed code coverage
    
    * AttachmentExtensions: use null coalesce expression, completed code coverage
    
    * CacheInfo: simplified default vals. + completed code coverage
    
    * ChannelInfo: simplified default vals. + completed code coverage
    
    * ConversationList: simplified default vals. + completed code coverage
    
    * FileConsentCard: simplified default vals. + completed code coverage

---
+++ tests/Microsoft.Bot.Schema.Tests/Teams/ChannelInfoTests.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema.Teams;
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests.Teams
+{
+    public class ChannelInfoTests
+    {
+        [Fact]
+        public void ChannelInfoInits()
+        {
+            var id = "channelId";
+            var name = "watercooler";
+            var channelInfo = new ChannelInfo(id, name);
+
+            Assert.NotNull(channelInfo);
+            Assert.IsType<ChannelInfo>(channelInfo);
+            Assert.Equal(id, channelInfo.Id);
+            Assert.Equal(name, channelInfo.Name);
+        }
+
+        [Fact]
+        public void ChannelInfoInitsWithNoArgs()
+        {
+            var channelInfo = new ChannelInfo();
+
+            Assert.NotNull(channelInfo);
+            Assert.IsType<ChannelInfo>(channelInfo);
+        }
+    }
+}
