d6c3b58a130297ed5d30b3f0c541b483e9c87f0a
Ashley Finafrock <35248895+Zerryth@users.noreply.github.com>
2021-06-16 21:46:06 -0700

    [Part 3] Increase Microsoft.Bot.Schema.Teams code coverage (#5644)
    * MessageActionsPayloadApp, MessageActionsPayloadAttachment: simplified default vals. + completed code coverage
    
    * Fixed MessageActionsPayloadAppTests name
    
    * Added missing coverage in MessageActionsPayloadAttachment
    
    * MessageActionsPayloadBody: simplified default vals + complete code coverage
    
    * MessageActionsPayloadConversation: simplified default vals. + completed code coverage
    
    * MessageActionsPayload -From, -Mention, -Reaction, -User: simplified default vals. + completed code coverage

---
+++ tests/Microsoft.Bot.Schema.Tests/Teams/MessageActionsPayloadBodyTests.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema.Teams;
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests.Teams
+{
+    public class MessageActionsPayloadBodyTests
+    {
+        [Fact]
+        public void MessageActionsPayloadBodyInits()
+        {
+            var contentType = "text/plain";
+            var content = "Have a wonderful day!";
+
+            var msgActionsPayloadBody = new MessageActionsPayloadBody(contentType, content);
+
+            Assert.NotNull(msgActionsPayloadBody);
+            Assert.IsType<MessageActionsPayloadBody>(msgActionsPayloadBody);
+            Assert.Equal(contentType, msgActionsPayloadBody.ContentType);
+            Assert.Equal(content, msgActionsPayloadBody.Content);
+        }
+        
+        [Fact]
+        public void MessageActionsPayloadBodyInitsWithNoArgs()
+        {
+            var msgActionsPayloadBody = new MessageActionsPayloadBody();
+
+            Assert.NotNull(msgActionsPayloadBody);
+            Assert.IsType<MessageActionsPayloadBody>(msgActionsPayloadBody);
+        }
+    }
+}
