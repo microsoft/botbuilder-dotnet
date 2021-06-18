d7d9357d6e5e93a9f3b3310059bafd4553491c87
Ashley Finafrock <35248895+Zerryth@users.noreply.github.com>
2021-06-17 17:35:34 -0700

    Bring Microsoft.Bot.Schema to 100% code coverage (Part 1) (#5613)
    * Brought to 100% coverage in Microsoft.Bot.Schema
    
    * Fixed conflict cleanup
    
    * Reverted ActivityTest.cs to commit 9a8a02e

---
+++ tests/Microsoft.Bot.Schema.Tests/ActivityTestData.cs
@@ -0,0 +1,99 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.Bot.Schema.Tests
+{
+    internal class ActivityTestData
+    {
+        internal class TestChannelData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new JObject() };
+                yield return new object[] { null };
+                yield return new object[] { new Dictionary<string, string>() };
+                yield return new object[] { new MyChannelData() };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        internal class HasContentData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[] { new Activity() { Text = "text" }, true };
+                yield return new object[] { new Activity() { Summary = "summary" }, true };
+                yield return new object[] { new Activity() { Attachments = GetAttachments() }, true };
+                yield return new object[] { new Activity() { ChannelData = new MyChannelData() }, true };
+                yield return new object[] { new Activity(), false };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+            private IList<Attachment> GetAttachments()
+            {
+                return new List<Attachment> { new Attachment() };
+            }
+        }
+
+        internal class MentionsData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new List<Entity>() { new Entity() },
+                    false,
+                };
+                yield return new object[]
+                {
+                    new List<Entity>()
+                    {
+                        new Entity()
+                        {
+                            Type = "mention",
+                            Properties = new JObject()
+                            {
+                                { 
+                                    "Mentioned",
+                                    new JObject()
+                                    {
+                                        { "Id", "ChannelAccountId" },
+                                        { "Name", "AccountName" },
+                                        { "Properties", new JObject() },
+                                        { "Role", "ChannelAccountRole" },
+                                    }
+                                },
+                                { "Text", "text" },
+                                { "Type", "mention" },
+                            },
+                        }
+                    },
+                    true
+                };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+
+        internal class MyChannelData
+        {
+            public string Ears { get; set; }
+
+            public string Whiskers { get; set; }
+        }
+
+        internal class TestActivity : Activity
+        {
+            public bool IsTargetActivityType(string activityType)
+            {
+                return IsActivity(activityType);
+            }
+        }
+    }
+}
