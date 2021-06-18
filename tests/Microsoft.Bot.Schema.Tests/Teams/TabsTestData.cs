bb2ddb1809536fb1e58250d72fe3b7e1ac0dcbbe
Ashley Finafrock <v-asho@microsoft.com>
2021-06-18 15:21:50 -0700

    TabRequest: tests + create TabsTestData

---
+++ tests/Microsoft.Bot.Schema.Tests/Teams/TabsTestData.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.Bot.Schema.Teams;
+
+namespace Microsoft.Bot.Schema.Tests.Teams
+{
+    internal class TabsTestData
+    {
+        internal class TabRequestTestData : IEnumerable<object[]>
+        {
+            public IEnumerator<object[]> GetEnumerator()
+            {
+                yield return new object[]
+                {
+                    new TabEntityContext(),
+                    new TabContext(),
+                    "oAuthStateMagicCode",
+                };
+
+                yield return new object[]
+                {
+                    null,
+                    null,
+                    null,
+                };
+            }
+
+            IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+        }
+    }
+}
