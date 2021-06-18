2296bb225c78fd68fd94dfc48858258cd7234722
Ashley Finafrock <v-asho@microsoft.com>
2021-06-18 14:42:22 -0700

    TabEntityContext: tests

--- tests/Microsoft.Bot.Schema.Tests/Teams/TabEntityContextTests.cs
+++ tests/Microsoft.Bot.Schema.Tests/Teams/TabEntityContextTests.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema.Teams;
+using Xunit;
 
 namespace Microsoft.Bot.Schema.Tests.Teams
 {
-    class TabEntityContextTests
+    public class TabEntityContextTests
     {
+        [Theory]
+        [InlineData("files")]
+        [InlineData(null)]
+        public void TabEntityContextInits(string tabEntityId)
+        {
+            var tabEntityContext = new TabEntityContext()
+            {
+                TabEntityId = tabEntityId
+            };
+
+            Assert.NotNull(tabEntityContext);
+            Assert.IsType<TabEntityContext>(tabEntityContext);
+            Assert.Equal(tabEntityId, tabEntityContext.TabEntityId);
+        }
     }
 }
