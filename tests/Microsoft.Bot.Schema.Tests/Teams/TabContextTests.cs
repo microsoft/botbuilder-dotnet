d2c859633f25428a41d19beee5243e14289a6799
Ashley Finafrock <v-asho@microsoft.com>
2021-06-18 14:32:50 -0700

    TabContext: tests

---
+++ tests/Microsoft.Bot.Schema.Tests/Teams/TabContextTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Bot.Schema.Teams;
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests.Teams
+{
+    public class TabContextTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("hi")]
+        public void TabContextInits(string theme)
+        {
+            var tabContext = new TabContext()
+            {
+                Theme = theme
+            };
+
+            Assert.NotNull(tabContext);
+            Assert.IsType<TabContext>(tabContext);
+            Assert.Equal(theme, tabContext.Theme);
+        }
+    }
+}
