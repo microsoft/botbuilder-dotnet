bb4d412fa613159aea9befb0daafd0115584975f
cosmicshuai <shuai_uf@outlook.com>
2021-06-21 15:26:35 +0800

    change doc

--- libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Testing/TestActions/AssertTelemetryContains.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Adaptive.Testing/TestActions/AssertTelemetryContains.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-using AdaptiveExpressions;
 using Microsoft.Bot.Builder.Adapters;
 using Moq;
 using Newtonsoft.Json;
@@ -14,7 +13,7 @@ using Newtonsoft.Json;
 namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Testing.TestActions
 {
     /// <summary>
-    /// Run assertions against memory.
+    /// Checks whether telemetry log contsain specific events.
     /// </summary>
     [DebuggerDisplay("AssertTelemetryContains")]
     public class AssertTelemetryContains : TestAction
@@ -37,18 +36,18 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Testing.TestActions
         }
 
         /// <summary>
-        /// Gets or sets the description of this assertion.
+        /// Gets or sets the description of this check.
         /// </summary>
-        /// <value>Description of what this assertion is.</value>
+        /// <value>Description of what this check is.</value>
         [JsonProperty("description")]
         public string Description { get; set; }
 
         /// <summary>
-        /// Gets the assertions.
+        /// Gets the events should be contained.
         /// </summary>
-        /// <value>The assertion expressions.</value>
-        [JsonProperty("contains")]
-        public List<string> Contains { get; } = new List<string>();
+        /// <value>The events names.</value>
+        [JsonProperty("events")]
+        public List<string> Events { get; } = new List<string>();
 
         /// <inheritdoc/>
         public override Task ExecuteAsync(TestAdapter adapter, BotCallbackHandler callback, Inspector inspector = null)
@@ -69,9 +68,9 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Testing.TestActions
                 msgs.Add(invocation.Arguments[0].ToString());
             }
 
-            foreach (var contain in Contains)
+            foreach (var eve in Events)
             {
-                if (!msgs.Contains(contain))
+                if (!msgs.Contains(eve))
                 {
                     flag = false;
                     break;
@@ -80,7 +79,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Testing.TestActions
 
             if (flag == false)
             {
-                throw new InvalidOperationException($"{Description} {Contains} AssertTelemetryContains failed");
+                throw new InvalidOperationException($"{Description} {string.Join(",", Events)} AssertTelemetryContains failed");
             }
 
             return Task.FromResult(flag);
