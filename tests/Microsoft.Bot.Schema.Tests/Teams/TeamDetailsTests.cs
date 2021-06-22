aff4adee438ab9ad8ccb8ffae32f643647e42718
Ashley Finafrock <v-asho@microsoft.com>
2021-06-22 15:40:37 -0700

    TeamDetails: bring to complete code coverage

--- tests/Microsoft.Bot.Schema.Tests/Teams/TeamDetailsTests.cs
+++ tests/Microsoft.Bot.Schema.Tests/Teams/TeamDetailsTests.cs
@@ -14,14 +14,22 @@ namespace Microsoft.Bot.Schema.Tests.Teams
             var id = "supportEngineers";
             var name = "Support Engineers";
             var aadGroupId = "0000-0000-0000-0000-0000-0000";
+            var channelCount = 1;
+            var memberCount = 2;
 
-            var teamDetails = new TeamDetails(id, name, aadGroupId);
+            var teamDetails = new TeamDetails(id, name, aadGroupId)
+            {
+                ChannelCount = channelCount,
+                MemberCount = memberCount,
+            };
 
             Assert.NotNull(teamDetails);
             Assert.IsType<TeamDetails>(teamDetails);
             Assert.Equal(id, teamDetails.Id);
             Assert.Equal(name, teamDetails.Name);
             Assert.Equal(aadGroupId, teamDetails.AadGroupId);
+            Assert.Equal(channelCount, teamDetails.ChannelCount);
+            Assert.Equal(memberCount, teamDetails.MemberCount);
         }
         
         [Fact]
