4b315aacdeb11eac9ab265b93898721838311e84
Ashley Finafrock <35248895+Zerryth@users.noreply.github.com>
2021-06-22 10:49:32 -0700

    [Part 8] [Unit Tests - Code Coverage]: Microsoft.Bot.Schema.Teams (#5709)
    * TabContext: tests
    
    * Added copyright header to TabContext
    
    * TabEntityContext: tests
    
    * TabRequest: tests + create TabsTestData
    
    * TabResponse: tests + created TabResponseTestData
    
    * TabResponseCard, TabResponseCards: tests + created TabResponseCardsTestData
    
    * TabResponsePayload: tests + created TabResponsePayloadTestData
    
    * TabSubmit: tests + created TabSubmitTestData
    
    * TabSubmitData: tests + created TabSubmitDataTestData
    
    * TabSuggestedActions: tests + created TabSuggestedActionsTestData

---
+++ tests/Microsoft.Bot.Schema.Tests/Teams/TabContextTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Schema.Teams;
+using Xunit;
+
+namespace Microsoft.Bot.Schema.Tests.Teams
+{
+    public class TabContextTests
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("hi")]
+        public void TabContextInits(string theme)
+        {
+            var tabContext = new TabContext()
+            {
+                Theme = theme
+            };
+
+            Assert.NotNull(tabContext);
+            Assert.IsType<TabContext>(tabContext);
+            Assert.Equal(theme, tabContext.Theme);
+        }
+    }
+}
