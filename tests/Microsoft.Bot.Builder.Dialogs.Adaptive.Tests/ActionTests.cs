e04d4e6569316eac4883b5796287c2ac12164144
Shuai Wang <shuwan@microsoft.com>
2021-07-08 21:01:30 +0800

    Skip Adaptive.Tests.ActionTests.Action_MissingProperty for dotnet 2.1  (#5771)
    * filter test based on framework version
    
    * change if condition
    
    * add else statement
    
    * re-run-pipeline-1

--- tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/ActionTests.cs
+++ tests/Microsoft.Bot.Builder.Dialogs.Adaptive.Tests/ActionTests.cs
@@ -488,9 +488,13 @@ namespace Microsoft.Bot.Builder.Dialogs.Adaptive.Tests
         [Fact]
         public async Task Action_MissingProperty()
         {
+#if NETCOREAPP2_1
+            await Task.Run(() => System.Console.WriteLine("This test is skipped under dotnet core 2.1"));
+#else
             await TestUtils.RunTestScript(_resourceExplorerFixture.ResourceExplorer);
+#endif
         }
-        
+
         [Fact]
         public async Task Action_SetProperties()
         {
