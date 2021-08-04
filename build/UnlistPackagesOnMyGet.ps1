d9ce7afc5aeeaaebf7229296d997e90fbdda8685
Bruce Haley <v-brucehaley@microsoft.com>
2021-08-03 17:52:33 -0700

    Fix $packageNames arg

--- build/UnlistPackagesOnMyGet.ps1
+++ build/UnlistPackagesOnMyGet.ps1
@@ -5,7 +5,7 @@
 param
 ( 
     [string]$versionToUnlist = "4.7.2-",
-    [string]$packageNames = "AdaptiveExpressions","Microsoft.Bot.Builder","Microsoft.Bot.Builder.Integration.AspNet.Core",
+    [string]$packageNames = "AdaptiveExpressions",
     [string]$myGetFeedName = "botbuilder-v4-dotnet-daily",
     [string]$myGetPersonalAccessToken,
     [switch]$deletePackagesForReal = $false
