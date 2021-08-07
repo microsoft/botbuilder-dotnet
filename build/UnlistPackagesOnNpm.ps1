843960bf8ee87f490d268037c90c0bee16c01300
Bruce Haley <v-brucehaley@microsoft.com>
2021-08-06 17:02:02 -0700

    Add UnlistPackagesOnNpm.ps1

---
+++ build/UnlistPackagesOnNpm.ps1
@@ -0,0 +1,62 @@
+#
+# Unlists package versions on npm feed lower than or equal to $versionToUnlist.
+# Run this first with $unlistPackagesForReal = false (default) to verify what versions will be affected.
+#
+param
+( 
+    [string]$versionToUnlist = "0.6.0",
+    [string[]]$packageNames = @( "adaptive-expressions","botbuilder","botbuilder-dialogs-adaptive-runtime-core" ),
+    [string]$adoPersonalAccessToken,
+    [string]$unlistPackagesForReal = "false"
+)
+
+$RegistryUrlSource = "https://pkgs.dev.azure.com/ConversationalAI/BotFramework/_packaging/SDK/npm/registry/";
+
+Function Get-Versions-From-Npm 
+{
+    param ( [string]$packageName );
+
+    $result = npm view $packageName versions
+    $versions = $result | ConvertFrom-Json;
+
+    return $versions;
+}
+
+"unlistPackagesForReal: " + $unlistPackagesForReal;
+"Target version: " + $versionToUnlist;
+" ";
+"Package versions to unlist:"
+
+foreach ($packageName in $packageNames) {
+    "========================";
+    $packageName;
+    "========================";
+
+    $versions = Get-Versions-From-Npm $packageName;
+    $versionsArray = $versions -split " " | Where-Object {$_};
+
+    #Get versions equal to or older than $versionToUnlist
+    $index = (0..($versionsArray.Count-1)) | where {$versionsArray[$_].StartsWith($versionToUnlist)};
+
+    if ($index -ne $Null) {
+        $versionsToUnlist = $versionsArray | select -First ($index[-1] + 1);
+        $versionsToUnlist;
+    } else {
+        $versionsToUnlist = $null;
+        "[none]";
+    }
+    "------------------------";
+
+    # Do the unlisting
+    foreach ($version in $versionsToUnlist) {
+        if ($unlistPackagesForReal -eq "true") {
+            "Unlisting $version"
+            "npm unpublish $packageName@$version";
+            npm unpublish $packageName@$version
+        } else {
+            "What-if: Unlisting $version"
+            "npm unpublish $packageName@$version --dry-run";
+            npm unpublish $packageName@$version --dry-run
+        }
+    }
+}
