b635fd6067017166d837fa5b2e3e750e13d49989
Muthuveer Somanathan <musomana@microsoft.com>
2021-10-04 11:34:41 -0700

    Add option to override new/legacy StreamingConnection on streaming request handler for BotFrameworkHttpAdapter.

--- libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/BotFrameworkHttpAdapter.cs
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/BotFrameworkHttpAdapter.cs
@@ -207,6 +207,30 @@ namespace Microsoft.Bot.Builder.Integration.AspNet.Core
             return new StreamingRequestHandler(bot, this, socket, audience, Logger);
         }
 
+        /// <summary>
+        /// Create the <see cref="StreamingRequestHandler"/> for processing for a new Web Socket connection request.
+        /// </summary>
+        /// <param name="bot">The <see cref="IBot"/> implementation which will process the request.</param>
+        /// <param name="context">The <see cref="HttpContext"/> instance on which to accept the web socket.</param>
+        /// <param name="audience">The authorized audience of the incoming connection request.</param>
+        /// <returns>Returns a new <see cref="StreamingRequestHandler"/> implementation.</returns>
+        protected virtual async Task<StreamingRequestHandler> CreateStreamingRequestHandlerAsync(IBot bot, HttpContext context, string audience)
+        {
+            if (bot == null)
+            {
+                throw new ArgumentNullException(nameof(bot));
+            }
+
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            var socket = await context.WebSockets.AcceptWebSocketAsync().ConfigureAwait(false);
+
+            return CreateStreamingRequestHandler(bot, socket, audience);
+        }
+
         private static async Task WriteUnauthorizedResponseAsync(string headerName, HttpRequest httpRequest)
         {
             httpRequest.HttpContext.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
@@ -253,12 +277,10 @@ namespace Microsoft.Bot.Builder.Integration.AspNet.Core
 
             try
             {
-                var socket = await httpRequest.HttpContext.WebSockets.AcceptWebSocketAsync().ConfigureAwait(false);
-
                 // Set ClaimsIdentity on Adapter to enable Skills and User OAuth in WebSocket-based streaming scenarios.
                 var audience = GetAudience(claimsIdentity);
 
-                var requestHandler = CreateStreamingRequestHandler(bot, socket, audience);
+                var requestHandler = await CreateStreamingRequestHandlerAsync(bot, httpRequest.HttpContext, audience).ConfigureAwait(false);
 
                 if (RequestHandlers == null)
                 {
