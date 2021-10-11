f0378dc46737a6d90584df7440c750bd9fd6c8d3
Cecilia Avila <44245136+ceciliaavila@users.noreply.github.com>
2021-10-11 13:27:29 -0300

    [#5904] Increase Microsoft.Bot.Builder.AI.Orchestrator code coverage (#5905)
    * Adding tests to Orchestrator library up to 88%
    
    * Applying feedback
    
    * Applying feedback, removing unused properties
    
    Co-authored-by: Martin Luccanera <18757147+MartinLuccanera@users.noreply.github.com>

---
+++ tests/Microsoft.Bot.Builder.AI.Orchestrator.Tests/OrchestratorBotComponentTests.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.AI.Orchestrator.Tests
+{
+    public class OrchestratorBotComponentTests
+    {
+        [Fact]
+        public void TestConfigureServices()
+        {
+            var service = new ServiceCollection();
+            new OrchestratorBotComponent().ConfigureServices(service, new ConfigurationBuilder().Build());
+            Assert.Equal(ServiceLifetime.Singleton, service[0].Lifetime);
+        }
+    }
+}
---
+++ tests/Microsoft.Bot.Builder.AI.Orchestrator.Tests/OrchestratorRecognizerTests.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Moq;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.Bot.Builder.AI.Orchestrator.Tests
+{
+    public class OrchestratorRecognizerTests
+    {
+        [Fact]
+        public async Task LogsTelemetryThrowsArgumentNullExceptionOnNullDialogContext()
+        {
+            var telemetryClient = new Mock<IBotTelemetryClient>();
+
+            var recognizer = new MyRecognizerSubclass { TelemetryClient = telemetryClient.Object };
+            var activity = MessageFactory.Text("hi");
+
+            await Assert.ThrowsAsync<ArgumentNullException>(() => recognizer.RecognizeAsync(null, activity));
+        }
+
+        /// <summary>
+        /// Subclass to test <see cref="OrchestratorRecognizer.FillRecognizerResultTelemetryProperties(RecognizerResult, Dictionary{string,string}, DialogContext)"/> functionality.
+        /// </summary>
+        private class MyRecognizerSubclass : OrchestratorRecognizer
+        {
+            public override async Task<RecognizerResult> RecognizeAsync(DialogContext dialogContext, Activity activity, CancellationToken cancellationToken = default, Dictionary<string, string> telemetryProperties = null, Dictionary<string, double> telemetryMetrics = null)
+            {
+                var text = activity.Text ?? string.Empty;
+
+                var recognizerResult = await Task.FromResult(new RecognizerResult
+                {
+                    Text = text,
+                    AlteredText = null,
+                    Intents = new Dictionary<string, IntentScore>
+                    {
+                        {
+                            "myTestIntent", new IntentScore
+                            {
+                                Score = 1.0,
+                                Properties = new Dictionary<string, object>()
+                            }
+                        }
+                    },
+                    Entities = new JObject(),
+                    Properties = new Dictionary<string, object>()
+                });
+
+                TrackRecognizerResult(dialogContext, $"{nameof(MyRecognizerSubclass)}Result", FillRecognizerResultTelemetryProperties(recognizerResult, telemetryProperties, dialogContext), telemetryMetrics);
+
+                return recognizerResult;
+            }
+        }
+    }
+}
