62e7dd6b423c679c94898a125ff66c84439c45a5
Eric Dahlvang <erdahlva@microsoft.com>
2021-11-06 16:44:13 -0500

    Remove tests containing expired secrets

--- tests/Microsoft.Bot.Connector.Tests/Authentication/JwtTokenValidationTests.cs
+++ tests/Microsoft.Bot.Connector.Tests/Authentication/JwtTokenValidationTests.cs
@@ -31,62 +31,12 @@ namespace Microsoft.Bot.Connector.Tests.Authentication
         }
 
         [Fact]
-        public async void Connector_AuthHeader_CorrectAppIdAndServiceUrl_ShouldValidate()
-        {
-            string header = $"Bearer {await new MicrosoftAppCredentials("2cd87869-38a0-4182-9251-d056e8f0ac24", "2.30Vs3VQLKt974F").GetTokenAsync()}";
-            var credentials = new SimpleCredentialProvider("2cd87869-38a0-4182-9251-d056e8f0ac24", string.Empty);
-            var result = await JwtTokenValidation.ValidateAuthHeader(header, credentials, new SimpleChannelProvider(), string.Empty, "https://webchat.botframework.com/", client);
-
-            Assert.True(result.IsAuthenticated);
-        }
-
-        [Fact]
-        public async void Connector_AuthHeader_BotAppIdDiffers_ShouldNotValidate()
-        {
-            string header = $"Bearer {await new MicrosoftAppCredentials("2cd87869-38a0-4182-9251-d056e8f0ac24", "2.30Vs3VQLKt974F").GetTokenAsync()}";
-            var credentials = new SimpleCredentialProvider("00000000-0000-0000-0000-000000000000", string.Empty);
-
-            await Assert.ThrowsAsync<UnauthorizedAccessException>(
-                async () => await JwtTokenValidation.ValidateAuthHeader(header, credentials, new SimpleChannelProvider(), string.Empty, null, client));
-        }
-
-        [Fact]
-        public async void Connector_AuthHeader_BotWithNoCredentials_ShouldNotValidate()
-        {
-            // token received and auth disabled
-            string header = $"Bearer {await new MicrosoftAppCredentials("2cd87869-38a0-4182-9251-d056e8f0ac24", "2.30Vs3VQLKt974F").GetTokenAsync()}";
-            var credentials = new SimpleCredentialProvider(string.Empty, string.Empty);
-
-            await Assert.ThrowsAsync<UnauthorizedAccessException>(
-                async () => await JwtTokenValidation.ValidateAuthHeader(header, credentials, new SimpleChannelProvider(), string.Empty, null, client));
-        }
-
-        [Fact]
         public async void EmptyHeader_BotWithNoCredentials_ShouldThrow()
         {
             var header = string.Empty;
             var credentials = new SimpleCredentialProvider(string.Empty, string.Empty);
 
             await Assert.ThrowsAsync<ArgumentNullException>(
-                async () => await JwtTokenValidation.ValidateAuthHeader(header, credentials, new SimpleChannelProvider(), string.Empty, null, emptyClient));
-        }
-
-        [Fact]
-        public async void Emulator_MsaHeader_CorrectAppIdAndServiceUrl_ShouldValidate()
-        {
-            string header = $"Bearer {await new MicrosoftAppCredentials("2cd87869-38a0-4182-9251-d056e8f0ac24", "2.30Vs3VQLKt974F").GetTokenAsync()}";
-            var credentials = new SimpleCredentialProvider("2cd87869-38a0-4182-9251-d056e8f0ac24", string.Empty);
-            var result = await JwtTokenValidation.ValidateAuthHeader(header, credentials, new SimpleChannelProvider(), string.Empty, "https://webchat.botframework.com/", emptyClient);
-
-            Assert.True(result.IsAuthenticated);
-        }
-
-        [Fact]
-        public async void Emulator_MsaHeader_BotAppIdDiffers_ShouldNotValidate()
-        {
-            string header = $"Bearer {await new MicrosoftAppCredentials("2cd87869-38a0-4182-9251-d056e8f0ac24", "2.30Vs3VQLKt974F").GetTokenAsync()}";
-            var credentials = new SimpleCredentialProvider("00000000-0000-0000-0000-000000000000", string.Empty);
-            await Assert.ThrowsAsync<UnauthorizedAccessException>(
                 async () => await JwtTokenValidation.ValidateAuthHeader(header, credentials, new SimpleChannelProvider(), string.Empty, null, emptyClient));
         }
 
@@ -133,33 +83,6 @@ namespace Microsoft.Bot.Connector.Tests.Authentication
 
             Assert.Equal(AuthenticationConstants.AnonymousAuthType, claimsPrincipal.AuthenticationType);
             Assert.Equal(AuthenticationConstants.AnonymousSkillAppId, JwtTokenValidation.GetAppIdFromClaims(claimsPrincipal.Claims));
-        }
-
-        [Fact]
-        public async void Emulator_AuthHeader_CorrectAppIdAndServiceUrl_WithGovChannelService_ShouldValidate()
-        {
-            await JwtTokenValidation_ValidateAuthHeader_WithChannelService_Succeeds(
-                "2cd87869-38a0-4182-9251-d056e8f0ac24",         // emulator creds
-                "2.30Vs3VQLKt974F",
-                GovernmentAuthenticationConstants.ChannelService);
-        }
-
-        [Fact]
-        public async void Emulator_AuthHeader_CorrectAppIdAndServiceUrl_WithPrivateChannelService_ShouldValidate()
-        {
-            await JwtTokenValidation_ValidateAuthHeader_WithChannelService_Succeeds(
-                "2cd87869-38a0-4182-9251-d056e8f0ac24",         // emulator creds
-                "2.30Vs3VQLKt974F",
-                "TheChannel");
-        }
-
-        [Fact]
-        public async void Connector_AuthHeader_CorrectAppIdAndServiceUrl_WithGovChannelService_ShouldValidate()
-        {
-            await JwtTokenValidation_ValidateAuthHeader_WithChannelService_Succeeds(
-                "2cd87869-38a0-4182-9251-d056e8f0ac24",         // emulator creds
-                "2.30Vs3VQLKt974F",
-                GovernmentAuthenticationConstants.ChannelService);
         }
 
         [Fact]
