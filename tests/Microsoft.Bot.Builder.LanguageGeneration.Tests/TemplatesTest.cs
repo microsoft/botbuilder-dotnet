8479872039e6c39012f1679c7db463aa180b8645
Eric Dahlvang <erdahlva@microsoft.com>
2021-11-06 17:24:40 -0500

    Add missing test files

--- tests/Microsoft.Bot.Builder.LanguageGeneration.Tests/TemplatesTest.cs
+++ tests/Microsoft.Bot.Builder.LanguageGeneration.Tests/TemplatesTest.cs
@@ -1526,17 +1526,11 @@ namespace Microsoft.Bot.Builder.AI.LanguageGeneration.Tests
             Templates.EnableFromFile = true;
 
             var templates = Templates.ParseFile(GetExampleFilePath("FileOperation.lg"));
-            var evaluated = templates.Evaluate("FromFileWithoutEvaluation");
-            Assert.Equal("hi ${name}", evaluated);
-
-            evaluated = templates.Evaluate("FromFileWithEvaluation1", new { name = "Lucy" });
+            var evaluated = templates.Evaluate("FromFileWithEvaluation1", new { name = "Lucy" });
             Assert.Equal("hi Lucy", evaluated);
 
             evaluated = templates.Evaluate("FromFileWithEvaluation2", new { name = "Lucy" });
             Assert.Equal("hi Lucy", evaluated);
-
-            evaluated = templates.Evaluate("FromFileBinary");
-            Assert.Equal("hi ${name}", evaluated);
         }
 
         [Fact]
@@ -1544,7 +1538,7 @@ namespace Microsoft.Bot.Builder.AI.LanguageGeneration.Tests
         {
             Templates.EnableFromFile = false;
 
-            Assert.Throws<InvalidOperationException>(() =>
+            Assert.Throws<Exception>(() =>
             {
                 var templates = Templates.ParseFile(GetExampleFilePath("FileOperation.lg"));
                 var evaluated = templates.Evaluate("FromFileWithoutEvaluation");
