78beded7f15d990c9bbb38b2936653de47dde391
Bruce Haley <v-brucehaley@microsoft.com>
2021-11-11 15:21:17 -0800

    Add the full request Uri to the HttpRequestException message

--- libraries/Adapters/Microsoft.Bot.Builder.Adapters.Facebook/FacebookClientWrapper.cs
+++ libraries/Adapters/Microsoft.Bot.Builder.Adapters.Facebook/FacebookClientWrapper.cs
@@ -90,7 +90,9 @@ namespace Microsoft.Bot.Builder.Adapters.Facebook
                     {
                         // In Azure view this exception via Application Insights/Failures.
                         // Or by App Service/Diagnose and solve problems/Diagnostic Tools/Application Event Logs
-                        throw new HttpRequestException($"SendMessageAsync():\r\nRequest payload: {json}\r\n\r\nResponse: {res.ToString()}");
+                        // throw new HttpRequestException($"SendMessageAsync():\r\nRequest payload: {json}\r\n\r\nResponse: {res.ToString()}");
+                        throw new HttpRequestException($"SendMessageAsync(): \r\nRequest: {request.ToString()}\r\n\r\nRequest payload: {json}\r\n\r\nResponse: {res.ToString()}");
+
                     }
                 }
             }
