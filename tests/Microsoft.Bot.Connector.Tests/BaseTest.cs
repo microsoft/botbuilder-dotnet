271893b22a424045d11a8823d258e8cb730af5e9
CeciliaAvila <cecilia.avila@southworks.com>
2021-11-18 16:28:30 -0300

    Remove netcoreapp2.1 in remaining projects

--- tests/Microsoft.Bot.Connector.Tests/BaseTest.cs
+++ tests/Microsoft.Bot.Connector.Tests/BaseTest.cs
@@ -32,7 +32,7 @@ namespace Microsoft.Bot.Connector.Tests
     ///
     ///    To re-record:
     ///      1. All from live/unmocked, except set HttpRecorderMode to Record.
-    ///      2. Once done recording, copy recording sessions from ...Microsoft.Bot.Connector.Tests\bin\Debug\netcoreapp2.1\SessionRecords
+    ///      2. Once done recording, copy recording sessions from ...Microsoft.Bot.Connector.Tests\bin\Debug\netcoreapp3.1\SessionRecords
     ///             to ...Microsoft.Bot.Connector.Tests\SessionRecords.
     /// </summary>
     public class BaseTest
