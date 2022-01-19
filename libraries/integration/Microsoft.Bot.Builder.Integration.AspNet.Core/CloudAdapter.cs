103ab70bf3900c3209191f8c5353ec9c96c2cade
Eric Dahlvang <erdahlva@microsoft.com>
2022-01-19 12:51:02 -0600

    Log BadRequest in adapter

--- libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/CloudAdapter.cs
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/CloudAdapter.cs
@@ -94,6 +94,7 @@ namespace Microsoft.Bot.Builder.Integration.AspNet.Core
                     if (string.IsNullOrEmpty(activity?.Type))
                     {
                         httpResponse.StatusCode = (int)HttpStatusCode.BadRequest;
+                        Logger.LogInformation($"BadRequest: Activity is missing the Type field.");
                         return;
                     }
 
