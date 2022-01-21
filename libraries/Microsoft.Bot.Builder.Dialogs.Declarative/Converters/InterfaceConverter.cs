470c73ec114360e65738dacd5c65a3ee1f6820d2
Gabo Gilabert <ggilaber@microsoft.com>
2022-01-21 14:55:30 -0500

    Removed Console.Writeline

--- libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Converters/InterfaceConverter.cs
+++ libraries/Microsoft.Bot.Builder.Dialogs.Declarative/Converters/InterfaceConverter.cs
@@ -237,9 +237,7 @@ namespace Microsoft.Bot.Builder.Dialogs.Declarative.Converters
             if (!passTwo && cachedTypes.ContainsKey(cacheKey))
             {
                 // Pull the type from the cache if found on pass one.
-                result = cachedTypes[cacheKey];
-                Console.WriteLine($"Found result of type {result.GetType().Name} for {cacheKey} in cache");
-            }
+                result = cachedTypes[cacheKey]; }
             else
             {
                 // Always build the type if it is pass two or the type is not found in the cache
