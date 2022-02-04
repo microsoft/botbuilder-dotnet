2a67994ab00ffe9a58339fa1a37bc99d523db199
Gabo Gilabert <gabog@users.noreply.github.com>
2022-02-04 12:34:33 -0500

    Update libraries/Microsoft.Bot.Schema/ActivityEx.cs
    Updated (C)

--- libraries/Microsoft.Bot.Schema/ActivityEx.cs
+++ libraries/Microsoft.Bot.Schema/ActivityEx.cs
@@ -1,4 +1,5 @@
-﻿// Licensed under the MIT License.
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
 using System;
 using System.Collections.Generic;
