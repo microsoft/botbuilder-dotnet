d4b8370e11678efe3fcfd77cbf6f0a4151256d85
Eric Dahlvang <erdahlva@microsoft.com>
2022-02-04 12:07:30 -0600

    V5 general cleanup of deprecated and duplicate classes

--- libraries/Microsoft.Bot.Builder/InvokeResponse{T}.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using System.Runtime.CompilerServices;
-
-// This type is now defined in the Microsoft.Bot.Connector assembly.
-[assembly: TypeForwardedTo(typeof(Microsoft.Bot.Builder.InvokeResponse<>))]
