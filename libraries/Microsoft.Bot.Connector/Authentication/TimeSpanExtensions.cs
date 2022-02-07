28725c70f09409b76542ad315b5f3f0015d83e1c
CeciliaAvila <cecilia.avila@southworks.com>
2022-02-07 11:35:52 -0300

    Use multiplier param instead of delete it

--- libraries/Microsoft.Bot.Connector/Authentication/TimeSpanExtensions.cs
+++ libraries/Microsoft.Bot.Connector/Authentication/TimeSpanExtensions.cs
@@ -13,18 +13,21 @@ namespace Microsoft.Bot.Connector.Authentication
         private static readonly Random _random = new Random();
 
         /// <summary>
-        /// Generates a uniform distribution between zero and ten percent of the proposed delay and adds it
-        /// to the delay as random noise to distribute requests across time.
+        /// Generates a uniform distribution of the proposed delay and adds it to the delay as random noise 
+        /// to distribute requests across time.
+        /// The distribution goes between zero and the percentage defined in multiplier (default 0.1 = 10%).
         /// </summary>
         /// <param name="delay">The requested delay.</param>
+        /// /// <param name="multiplier">A multiplier.</param>
         /// <returns>A uniformly distributed time span.</returns>
-        public static TimeSpan WithJitter(this TimeSpan delay)
+        public static TimeSpan WithJitter(this TimeSpan delay, double multiplier = 0.1)
         {
-            // Generate an uniform distribution between zero and 10% of the proposed delay and add it as
-            // random noise. The reason for this is that if there are multiple threads about to retry
+            // Generate an uniform distribution between zero and the percentage defined in multiplier (default 0.1 = 10%)
+            // of the proposed delay and add it as random noise.
+            // The reason for this is that if there are multiple threads about to retry
             // at the same time, it can overload the server again and trigger throttling again.
             // By adding a bit of random noise, we distribute requests a across time.
-            return delay + TimeSpan.FromMilliseconds(_random.NextDouble() * delay.TotalMilliseconds * 0.1);
+            return delay + TimeSpan.FromMilliseconds(_random.NextDouble() * delay.TotalMilliseconds * multiplier);
         }
     }
 }
