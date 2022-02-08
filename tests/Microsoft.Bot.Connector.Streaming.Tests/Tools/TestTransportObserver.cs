57f9828a021499c70fc0aa7192371b2978585e15
msomanathan <msgoogol@gmail.com>
2022-02-08 00:37:45 -0800

    Header Serializer tests

--- tests/Microsoft.Bot.Connector.Streaming.Tests/Tools/TestTransportObserver.cs
+++ tests/Microsoft.Bot.Connector.Streaming.Tests/Tools/TestTransportObserver.cs
@@ -4,12 +4,13 @@
 using System;
 using System.Buffers;
 using System.Collections.Generic;
+using Microsoft.Bot.Connector.Streaming.Payloads;
 
 namespace Microsoft.Bot.Connector.Streaming.Tests.Features
 {
-    internal class TestTransportObserver : IObserver<(Payloads.Header Header, ReadOnlySequence<byte> Payload)>
+    internal class TestTransportObserver : IObserver<(Header Header, ReadOnlySequence<byte> Payload)>
     {
-        public List<(Payloads.Header Header, byte[] Payload)> Received { get; private set; } = new List<(Payloads.Header Header, byte[] Payload)>();
+        public List<(Header Header, byte[] Payload)> Received { get; private set; } = new List<(Header Header, byte[] Payload)>();
 
         public void OnCompleted()
         {
@@ -21,7 +22,7 @@ namespace Microsoft.Bot.Connector.Streaming.Tests.Features
             throw new NotImplementedException();
         }
 
-        public void OnNext((Payloads.Header Header, ReadOnlySequence<byte> Payload) value)
+        public void OnNext((Header Header, ReadOnlySequence<byte> Payload) value)
         {
             Received.Add((value.Header, value.Payload.ToArray()));
         }
