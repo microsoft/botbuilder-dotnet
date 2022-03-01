1fc5bae908784ba95ed93163ca57dabc2f6ca019
Lee Parrish <30470292+LeeParrishMSFT@users.noreply.github.com>
2022-03-01 15:38:45 -0600

    Add Corebot set up to execute as an Azure Function.

---
+++ testbots/CoreFunctionBot/FunctionBotAdapter.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace CoreFunctionBot
+{
+    internal class FunctionBotAdapter : CloudAdapter
+#pragma warning restore CA1812 // Avoid uninstantiated internal classes
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FunctionBotAdapter"/> class.
+        /// </summary>
+        /// <param name="botFrameworkAuthentication">An instance of a BotFrameworkAuthentication.</param>
+        /// <param name="middlewares">The collection of middleware instances.</param>
+        /// <param name="logger">The logger instance.</param>
+        public FunctionBotAdapter(
+            BotFrameworkAuthentication botFrameworkAuthentication,
+            ILogger<FunctionBotAdapter> logger = null)
+            : base(botFrameworkAuthentication, logger)
+        {
+            OnTurnError = async (turnContext, exception) =>
+            {
+                // Log any leaked exception from the application.
+                Logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
+
+                // Send the exception message to the user. Since the default behavior does not
+                // send logs or trace activities, the bot appears hanging without any activity
+                // to the user.
+                await turnContext.SendActivityAsync(exception.Message).ConfigureAwait(false);
+
+                var conversationState = turnContext.TurnState.Get<ConversationState>();
+
+                if (conversationState != null)
+                {
+                    // Delete the conversationState for the current conversation to prevent the
+                    // bot from getting stuck in a error-loop caused by being in a bad state.
+                    await conversationState.DeleteAsync(turnContext).ConfigureAwait(false);
+                }
+            };
+        }
+    }
+}
---
+++ testbots/CoreFunctionBot/Startup.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.BotBuilderSamples;
+using Microsoft.BotBuilderSamples.Bots;
+using Microsoft.BotBuilderSamples.Dialogs;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(CoreFunctionBot.Startup))]
+
+namespace CoreFunctionBot
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient().AddControllers().AddNewtonsoftJson();
+
+            // Create the Bot Framework Authentication to be used with the Bot Adapter.
+            builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+            // Create the Bot Adapter with error handling enabled.
+            builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            builder.Services.AddSingleton<IStorage>(new MemoryStorage());
+
+            // Create the User state. (Used in this bot's Dialog implementation.)
+            builder.Services.AddSingleton<UserState>();
+
+            // Create the Conversation state. (Used by the Dialog system itself.)
+            builder.Services.AddSingleton<ConversationState>();
+
+            // Register LUIS recognizer
+            builder.Services.AddSingleton<FlightBookingRecognizer>();
+
+            // Register the BookingDialog.
+            builder.Services.AddSingleton<BookingDialog>();
+
+            // The MainDialog that will be run by the bot.
+            builder.Services.AddSingleton<MainDialog>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            builder.Services.AddTransient<IBot, DialogAndWelcomeBot<MainDialog>>();
+
+            // Use this startup code if using the V5 AddBotRuntime startup method
+
+            //// Register LUIS recognizer
+            //builder.Services.AddSingleton<FlightBookingRecognizer>();
+
+            //builder.Services.AddSingleton<IBotFrameworkHttpAdapter, FunctionBotAdapter>();
+
+            //// Register the BookingDialog.
+            //builder.Services.AddSingleton<BookingDialog>();
+            //builder.Services.AddBotRuntime<DialogAndWelcomeBot<MainDialog>, MainDialog>(builder.GetContext().Configuration);
+        }
+    }
+}
