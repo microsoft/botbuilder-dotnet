dc4f07b50b1869e5b02a422b5961b5dfbee38021
msomanathan <msgoogol@gmail.com>
2022-03-01 16:28:07 -0800

    Update schema lib dependency to STJ and add serialization helpers.

---
+++ libraries/Microsoft.Bot.Connector.Schema/SerializationConfig.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+
+namespace Microsoft.Bot.Connector.Schema
+{
+    /// <summary>
+    /// This class defines the configuration settings used when serializing and deserializing data
+    /// contained in objects defined by the Bot Framework Protocol schema.
+    /// </summary>
+    public static class SerializationConfig
+    {
+        /// <summary>
+        /// The default serialization settings for use in most cases.
+        /// </summary>
+        public static readonly JsonSerializerOptions DefaultSerializeOptions = new JsonSerializerOptions
+        {
+            IgnoreNullValues = true
+        };
+
+        /// <summary>
+        /// The default deserialization settings for use in most cases.
+        /// </summary>
+        public static readonly JsonSerializerOptions DefaultDeserializeOptions = new JsonSerializerOptions
+        {
+            IgnoreNullValues = true,
+            PropertyNameCaseInsensitive = true
+        };
+    }
+}
---
+++ libraries/Microsoft.Bot.Connector.Schema/SerializationExtensions.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Microsoft.Bot.Connector.Schema
+{
+    /// <summary>
+    /// Extensions for converting objects to desired types using serialization.
+    /// </summary>
+    public static class SerializationExtensions
+    {
+        /// <summary>
+        /// Decompose an object into its constituent JSON elements.
+        /// </summary>
+        /// <param name="value">The object to be decomposed into JSON elements.</param>
+        /// <returns>A dictionary of JSON elements keyed by property name.</returns>
+        public static Dictionary<string, JsonElement> ToJsonElements(this object value)
+        {
+            var elements = new Dictionary<string, JsonElement>();
+
+            if (value != null)
+            {
+                using (var document = value is string json
+                           ? JsonDocument.Parse(json)
+                           : JsonDocument.Parse(JsonSerializer.SerializeToUtf8Bytes(value, SerializationConfig.DefaultSerializeOptions)))
+                {
+                    foreach (var property in document.RootElement.Clone().EnumerateObject())
+                    {
+                        elements.Add(property.Name, property.Value);
+                    }
+                }
+            }
+
+            return elements;
+        }
+
+        /// <summary>
+        /// Convert an object to the desired type using serialization and deserialization.
+        /// </summary>
+        /// <param name="value">The object to be converted to desired type.</param>
+        /// <typeparam name="T">The type of object to convert to.</typeparam>
+        /// <returns>The converted object.</returns>
+        public static T ToObject<T>(this object value)
+        {
+            if (value == null)
+            {
+                return default;
+            }
+
+            return value is string json
+                ? JsonSerializer.Deserialize<T>(json, SerializationConfig.DefaultDeserializeOptions)
+                : JsonSerializer.Deserialize<T>(
+                    JsonSerializer.SerializeToUtf8Bytes(value, SerializationConfig.DefaultSerializeOptions),
+                    SerializationConfig.DefaultDeserializeOptions);
+        }
+    }
+}
