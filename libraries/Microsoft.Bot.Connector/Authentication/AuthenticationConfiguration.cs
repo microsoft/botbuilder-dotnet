8a1ce34a0c44dd5086e7d0c1a272fcafcc935376
mostafa <mostafaa@microsoft.com>
2022-07-14 16:38:57 -0700

    feat(add-support-msal-sni): Mostafa
    + Updated AuthenticationConfiguration class to use for SNI configuration.
    
    ref #6413

--- libraries/Microsoft.Bot.Connector/Authentication/AuthenticationConfiguration.cs
+++ libraries/Microsoft.Bot.Connector/Authentication/AuthenticationConfiguration.cs
@@ -40,5 +40,41 @@ namespace Microsoft.Bot.Connector.Authentication
         /// A collection of valid JWT token issuers.
         /// </value>
         public IEnumerable<string> ValidTokenIssuers { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether
+        /// the x5c claim (public key of the certificate) should be sent to
+        /// the STS. Sending the x5c enables application developers to achieve easy certificate
+        /// roll-over in Azure AD: this method will send the public certificate to Azure
+        /// AD along with the token request, so that Azure AD can use it to validate the
+        /// subject name based on a trusted issuer policy. This saves the application admin
+        /// from the need to explicitly manage the certificate rollover (either via portal
+        /// or PowerShell/CLI operation). For details see https://aka.ms/msal-net-sni.
+        ///
+        /// <example>
+        /// Register this in startup.cs of your application along with <see cref="MsalServiceClientCredentialsFactory"/>>.
+        /// <see cref="MsalAppCredentials"/> for more infromation.
+        /// <code>
+        ///  // MSAL certificate authentication
+        ///  services.AddTransient&lt;IConfidentialClientApplication&gt;(
+        ///  serviceProvider => ConfidentialClientApplicationBuilder.Create(Configuration.GetSection("MicrosoftAppId").Value)
+        ///      .WithCertificate(new X509Certificate2("pathToCertFile", "certPassword"))
+        ///      .Build());
+        ///      
+        ///  // Configure Authentication configuration.
+        ///  services.AddSingleton(sp => new AuthenticationConfiguration { SendX5c = true });
+        ///  
+        ///  // MSAL credential factory: regardless of secret, cert or custom auth, need to add the line below
+        ///  services.AddTransient&lt;ServiceClientCredentialsFactory, Authentication.MsalServiceClientCredentialsFactory&gt;();
+        ///  
+        ///  // Create the Bot Framework Authentication to be used with the Bot Adapter.
+        ///  services.AddSingleton&lt;BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication&gt;();
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <value>
+        /// true if the x5c should be sent. Otherwise false. The default is false.
+        /// </value>
+        public bool SendX5c { get; set; } = false;
     }
 }
