2407313647fa4e44f32463ca67f93e9033a3d915
CeciliaAvila <cecilia.avila@southworks.com>
2022-10-24 14:38:13 -0300

    Update Facebook Functional Tests

--- FunctionalTests/Microsoft.Bot.Builder.FunctionalTests/FacebookChatTests.cs
+++ FunctionalTests/Microsoft.Bot.Builder.FunctionalTests/FacebookChatTests.cs
@@ -91,11 +91,10 @@ namespace Microsoft.Bot.Builder.FunctionalTests
         {
             var hashResult = string.Empty;
 
-            using (var hmac = new System.Security.Cryptography.HMACSHA1(Encoding.UTF8.GetBytes(_appSecret)))
+            using (var hmac = new System.Security.Cryptography.HMACSHA256(Encoding.UTF8.GetBytes(_appSecret)))
             {
-                hmac.Initialize();
                 var hashArray = hmac.ComputeHash(Encoding.UTF8.GetBytes(bodyMessage));
-                var hash = $"SHA1={BitConverter.ToString(hashArray).Replace("-", string.Empty)}";
+                var hash = BitConverter.ToString(hashArray).Replace("-", string.Empty);
 
                 hashResult = hash;
             }
