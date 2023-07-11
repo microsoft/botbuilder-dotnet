f5c6e1ddf20038b20b660b3660f6381dc9148d4f
CeciliaAvila <cecilia.avila@southworks.com>
2023-07-11 16:30:04 -0300

    Upgrade Antlr4.Runtime to v.4.11.1

--- tests/Parsers/Microsoft.Bot.Builder.Parsers.LU.Tests/Parser/LuParserTests.cs
+++ tests/Parsers/Microsoft.Bot.Builder.Parsers.LU.Tests/Parser/LuParserTests.cs
@@ -14,6 +14,22 @@ namespace Microsoft.Bot.Builder.Parsers.LU.Tests.Parser
 {
     public class LuParserTests
     {
+        //[Fact]
+        //public void ParseLuFile()
+        //{
+        //    // var luContent = "# Help"+ Environment.NewLine + "- help" + Environment.NewLine + "- I need help" + Environment.NewLine + "- please help";
+        //    var path = Path.Combine(Directory.GetCurrentDirectory(), "Fixtures", "main.en-us.lu");
+        //    Console.WriteLine(path);
+        //    var luContent = File.ReadAllText(path);
+        //    luContent = luContent.Substring(0, luContent.Length);
+        //    var result = LuParser.Parse(luContent);
+
+        //    //LuResource expected = JsonConvert.DeserializeObject<LuResource>(File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Fixtures", fileName + ".json")));
+        //    //var serializedResult = JsonConvert.SerializeObject(result).Replace("\\r", string.Empty);
+        //    //var serializedExpected = JsonConvert.SerializeObject(expected).Replace("\\r", string.Empty);
+        //    //Assert.Equal(serializedResult, serializedExpected);
+        //}
+
         [Theory]
         [InlineData("LU_Sections")]
         [InlineData("SectionsLU")]
