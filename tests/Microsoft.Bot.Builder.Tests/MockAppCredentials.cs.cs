14242b0178603a02c448994608e65d1d8111f3a0
Hongyang Du <hond@microsoft.com>
2023-08-30 09:03:02 +0800

    update

---
+++ tests/Microsoft.Bot.Builder.Tests/MockAppCredentials.cs.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Net.Http;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.Bot.Builder.Tests
+{
+    public class MockAppCredentials : AppCredentials
+    {
+        public MockAppCredentials(string channelAuthTenant = null, HttpClient customHttpClient = null, ILogger logger = null)
+            : base(channelAuthTenant, customHttpClient, logger)
+        {
+        }
+
+        protected override Lazy<IAuthenticator> BuildIAuthenticator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
