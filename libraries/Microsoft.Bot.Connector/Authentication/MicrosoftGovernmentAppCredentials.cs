822ae31831ed4b9ec19dbcdee17dcbd9aa0f5b02
fangyangci <133664123+fangyangci@users.noreply.github.com>
2023-12-19 22:43:50 +0800

    fix: USGovSingleTenant OAuthEndpoint (#6714)
    * fixUSGovSingleTenant
    
    * Add UT
    
    * Rollback AuthTenant Property Name
    
    * The Ctor do contains the old ones, Add Ctor to ApiCompatBaseline

--- libraries/Microsoft.Bot.Connector/Authentication/MicrosoftGovernmentAppCredentials.cs
+++ libraries/Microsoft.Bot.Connector/Authentication/MicrosoftGovernmentAppCredentials.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System.Globalization;
 using System.Net.Http;
 using Microsoft.Extensions.Logging;
 
@@ -22,20 +23,10 @@ namespace Microsoft.Bot.Connector.Authentication
         /// <param name="appId">The Microsoft app ID.</param>
         /// <param name="password">The Microsoft app password.</param>
         /// <param name="customHttpClient">Optional <see cref="HttpClient"/> to be used when acquiring tokens.</param>
-        public MicrosoftGovernmentAppCredentials(string appId, string password, HttpClient customHttpClient = null)
-            : this(appId, password, customHttpClient, null, GovernmentAuthenticationConstants.ToChannelFromBotOAuthScope)
-        {
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MicrosoftGovernmentAppCredentials"/> class.
-        /// </summary>
-        /// <param name="appId">The Microsoft app ID.</param>
-        /// <param name="password">The Microsoft app password.</param>
-        /// <param name="customHttpClient">Optional <see cref="HttpClient"/> to be used when acquiring tokens.</param>
         /// <param name="logger">Optional <see cref="ILogger"/> to gather telemetry data while acquiring and managing credentials.</param>
-        public MicrosoftGovernmentAppCredentials(string appId, string password, HttpClient customHttpClient, ILogger logger)
-            : this(appId, password, customHttpClient, logger, GovernmentAuthenticationConstants.ToChannelFromBotOAuthScope)
+        /// <param name="oAuthScope">The scope for the token (defaults to <see cref="GovernmentAuthenticationConstants.ToChannelFromBotOAuthScope"/> if null).</param>
+        public MicrosoftGovernmentAppCredentials(string appId, string password, HttpClient customHttpClient = null, ILogger logger = null, string oAuthScope = null)
+            : base(appId, password, customHttpClient, logger, oAuthScope)
         {
         }
 
@@ -44,37 +35,22 @@ namespace Microsoft.Bot.Connector.Authentication
         /// </summary>
         /// <param name="appId">The Microsoft app ID.</param>
         /// <param name="password">The Microsoft app password.</param>
+        /// <param name="channelAuthTenant">Optional. The oauth token tenant.</param>
         /// <param name="customHttpClient">Optional <see cref="HttpClient"/> to be used when acquiring tokens.</param>
         /// <param name="logger">Optional <see cref="ILogger"/> to gather telemetry data while acquiring and managing credentials.</param>
         /// <param name="oAuthScope">The scope for the token (defaults to <see cref="GovernmentAuthenticationConstants.ToChannelFromBotOAuthScope"/> if null).</param>
-        public MicrosoftGovernmentAppCredentials(string appId, string password, HttpClient customHttpClient, ILogger logger, string oAuthScope = null)
-            : this(appId, password, tenantId: string.Empty, customHttpClient, logger, oAuthScope)
+        public MicrosoftGovernmentAppCredentials(string appId, string password, string channelAuthTenant, HttpClient customHttpClient = null, ILogger logger = null, string oAuthScope = null)
+            : base(appId, password, channelAuthTenant, customHttpClient, logger, oAuthScope)
         {
         }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MicrosoftGovernmentAppCredentials"/> class.
-        /// </summary>
-        /// <param name="appId">The Microsoft app ID.</param>
-        /// <param name="password">The Microsoft app password.</param>
-        /// <param name="tenantId">Tenant ID of the Azure AD tenant where the bot is created.</param>
-        /// <param name="customHttpClient">Optional <see cref="HttpClient"/> to be used when acquiring tokens.</param>
-        /// <param name="logger">Optional <see cref="ILogger"/> to gather telemetry data while acquiring and managing credentials.</param>
-        /// <param name="oAuthScope">The scope for the token (defaults to <see cref="GovernmentAuthenticationConstants.ToChannelFromBotOAuthScope"/> if null).</param>
-        public MicrosoftGovernmentAppCredentials(string appId, string password, string tenantId, HttpClient customHttpClient, ILogger logger, string oAuthScope = null)
-            : base(appId, password, tenantId, customHttpClient, logger, oAuthScope ?? GovernmentAuthenticationConstants.ToChannelFromBotOAuthScope)
-        {
-        }
+        /// <inheritdoc/>
+        protected override string DefaultChannelAuthTenant => GovernmentAuthenticationConstants.DefaultChannelAuthTenant;
 
-        /// <summary>
-        /// Gets the OAuth endpoint to use.
-        /// </summary>
-        /// <value>
-        /// The OAuth endpoint to use.
-        /// </value>
-        public override string OAuthEndpoint
-        {
-            get { return GovernmentAuthenticationConstants.ToChannelFromBotLoginUrl; }
-        }
+        /// <inheritdoc/>
+        protected override string ToChannelFromBotOAuthScope => GovernmentAuthenticationConstants.ToChannelFromBotOAuthScope;
+
+        /// <inheritdoc/>
+        protected override string ToChannelFromBotLoginUrlTemplate => GovernmentAuthenticationConstants.ToChannelFromBotLoginUrlTemplate;
     }
 }
