9c4a1af6e93e1eaf55f44106d6c8e333acce0891
tracyboehrer <tracyboehrer@users.noreply.github.com>
2024-02-01 14:25:47 -0600

    Removed Copyright from generated code (#6740)
    Co-authored-by: Tracy Boehrer <trboehre@microsoft.com>

--- generators/dotnet-templates/Microsoft.BotFramework.CSharp.CoreBot/content/CoreBot.Tests/Dialogs/TestData/BookingDialogTestCase.cs
+++ generators/dotnet-templates/Microsoft.BotFramework.CSharp.CoreBot/content/CoreBot.Tests/Dialogs/TestData/BookingDialogTestCase.cs
@@ -1,7 +1,4 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with CoreBot .NET Template version __vX.X.X__
+﻿// Generated with CoreBot .NET Template version __vX.X.X__
 
 using CoreBot;
 
