da0ebee3e715555279fbd9df949b0294b7c6dc49
CeciliaAvila <cecilia.avila@southworks.com>
2025-03-19 11:09:02 -0300

    Add NET9 and remove NET6 target frameworks

--- tests/Microsoft.Bot.Connector.Tests/AttachmentsTests.cs
+++ tests/Microsoft.Bot.Connector.Tests/AttachmentsTests.cs
@@ -138,7 +138,7 @@ namespace Microsoft.Bot.Connector.Tests
                 stream.Position = 0;
                 var length = stream.Length > int.MaxValue ? int.MaxValue : Convert.ToInt32(stream.Length);
                 var buffer = new byte[length];
-                stream.Read(buffer, 0, length);
+                stream.ReadExactly(buffer);
 
                 var actualAsString = Convert.ToBase64String(buffer, Base64FormattingOptions.None);
 
