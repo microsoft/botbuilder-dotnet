d2817f33b86c0200b17e01cf23101eba59b07a24
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-10-25 01:15:55 -0700

    AddBotFrameworkAuthFromConfiguration

--- agentic/Sample.Agentic/Program.cs
+++ agentic/Sample.Agentic/Program.cs
@@ -3,64 +3,11 @@
 
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Connector.Authentication;
-using Microsoft.Identity.Abstractions;
-using Microsoft.Identity.Web;
-using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
 using Sample.Agentic;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddTokenAcquisition(true);
-
-builder.Services.AddAgentIdentities();
-
-builder.Services.AddInMemoryTokenCaches();
-
-builder.Services.AddHttpClient();
-
-CredentialDescription credSecret;
-
-if (!string.IsNullOrEmpty(builder.Configuration["MicrosoftAppClientId"]))
-{
-    builder.Services.AddSingleton<ServiceClientCredentialsFactory>(provider =>
-        new FederatedServiceClientCredentialsFactory(
-            provider.GetRequiredService<IAuthorizationHeaderProvider>(),
-            builder.Configuration["MicrosoftAppId"],
-            builder.Configuration["MicrosoftAppClientId"],
-            builder.Configuration["MicrosoftAppTenantId"]));
-
-    credSecret = new CredentialDescription()
-    {
-        SourceType = CredentialSource.SignedAssertionFromManagedIdentity,
-        ManagedIdentityClientId = builder.Configuration["MicrosoftAppClientId"]
-    };
-}
-else
-{
-    credSecret = new CredentialDescription()
-    {
-        SourceType = CredentialSource.ClientSecret,
-        ClientSecret = builder.Configuration["MicrosoftAppPassword"]
-    };
-}
-
-builder.Services.Configure<MicrosoftIdentityApplicationOptions>(ops =>
-{
-    ops.Instance = "https://login.microsoftonline.com/";
-    ops.TenantId = builder.Configuration["MicrosoftAppTenantId"];
-    ops.ClientId = builder.Configuration["MicrosoftAppId"];
-    ops.ClientCredentials = [credSecret];
-});
-
-builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
-
-builder.Services.AddSingleton<IBotFrameworkHttpAdapter>(provider =>
-    new CloudAdapter(
-        provider.GetRequiredService<IAuthorizationHeaderProvider>(),
-        provider.GetRequiredService<IConfiguration>(),
-        provider.GetRequiredService<IHttpClientFactory>(),
-        provider.GetRequiredService<ILogger<CloudAdapter>>()));
+builder.Services.AddBotFrameworkAuthFromConfiguration(builder.Configuration);
 
 builder.Services.AddTransient<IBot, EchoBot>();
 
---
+++ libraries/integration/Microsoft.Bot.Builder.Integration.AspNet.Core/AddBotFrameworkAuthenticationFromConfiguration.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Reflection;
+using System.Text;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Abstractions;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.TokenCacheProviders.InMemory;
+
+namespace Microsoft.Bot.Builder.Integration.AspNet.Core
+{
+    /// <summary>
+    /// Extension methods to add Bot Framework Authentication from configuration.
+    /// </summary>
+    public static class AddBotFrameworkAuthenticationFromConfiguration
+    {
+        /// <summary>
+        /// Adds Bot Framework Authentication to the service collection from configuration.
+        /// </summary>
+        /// <param name="services">Services.</param>
+        /// <param name="configuration">Configuration.</param>
+        public static void AddBotFrameworkAuthFromConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTokenAcquisition(true);
+
+            services.AddAgentIdentities();
+
+            services.AddInMemoryTokenCaches();
+
+            services.AddHttpClient();
+
+            CredentialDescription credSecret;
+
+            // Enable Federated Identity Credential
+            if (!string.IsNullOrEmpty(configuration["MicrosoftAppClientId"])) 
+            {
+                services.AddSingleton<ServiceClientCredentialsFactory>(provider =>
+                    new FederatedServiceClientCredentialsFactory(
+                        provider.GetRequiredService<IAuthorizationHeaderProvider>(),
+                        configuration["MicrosoftAppId"],
+                        configuration["MicrosoftAppClientId"],
+                        configuration["MicrosoftAppTenantId"]));
+
+                // Set up to use Managed Identity
+                credSecret = new CredentialDescription() 
+                {
+                    SourceType = CredentialSource.SignedAssertionFromManagedIdentity,
+                    ManagedIdentityClientId = configuration["MicrosoftAppClientId"]
+                };
+            }
+
+            // otherwise use Client Secret
+            else
+            {
+                credSecret = new CredentialDescription()
+                {
+                    SourceType = CredentialSource.ClientSecret,
+                    ClientSecret = configuration["MicrosoftAppPassword"]
+                };
+            }
+
+            //TODO: Support more CredentialDescription types if needed, such as X509Certificate
+
+            services.Configure<MicrosoftIdentityApplicationOptions>(ops =>
+            {
+                //TODO: Make configurable if needed
+                ops.Instance = "https://login.microsoftonline.com/"; 
+                ops.TenantId = configuration["MicrosoftAppTenantId"];
+                ops.ClientId = configuration["MicrosoftAppId"];
+                ops.ClientCredentials = new List<CredentialDescription> { credSecret };
+            });
+
+            services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+            services.AddSingleton<IBotFrameworkHttpAdapter>(provider =>
+                new CloudAdapter(
+                    provider.GetRequiredService<IAuthorizationHeaderProvider>(),
+                    provider.GetRequiredService<IConfiguration>(),
+                    provider.GetRequiredService<IHttpClientFactory>(),
+                    provider.GetRequiredService<ILogger<CloudAdapter>>()));
+        }
+    }
+}
